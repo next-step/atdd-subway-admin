a4d432b6072b16c05a5907fdb39a21739721426e
boorownie <probitanima11@gmail.com>
2020-10-31 17:18:35 +0900

    feat(subway): front

---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import stationRoutes from '@/router/modules/station'
+import lineRoutes from '@/router/modules/line'
+import mainRoutes from '@/router/modules/main'
+import edgeRoutes from '@/router/modules/edge'
+import mapRoutes from '@/router/modules/map'
+import pathRoutes from '@/router/modules/path'
+import authRoutes from '@/router/modules/auth'
+import favoriteRoutes from '@/router/modules/favorite'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [...mapRoutes, ...pathRoutes, ...stationRoutes, ...lineRoutes, ...edgeRoutes, ...mainRoutes, ...authRoutes, ...favoriteRoutes]
+})
---
+++ frontend/src/views/edge/components/EdgeCreateButton.vue
@@ -0,0 +1,194 @@
+<template>
+  <Dialog :close="close">
+    <template slot="trigger">
+      <v-btn @click="initLineView" class="mx-2 absolute right-30 z-1 edge-create-button" fab color="amber">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </template>
+    <template slot="title">
+      <div class="width-100 text-center mt-6">구간 추가</div>
+    </template>
+    <template slot="text">
+      <v-form ref="edgeForm" v-model="valid" @submit.prevent>
+        <v-select
+          v-model="edgeForm.lineId"
+          :items="lineNameViews"
+          @change="onChangeLine"
+          label="노선 선택"
+          width="400"
+          item-color="amber darken-3"
+          color="grey darken-1"
+          outlined
+          dense
+        ></v-select>
+        <div class="d-flex">
+          <v-select
+            v-model="edgeForm.lineStationId"
+            class="pr-5"
+            :items="lineStationsNameViews"
+            label="등록된 역"
+            width="400"
+            color="grey darken-1"
+            item-color="amber darken-3"
+            outlined
+            dense
+          ></v-select>
+          <v-select
+            v-model="edgeForm.toward"
+            :items="toward"
+            label="방향"
+            width="50"
+            color="grey darken-1"
+            item-color="amber darken-3"
+            outlined
+            dense
+          ></v-select>
+          <v-select
+            v-model="edgeForm.targetStationId"
+            class="pl-5"
+            :items="allStationsView"
+            label="추가할 역"
+            width="400"
+            color="grey darken-1"
+            item-color="amber darken-3"
+            outlined
+            dense
+          ></v-select>
+        </div>
+      </v-form>
+    </template>
+    <template slot="action">
+      <v-btn :disabled="!valid" @click.prevent="onCreateEdge" color="amber">확인</v-btn>
+    </template>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from '@/components/dialogs/Dialog'
+import { SNACKBAR_MESSAGES } from '@/utils/constants'
+import dialog from '@/mixins/dialog'
+import validator from '@/utils/validator'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+import { SHOW_SNACKBAR } from '@/store/shared/mutationTypes'
+import { CREATE_EDGE, CREATE_LINE, DELETE_LINE, EDIT_LINE, FETCH_LINE, FETCH_LINES, FETCH_STATIONS } from '@/store/shared/actionTypes'
+
+export default {
+  name: 'EdgeCreateButton',
+  mixins: [dialog],
+  components: { Dialog },
+  computed: {
+    ...mapGetters(['lines', 'stations'])
+  },
+  methods: {
+    ...mapMutations([SHOW_SNACKBAR]),
+    ...mapActions([FETCH_LINES, CREATE_LINE, DELETE_LINE, EDIT_LINE, FETCH_LINE, FETCH_STATIONS, CREATE_EDGE]),
+    initLineView() {
+      if (this.lines.length > 0) {
+        this.lineNameViews = this.lines.map(line => {
+          return {
+            text: line.name,
+            value: line.id
+          }
+        })
+      }
+    },
+    async initLineStationsView() {
+      try {
+        this.selectedLine = await this.fetchLine(this.edgeForm.lineId)
+        if (this.selectedLine.stations && this.selectedLine.stations.length > 0) {
+          this.lineStationsNameViews = this.selectedLine.stations.map((station) => {
+            return {
+              text: station.name,
+              value: station.id
+            }
+          })
+        }
+      } catch (e) {
+        this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL)
+      }
+    },
+    async initAllStationsView() {
+      try {
+        await this.fetchStations()
+        if (this.stations.length > 0) {
+          this.allStationsView = this.stations.map(station => {
+            return {
+              text: station.name,
+              value: station.id
+            }
+          })
+        }
+      } catch (e) {
+        this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL)
+      }
+    },
+    setLineColor(color) {
+      this.edgeForm.color = color
+    },
+    isValid() {
+      return this.$refs.edgeForm.validate()
+    },
+    onChangeLine() {
+      this.initLineStationsView()
+      this.initAllStationsView()
+    },
+    async onCreateEdge() {
+      if (!this.isValid()) {
+        return
+      }
+      try {
+        await this.createEdge({
+          lineId: this.selectedLine.id,
+          edge: this.edgeForm
+        })
+        this.closeDialog()
+        this.fetchLines()
+        this.fetchLine(this.selectedLine.id)
+        this.$refs.edgeForm.resetValidation()
+        this.initEdgeForm()
+        this.showSnackbar(SNACKBAR_MESSAGES.COMMON.SUCCESS)
+      } catch (e) {
+        this.showSnackbar(SNACKBAR_MESSAGES.COMMON.FAIL)
+      }
+    },
+    initEdgeForm() {
+      this.edgeForm = {
+        lineId: '',
+        lineStationId: '',
+        targetStationId: ''
+      }
+    }
+  },
+  data() {
+    return {
+      rules: { ...validator },
+      edgeForm: {
+        lineId: '',
+        lineStationId: '',
+        targetStationId: ''
+      },
+      selectedLine: {},
+      lineStationsNameViews: [],
+      allStationsView: [],
+      toward:[
+        {
+          text: "의 상행으로",
+          value: "UP"
+        },
+        {
+          text: "의 하행으로",
+          value: "DOWN"
+        }
+      ],
+      lineNameViews: [],
+      valid: false
+    }
+  }
+}
+</script>
+
+<style lange="scss" scoped>
+.edge-create-button {
+  bottom: -25px;
+}
+</style>
