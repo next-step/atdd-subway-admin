4726175150aaaa7527f9ad6a4feb5776c87e9179
poppohoo <ccf05017@gmail.com>
2020-12-15 17:30:37 +0900

    refactor(LineAcceptanceTest): extract method
    - 코드 가독성을 높이기 위해 복잡한 테스트 로직 메서드 추출

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -6,16 +6,15 @@ import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.step.LineAcceptanceStep;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import javax.xml.stream.Location;
-
+import static nextstep.subway.line.step.LineAcceptanceStep.NEW_LINE_CREATED;
+import static nextstep.subway.line.step.LineAcceptanceStep.REQUEST_CREATE_NEW_LINE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
@@ -24,27 +23,14 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine() {
         String lineName = "9호선";
         String lineColor = "금색";
-        LineRequest lineRequest = new LineRequest(lineName, lineColor);
 
         // when
         // 지하철_노선_생성_요청
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .body(lineRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/lines")
-                .then()
-                .log().all()
-                .extract();
+        ExtractableResponse<Response> response = REQUEST_CREATE_NEW_LINE(lineName, lineColor);
 
         // then
         // 지하철_노선_생성됨
-        LineResponse lineResponse = response.as(LineResponse.class);
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("Location")).isNotEmpty();
-        assertThat(lineResponse.getColor()).isEqualTo(lineColor);
-        assertThat(lineResponse.getName()).isEqualTo(lineName);
+        NEW_LINE_CREATED(response, lineName, lineColor);
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
---
+++ src/test/java/nextstep/subway/line/step/LineAcceptanceStep.java
@@ -0,0 +1,40 @@
+package nextstep.subway.line.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LineAcceptanceStep {
+    public static ExtractableResponse<Response> REQUEST_CREATE_NEW_LINE(
+            final String lineName, final String lineColor
+    ) {
+        LineRequest lineRequest = new LineRequest(lineName, lineColor);
+
+        return RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then()
+                .log().all()
+                .extract();
+    }
+
+    public static void NEW_LINE_CREATED(
+            final ExtractableResponse<Response> response, final String lineName, final String lineColor
+    ) {
+        LineResponse lineResponse = response.as(LineResponse.class);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotEmpty();
+        assertThat(lineResponse.getColor()).isEqualTo(lineColor);
+        assertThat(lineResponse.getName()).isEqualTo(lineName);
+    }
+}
