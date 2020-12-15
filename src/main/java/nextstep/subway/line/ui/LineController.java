35fe96bc0254396d64b42f748e72a7b41defe5ae
poppohoo <ccf05017@gmail.com>
2020-12-15 17:59:13 +0900

    feat(LineController): add exception handler of already existing line handle
    - 사용자가 잘못된 방법으로 라인 생성 요청을 할 수 없도록 정합성을 확인하는 기능 추가

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -3,11 +3,9 @@ package nextstep.subway.line.ui;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.application.exceptions.AlreadyExistLineException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -24,5 +22,10 @@ public class LineController {
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @ExceptionHandler(AlreadyExistLineException.class)
+    public ResponseEntity AlreadyExistLineException(AlreadyExistLineException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
