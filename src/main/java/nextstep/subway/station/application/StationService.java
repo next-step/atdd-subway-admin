e1fa6feb22fc94be5c17745a0358f75993d49394
cionman <topspin1278@naver.com>
2020-12-21 22:57:17 +0900

    refactor : StationService 필드 final 할당 등의 소소한 수정

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -12,25 +12,25 @@ import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class StationService {
-	private StationRepository stationRepository;
+	private final StationRepository stationRepository;
 
 	public StationService(StationRepository stationRepository) {
 		this.stationRepository = stationRepository;
 	}
 
+	@Transactional
 	public StationResponse saveStation(StationRequest stationRequest) {
 		Station persistStation = stationRepository.save(stationRequest.toStation());
 		return StationResponse.of(persistStation);
 	}
 
-	@Transactional(readOnly = true)
 	public List<StationResponse> findAllStations() {
 		List<Station> stations = stationRepository.findAll();
 
 		return stations.stream()
-			.map(station -> StationResponse.of(station))
+			.map(StationResponse::of)
 			.collect(Collectors.toList());
 	}
 
