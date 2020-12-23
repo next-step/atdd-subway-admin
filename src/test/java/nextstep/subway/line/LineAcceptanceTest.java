2f118470c7e73b334e269739097eff496948579f
lazy-moon <magyeon@naver.com>
2020-12-24 04:44:54 +0900

    feature: 전체 노선 조회 기능 추가

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -3,12 +3,20 @@ package nextstep.subway.line;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,7 +27,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine() {
         // when
         // 지하철_노선_생성_요청
-        ExtractableResponse<Response> response = request("/lines", new LineRequest("2호선", "초록"));
+        ExtractableResponse<Response> response = doPost("/lines", new LineRequest("2호선", "초록"));
 
         // then
         // 지하철_노선_생성됨
@@ -32,10 +40,10 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // given
         // 지하철_노선_등록되어_있음
         LineRequest requestBody = new LineRequest("2호선", "초록");
-        request("/lines", requestBody);
+        doPost("/lines", requestBody);
         // when
         // 지하철_노선_생성_요청
-        ExtractableResponse<Response> response = request("/lines", requestBody);
+        ExtractableResponse<Response> response = doPost("/lines", requestBody);
 
         // then
         // 지하철_노선_생성_실패됨
@@ -47,14 +55,30 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLines() {
         // given
         // 지하철_노선_등록되어_있음
+        ExtractableResponse<Response> createResponse = doPost("/lines", new LineRequest("2호선", "초록"));
         // 지하철_노선_등록되어_있음
+        ExtractableResponse<Response> createResponse2 = doPost("/lines", new LineRequest("1호선", "파랑"));
 
         // when
         // 지하철_노선_목록_조회_요청
+        ExtractableResponse<Response> response = doGet("/lines");
 
         // then
         // 지하철_노선_목록_응답됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         // 지하철_노선_목록_포함됨
+        List<Long> expected = Stream.of(createResponse, createResponse2)
+                .map(it -> it.header("Location"))
+                .map(this::pathVariableToLong)
+                .collect(Collectors.toList());
+
+        List<Long> result = response.jsonPath()
+                .getList(".", LineResponse.class)
+                .stream()
+                .map(LineResponse::getId)
+                .collect(Collectors.toList());
+
+        assertThat(expected).containsAll(result);
     }
 
     @DisplayName("지하철 노선을 조회한다.")
@@ -96,7 +120,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_삭제됨
     }
 
-    private ExtractableResponse<Response> request(String url, Object body) {
+    private ExtractableResponse<Response> doPost(String url, Object body) {
         return RestAssured
                 .given().log().all()
                 .body(body)
@@ -105,5 +129,32 @@ public class LineAcceptanceTest extends AcceptanceTest {
                 .post(url)
                 .then().log().all()
                 .extract();
+    }
+
+    private ExtractableResponse<Response> doGet(String url) {
+        return doGetWithParameter(url, null);
+    }
+
+
+    private ExtractableResponse<Response> doGetWithParameter(String url, Object body) {
+        RequestSpecification specification = RestAssured.given().log().all();
+        if (body != null) {
+            specification = specification.body(body)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE);
+        }
+        return specification
+                .when()
+                .get(url)
+                .then().log().all()
+                .extract();
+    }
+
+    private Long pathVariableToLong(String location) {
+        String regex = "/lines/(\\d)$";
+        Matcher m = Pattern.compile(regex).matcher(location);
+        if (m.find()) {
+            return Long.parseLong(m.group(1));
+        }
+        throw new RuntimeException("Not matched expression : " + regex);
     }
 }
