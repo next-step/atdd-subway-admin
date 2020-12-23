bbdab3aa0f724f34c777ae933c22a80eaa49ee3a
poppohoo <ccf05017@gmail.com>
2020-12-23 11:43:14 +0900

    test(ChangeOriginalAndAddSectionPolicyTest): check when down station same
    - 좀 더 안정적인 테스트를 위해 하행역이 일치하는 경우에도 테스트를 진행하도록 기능 구현

--- src/test/java/nextstep/subway/line/domain/ChangeOriginalAndAddSectionPolicyTest.java
+++ src/test/java/nextstep/subway/line/domain/ChangeOriginalAndAddSectionPolicyTest.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ChangeOriginalAndAddSectionPolicyTest {
-    @DisplayName("기존 구간과 상행역이 겹치는 경우 기존 구간을 변경하고 새로운 구간을 추가할 수 있다.")
+    @DisplayName("기존 구간과 상행역이나 하행역이 겹치는 경우 기존 구간을 변경하고 새로운 구간을 추가할 수 있다.")
     @ParameterizedTest
     @MethodSource("addSectionTestWhenSameUpStationTestResource")
     void addSectionTestWhenSameUpStationTest(Sections sections, Section newSection, List<Section> changedSections) {
@@ -37,7 +37,19 @@ class ChangeOriginalAndAddSectionPolicyTest {
                                new Section(2L, 4L, 5L),
                                new Section(4L, 3L, 5L)
                        )
-               )
+               ),
+                Arguments.of(
+                        new Sections(new ArrayList<>(Arrays.asList(
+                                new Section(1L, 2L, 10L),
+                                new Section(2L, 3L, 10L)
+                        ))),
+                        new Section (4L, 2L, 5L),
+                        Arrays.asList(
+                                new Section(1L, 4L, 5L),
+                                new Section(4L, 2L, 5L),
+                                new Section(2L, 3L, 10L)
+                        )
+                )
         );
     }
 }
