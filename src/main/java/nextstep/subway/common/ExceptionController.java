6f5778bbb6f1c574ae75930b500f9a3bc24e8ca6
lazy-moon <magyeon@naver.com>
2020-12-24 05:12:56 +0900

    feature: Exception handling 공통화

---
+++ src/main/java/nextstep/subway/common/ExceptionController.java
@@ -0,0 +1,15 @@
+package nextstep.subway.common;
+
+import nextstep.subway.common.exception.NotExistsException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler({DataIntegrityViolationException.class, NotExistsException.class})
+    public ResponseEntity<?> handleIllegalArgsException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
---
+++ src/main/java/nextstep/subway/common/exception/NotExistsException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.common.exception;
+
+public class NotExistsException extends RuntimeException{
+    public NotExistsException(String message) {
+        super(message);
+    }
+}
