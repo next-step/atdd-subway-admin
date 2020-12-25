76f1db91269aca8e0ef1a965df57cb06497b8620
eversong0723 <eversong0723@gmail.com>
2020-12-25 10:46:55 +0900

    feat: 생성 요청 테스트 완료

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,19 +1,47 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.utils.DatabaseCleanup;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
+    @Autowired
+    private DatabaseCleanup cleanup;
+
+    @BeforeEach
+    void beforeAll() {
+        cleanup.execute();
+    }
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
         // when
         // 지하철_노선_생성_요청
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "2호선");
+        params.put("color", "green");
+
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(params);
 
         // then
         // 지하철_노선_생성됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
@@ -21,12 +49,19 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine2() {
         // given
         // 지하철_노선_등록되어_있음
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "2호선");
+        params.put("color", "green");
+
+        지하철_노선_생성_요청(params);
 
         // when
         // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response2 = 지하철_노선_생성_요청(params);
 
         // then
         // 지하철_노선_생성_실패됨
+        assertThat(response2.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
@@ -81,5 +116,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         // 지하철_노선_삭제됨
+    }
+
+    static ExtractableResponse<Response> 지하철_노선_생성_요청(Map<String, String> params){
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
     }
 }
