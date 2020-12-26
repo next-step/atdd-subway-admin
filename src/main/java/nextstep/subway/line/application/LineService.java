db26b28124d82fad9b08a29abde23f83e297a5a8
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-12-26 23:20:36 +0900

    add : 지하철 노선 관련 기능 구현 - 노선 목록 조회 - 노선 조회 - 노선 수정 - 노선 삭제

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,17 +7,46 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+	private LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
-        this.lineRepository = lineRepository;
-    }
+	public LineService(LineRepository lineRepository) {
+		this.lineRepository = lineRepository;
+	}
 
-    public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
-    }
+	public LineResponse saveLine(LineRequest request) {
+		Line persistLine = lineRepository.save(request.toLine());
+		return LineResponse.of(persistLine);
+	}
+
+	public List<LineResponse> listLine() {
+		List<Line> findLineList = lineRepository.findAll();
+		List<LineResponse> lineListResponse = new ArrayList<>();
+		for (Line findList : findLineList) {
+			lineListResponse.add(LineResponse.of(findList));
+		}
+		return lineListResponse;
+	}
+
+	public LineResponse findLineById(Long id) {
+		Line line = lineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 노선이 없습니다 id=" + id));
+		return LineResponse.of(line);
+	}
+
+	public void update(LineRequest lineRequest) {
+		Line line = lineRepository.findById(lineRequest.getId()).orElseThrow(() -> new IllegalArgumentException("해당 노선이 없습니다 id=" + lineRequest.getId()));
+		line.update(lineRequest.toLine());
+		lineRepository.save(line);
+	}
+
+	public void delete(Long id) {
+		Line line = lineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 노선이 없습니다 id=" + id));
+		lineRepository.delete(line);
+	}
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -3,16 +3,16 @@ package nextstep.subway.line.ui;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/lines")
+//@RequestMapping("/lines")
 public class LineController {
     private final LineService lineService;
 
@@ -20,9 +20,35 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @PostMapping
+    @PostMapping("/lines")
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
-        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+        ResponseEntity responseEntity = ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+        return responseEntity;
+    }
+
+    @GetMapping("/lines")
+    public ResponseEntity listLine() {
+        List<LineResponse> lineResponseList = lineService.listLine();
+        ResponseEntity responseEntity = ResponseEntity.ok(lineResponseList);
+        return responseEntity;
+    }
+
+    @GetMapping(value = "/lines/{id}")
+    public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
+        LineResponse lineResponse =  lineService.findLineById(id);
+        return ResponseEntity.ok().body(lineResponse);
+    }
+
+    @PatchMapping(value = "/lines")
+    public ResponseEntity modifyLine(@RequestBody LineRequest lineRequest) {
+        lineService.update(lineRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/lines/{id}")
+    public ResponseEntity deleteLine(@PathVariable Long id) {
+        lineService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
