c85a2a3d188dbf8ea90636346f859815b9371a30
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-26 22:22:39 +0900

    fix(SecionCreateButton): bug fix SecionCreateButton.vue file

--- frontend/src/views/section/components/SectionCreateButton.vue
+++ frontend/src/views/section/components/SectionCreateButton.vue
@@ -45,6 +45,16 @@
             dense
           ></v-select>
         </div>
+        <div class="d-flex">
+          <v-text-field
+              v-model="sectionForm.distance"
+              :rules="rules.section.distance"
+              color="grey darken-1"
+              label="거리"
+              placeholder="거리"
+              outlined
+          ></v-text-field>
+        </div>
       </v-form>
     </template>
     <template slot="action">
@@ -145,7 +155,8 @@ export default {
       this.sectionForm = {
         lineId: '',
         upStationId: '',
-        downStationId: ''
+        downStationId: '',
+        distance: ''
       }
     }
   },
