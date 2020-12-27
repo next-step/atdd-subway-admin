e7d272caa8bd3c7e3d05d69fda580ad790edaf76
bss <baekss0409@gmail.com>
2020-12-27 22:17:15 +0900

    feat: 지하철 노선 관리 API 구축
    * 노선 생성
    * 노선 조회
    * 노선 수정
    * 노선 삭제

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,8 +4,15 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+
+import java.util.List;
+
+import javax.persistence.NoResultException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.stream.Collectors.*;
 
 @Service
 @Transactional
@@ -19,5 +26,31 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(LineResponse::of)
+                .collect(toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findLineByName(String name) {
+        return LineResponse.of(lineRepository.findLineByName(name)
+                .orElseThrow(() -> new NoResultException(name + "은(는) 존재하지 않습니다")));
+    }
+
+    public void updateLine(Line line) {
+        // 변경 감지 방법으로 수정한다.
+        Line findLine = lineRepository.findById(line.getId())
+                .orElseThrow(() -> new NoResultException(line.getName() + "을 수정할 수 없습니다"));
+        findLine.update(line);
+    }
+
+    public void removeLine(Long id) {
+        lineRepository.deleteById(id);
     }
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,15 +1,28 @@
 package nextstep.subway.line.ui;
 
 import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import static org.springframework.http.MediaType.*;
 
 @RestController
 @RequestMapping("/lines")
@@ -20,9 +33,41 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @PostMapping
-    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok(lineService.findAllLines());
+    }
+
+    @GetMapping(value = "/{name}", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<LineResponse> showLine(@PathVariable("name") String name) {
+        return ResponseEntity.ok(lineService.findLineByName(name));
+    }
+
+    @PutMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> updateLine(@RequestBody Line line) {
+        lineService.updateLine(line);
+        return ResponseEntity.ok(line.getName() + "으로 수정 완료");
+    }
+
+    @DeleteMapping(value = "/{id}", produces = TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> removeLine(@PathVariable("id") Long id) {
+        lineService.removeLine(id);
+        return ResponseEntity.ok("노선 삭제 완료");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(NoResultException.class)
+    public ResponseEntity handleNoResultException(NoResultException e) {
+        return ResponseEntity.notFound().build();
     }
 }
