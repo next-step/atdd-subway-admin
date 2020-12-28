05c9fe7b1a52300e951789da338d1bd489471ede
bss <baekss0409@gmail.com>
2020-12-28 10:31:34 +0900

    refactor: LineService refactoring

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -30,23 +30,19 @@ public class LineService {
 
     @Transactional(readOnly = true)
     public List<LineResponse> findAllLines() {
-        List<Line> lines = lineRepository.findAll();
-
-        return lines.stream()
-                .map(LineResponse::of)
-                .collect(toList());
+        return LineResponse.ofList(lineRepository.findAll());
     }
 
     @Transactional(readOnly = true)
     public LineResponse findLineByName(String name) {
-        return LineResponse.of(lineRepository.findLineByName(name)
-                .orElseThrow(() -> new NoResultException(name + "은(는) 존재하지 않습니다")));
+        return lineRepository.findLineByName(name)
+                .map(LineResponse::of)
+                .orElseThrow(() -> new NoResultException(name + "은(는) 존재하지 않습니다"));
     }
 
     public void updateLine(Line line) {
-        // 변경 감지 방법으로 수정한다.
         Line findLine = lineRepository.findById(line.getId())
-                .orElseThrow(() -> new NoResultException(line.getName() + "을 수정할 수 없습니다"));
+                .orElseThrow(() -> new NoResultException(line.getName() + "을(를) 수정할 수 없습니다"));
         findLine.update(line);
     }
 
