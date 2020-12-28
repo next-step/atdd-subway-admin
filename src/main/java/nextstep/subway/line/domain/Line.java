2e22be61a9c471628cc99c04c632fea5b3b0a496
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-28 20:42:49 +0900

    (refactor) 예외처리 관련 수정

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -9,6 +9,7 @@ public class Line extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
     private String color;
