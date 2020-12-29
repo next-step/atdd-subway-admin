cfe1c79cfa29b48fd2da3c8997008e656e441486
bss <baekss0409@gmail.com>
2020-12-29 21:55:12 +0900

    refactor: StationAcceptanceTest refactoring

--- src/main/java/nextstep/subway/station/dto/StationRequest.java
+++ src/main/java/nextstep/subway/station/dto/StationRequest.java
@@ -5,6 +5,12 @@ import nextstep.subway.station.domain.Station;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {}
+
+    public StationRequest(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
