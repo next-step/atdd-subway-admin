7f35391951ffb3ebdd8716a54d88f018e3e3eeda
hwanseok-joo <rokmctkd6@gmail.com>
2020-12-29 22:23:35 +0900

    feat: 예외케이스 인수테스트 작성

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -14,12 +14,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 
 @DisplayName("지하철 구간 관련 기능 테스")
 public class SectionAcceptanceTest extends AcceptanceTest {
     StationResponse station1;
     StationResponse station2;
+    StationResponse station3;
     LineResponse line;
 
     @BeforeEach
@@ -28,7 +30,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         // given
          station1 = StationAcceptanceTest.createStation("강남역").as(StationResponse.class);
          station2 = StationAcceptanceTest.createStation("광교역").as(StationResponse.class);
-
+         station3 = StationAcceptanceTest.createStation("판교역").as(StationResponse.class);
          line = LineAcceptanceTest
                 .createSubwayLine("신분당선", "bg-red-600",
                         station1.getId() + "", station2.getId() + "",
@@ -40,8 +42,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     void addSection() {
         // when
         // 지하철_노선에_지하철역_등록_요청
-        StationResponse station = StationAcceptanceTest.createStation("판교역").as(StationResponse.class);
-        LineAcceptanceTest.addSubwayStation(line.getId(), station2.getId() + "", station.getId() + "", 6 + "");
+        LineAcceptanceTest.addSubwayStation(line.getId(), station2.getId() + "", station3.getId() + "", 6 + "");
 
         // then
         // 지하철_노선에_지하철역_등록됨
@@ -50,7 +51,56 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         Assertions.assertThat(lineResponse.getName()).isEqualTo(line.getName());
         Assertions.assertThat(lineResponse.getStations())
                 .extracting(StationResponse::getName)
-                .contains(station1.getName(), station2.getName(), station.getName());
+                .contains(station1.getName(), station2.getName(), station3.getName());
+    }
+
+    @DisplayName("노선에 구간등록 실패한다. case1 : 구간길이가 큰 경우")
+    @Test
+    void failAddSection1() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        ExtractableResponse<Response> response = LineAcceptanceTest.addSubwayStation(line.getId(), station2.getId() + "", station3.getId() + "", 12 + "");
+
+        // then
+        // 지하철_노선에_지하철역_등록 실패됨
+         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @DisplayName("노선에 구간등록 실패한다. case1 : 구간길이가 같은 경우")
+    @Test
+    void failAddSection2() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        ExtractableResponse<Response> response = LineAcceptanceTest.addSubwayStation(line.getId(), station2.getId() + "", station3.getId() + "", 10 + "");
+
+        // then
+        // 지하철_노선에_지하철역_등록 실패됨
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @DisplayName("노선에 구간등록 실패한다. case2 : 이미 등록되어 있는 구간인 경우")
+    @Test
+    void failAddSection3() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        ExtractableResponse<Response> response = LineAcceptanceTest.addSubwayStation(line.getId(), station1.getId() + "", station2.getId() + "", 6 + "");
+
+        // then
+        // 지하철_노선에_지하철역_등록 실패됨
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+    @DisplayName("노선에 구간등록 실패한다. case3 : 상행 하행 둘다 기존 구간에 등록되어 있지 않은 경우")
+    @Test
+    void failAddSection4() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        StationResponse station3 = StationAcceptanceTest.createStation("판교역").as(StationResponse.class);
+        StationResponse station4 = StationAcceptanceTest.createStation("정자역").as(StationResponse.class);
+        ExtractableResponse<Response> response = LineAcceptanceTest.addSubwayStation(line.getId(), station3.getId() + "", station4.getId() + "", 6 + "");
+
+        // then
+        // 지하철_노선에_지하철역_등록 실패됨
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
 
