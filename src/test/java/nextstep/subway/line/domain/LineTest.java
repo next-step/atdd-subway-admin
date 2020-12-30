d7add4535b2b4e3be06573d8e4bb6699f71b680d
jiho <jhhj424@naver.com>
2020-12-31 00:30:49 +0900

    test: 지하철 구간 등록 단위 테스트 작성

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -2,24 +2,58 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
 import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
-    @Test
-    public void addSection() {
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 잠실역;
+    private Line line;
+
+    @BeforeEach
+    public void setUp() {
         // given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 잠실역 = new Station("잠실역");
-        Line line = new Line("2호선", "green", 강남역, 역삼역, 10L);
+        강남역 = new Station("강남역");
+        역삼역 = new Station("역삼역");
+        잠실역 = new Station("잠실역");
+        line = new Line("2호선", "green", 강남역, 역삼역, 10L);
+    }
+
+    @Test
+    @DisplayName("구간에 새로운 역 등록 가능")
+    public void addSection() {
 
         // when
         line.addSection(강남역, 잠실역, 5L);
 
         // then
         assertThat(line.getStations()).containsExactly(Arrays.array(강남역, 잠실역, 역삼역));
+    }
+
+    @Test
+    @DisplayName("새로운 역을 상행 종점으로 등록 가능")
+    public void addUpStationsInSection() {
+
+        // when
+        line.addSection(잠실역, 강남역, 5L);
+
+        // then
+        assertThat(line.getStations()).containsExactly(Arrays.array(잠실역, 강남역, 역삼역));
+    }
+
+    @Test
+    @DisplayName("새로운 역을 하행 종점으로 등록 가능")
+    public void addDownStationsInSection() {
+
+        // when
+        line.addSection(역삼역, 잠실역, 5L);
+
+        // then
+        assertThat(line.getStations()).containsExactly(Arrays.array(강남역, 역삼역, 잠실역));
     }
 }
