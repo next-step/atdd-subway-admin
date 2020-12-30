f0e2c8240d12a0626d137d8f14efd0ba01c70a3f
jiho <jhhj424@naver.com>
2020-12-31 01:12:46 +0900

    test: 구간 등록 예외 케이스 인수 테스트 작성

--- src/test/java/nextstep/subway/line/SectionAddAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAddAcceptanceTest.java
@@ -20,6 +20,7 @@ public class SectionAddAcceptanceTest extends AcceptanceTest {
     private Long 강남ID;
     private Long 역삼ID;
     private Long 선릉ID;
+    private Long 잠실ID;
     private Long DEFAULT_DISTANCE;
 
     @BeforeEach
@@ -30,10 +31,12 @@ public class SectionAddAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> 강남역 = 지하철역_등록되어_있음("강남역");
         ExtractableResponse<Response> 역삼역 = 지하철역_등록되어_있음("역삼역");
         ExtractableResponse<Response> 선릉역 = 지하철역_등록되어_있음("선릉역");
+        ExtractableResponse<Response> 잠실역 = 지하철역_등록되어_있음("잠실역");
 
         강남ID = 강남역.as(StationResponse.class).getId();
         역삼ID = 역삼역.as(StationResponse.class).getId();
         선릉ID = 선릉역.as(StationResponse.class).getId();
+        잠실ID = 잠실역.as(StationResponse.class).getId();
 
         DEFAULT_DISTANCE = 10L;
         LineRequest 노선_요청 = 노선_구간_요청_생성("2호선", "bg-green-600", 강남ID, 역삼ID, DEFAULT_DISTANCE);
@@ -46,7 +49,6 @@ public class SectionAddAcceptanceTest extends AcceptanceTest {
     @Test
     void addStationBetweenStations() {
         // when
-        // 지하철_노선에_지하철역_등록_요청
         ExtractableResponse<Response> response = 노선에_지하철역_등록_요청(노선ID, 강남ID, 선릉ID, DEFAULT_DISTANCE - 5L);
 
         // then
@@ -60,7 +62,6 @@ public class SectionAddAcceptanceTest extends AcceptanceTest {
     @Test
     void addUpStationsInSection() {
         // when
-        // 지하철_노선에_지하철역_등록_요청
         ExtractableResponse<Response> response = 노선에_지하철역_등록_요청(노선ID, 선릉ID, 강남ID,DEFAULT_DISTANCE - 5L);
 
         // then
@@ -74,7 +75,6 @@ public class SectionAddAcceptanceTest extends AcceptanceTest {
     @Test
     void addDownStationsInSection() {
         // when
-        // 지하철_노선에_지하철역_등록_요청
         ExtractableResponse<Response> response = 노선에_지하철역_등록_요청(노선ID, 역삼ID, 선릉ID, DEFAULT_DISTANCE - 5L);
 
         // then
@@ -82,5 +82,35 @@ public class SectionAddAcceptanceTest extends AcceptanceTest {
         assertThat(response.as(SectionResponse.class).getUpStation().getId()).isEqualTo(역삼ID);
         assertThat(response.as(SectionResponse.class).getDownStation().getId()).isEqualTo(선릉ID);
         assertThat(response.as(SectionResponse.class).getDistance()).isEqualTo(5L);
+    }
+
+    @DisplayName("기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음")
+    @Test
+    void addSectionLongerDistance() {
+        // when
+        ExtractableResponse<Response> response = 노선에_지하철역_등록_요청(노선ID, 강남ID, 선릉ID, DEFAULT_DISTANCE + 5L);
+
+        // then
+        지하철_노선_구간_등록_예외_케이스로_등록되지_않음(response);
+    }
+
+    @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+    @Test
+    void alreadyAllStation() {
+        // when
+        ExtractableResponse<Response> response = 노선에_지하철역_등록_요청(노선ID, 강남ID, 역삼ID, DEFAULT_DISTANCE - 5);
+
+        // then
+        지하철_노선_구간_등록_예외_케이스로_등록되지_않음(response);
+    }
+
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
+    @Test
+    void notExistAllStation() {
+        // when
+        ExtractableResponse<Response> response = 노선에_지하철역_등록_요청(노선ID, 잠실ID, 선릉ID, DEFAULT_DISTANCE - 5);
+
+        // then
+        지하철_노선_구간_등록_예외_케이스로_등록되지_않음(response);
     }
 }
--- src/test/java/nextstep/subway/line/SectionAddAcceptanceTestHelper.java
+++ src/test/java/nextstep/subway/line/SectionAddAcceptanceTestHelper.java
@@ -58,4 +58,8 @@ public class SectionAddAcceptanceTestHelper {
                 log().all().
                 extract();
     }
+
+    public static void 지하철_노선_구간_등록_예외_케이스로_등록되지_않음(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
