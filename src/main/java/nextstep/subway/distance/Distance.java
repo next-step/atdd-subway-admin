4f5acbaf422ae9208dddf9ddc49465a15058dc77
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-31 00:56:25 +0900

    feat(Distance): add distance value validate function

--- src/main/java/nextstep/subway/distance/Distance.java
+++ src/main/java/nextstep/subway/distance/Distance.java
@@ -10,9 +10,14 @@ import javax.persistence.Embeddable;
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Distance {
+    private static final int EMPTY = 0;
     private long distance;
 
     public Distance(final long distance) {
+        if (EMPTY >= distance) {
+            throw new IllegalArgumentException();
+        }
+
         this.distance = distance;
     }
 }
