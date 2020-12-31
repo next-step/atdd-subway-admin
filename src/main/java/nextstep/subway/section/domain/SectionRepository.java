3e3c10b677325e13c44490126dd278421d2a5472
lazy-moon <magyeon@naver.com>
2021-01-01 03:30:15 +0900

    feature: 구간 조회에 필요한 메서드 추가

--- src/main/java/nextstep/subway/section/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/section/domain/SectionRepository.java
@@ -1,11 +1,16 @@
 package nextstep.subway.section.domain;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
+    List<Section> findByLineId(Long lineId, Sort sort);
     List<Section> findByLineId(Long lineId);
-    Optional<Section> findTop1ByLineIdOrderBySectionNumberDesc(Long lineId);
+    Optional<Section> findByLineIdAndDownStationId(Long lineId, Long downStationId);
+    Optional<Section> findByLineIdAndUpStationId(Long lineId, Long upStationId);
+    Optional<Section> findByLineIdAndUpStationIdAndDownStationId(Long lineId, Long upStationId, Long downStationId);
+    List<Section> findByLineIdAndSectionNumberGreaterThanEqual(Long lineId, Integer sectionNumber);
 }
