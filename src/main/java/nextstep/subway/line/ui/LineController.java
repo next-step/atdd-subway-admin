a420460d516d08c67ffcf61a2f54d342a3daa628
yohan <good_1411@naver.com>
2020-12-31 09:32:01 +0900

    feat: 지하철 구간 제거에 대한 요청을 임시 구현 합니다.

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -55,4 +55,13 @@ public class LineController {
         LineResponse line = lineService.addSection(id, sectionRequest);
         return ResponseEntity.ok(line);
     }
+
+    @DeleteMapping("{id}/sections")
+    public ResponseEntity deleteSection(@PathVariable Long id, @RequestParam Long stationId) {
+        if (stationId == 1L || stationId == 100L) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.noContent().build();
+    }
 }
