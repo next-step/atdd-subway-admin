24e6b11f5606bb7c273a38e79d3be34b3c4f2afc
charlloss.dev <charlloss.dev@gmail.com>
2021-01-02 03:38:40 +0900

    fix(line): 라인 응답 dto에 지하철리스트 응답 추가

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,48 +1,64 @@
 package nextstep.subway.line.dto;
 
-import nextstep.subway.line.domain.Line;
-
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.dto.StationResponse;
 
 public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+	private Long id;
+	private String name;
+	private String color;
+	private List<StationResponse> stations;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
 
-    public LineResponse() {
-    }
+	public LineResponse() {
+	}
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
-    }
+	public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate,
+		List<StationResponse> stations) {
+		this.id = id;
+		this.name = name;
+		this.color = color;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+		this.stations = stations;
+	}
 
-    public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
-    }
+	public static LineResponse of(Line line) {
+		List<StationResponse> stations = line.getStations().stream()
+			.map(StationResponse::of)
+			.collect(Collectors.toList());
 
-    public Long getId() {
-        return id;
-    }
+		return new LineResponse(line.getId(), line.getName(), line.getColor(),
+			line.getCreatedDate(),
+			line.getModifiedDate(), stations);
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public List<StationResponse> getStations() {
+		return stations;
+	}
 }
