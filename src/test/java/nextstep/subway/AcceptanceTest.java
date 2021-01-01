d008004d3d4de18c422c41bf88a2020cdaf9b2d9
Sanguk Ahn <suahnn@gmail.com>
2021-01-01 17:28:44 +0900

    Step1 (#53)
    * test: 지하철 노선 관련 기능 인수 테스트 작성
    
    * feat: 지하철 노선 관련 기능 구현
    
    * refactor: 인수 테스트 refactoring
    
    * refactor: 중복 메서드 처리

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -1,11 +1,16 @@
 package nextstep.subway;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.utils.DatabaseCleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
@@ -23,5 +28,21 @@ public class AcceptanceTest {
         }
 
         databaseCleanup.execute();
+    }
+
+    protected void assertResponseHttpStatusIsOk(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    protected void assertResponseHttpStatusIsBadRequest(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    protected void assertResponseHttpStatusIsCreate(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    protected void assertResponseHttpStatusIsNoContent(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
