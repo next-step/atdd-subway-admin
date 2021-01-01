3e8208c5cb91d99c5f7aedc39733866e1014de08
charlloss.dev <charlloss.dev@gmail.com>
2021-01-01 17:53:42 +0900

    test(line): 지하철 노선 stubbing 테스트

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.line.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -18,18 +20,26 @@ class LineServiceTest {
 	@Mock
 	private LineRepository lineRepository;
 
+	private LineService lineService;
+
+	@BeforeEach
+	void setUp() {
+		lineService = new LineService(lineRepository);
+	}
+
+	@DisplayName("지하철 노선 저장: DB Stubbing 테스트")
 	@Test
 	void saveTest() {
 		// given
-		LineService lineService = new LineService(lineRepository);
 		LineRequest lineRequest = new LineRequest("2호선", "green");
-		when(lineRepository.save(any())).thenReturn(lineRepository.save(new Line("2호선", "green")));
+		when(lineRepository.save(any())).thenReturn(new Line(1L, "2호선", "green"));
 
 		// when
-		LineResponse lineResponse = lineService.saveLine(lineRequest);
+		LineResponse lineResponse = lineService.showLine(1L);
 
 		// then
 		assertThat(lineResponse).isNotNull();
 		assertThat(lineResponse.getId()).isNotNull();
+
 	}
 }
