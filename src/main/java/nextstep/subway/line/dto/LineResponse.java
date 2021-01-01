93ada1a806a327d024929f2b2a3fb00758d2411d
bss <baekss0409@gmail.com>
2021-01-01 16:51:10 +0900

    feat: 응답 dto 분리 작업
    * 조회 노선 정보만 응답
    * 조회 노선 + 구간에 포함된 지하철(오름차순) 응답

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,12 +1,14 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
-import nextstep.subway.line.domain.LineStation;
+import nextstep.subway.line.domain.LineStations;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.util.Collections.*;
 import static java.util.stream.Collectors.*;
 
 public class LineResponse {
@@ -19,24 +21,37 @@ public class LineResponse {
 
     public LineResponse() {}
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate, List<LineStation> lineStations) {
+    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate, List<Station> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
-        this.stations = StationResponse.ofList(lineStations.stream()
-                .map(LineStation::getStation)
-                .collect(toList()));
+        this.stations = StationResponse.ofList(stations);
     }
 
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate(), line.getLineStations());
+        return new LineResponse(line.getId(),
+                line.getName(),
+                line.getColor(),
+                line.getCreatedDate(),
+                line.getModifiedDate(),
+                new LineStations(line.getLineStations()).getSortedStations());
     }
 
     public static List<LineResponse> ofList(List<Line> lines) {
         return lines.stream()
                 .map(LineResponse::of)
+                .collect(toList());
+    }
+
+    public static LineResponse valueOf(Line line) {
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate(), emptyList());
+    }
+
+    public static List<LineResponse> valueOfList(List<Line> lines) {
+        return lines.stream()
+                .map(LineResponse::valueOf)
                 .collect(toList());
     }
 
