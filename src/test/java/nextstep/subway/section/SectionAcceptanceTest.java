e93cbc1ceea8aec01a2d424809845bd50c7671cb
eversong0723 <eversong0723@gmail.com>
2021-01-02 12:02:56 +0900

    feat: Step 3 시작
    Read.md 수정

---
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -0,0 +1,14 @@
+package nextstep.subway.section;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SectionAcceptanceTest {
+
+    @DisplayName("지하철 구간을 등록한다.")
+    @Test
+    public void addSection() {
+
+    }
+
+}
