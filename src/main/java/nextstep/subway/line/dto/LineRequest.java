1d6683c1eadebee0f4bcdf83d9e52b744a684cc5
charlloss.dev <charlloss.dev@gmail.com>
2021-01-03 20:41:54 +0900

    feat(line): 지하철 라인 request 정보 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -1,28 +1,37 @@
 package nextstep.subway.line.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
 
+@Getter
+@AllArgsConstructor
 public class LineRequest {
-    private String name;
-    private String color;
+	@NotBlank
+	private String name;
+	@NotBlank
+	private String color;
+	@NotBlank
+	private String upStationId;
+	@NotBlank
+	private String downStationId;
+	@Min(1)
+	private String distance;
 
-    public LineRequest() {
-    }
+	public LineRequest(String name, String color) {
+		this.name = name;
+		this.color = color;
+	}
 
-    public LineRequest(String name, String color) {
-        this.name = name;
-        this.color = color;
-    }
+	public Line toLine() {
+		return new Line(name, color);
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Line toLine() {
-        return new Line(name, color);
-    }
+	public Line toLine(Station upStation, Station downStation) {
+		return new Line(name, color, upStation, downStation, Integer.parseInt(distance));
+	}
 }
