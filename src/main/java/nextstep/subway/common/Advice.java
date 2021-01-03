43445d446c7c01f9c1f235e6e5856dc24607d8c8
charlloss.dev <charlloss.dev@gmail.com>
2021-01-03 22:52:39 +0900

    feat(exception): line, station 관리 exception 추가

--- src/main/java/nextstep/subway/common/Advice.java
+++ src/main/java/nextstep/subway/common/Advice.java
@@ -4,13 +4,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.exception.LineNotFoundException;
+import nextstep.subway.station.exception.StationNotFoundException;
 
+@Slf4j
 @RestControllerAdvice
 public class Advice {
-	
-	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<LineResponse> handleLineRuntimeException(RuntimeException e) {
+
+	@ExceptionHandler(LineNotFoundException.class)
+	public ResponseEntity<LineResponse> handleLineNotFoundException(LineNotFoundException e) {
+		log.error("LineNotFoundException: " + e.getMessage());
+		return ResponseEntity.badRequest().build();
+	}
+
+	@ExceptionHandler(StationNotFoundException.class)
+	public ResponseEntity<LineResponse> handleStationNotFoundException(StationNotFoundException e) {
+		log.error("StationNotFoundException: " + e.getMessage());
 		return ResponseEntity.badRequest().build();
 	}
 }
