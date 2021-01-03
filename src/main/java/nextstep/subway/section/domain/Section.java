6372ea4f41e0d1a8649d788dea2ae86ac82fa1c5
charlloss.dev <charlloss.dev@gmail.com>
2021-01-03 20:49:56 +0900

    feat(section): section 엔티티 추가

---
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -0,0 +1,54 @@
+package nextstep.subway.section.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Section {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn
+	private Line line;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "up_station_id")
+	private Station upStation;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "down_station_id")
+	private Station downStation;
+
+	private int distance;
+
+	public List<Station> getStations() {
+		return Arrays.asList(upStation, downStation);
+	}
+
+	public Section(Line line, Station upStation, Station downStation, int distance) {
+		this.line = line;
+		this.upStation = upStation;
+		this.downStation = downStation;
+		this.distance = distance;
+	}
+}
