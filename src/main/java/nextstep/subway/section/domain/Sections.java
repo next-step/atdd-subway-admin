383df8b8a8756557636e8db940e57d6625ecc62c
slamdunk7575 <slamdunk7575@gmail.com>
2021-01-04 03:58:20 +0900

    (refactor) 구간 삭제시 예외 케이스 처리 기능 구현

--- src/main/java/nextstep/subway/section/domain/Sections.java
+++ src/main/java/nextstep/subway/section/domain/Sections.java
@@ -14,6 +14,9 @@ import java.util.*;
 public class Sections {
     private static final String SECTION_ALREADY_EXIST_ERROR_MESSAGE = "노선에 이미 구간이 등록되어 있습니다.";
     private static final String NOT_MATCH_STATION_ERROR_MESSAGE = "노선에 선택한 상행역과 하행역 둘다 포함되어 있지 않습니다.";
+    private static final String NOT_REMOVE_SECTION_ERROR_MESSAGE = "노선에 구간이 하나이기 때문에 제거할 수 없습니다.";
+    private static final String NOT_REMOVE_UNREGISTERED_STATION = "노선에 등록되지 않은 역은 제거할 수 없습니다.";
+    private static final int REMOVABLE_SECTION_SIZE = 3;
 
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
@@ -44,26 +47,6 @@ public class Sections {
         this.sections.add(newSection);
     }
 
-    public void removeSection(Long stationId) {
-        Section removeSection = findSection(stationId);
-        updateSectionByRemove(removeSection);
-        this.sections.remove(removeSection);
-    }
-
-    private Section findSection(Long stationId) {
-        return this.sections.stream()
-                .filter(section -> section.isDownStationInSection(stationId))
-                .findFirst()
-                .get();
-    }
-
-    private void updateSectionByRemove(Section removeSection) {
-        this.sections.stream()
-                .filter(section -> section.isUpStationInSection(removeSection.getDownStation()))
-                .findFirst()
-                .ifPresent(section -> section.updateUpStationToRemove(removeSection.getUpStation(), removeSection.getDistance()));
-    }
-
     private void validateSection(boolean isUpStationInSection, boolean isDownStationInSection) {
         if (isUpStationInSection && isDownStationInSection) {
             throw new IllegalArgumentException(SECTION_ALREADY_EXIST_ERROR_MESSAGE);
@@ -77,6 +60,33 @@ public class Sections {
     private boolean isStationInSection(Station station) {
         return this.sections.stream()
                 .anyMatch(section -> section.isDownStationInSection(station));
+    }
+
+    public void removeSection(Long stationId) {
+        validateRemovableStation(stationId);
+        Section removeSection = findSection(stationId);
+        updateSectionByRemove(removeSection);
+        this.sections.remove(removeSection);
+    }
+
+    private void validateRemovableStation(Long stationId) {
+        if(this.sections.size() < REMOVABLE_SECTION_SIZE) {
+            throw new IllegalArgumentException(NOT_REMOVE_SECTION_ERROR_MESSAGE);
+        }
+    }
+
+    private Section findSection(Long stationId) {
+        return this.sections.stream()
+                .filter(section -> section.isDownStationInSection(stationId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NOT_REMOVE_UNREGISTERED_STATION));
+    }
+
+    private void updateSectionByRemove(Section removeSection) {
+        this.sections.stream()
+                .filter(section -> section.isUpStationInSection(removeSection.getDownStation()))
+                .findFirst()
+                .ifPresent(section -> section.updateUpStationToRemove(removeSection.getUpStation(), removeSection.getDistance()));
     }
 
     public List<Section> getSections() {
