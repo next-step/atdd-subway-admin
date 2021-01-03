83795573510e8f03dfb2638fd74197a641d2be56
charlloss.dev <charlloss.dev@gmail.com>
2021-01-03 20:45:53 +0900

    fix(station): 지하철 역 엔티티 양방향 관계 삭제

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -2,16 +2,17 @@ package nextstep.subway.station.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import nextstep.subway.common.BaseEntity;
-import nextstep.subway.line.domain.Line;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
 public class Station extends BaseEntity {
 	@Id
@@ -19,22 +20,8 @@ public class Station extends BaseEntity {
 	private Long id;
 	@Column(unique = true)
 	private String name;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn
-	private Line line;
-
-	public Station() {
-	}
 
 	public Station(String name) {
 		this.name = name;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
 	}
 }
