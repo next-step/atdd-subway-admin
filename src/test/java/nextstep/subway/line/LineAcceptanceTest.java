974bf2ff861af5e82845dca481b3487bc87169ad
Donghwan Noh <namul.bap@gmail.com>
2021-01-05 02:57:34 +0900

    test: Step2에 추가된 기능을 반영하여 인수 테스트 먼저 변경

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.line;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,11 +19,12 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.station.StationAcceptanceTest;
 
 /**
  * Feature: 지하철 노선 관련 기능
  *
- *   Scenario: 지하철 노선을 생성한다.
+ *   Scenario: 지하철 노선을 종점역(상행, 하행, 거리)을 포함하여 생성한다.
  *     When 지하철 노선을 생성 요청한다.
  *     Then 지하철 노선이 생성된다.
  *
@@ -31,16 +33,18 @@ import nextstep.subway.AcceptanceTest;
  *     When 지하철 노선을 생성 요청한다.
  *     Then 지하철 노선 생성이 실패된다.
  *
- *   Scenario: 지하철 노선 목록을 조회한다.
+ *   Scenario: 종점역을 포함하는 지하철 노선 목록을 조회한다.
  *     Given 지하철 노선이 등록되어 있다
  *     When 지하철 노선 목록을 조회 요청한다.
  *     Then 지하철 노선 목록이 응답된다.
  *     AND  지하철 노선 목록이 포함되어 있다.
+ *     AND  지하철 노선에 역 목록이 포함되어 있다.
  *
- *   Scenario: 지하철 노선을 조회한다.
+ *   Scenario: 종점역을 포함하는 지하철 노선을 조회한다.
  *     Given 지하철 노선이 등록되어 있다
  *     When 지하철 노선을 조회 요청한다.
  *     Then 지하철 노선이 응답된다.
+ *     AND  지하철 노선에 역 목록이 포함되어 있다.
  *
  *   Scenario: 지하철 노선을 수정한다.
  *     Given 지하철 노선이 등록되어 있다
@@ -61,6 +65,17 @@ public class LineAcceptanceTest extends AcceptanceTest {
 	void createLine() {
 		// when : 지하철_노선_생성_요청
 		ExtractableResponse<Response> response = 지하철노선_생성_요청("2호선", "green");
+
+		// then : 지하철_노선_생성됨
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+	}
+
+	@DisplayName("지하철 노선을 종점역(상행, 하행, 거리)을 포함하여 생성한다.")
+	@Test
+	void createLineWithLastStop() {
+		// when : 지하철_노선_생성_요청
+		ExtractableResponse<Response> response = 지하철노선_생성_요청("2호선", "green",
+			"강남역", "역삼역", 10);
 
 		// then : 지하철_노선_생성됨
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -102,6 +117,32 @@ public class LineAcceptanceTest extends AcceptanceTest {
 		);
 	}
 
+	@DisplayName("종점역을 포함하는 종점역을 포함한 지하철 노선 목록을 조회한다.")
+	@Test
+	void getLinesWithLastStop() {
+		// given : 지하철_노선_등록되어_있음
+		List<Long> 지하철노선_ID_목록 = Stream.of(
+			지하철노선_생성_요청("2호선", "green", "강남역", "역삼역", 10),
+			지하철노선_생성_요청("5호선", "purple", "천호역", "군자역", 15)
+		).map(this::location_header에서_ID_추출)
+			.collect(Collectors.toList());
+
+		// when : 지하철_노선_목록_조회_요청
+		ExtractableResponse<Response> response = RestAssured
+			.given().log().all()
+			.when().get("/lines")
+			.then().log().all().extract();
+
+		// then : 지하철_노선_목록_응답됨 & 지하철_노선_목록_포함됨 & 지하철_노선_지하철역_목록_포함됨
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+			() -> assertThat(response.jsonPath().getList("id", Long.class)).containsAll(지하철노선_ID_목록),
+			() -> assertThat(response.jsonPath().getList("stations.name", List.class))
+				.map(list -> list.stream().sorted().collect(Collectors.joining(",")))
+				.containsAll(Arrays.asList("강남역,역삼역", "군자역,천호역"))
+		);
+	}
+
 	@DisplayName("지하철 노선을 조회한다.")
 	@Test
 	void getLine() {
@@ -113,6 +154,25 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
 		// then : 지하철_노선_응답됨
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+	}
+
+	@DisplayName("종점역을 포함하는 지하철 노선을 조회한다.")
+	@Test
+	void getLineWithLastStop() {
+		// given : 지하철_노선_등록되어_있음
+		long 이호선_ID = location_header에서_ID_추출(지하철노선_생성_요청("2호선", "green",
+			"강남역", "역삼역", 10));
+
+		// when : 지하철_노선_조회_요청
+		ExtractableResponse<Response> response = 지하철_노선_조회_요청(이호선_ID);
+
+		// then : 지하철_노선_응답됨 & 지하철_노선_지하철역_목록_포함됨
+		assertAll(
+			() -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+			() -> assertThat(response.jsonPath().getList("stations.name", String.class))
+				.containsAll(Arrays.asList("강남역","역삼역"))
+		);
 	}
 
 	@DisplayName("지하철 노선을 수정한다.")
@@ -164,10 +224,26 @@ public class LineAcceptanceTest extends AcceptanceTest {
 	}
 
 	private ExtractableResponse<Response> 지하철노선_생성_요청(String name, String color) {
-		Map<String, String> params = new HashMap<>();
+		Map<String, Object> params = new HashMap<>();
 		params.put("name", name);
 		params.put("color", color);
 
+		return 지하철노선_생성_요청(params);
+	}
+
+	private ExtractableResponse<Response> 지하철노선_생성_요청(String name, String color,
+		String upStationName, String downStationName, int distance) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("name", name);
+		params.put("color", color);
+		params.put("upStationId", location_header에서_ID_추출(StationAcceptanceTest.지하철_생성_요청(upStationName)));
+		params.put("downStationId", location_header에서_ID_추출(StationAcceptanceTest.지하철_생성_요청(downStationName)));
+		params.put("distance", distance);
+
+		return 지하철노선_생성_요청(params);
+	}
+
+	private ExtractableResponse<Response> 지하철노선_생성_요청(Map<String, Object> params) {
 		return RestAssured
 			.given().log().all()
 			.body(params)
