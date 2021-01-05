10fc6cd963c78e4d78d744e335e6188088864872
charlloss.dev <charlloss.dev@gmail.com>
2021-01-05 21:11:40 +0900

    test(section): 구간 추카 실패케이스 테스트 작성

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import nextstep.subway.common.exception.ExistException;
+import nextstep.subway.common.exception.NothingException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
@@ -29,16 +31,32 @@ class LineServiceTest {
 
 	private LineService lineService;
 
+	private Line line;
+	private Station 강남역;
+	private Station 서초역;
+	private Station 홍대역;
+	private Station 시청역;
+
+	private LineRequest lineRequest;
+
 	@BeforeEach
 	void setUp() {
 		lineService = new LineService(lineRepository, stationRepository);
+
+		강남역 = new Station(1L, "강남역");
+		서초역 = new Station(2L, "서초역");
+		홍대역 = new Station(3L, "홍대역");
+		시청역 = new Station(4L, "시청역");
+
+		line = new Line(1L, "2호선", "green", 강남역, 서초역, 100);
+		lineRequest = new LineRequest(line.getName(), line.getColor(), 서초역.getId(), 강남역.getId(), 100);
 	}
 
 	@DisplayName("지하철 노선 조회: DB Stubbing 테스트")
 	@Test
 	void saveTest() {
 		// given
-		when(lineRepository.findById(any())).thenReturn(java.util.Optional.of(new Line(1L, "2호선", "green")));
+		when(lineRepository.findById(any())).thenReturn(java.util.Optional.of(line));
 
 		// when
 		LineResponse lineResponse = lineService.showLine(1L);
@@ -52,13 +70,12 @@ class LineServiceTest {
 	@Test
 	void saveLineAddSectionTest() {
 		// given
-		LineRequest request = new LineRequest("2호선", "green", 1L, 2L, 100);
-		when(lineRepository.save(any())).thenReturn(new Line(1L, "2호선", "green"));
-		when(stationRepository.findById(1L)).thenReturn(java.util.Optional.of(new Station(1L, "서울역")));
-		when(stationRepository.findById(2L)).thenReturn(java.util.Optional.of(new Station(2L, "강남역")));
+		when(lineRepository.save(any())).thenReturn(line);
+		when(stationRepository.findById(강남역.getId())).thenReturn(java.util.Optional.of(강남역));
+		when(stationRepository.findById(서초역.getId())).thenReturn(java.util.Optional.of(서초역));
 
 		// when
-		LineResponse response = lineService.saveLine(request);
+		LineResponse response = lineService.saveLine(lineRequest);
 
 		// then
 		assertAll(
@@ -66,29 +83,43 @@ class LineServiceTest {
 		);
 	}
 
-	@DisplayName("지하철 노선 추가: 기존 노선에 section 추가 테스트 ")
+	@DisplayName("지하철 노선 추가 예외: 상행,하행역 모두 미포함 구간추가 예외")
 	@Test
-	void AddSectionAboutLineTest() {
+	void AddSectionNothingExceptionTest() {
 		// given
 		LineResponse response = 지하철_2호선_생성();
-		SectionRequest request = new SectionRequest(3L, 4L, 20);
-		when(stationRepository.findById(3L)).thenReturn(java.util.Optional.of(new Station(3L, "홍대역")));
-		when(stationRepository.findById(4L)).thenReturn(java.util.Optional.of(new Station(4L, "서초역")));
+		when(stationRepository.findById(홍대역.getId())).thenReturn(java.util.Optional.of(홍대역));
+		when(stationRepository.findById(시청역.getId())).thenReturn(java.util.Optional.of(시청역));
+		SectionRequest request = new SectionRequest(홍대역.getId(), 시청역.getId(), 20);
 
-		// when
-		LineResponse lineResponse = lineService.addSection(response.getId(), request);
+		// when //then
+		assertThatThrownBy(
+			() -> lineService.addSection(response.getId(), request)
+		).isInstanceOf(NothingException.class);
+	}
 
-		// then
-		assertAll(
-			() -> assertThat(response.getName()).isEqualTo("2호선")
-		);
+	@DisplayName("지하철 노선 추가 예외: 상행,하행역 모두 포함 구간추가 예외")
+	@Test
+	void AddSectionExistExceptionTest() {
+		// given
+		LineResponse response = 지하철_2호선_생성();
+		SectionRequest request = new SectionRequest(강남역.getId(), 서초역.getId(), 20);
+
+		// when //then
+		assertThatThrownBy(
+			() -> lineService.addSection(response.getId(), request)
+		).isInstanceOf(ExistException.class);
+	}
+
+	private void setStubbing() {
+		when(lineRepository.save(any())).thenReturn(line);
+		when(lineRepository.findById(any())).thenReturn(java.util.Optional.of(line));
+		when(stationRepository.findById(서초역.getId())).thenReturn(java.util.Optional.of(서초역));
+		when(stationRepository.findById(강남역.getId())).thenReturn(java.util.Optional.of(강남역));
 	}
 
 	private LineResponse 지하철_2호선_생성() {
-		when(lineRepository.save(any())).thenReturn(new Line(1L, "2호선", "green"));
-		when(lineRepository.findById(any())).thenReturn(java.util.Optional.of(new Line(1L, "2호선", "green")));
-		when(stationRepository.findById(1L)).thenReturn(java.util.Optional.of(new Station(1L, "서초역")));
-		when(stationRepository.findById(2L)).thenReturn(java.util.Optional.of(new Station(2L, "강남역")));
-		return lineService.saveLine(new LineRequest("2호선", "green", 1L, 2L, 100));
+		setStubbing();
+		return lineService.saveLine(lineRequest);
 	}
 }
