d32c2996f744ec03a28ea2d0a04044224aa1b133
charlloss.dev <charlloss.dev@gmail.com>
2021-01-05 20:41:17 +0900

    fix(exception): 예외처리 advice 수정

--- src/main/java/nextstep/subway/common/Advice.java
+++ src/main/java/nextstep/subway/common/Advice.java
@@ -5,24 +5,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
+import nextstep.subway.common.exception.NothingException;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.line.exception.LineNotFoundException;
-import nextstep.subway.station.dto.StationResponse;
-import nextstep.subway.station.exception.StationNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class Advice {
 
-	@ExceptionHandler(LineNotFoundException.class)
-	public ResponseEntity<LineResponse> handleLineNotFoundException(LineNotFoundException e) {
-		log.error("LineNotFoundException: " + e.getMessage());
-		return ResponseEntity.badRequest().build();
-	}
-
-	@ExceptionHandler(StationNotFoundException.class)
-	public ResponseEntity<StationResponse> handleStationNotFoundException(StationNotFoundException e) {
-		log.error("StationNotFoundException: " + e.getMessage());
+	@ExceptionHandler(NothingException.class)
+	public ResponseEntity<LineResponse> handleLineNotFoundException(NothingException e) {
+		log.error("Target NotFoundException: " + e.getMessage());
 		return ResponseEntity.badRequest().build();
 	}
 
