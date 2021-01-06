50e7ad168ff63f97095c581644f883c7a1769298
mkkim90 <mkzzang0928@gmail.com>
2021-01-06 19:14:47 +0900

    refactor : 이벤트핸들러 생성 및 MockMvc 활용

---
+++ src/main/java/nextstep/subway/exception/BaseExceptionHandler.java
@@ -0,0 +1,23 @@
+package nextstep.subway.exception;
+
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class BaseExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.badRequest().build();
+    }
+}
--- src/test/java/nextstep/subway/line/ui/LineControllerTest.java
+++ src/test/java/nextstep/subway/line/ui/LineControllerTest.java
@@ -1,30 +1,50 @@
 package nextstep.subway.line.ui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class LineControllerTest {
-    @Mock
-    private LineService lineService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    void createLineWithBadRequest() {
-        when(lineService.save(any())).thenThrow(RuntimeException.class);
-        LineController lineController = new LineController(lineService);
+    void createLineWithBadRequest() throws Exception {
+        // given
+        String content = objectMapper.writeValueAsString(new LineRequest("2호선", "green"));
 
-        LineRequest lineRequest = new LineRequest("2호선", "green");
-        ResponseEntity responseEntity = lineController.createLine(lineRequest);
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        // when
+        mockMvc.perform(post("/lines")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print());
     }
 }
