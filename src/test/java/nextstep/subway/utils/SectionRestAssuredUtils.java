59d980e59bdcb22af20e4d36c73e8e6b23d01045
shk3029 <shk30290@naver.com>
2021-01-09 19:43:18 +0900

    refactor: 테스트코드 param map 리팩토링

--- src/test/java/nextstep/subway/utils/SectionRestAssuredUtils.java
+++ src/test/java/nextstep/subway/utils/SectionRestAssuredUtils.java
@@ -10,15 +10,12 @@ import java.util.Map;
 
 public class SectionRestAssuredUtils {
 
-    public static Map<String, String> 구간_등록_요청_파라미터_생성(String upStationId, String downStationId, String distance) {
-        Map<String,String> param = new HashMap<>();
-        param.put("upStationId", upStationId);
-        param.put("downStationId", downStationId);
-        param.put("distance", distance);
-        return param;
-    }
+    public static ExtractableResponse<Response> 구간_등록_요청(Long lineId, String upStationId, String downStationId, String distance) {
+        Map<String,String> params = new HashMap<>();
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", distance);
 
-    public static ExtractableResponse<Response> 구간_등록_요청(Long lineId, Map<String, String> params) {
         return RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
--- src/test/java/nextstep/subway/utils/StationRestAssuredUtils.java
+++ src/test/java/nextstep/subway/utils/StationRestAssuredUtils.java
@@ -5,26 +5,19 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
-import javax.servlet.http.PushBuilder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StationRestAssuredUtils {
 
-    public static ExtractableResponse<Response> 지하철_역_생성_요청(Map<String, String> params) {
+    public static ExtractableResponse<Response> 지하철_역_생성_요청(String name) {
+        Map<String,String> params = new HashMap<>();
+        params.put("name", name);
+
         return RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/stations")
                 .then().log().all().extract();
     }
-
-    public static void 지하철_역_여러개_생성_요청() {
-        Map<String, String> stationParams = new HashMap<>();
-        stationParams.put("name", "강남역");
-        지하철_역_생성_요청(stationParams);
-        stationParams.put("name", "역삼역");
-        지하철_역_생성_요청(stationParams);
-    }
-
 }
