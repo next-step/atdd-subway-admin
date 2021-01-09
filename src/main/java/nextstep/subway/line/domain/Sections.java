8c0b607dc40bfa84aefdc2feda5daf7d67bbef73
mkkim90 <mkzzang0928@gmail.com>
2021-01-09 21:55:34 +0900

    refactor : sections 구간 추가 메서드 분리

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -6,8 +6,10 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @Embeddable
 public class Sections {
@@ -15,17 +17,39 @@ public class Sections {
     private final List<Section> sections = new ArrayList<>();
 
     public void add(Section section) {
-        sections.stream()
-                .filter(existSection -> existSection.isSameDownStation(section))
-                .findFirst()
-                .ifPresent(existSection -> existSection.replaceDownStation(section));
+        if (sections.isEmpty()) {
+            sections.add(section);
+            return;
+        }
 
+        validateAdd(section);
+        replaceDownStation(section);
+        replaceUpStation(section);
+        sections.add(section);
+    }
+
+    private void replaceUpStation(Section section) {
         sections.stream()
                 .filter(existSection -> existSection.isSameUpStation(section))
                 .findFirst()
                 .ifPresent(existSection -> existSection.replaceUpStation(section));
+    }
 
-        sections.add(section);
+    private void replaceDownStation(Section section) {
+        sections.stream()
+                .filter(existSection -> existSection.isSameDownStation(section))
+                .findFirst()
+                .ifPresent(existSection -> existSection.replaceDownStation(section));
+    }
+
+    private void validateAdd(Section section) {
+        if (isNotContainUpStationAndDownStation(section, getStations())) {
+            throw new IllegalArgumentException("상행역과 하행역 둘 중 하나도 포함되어 있지 않습니다.");
+        }
+    }
+
+    private boolean isNotContainUpStationAndDownStation(Section section, List<Station> staions) {
+        return !staions.contains(section.getUpStation()) && !staions.contains(section.getDownStation());
     }
 
     public List<Section> getSections() {
@@ -33,17 +57,9 @@ public class Sections {
     }
 
     public List<Station> getStations() {
-        return sections.stream()
-                .map(section -> section.getStations())
-                .flatMap(Collection::stream)
-                .distinct()
-                .collect(Collectors.toList());
-    }
-
-    public List<Station> getStationsInOrder() {
         Optional<Section> section = sections.stream()
                 .findFirst();
-        
+
         List<Station> result = findDownToUpStation(section);
         result.addAll(findUpToDownStation(section));
         return result;
