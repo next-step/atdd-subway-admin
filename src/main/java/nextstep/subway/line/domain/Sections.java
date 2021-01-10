6c206f45ba54fec881697bbaffa072423c9df69d
byungkyu-ju <noel1227@gmail.com>
2021-01-10 12:09:53 +0900

    refactor: predicate로 유사한 패턴의 stream을 동적으로 수행하도록 변경

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import javax.persistence.CascadeType;
@@ -41,21 +42,92 @@ public class Sections {
 
 		validateSection(section);
 		//역 사이에 새로운 역 등록
-		if (isEqualToUpStation(section.getUpStationId())) {
-			addSectionSameUpStation(section);
+		if (addStationMiddleOfSection(section)){
 			return;
 		}
 
 		// 새로운 역을 상행 종점으로 등록할 경우
-		if (isEqualToUpStation(section.getDownStationId())) {
-			addSectionNewUpStation(section);
+		if (addStationTopOfSection(section)){
 			return;
 		}
 
 		//새로운 역을 하향 종점으로 등록할 경우
-		if (isEqualToDownStation(section.getUpStationId())) {
-			addSectionNewDownStation(section);
+		addStationBottomOfSection(section);
+	}
+
+	private void addStationBottomOfSection(Section section) {
+		Predicate<Section> downStationEqualDownStationSectionPredicate = equalDownStationIdPredicate(
+			section.getUpStationId());
+
+		if (isEqualToSectionsInStation(downStationEqualDownStationSectionPredicate)) {
+
+			Consumer<Section> sectionConsumer = upStationToDownStationConsumer(section);
+			addSectionNewStation(downStationEqualDownStationSectionPredicate, sectionConsumer, section);
 		}
+	}
+
+	private boolean addStationTopOfSection(Section section) {
+		Predicate<Section> downStationEqualUpStationSectionPredicate = equalUpStationIdPredicate(
+			section.getDownStationId());
+
+		if (isEqualToSectionsInStation(downStationEqualUpStationSectionPredicate)) {
+			Consumer<Section> sectionConsumer = downStationToUpStationConsumer(section);
+
+			addSectionNewStation(downStationEqualUpStationSectionPredicate, sectionConsumer, section);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean addStationMiddleOfSection(Section section) {
+		Predicate<Section> upStationEqualUpStationSectionPredicate = equalUpStationIdPredicate(
+			section.getUpStationId());
+
+		if (isEqualToSectionsInStation(upStationEqualUpStationSectionPredicate)) {
+			addSectionSameUpStation(section);
+			return true;
+		}
+		return false;
+	}
+
+	private Consumer<Section> downStationToUpStationConsumer(Section section) {
+		return originSection -> originSection.changeUpStation(
+			section.getDownStationId(),
+			section.getDistance());
+	}
+
+	private Consumer<Section> upStationToDownStationConsumer(Section section) {
+		return originSection -> originSection.changeDownStation(
+			section.getUpStationId(),
+			section.getDistance());
+	}
+
+	private void addSectionNewStation(Predicate<Section> predicateFilter, Consumer<Section> sectionConsumer,
+		Section section) {
+		sections.stream()
+			.filter(predicateFilter)
+			.findFirst()
+			.ifPresent(
+				sectionConsumer);
+
+		sections.add(section);
+	}
+
+	private boolean isEqualToSectionsInStation(Predicate<Section> equalStationIdPredicate) {
+		return sections.stream()
+			.anyMatch(equalStationIdPredicate);
+	}
+
+	private void addSectionSameUpStation(Section section) {
+		sections.stream()
+			.filter(originSection -> originSection.getUpStationId().equals(section.getUpStationId())
+				&& originSection.getDistance() > section
+				.getDistance())
+			.findFirst()
+			.ifPresent(originSection -> originSection.changeUpStation(section.getDownStationId(),
+				originSection.getDistance() - section.getDistance()));
+
+		sections.add(section);
 	}
 
 	private void validateSection(Section section) {
@@ -86,55 +158,13 @@ public class Sections {
 		});
 	}
 
-	private void addSectionNewDownStation(Section section) {
-		sections.stream()
-			.filter(originSection -> originSection.getUpStationId().equals(section.getDownStationId()))
-			.findFirst()
-			.ifPresent(
-				originSection -> originSection.changeDownStation(section.getUpStationId(), section.getDistance()));
-
-		sections.add(section);
-	}
-
-	private void addSectionNewUpStation(Section section) {
-		sections.stream()
-			.filter(originSection -> originSection.getUpStationId().equals(section.getDownStationId()))
-			.findFirst()
-			.ifPresent(
-				originSection -> originSection.changeUpStation(section.getDownStationId(), section.getDistance()));
-
-		sections.add(section);
-	}
-
-	private void addSectionSameUpStation(Section section) {
-		sections.stream()
-			.filter(originSection -> originSection.getUpStationId().equals(section.getUpStationId())
-				&& originSection.getDistance() > section
-				.getDistance())
-			.findFirst()
-			.ifPresent(originSection -> originSection.changeUpStation(section.getDownStationId(),
-				originSection.getDistance() - section.getDistance()));
-
-		sections.add(section);
-	}
-
-	private boolean isEqualToDownStation(Long stationId) {
-		return sections.stream()
-			.anyMatch(originSection -> originSection.getDownStationId().equals(stationId));
-	}
-
-	private boolean isEqualToUpStation(Long stationId) {
-		return sections.stream()
-			.anyMatch(originSection -> originSection.getUpStationId().equals(stationId));
-	}
-
 	public void removeSectionByStationId(Long stationId) {
 		if (sections.size() == 1) {
 			throw new OneSectionCannotRemoveException();
 		}
 
-		Section frontStationSection = getSectionContainByStationId(filterEqualDownStationIdPredicate(stationId));
-		Section backStationSection = getSectionContainByStationId(filterEqualUpStationIdPredicate(stationId));
+		Section frontStationSection = getSectionContainByStationId(equalDownStationIdPredicate(stationId));
+		Section backStationSection = getSectionContainByStationId(equalUpStationIdPredicate(stationId));
 
 		if (frontStationSection == null && backStationSection == null)
 			throw new NotFoundException();
@@ -147,18 +177,18 @@ public class Sections {
 		}
 	}
 
-	private Section getSectionContainByStationId(Predicate<Section> filterEqualStationIdPredicate) {
+	private Section getSectionContainByStationId(Predicate<Section> equalStationIdPredicate) {
 		return sections.stream()
-			.filter(filterEqualStationIdPredicate)
+			.filter(equalStationIdPredicate)
 			.findFirst()
 			.orElse(null);
 	}
 
-	private Predicate<Section> filterEqualDownStationIdPredicate(Long stationId) {
+	private Predicate<Section> equalDownStationIdPredicate(Long stationId) {
 		return section -> section.getDownStationId().equals(stationId);
 	}
 
-	private Predicate<Section> filterEqualUpStationIdPredicate(Long stationId) {
+	private Predicate<Section> equalUpStationIdPredicate(Long stationId) {
 		return section -> section.getUpStationId().equals(stationId);
 	}
 
