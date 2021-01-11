0f15f49914ed7ba6cfd761f1de052a8dcdb5b49a
Do Yeon Kim <chikeem90@gmail.com>
2021-01-12 01:10:33 +0900

    test: 2, 3번 케이스 테스트 추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -29,9 +29,12 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @Autowired
     StationRepository stationRepository;
 
-    Station gangnam;
-    Station seocho;
-    Station gyodae;
+    Station 사당역;
+    Station 교대역;
+    Station 강남역;
+    Station 선릉역;
+    Station 삼성역;
+    Station 잠실역;
 
     Line line;
 
@@ -41,20 +44,23 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     public void setUp() {
         super.setUp();
         line = new Line("2호선", "green");
-        gangnam = stationRepository.save(new Station("강남역"));
-        seocho = stationRepository.save(new Station("서초역"));
-        gyodae = stationRepository.save(new Station("교대역"));
-        Section section = new Section(line, gangnam, seocho, 10);
+        사당역 = stationRepository.save(new Station("사당역"));
+        교대역 = stationRepository.save(new Station("교대역"));
+        강남역 = stationRepository.save(new Station("강남역"));
+        선릉역 = stationRepository.save(new Station("선릉역"));
+        삼성역 = stationRepository.save(new Station("삼성역"));
+        잠실역 = stationRepository.save(new Station("잠실역"));
+        Section section = new Section(line, 교대역, 삼성역, 10);
         line.addSection(section);
         lineRepository.save(line);
     }
 
     @DisplayName("역 사이에 새로운 역을 등록할 경우")
     @Test
-    void addSectionInTheMiddleOf() {
+    void addSectionInTheMiddleOfUp() {
         params = new HashMap<>();
-        params.put("upStationId", Long.toString(gangnam.getId()));
-        params.put("downStationId", Long.toString(gyodae.getId()));
+        params.put("upStationId", Long.toString(교대역.getId()));
+        params.put("downStationId", Long.toString(강남역.getId()));
         params.put("distance", Integer.toString(4));
 
         ExtractableResponse<Response> response = RestAssured.given().log().all()
@@ -65,5 +71,77 @@ public class SectionAcceptanceTest extends AcceptanceTest {
             .then().log().all()
             .extract();
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("역 사이에 새로운 역을 등록할 경우")
+    @Test
+    void addSectionInTheMiddleOfDown() {
+        params = new HashMap<>();
+        params.put("upStationId", Long.toString(선릉역.getId()));
+        params.put("downStationId", Long.toString(삼성역.getId()));
+        params.put("distance", Integer.toString(4));
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines/" + line.getId() + "/sections")
+            .then().log().all()
+            .extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("새로운 역을 상행 종점으로 등록할 경우")
+    @Test
+    void addSectionNewUpStation() {
+        params = new HashMap<>();
+        params.put("upStationId", Long.toString(사당역.getId()));
+        params.put("downStationId", Long.toString(교대역.getId()));
+        params.put("distance", Integer.toString(4));
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines/" + line.getId() + "/sections")
+            .then().log().all()
+            .extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("새로운 역을 하행 종점으로 등록할 경우")
+    @Test
+    void addSectionNewDownStation() {
+        params = new HashMap<>();
+        params.put("upStationId", Long.toString(삼성역.getId()));
+        params.put("downStationId", Long.toString(잠실역.getId()));
+        params.put("distance", Integer.toString(4));
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines/" + line.getId() + "/sections")
+            .then().log().all()
+            .extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음")
+    @Test
+    void addSectionInTheMiddleOfException() {
+
+    }
+
+    @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+    @Test
+    void addSectionAlreadyExistException() {
+
+    }
+
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
+    @Test
+    void addSectionNotIncludeException() {
+
     }
 }
