3fea51e3b2331f400e7ddb212f46966f378acf20
Do Yeon Kim <chikeem90@gmail.com>
2021-01-12 22:14:11 +0900

    feat: delete URL 추가

--- src/main/java/nextstep/subway/section/ui/SectionController.java
+++ src/main/java/nextstep/subway/section/ui/SectionController.java
@@ -2,10 +2,12 @@ package nextstep.subway.section.ui;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import nextstep.subway.section.application.SectionService;
@@ -20,18 +22,24 @@ public class SectionController {
 	}
 
 	@PostMapping("/lines/{lineId}/sections")
-	public ResponseEntity addSection(@PathVariable Long lineId, @RequestBody SectionRequest sectionRequest) {
+	public ResponseEntity<Void> addSection(@PathVariable Long lineId, @RequestBody SectionRequest sectionRequest) {
 		sectionService.saveSection(lineId, sectionRequest);
 		return ResponseEntity.ok().build();
 	}
 
+	@DeleteMapping("/lines/{lineId}/sections")
+	public ResponseEntity<Void> removeLineStation(@PathVariable Long lineId, @RequestParam Long stationId) {
+		sectionService.removeSectionByStationId(lineId, stationId);
+		return ResponseEntity.ok().build();
+	}
+
 	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity handleIllegalArgsException(IllegalArgumentException e) {
+	public ResponseEntity<Void> handleIllegalArgsException(IllegalArgumentException e) {
 		return ResponseEntity.badRequest().build();
 	}
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+	public ResponseEntity<Void> handleIllegalArgsException(DataIntegrityViolationException e) {
 		return ResponseEntity.badRequest().build();
 	}
 }
