8f915e02b8a44a4a892248a2499fa7472c720e03
Mingyu Lee <leemingyu05@gmail.com>
2021-01-13 23:45:45 +0900

    test: 기존에 존재하는 지하철 노선 이름으로 지하철 노선 생성 테스트

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.common.exception.dto.ErrorResponse;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 
@@ -34,15 +35,16 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
-    void createLine2() {
+    void createLineDuplicated() {
         // given
-        // 지하철_노선_등록되어_있음();
+        LineRequest request = new LineRequest("신분당선", "bg-red-600");
+        지하철_노선_등록되어_있음(request);
 
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(request);
 
         // then
-        // 지하철_노선_생성_실패됨
+        지하철_노선_중복_생성_실패됨(response);
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
@@ -116,6 +118,21 @@ public class LineAcceptanceTest extends AcceptanceTest {
             () -> assertThat(response.header(HttpHeaders.LOCATION)).isNotBlank(),
             () -> assertThat(response.as(LineResponse.class).getName()).isEqualTo(request.getName()),
             () -> assertThat(response.as(LineResponse.class).getColor()).isEqualTo(request.getColor())
+        );
+    }
+
+    public Long 지하철_노선_등록되어_있음(final LineRequest request) {
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(request);
+        final String URL_PATH_SEPARATOR = "/";
+        String[] locationParts = response.header(HttpHeaders.LOCATION).split(URL_PATH_SEPARATOR);
+        return Long.parseLong(locationParts[locationParts.length - 1]);
+    }
+
+    private void 지하철_노선_중복_생성_실패됨(final ExtractableResponse<Response> response) {
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+            () -> assertThat(response.as(ErrorResponse.class).getMessage())
+                .isEqualTo("기본 키 또는 유니크 제약조건에 위배됩니다.")
         );
     }
 
