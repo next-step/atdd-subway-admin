07b8065100798ad900c45037ab7c133e72771fc3
Mingyu Lee <leemingyu05@gmail.com>
2021-01-14 00:18:42 +0900

    feat: 지하철 노선 목록 조회 컨트롤러 및 서비스 구현
    * @Transactional 어노테이션을 클래스 레벨에선 readOnly로 구현
    * DML 메서드에서 @Transactional 어노테이션 선언하여 트랜잭션 처리

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,23 +1,35 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
     }
 
+    public List<LineResponse> findAll() {
+        return lineRepository.findAll().stream()
+            .map(LineResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
     }
+
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -4,12 +4,14 @@ import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -18,6 +20,11 @@ public class LineController {
 
     public LineController(final LineService lineService) {
         this.lineService = lineService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> findAll() {
+        return ResponseEntity.ok(lineService.findAll());
     }
 
     @PostMapping
