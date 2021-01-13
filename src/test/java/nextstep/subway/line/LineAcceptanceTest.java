505efa972cff1f214e7f797a940bae8206cb87c2
Mingyu Lee <leemingyu05@gmail.com>
2021-01-13 22:55:04 +0900

    feat: 지하철 노선 생성 인수 테스트

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,26 +1,42 @@
 package nextstep.subway.line;
 
-import nextstep.subway.AcceptanceTest;
+import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
+        // given
+        LineRequest request = new LineRequest("신분당선", "bg-red-600");
+
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(request);
 
         // then
-        // 지하철_노선_생성됨
+        지하철_노선_생성됨(response, request);
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
     void createLine2() {
         // given
-        // 지하철_노선_등록되어_있음
+        // 지하철_노선_등록되어_있음();
 
         // when
         // 지하철_노선_생성_요청
@@ -82,4 +98,25 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // then
         // 지하철_노선_삭제됨
     }
+
+    private ExtractableResponse<Response> 지하철_노선_생성_요청(final LineRequest lineRequest) {
+        return given().log().all()
+            .body(lineRequest)
+            .accept(MediaType.ALL_VALUE)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
+    }
+
+    private void 지하철_노선_생성됨(final ExtractableResponse<Response> response, final LineRequest request) {
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+            () -> assertThat(response.header(HttpHeaders.LOCATION)).isNotBlank(),
+            () -> assertThat(response.as(LineResponse.class).getName()).isEqualTo(request.getName()),
+            () -> assertThat(response.as(LineResponse.class).getColor()).isEqualTo(request.getColor())
+        );
+    }
+
 }
