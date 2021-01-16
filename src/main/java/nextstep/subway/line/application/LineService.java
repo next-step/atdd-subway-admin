5e2b7f4456c16ddd47921321851af4660e513379
chh <chh2644@gmail.com>
2021-01-16 17:58:37 +0900

    feat : 지하철_노선 테스트 코드 수정

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,6 +4,8 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +16,18 @@ import java.util.stream.Collectors;
 @Transactional
 public class LineService {
     private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
     public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
+        Station upStation = getStation(request.getUpStationId());
+        Station downStation = getStation(request.getDownStationId());
+
+        Line persistLine = lineRepository.save(request.toLine(upStation, downStation));
         return LineResponse.of(persistLine);
     }
 
@@ -40,7 +47,8 @@ public class LineService {
     public LineResponse updateLine(long id, LineRequest lineRequest) {
         Line line = lineRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("데이터가 존재하지 않습니다."));
-        line.update(lineRequest.toLine());
+        line.update(new Line(lineRequest.getName(), lineRequest.getColor()));
+
         return LineResponse.of(line);
     }
 
@@ -48,5 +56,10 @@ public class LineService {
         Line line = lineRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("데이터가 존재하지 않습니다."));
         lineRepository.delete(line);
+    }
+
+    public Station getStation(long id) {
+        return stationRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("데이터가 존재하지 않습니다."));
     }
 }
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.StationAcceptanceUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,10 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine() {
         // when
         // 지하철_노선_생성_요청
-        ExtractableResponse<Response> response = LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600");
+        long upStationId = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long downStationId = StationAcceptanceUtil.지하철_역_생성_요청("판교역");
+        ExtractableResponse<Response> response = LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600"
+                                                                            , upStationId, downStationId, 10);
 
         // then
         // 지하철_노선_생성됨
@@ -36,11 +40,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine2() {
         // given
         // 지하철_노선_등록되어_있음
-        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600");
+        long upStationId = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long downStationId = StationAcceptanceUtil.지하철_역_생성_요청("판교역");
+        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600",
+                                                upStationId, downStationId, 2);
 
         // when
         // 지하철_노선_생성_요청
-        ExtractableResponse<Response> response = LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600");
+        ExtractableResponse<Response> response = LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600",
+                                                                              upStationId, downStationId, 2);
 
         // then
         // 지하철_노선_생성_실패됨
@@ -54,8 +62,14 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // given
         // 지하철_노선_등록되어_있음
-        expected.add(LineAcceptanceUtil.지하철_노선_생성_아이디_조회(LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600")));
-        expected.add(LineAcceptanceUtil.지하철_노선_생성_아이디_조회(LineAcceptanceUtil.지하철_노선_생성_요청("2호선", "bg-green-600")));
+        long upStationId1 = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long downStationId1 = StationAcceptanceUtil.지하철_역_생성_요청("판교역");
+        long downStationId2 = StationAcceptanceUtil.지하철_역_생성_요청("당산역");
+
+        ExtractableResponse<Response> response1 = LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600", upStationId1, downStationId1, 10);
+        ExtractableResponse<Response> response2 = LineAcceptanceUtil.지하철_노선_생성_요청("2호선", "bg-green-600", upStationId1, downStationId2, 10);
+        expected.add(LineAcceptanceUtil.지하철_노선_생성_아이디_조회(response1));
+        expected.add(LineAcceptanceUtil.지하철_노선_생성_아이디_조회(response2));
 
         // when
         // 지하철_노선_목록_조회_요청
@@ -73,6 +87,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
         assertThat(result.get(0).getColor()).isEqualTo("bg-red-600");
         assertThat(result.get(1).getName()).isEqualTo("2호선");
         assertThat(result.get(1).getColor()).isEqualTo("bg-green-600");
+        
+        assertThat(result.get(0).getStations().get(0).getName()).isEqualTo("잠실역");
+        assertThat(result.get(0).getStations().get(1).getName()).isEqualTo("판교역");
+        assertThat(result.get(1).getStations().get(0).getName()).isEqualTo("잠실역");
+        assertThat(result.get(1).getStations().get(1).getName()).isEqualTo("당산역");
     }
 
     @DisplayName("지하철 노선을 조회한다.")
@@ -80,7 +99,9 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLine() {
         // given
         // 지하철_노선_등록되어_있음
-        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600");
+        long upStationId = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long downStationId = StationAcceptanceUtil.지하철_역_생성_요청("판교역");
+        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
 
         // when
         // 지하철_노선_조회_요청
@@ -99,7 +120,9 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void updateLine() {
         // given
         // 지하철_노선_등록되어_있음
-        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600");
+        long upStationId = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long downStationId = StationAcceptanceUtil.지하철_역_생성_요청("판교역");
+        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
 
         // when
         // 지하철_노선_수정_요청
@@ -118,7 +141,9 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void deleteLine() {
         // given
         // 지하철_노선_등록되어_있음
-        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600");
+        long upStationId = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long downStationId = StationAcceptanceUtil.지하철_역_생성_요청("판교역");
+        LineAcceptanceUtil.지하철_노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
 
         // when
         // 지하철_노선_제거_요청
