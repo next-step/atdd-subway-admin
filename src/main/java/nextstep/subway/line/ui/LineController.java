f2671127ca25622a9c09401292f8ab1a39db1aa7
ssovely <hijh1224@gmail.com>
2021-01-18 02:34:31 +0900

    feat(line): add line feature

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,28 +1,67 @@
 package nextstep.subway.line.ui;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
 
 @RestController
-@RequestMapping("/lines")
 public class LineController {
-    private final LineService lineService;
+	private final LineService lineService;
 
-    public LineController(final LineService lineService) {
-        this.lineService = lineService;
-    }
+	public LineController(final LineService lineService) {
+		this.lineService = lineService;
+	}
 
-    @PostMapping
-    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
-        LineResponse line = lineService.saveLine(lineRequest);
-        return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
-    }
+	@PostMapping("/lines")
+	public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
+		LineResponse line = lineService.saveLine(lineRequest);
+		return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+	}
+
+	@GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<LineResponse>> showLines() {
+		return ResponseEntity.ok(lineService.findAllLines());
+	}
+
+	@GetMapping(value = "/line/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
+		return ResponseEntity.ok(lineService.findLineById(id));
+	}
+
+	@PutMapping("/line/{id}")
+	public ResponseEntity<LineResponse> updateLine(@PathVariable Long id, @RequestBody LineRequest lineRequest){
+		LineResponse line = lineService.updateLine(id, lineRequest);
+		return ResponseEntity.ok().location(URI.create("/lines/" + line.getId())).body(line);
+	}
+
+	@DeleteMapping("/line/{id}")
+	public ResponseEntity<?> deleteStation(@PathVariable Long id) {
+		lineService.deleteLineById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<?> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+		return ResponseEntity.badRequest().build();
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<?> handleIllegalArgsException(IllegalArgumentException e) {
+		return ResponseEntity.notFound().build();
+	}
 }
