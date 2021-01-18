2b304c1e059c1e0fd14a98c09a93b774e480bb35
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-01-18 16:21:08 +0900

    feat, test - 지하철 노선 목록 조회

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -4,10 +4,12 @@ import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -22,6 +24,11 @@ public class LineController {
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> selectLines() {
+        return ResponseEntity.ok().body(lineService.findAllStations());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineResponse;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 노선 관련 기능")
@@ -85,14 +88,52 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLines() {
         // given
         // 지하철_노선_등록되어_있음
-        // 지하철_노선_등록되어_있음
+        Map<String, String> params = new HashMap<>();
+        params.put("name","1호선");
+        params.put("color","파란색");
 
+        RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all().extract();
+        // 지하철_노선_등록되어_있음
+        params.clear();
+        params.put("name","2호선");
+        params.put("color","초록색");
+        RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all().extract();
         // when
         // 지하철_노선_목록_조회_요청
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when()
+                .get("/lines")
+                .then().log().all().extract();
 
         // then
         // 지하철_노선_목록_응답됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         // 지하철_노선_목록_포함됨
+        response.jsonPath().getList(".", LineResponse.class)
+                .forEach(line -> {
+                    MatcherAssert.assertThat(line.getId(),
+                            anyOf(equalTo(1L), equalTo(2L)));
+                    MatcherAssert.assertThat(line.getName(),
+                            either(containsString("1호선")).or(containsString("2호선")));
+                    MatcherAssert.assertThat(line.getColor(),
+                            either(containsString("파란색")).or(containsString("초록색")));
+                    assertThat(line.getCreatedDate()).isNotNull();
+                    assertThat(line.getModifiedDate()).isNotNull();
+                });
     }
 
     @DisplayName("지하철 노선을 조회한다.")
