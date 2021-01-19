4d4cee739a13d8e3076163459c3b8a890b9e1a96
Mingyu Lee <leemingyu05@gmail.com>
2021-01-20 00:05:14 +0900

    refactor: 현시점에선 불필요하고 식별하기 어려운 eqauls 메서드 삭제

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -1,7 +1,6 @@
 package nextstep.subway.line.domain;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -69,22 +68,6 @@ public class Section extends BaseEntity {
 
 	public List<Station> getStations() {
 		return Stream.of(up, down).collect(Collectors.toList());
-	}
-
-	@Override
-	public boolean equals(final Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		final Section section = (Section)o;
-		return Objects.equals(id, section.id) && Objects.equals(line, section.line) && Objects
-			.equals(up, section.up) && Objects.equals(down, section.down);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, line, up, down);
 	}
 
 	@Override
