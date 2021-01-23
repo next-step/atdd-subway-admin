e326c78fa25eced3736fced4d97988cdc1eba262
chh <chh2644@gmail.com>
2021-01-23 15:33:58 +0900

    feat : default Exception 처리

--- src/main/java/nextstep/subway/common/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package nextstep.subway.common.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleCustom(MyException e) {
         ErrorResponse response = new ErrorResponse(e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void defaultExceptionHandler(Exception e) {
     }
 }
