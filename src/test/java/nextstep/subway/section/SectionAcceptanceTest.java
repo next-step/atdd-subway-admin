3f85ae2cc84c80a327a9255dbdff1701c074287f
chh <chh2644@gmail.com>
2021-01-23 14:46:47 +0900

    test : 구간 테스트 추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -20,8 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @DisplayName("노선 구간 관련 기능")
 public class SectionAcceptanceTest extends AcceptanceTest {
 
-    long 강남역_id;
     long 광교역_id;
+    long 강남역_id;
     ExtractableResponse<Response> 신분당선;
 
     @BeforeEach
@@ -29,6 +29,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         super.setUp();
 
         신분당선 = LineAcceptanceUtil.지하철_노선_구간_생성_요청("신분당선", "bg-red-600", "강남역", "광교역", 10);
+        강남역_id = StationAcceptanceUtil.지하철_역_조회("강남역").jsonPath().getObject(".", StationResponse.class).getId();
         광교역_id = StationAcceptanceUtil.지하철_역_조회("광교역").jsonPath().getObject(".", StationResponse.class).getId();
     }
 
@@ -72,7 +73,44 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
         // then
         // 지하철_노선에_지하철역_등록됨
-        지하철_노선_응답_결과(지하철_노선_구간_등록, HttpStatus.INTERNAL_SERVER_ERROR);
+        지하철_노선_응답_결과(지하철_노선_구간_등록, HttpStatus.BAD_REQUEST);
+    }
+
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
+    @Test
+    void not_included_station() {
+        // given
+        long 잠실역_id = StationAcceptanceUtil.지하철_역_생성_요청("잠실역");
+        long 당산역_id = StationAcceptanceUtil.지하철_역_생성_요청("당산역");
+
+        // 노선 조회 후 구간 등록
+        LineResponse 신분당선_조회 = 신분당선.body().as(LineResponse.class);
+        SectionRequest sectionRequest = new SectionRequest(잠실역_id, 당산역_id, 10);
+
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        ExtractableResponse<Response> 지하철_노선_구간_등록 = SectionAcceptanceUtil.지하철_노선_구간_등록_요청(신분당선_조회.getId(), sectionRequest);
+
+        // then
+        // 지하철_노선에_지하철역_등록됨
+        지하철_노선_응답_결과(지하철_노선_구간_등록, HttpStatus.BAD_REQUEST);
+    }
+
+    @DisplayName("요청된 상행역과 하행역이 이미 있으면 추가할 수 없음")
+    @Test
+    void already_section() {
+        // given
+        // 노선 조회 후 구간 등록
+        LineResponse 신분당선_조회 = 신분당선.body().as(LineResponse.class);
+        SectionRequest sectionRequest = new SectionRequest(강남역_id, 광교역_id, 10);
+
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        ExtractableResponse<Response> 지하철_노선_구간_등록 = SectionAcceptanceUtil.지하철_노선_구간_등록_요청(신분당선_조회.getId(), sectionRequest);
+
+        // then
+        // 지하철_노선에_지하철역_등록됨
+        지하철_노선_응답_결과(지하철_노선_구간_등록, HttpStatus.BAD_REQUEST);
     }
 
     private void 지하철_노선_응답_결과(ExtractableResponse<Response> response, HttpStatus status) {
