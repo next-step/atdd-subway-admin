386603d4c01dc13c607647117eaaba7be234020f
dhmin5693 <dhmin5693@naver.com>
2021-05-29 02:23:40 +0900

    feat: 지하철 노선 생성 기능 추가

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,19 +1,41 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
+    private final LineRequest INCHEON_SUBWAY_LINE_1 = new LineRequest("인천 1호선", "#7CA8D5");
+    private final LineRequest AIRPORT_EXPRESS = new LineRequest("공항철도", "#0065B3");
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
-    void createLine() {
+    void createLineRequest() {
+
         // when
         // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = createLineRequest(INCHEON_SUBWAY_LINE_1);
 
         // then
         // 지하철_노선_생성됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location").startsWith("/lines")).isTrue();
+
+        assertThat(response.body().jsonPath().getString("name"))
+            .isEqualTo(INCHEON_SUBWAY_LINE_1.getName());
+
+        assertThat(response.body().jsonPath().getString("color"))
+            .isEqualTo(INCHEON_SUBWAY_LINE_1.getColor());
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
@@ -21,12 +43,16 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine2() {
         // given
         // 지하철_노선_등록되어_있음
+        createdLine(INCHEON_SUBWAY_LINE_1);
 
         // when
         // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = createLineRequest(INCHEON_SUBWAY_LINE_1);
 
         // then
         // 지하철_노선_생성_실패됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(response.header("Location")).isBlank();
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
@@ -81,5 +107,19 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         // 지하철_노선_삭제됨
+    }
+
+    private ExtractableResponse<Response> createLineRequest(LineRequest lineRequest) {
+        return RestAssured.given().log().all()
+                          .body(lineRequest)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .when()
+                          .post("/lines")
+                          .then().log().all()
+                          .extract();
+    }
+
+    private void createdLine(LineRequest lineRequest) {
+        createLineRequest(lineRequest);
     }
 }
