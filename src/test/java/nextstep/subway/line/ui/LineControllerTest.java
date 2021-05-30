e9604bef989d15757f59f1fdd0f055617e997049
etff <wish2rich@gmail.com>
2021-05-30 20:45:16 +0900

    test: line 생성 테스트를 추가한다

--- src/test/java/nextstep/subway/line/ui/LineControllerTest.java
+++ src/test/java/nextstep/subway/line/ui/LineControllerTest.java
@@ -1,6 +1,70 @@
 package nextstep.subway.line.ui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class LineControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LineService lineService;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    private LineRequest lineRequest = new LineRequest("2호선", "green");
+    private LineResponse lineResponse = new LineResponse(1L, "3호선", "orange", LocalDateTime.now(), LocalDateTime.now());
+
+    @Nested
+    @DisplayName("POST /lines는")
+    class Describe_create_line {
+
+        @Nested
+        @DisplayName("노선이 주어지면")
+        class Context_with_line {
+            final LineRequest givenLineRequest = lineRequest;
+
+            @BeforeEach
+            void setUp() {
+                when(lineService.saveLine(any(LineRequest.class)))
+                        .thenReturn(lineResponse);
+            }
+
+            @DisplayName("201 create를 응답한다.")
+            @Test
+            void It_responds_create() throws Exception {
+                mockMvc.perform(
+                        post("/lines")
+                                .accept(MediaType.APPLICATION_JSON_UTF8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(givenLineRequest))
+                )
+                        .andExpect(status().isCreated());
+            }
+        }
+    }
 
 }
