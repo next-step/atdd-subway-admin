30b82a05d9ec6a9f7a76ee9613a86dc5bb3870ec
dhmin5693 <dhmin5693@naver.com>
2021-06-02 02:56:37 +0900

    feat: `Section` 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,17 +1,26 @@
 package nextstep.subway.line.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import nextstep.subway.common.BaseEntity;
 
 import javax.persistence.*;
+import nextstep.subway.section.domain.Section;
 
 @Entity
 public class Line extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     private String color;
+
+    @OneToMany(mappedBy = "line")
+    private List<Section> sections = new ArrayList<>();
 
     public Line() {
     }
@@ -26,6 +35,11 @@ public class Line extends BaseEntity {
         this.color = line.getColor();
     }
 
+    public void addSection(Section section) {
+        sections.add(section);
+        section.toLine(this);
+    }
+
     public Long getId() {
         return id;
     }
@@ -36,5 +50,9 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
+    }
+
+    public List<Section> getSections() {
+        return sections;
     }
 }
---
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -0,0 +1,66 @@
+package nextstep.subway.section.domain;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import nextstep.subway.common.BaseEntity;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+@Entity
+public class Section extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -7761205882129160224L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    @Column(nullable = false)
+    private Integer distance;
+
+    protected Section() {
+
+    }
+
+    public Section(Station upStation, Station downStation, Integer distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Section toLine(Line line) {
+        this.line = line;
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+}
