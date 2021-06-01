d1dd5904910e55bd27d3b6a805cb7a38984e9012
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-01 12:46:10 +0900

    refactor : ResponseEntity 호출메소드 변경 (created -> ok)

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -3,14 +3,17 @@ package nextstep.subway.line.ui;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lines")
 public class LineController {
     private final LineService lineService;
 
@@ -18,15 +21,15 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @PostMapping
+    @PostMapping("/lines")
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
-    @GetMapping
+    @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<LineResponse>> findAllLines() {
-        List<LineResponse> lines = lineService.findAllLine();
-        return ResponseEntity.created(URI.create("/lines")).body(lines);
+        List<LineResponse> lines = lineService.findAllLines();
+        return ResponseEntity.ok().body(lines);
     }
 }
