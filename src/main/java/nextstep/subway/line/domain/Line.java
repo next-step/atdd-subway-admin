b12c7f9dabddf747d218699eb8a683168251f2f3
dhmin5693 <dhmin5693@naver.com>
2021-06-02 20:46:06 +0900

    refactor: `Sections` 일급 콜렉션 생성

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,21 +1,16 @@
 package nextstep.subway.line.domain;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import nextstep.subway.common.BaseEntity;
 import nextstep.subway.section.domain.Section;
+import nextstep.subway.section.domain.LineSections;
 import nextstep.subway.station.domain.Station;
-
-import static java.util.stream.Collectors.toList;
 
 @Entity
 public class Line extends BaseEntity {
@@ -29,15 +24,16 @@ public class Line extends BaseEntity {
 
     private String color;
 
-    @OneToMany(mappedBy = "line", cascade = CascadeType.ALL)
-    private List<Section> sections = new ArrayList<>();
+    @Embedded
+    private LineSections lineSections;
 
-    public Line() {
+    protected Line() {
     }
 
     public Line(String name, String color) {
         this.name = name;
         this.color = color;
+        this.lineSections = new LineSections();
     }
 
     public void update(Line line) {
@@ -46,7 +42,7 @@ public class Line extends BaseEntity {
     }
 
     public void addSection(Section section) {
-        sections.add(section);
+        lineSections.add(section);
         section.toLine(this);
     }
 
@@ -62,16 +58,11 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public List<Section> getSections() {
-        return sections;
+    public LineSections getSections() {
+        return lineSections;
     }
 
     public List<Station> toStations() {
-        return sections.stream()
-                       .flatMap(
-                           section -> Stream.of(section.getUpStation(), section.getDownStation()))
-                       .distinct()
-                       .sorted(Comparator.comparingLong(Station::getId))
-                       .collect(toList());
+        return lineSections.toStations();
     }
 }
