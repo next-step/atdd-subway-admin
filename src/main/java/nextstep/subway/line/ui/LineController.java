1ba431937f1eee49fb27ac6f01a3430c99f3de3b
bbbnam <bbbnam@naver.com>
2021-06-02 17:04:16 +0900

    feat : 기존에 존재하는 지하철 노선 이름으로 지하철 노선 생성(실패) 기능 구현

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -4,6 +4,7 @@ import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.dao.DataIntegrityViolationException;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
