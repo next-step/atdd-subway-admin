7b612f7faf7375b94a73880f5f5cb7f6bbf285e5
박소정 <psj6414@naver.com>
2021-06-02 23:19:08 +0900

    feat: 지하철 노선 관련 기능 구현

--- src/main/java/nextstep/subway/common/BaseEntity.java
+++ src/main/java/nextstep/subway/common/BaseEntity.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreatedDate
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,11 +1,16 @@
 package nextstep.subway.line.application;
 
+import nextstep.subway.exception.NotFountException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -19,5 +24,28 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+        return lines.stream()
+                .map(line -> LineResponse.of(line))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findLine(Long id) {
+        Line line = lineRepository.findById(id).orElseThrow(NotFountException::new);
+        return LineResponse.of(line);
+    }
+
+    public void updateLine(Long id, LineRequest request) {
+        Line line = lineRepository.findById(id).orElseThrow(NotFountException::new);
+        line.update(request.toLine());
+    }
+
+    public void deleteStationById(Long id) {
+        lineRepository.deleteById(id);
     }
 }
