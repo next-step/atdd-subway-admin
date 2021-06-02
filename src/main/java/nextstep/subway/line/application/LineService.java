6cc831af8d8b8791908d7e71e8c7b4408a4cdc1f
prodo-developer <wjdals300@gmail.com>
2021-06-02 23:53:38 +0900

    feat: 지하철 노선 관련 기능 비지니스 로직들을 구현한다.

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,9 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class LineService {
@@ -19,5 +22,32 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(line -> LineResponse.of(line))
+                .collect(Collectors.toList());
+    }
+
+    public LineResponse findById(Long id) {
+        Line line = lineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 LINE 입니다."));
+
+        return LineResponse.of(line);
+    }
+
+    public LineResponse updateLine(Long lineId, LineRequest lineRequest) {
+        Line line = lineRepository.findById(lineId).get();
+        line.setName(lineRequest.getName());
+        line.setColor(lineRequest.getColor());
+        return LineResponse.of(line);
+    }
+
+    public void deleteLine(Long lineId) {
+        lineRepository.findById(lineId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 LINE 입니다."));
+        lineRepository.deleteById(lineId);
     }
 }
