bbfbe61180f188657890348c5be2ef51d2196201
younghoon <bblnotbb@yahho.com>
2021-06-04 05:54:26 +0900

    chore : apply code convention on project

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,23 +1,24 @@
 package nextstep.subway.line.application;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+	private LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
-        this.lineRepository = lineRepository;
-    }
+	public LineService(LineRepository lineRepository) {
+		this.lineRepository = lineRepository;
+	}
 
-    public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
-    }
+	public LineResponse saveLine(LineRequest request) {
+		Line persistLine = lineRepository.save(request.toLine());
+		return LineResponse.of(persistLine);
+	}
 }
