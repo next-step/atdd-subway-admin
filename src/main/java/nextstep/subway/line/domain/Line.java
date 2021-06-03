c3885ffcb50b7183876c9214157a4a36afdaffb3
itdar <thesockerr@gmail.com>
2021-06-03 17:05:03 +0900

    feat: Line, LineRequest 에 부가정보(상행성, 하행선, 거리) 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -9,21 +9,43 @@ public class Line extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
+    @Column
     private String color;
+
+    @Column
+    private Long upStationId;
+
+    @Column
+    private Long downStationId;
+
+    @Column
+    private int distance;
 
     public Line() {
     }
 
     public Line(String name, String color) {
+        this(name, color, null, null, 0);
+    }
+
+    public Line(String name, String color, Long upStationId, Long downStationId, int distance) {
         this.name = name;
         this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
     }
 
     public void update(Line line) {
         this.name = line.getName();
         this.color = line.getColor();
+        this.upStationId = line.getUpStationId();
+        this.downStationId = line.getDownStationId();
+        this.distance = line.getDistance();
     }
 
     public Long getId() {
@@ -36,5 +58,17 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -5,13 +5,23 @@ import nextstep.subway.line.domain.Line;
 public class LineRequest {
     private String name;
     private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
 
     public LineRequest() {
     }
 
     public LineRequest(String name, String color) {
+        this(name, color, null, null, 0);
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
         this.name = name;
         this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
     }
 
     public String getName() {
@@ -24,5 +34,17 @@ public class LineRequest {
 
     public Line toLine() {
         return new Line(name, color);
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
