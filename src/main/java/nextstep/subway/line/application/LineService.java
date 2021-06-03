d925a73c3781da76918d5cce26c3cf1300f7eed0
hJJANG <dacapolife87@gmail.com>
2021-06-03 12:41:26 +0900

    feat : 지하철 노선 관련 기능 구현하기 - 노선 등록 - 노선 목록 조회 - 대상 노선 조회 - 노선 수정 - 노선 삭제

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,8 +7,12 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class LineService {
     private LineRepository lineRepository;
 
@@ -16,8 +20,34 @@ public class LineService {
         this.lineRepository = lineRepository;
     }
 
+    @Transactional
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    public List<LineResponse> findLines() {
+        List<Line> findLines = lineRepository.findAll();
+        return findLines.stream()
+                .map(line -> LineResponse.of(line))
+                .collect(Collectors.toList());
+    }
+
+    public LineResponse findLine(long lineId) {
+        return LineResponse.of(lineRepository.findById(lineId)
+                .orElseThrow(NoSuchElementException::new));
+    }
+
+    @Transactional
+    public void updateLine(long lineId, Line lineRequest) {
+        Line line = lineRepository.findById(lineId)
+                .orElseThrow(NoSuchElementException::new);
+
+        line.update(lineRequest);
+    }
+
+    @Transactional
+    public void deleteLine(long lineId) {
+        lineRepository.deleteById(lineId);
     }
 }
