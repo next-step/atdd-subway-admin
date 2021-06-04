a32f4532ec0d81ea1095a5fcc4a1aeb70c390f1d
tmkwak <tmkwak@hmm21.com>
2021-06-04 16:44:08 +0900

    feat : LineService 구현

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,23 +1,54 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+	private LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
-        this.lineRepository = lineRepository;
-    }
+	public LineService(LineRepository lineRepository) {
+		this.lineRepository = lineRepository;
+	}
 
-    public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
-    }
+	public LineResponse saveLine(LineRequest request) {
+		Line persistLine = lineRepository.save(request.toLine());
+		return LineResponse.of(persistLine);
+	}
+
+	@Transactional(readOnly = true)
+	public List<LineResponse> findAllLines() {
+		List<Line> lines = lineRepository.findAll();
+
+		return lines.stream()
+			.map(line -> LineResponse.of(line))
+			.collect(Collectors.toList());
+	}
+
+	public LineResponse findLineById(Long id) {
+		Line line = lineRepository.getOne(id);
+		return LineResponse.of(line);
+
+	}
+
+	public void deleteLineById(Long id) {
+		lineRepository.deleteById(id);
+	}
+
+	@Transactional
+	public LineResponse updateSLine(Long id, LineRequest request) {
+		Line line = lineRepository.getOne(id);
+		line.update(line);
+		return LineResponse.of(line);
+	}
+
 }
