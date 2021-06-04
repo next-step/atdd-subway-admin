b4d32d828389ba088b19abff05b13b13fe341f81
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-06-05 00:58:53 +0900

    feat(Sections): cascade type 설정, Station 줄세우는 메서드 추가
    - cascade type 설정
    - Station 줄세우는 메서드 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -2,16 +2,17 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Embeddable
 public class Sections {
 
-    @OneToMany(mappedBy = "line", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
     private List<Section> sections = new ArrayList<>();
 
     public Sections() {
@@ -25,8 +26,30 @@ public class Sections {
         return sections;
     }
 
+    public void add(Section section) {
+        sections.add(section);
+    }
+
     public List<Station> stations() {
-        //TODO : sections 불러와서 차례대로 정렬
-        return new ArrayList<>();
+        List<Station> stations = new ArrayList<>();
+
+        List<Station> upStations = sections.stream().map(Section::getUpStation).collect(Collectors.toList());
+        List<Station> downStations = sections.stream().map(Section::getDownStation).collect(Collectors.toList());
+
+        Station firstStation = findNotContainStation(upStations, downStations);
+        Station lastStation = findNotContainStation(downStations, upStations);
+
+        for(Section section : sections) {
+            stations.add(section.getUpStation());
+            stations.add(section.getDownStation());
+        }
+        return stations;
+    }
+
+    private Station findNotContainStation(List<Station> toFindStations, List<Station> criteriaStations) {
+        return toFindStations.stream()
+                .filter(toFindStation -> !criteriaStations.contains(toFindStation))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 }
