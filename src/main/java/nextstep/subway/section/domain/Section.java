7157210b4296be0fe4eaa4ec727671f2fb7a8022
Donghui-Lee <ehdgml3206@naver.com>
2021-06-04 15:15:35 +0900

    refactor: Section Entity 수정 - Validate 추가 - 테스트 코드 추가

--- src/main/java/nextstep/subway/section/domain/Section.java
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -13,6 +13,8 @@ import nextstep.subway.station.domain.Station;
 
 @Entity
 public class Section extends BaseEntity {
+    private final String CAN_NOT_STATION_EQUALS = "동일한 역을 등록할 수 없습니다.";
+    private final String NOT_ENOUGH_DISTANCE = "충분하지 않은 구간 크기입니다.";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,9 +35,24 @@ public class Section extends BaseEntity {
     }
 
     public Section(Station upStation, Station downStation, int distance) {
+        stationEqualsValidate(upStation, downStation);
+        distanceValidate(distance);
+
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
+    }
+
+    private void stationEqualsValidate(Station upStation, Station downStation)  {
+        if (upStation.equals(downStation)) {
+            throw new IllegalArgumentException(CAN_NOT_STATION_EQUALS);
+        }
+    }
+
+    private void distanceValidate(int distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException(NOT_ENOUGH_DISTANCE);
+        }
     }
 
     public Station getUpStation() {
--- src/test/java/nextstep/subway/section/domain/SectionTest.java
+++ src/test/java/nextstep/subway/section/domain/SectionTest.java
@@ -3,6 +3,7 @@ package nextstep.subway.section.domain;
 import static nextstep.subway.station.domain.StationTest.*;
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,19 @@ public class SectionTest {
         assertThat(section.getUpStation()).isEqualTo(강남역);
         assertThat(section.getDownStation()).isEqualTo(판교역);
         assertThat(section.getDistance()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("동일한 상행/하행선 등록 테스트")
+    void up_down_station_equals_register_exception() {
+        // given & when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Section(강남역, 강남역, 5));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 거리 등록 테스트")
+    void distance_register_exception() {
+        // given & when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Section(강남역, 판교역, -1));
     }
 }
