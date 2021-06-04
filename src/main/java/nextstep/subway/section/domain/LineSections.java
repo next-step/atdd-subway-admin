03aed169fa057038ddec8fb80d4dde863c89292b
dhmin5693 <dhmin5693@naver.com>
2021-06-05 02:03:20 +0900

    refactor: toStation 메소드 분리

--- src/main/java/nextstep/subway/section/domain/LineSections.java
+++ src/main/java/nextstep/subway/section/domain/LineSections.java
@@ -2,7 +2,7 @@ package nextstep.subway.section.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,10 +11,13 @@ import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import nextstep.subway.station.domain.Station;
 
+import static java.util.stream.Collectors.toMap;
+
 @Embeddable
 public class LineSections implements Serializable {
 
     private static final long serialVersionUID = -4483053178441994936L;
+    private static final String MESSAGE_NOT_FOUND_UPSTATION = "상행역을 찾을 수 없습니다.";
 
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL)
     private final List<Section> sections = new ArrayList<>();
@@ -24,33 +27,44 @@ public class LineSections implements Serializable {
     }
 
     public List<Station> toStations() {
+        Station startStation = getStartStation();
+        return linkToLastStation(startStation);
+    }
 
-        Map<Station, Station> map = new HashMap<>();
-        for (Section section : sections) {
-            map.put(section.getDownStation(), section.getUpStation());
-        }
+    private Station getStartStation() {
 
-        Station start = null;
-        for (Entry<Station, Station> entry : map.entrySet()) {
-            if (!map.containsKey(entry.getValue())) {
-                start = entry.getValue();
-            }
-        }
+        Map<Station, Station> stationRelationship =
+            sections.stream()
+                    .collect(toMap(Section::getDownStation,
+                                   Section::getUpStation));
 
-        map.clear();
-        for (Section section : sections) {
-            map.put(section.getUpStation(), section.getDownStation());
-        }
+        Entry<Station, Station> startEntry =
+            stationRelationship.entrySet()
+                               .stream()
+                               .filter(entry -> !stationRelationship.containsKey(entry.getValue()))
+                               .findAny()
+                               .orElseThrow(
+                                   () -> new IllegalStateException(MESSAGE_NOT_FOUND_UPSTATION));
+
+        return startEntry.getValue();
+    }
+
+    private List<Station> linkToLastStation(Station startStation) {
+
+        Map<Station, Station> stationRelationship =
+            sections.stream()
+                    .collect(toMap(Section::getUpStation,
+                                   Section::getDownStation));
 
         List<Station> stations = new ArrayList<>();
-        stations.add(start);
+        stations.add(startStation);
 
-        Station next = map.get(start);
-        while (next != null) {
-            stations.add(next);
-            next = map.get(next);
+        Station nextStation = stationRelationship.get(startStation);
+        while (nextStation != null) {
+            stations.add(nextStation);
+            nextStation = stationRelationship.get(nextStation);
         }
 
-        return stations;
+        return Collections.unmodifiableList(stations);
     }
 }
