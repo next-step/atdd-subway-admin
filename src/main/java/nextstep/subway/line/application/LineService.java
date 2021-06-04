905e45531587f7fb51ebc2d1255c7738dc5b8f7a
Len kim <joenggyu0@gmail.com>
2021-06-04 20:05:43 +0900

    [Step1] 지하철 노선 관리 (#166)
    * docs(README): 요구사항 정의
    
    * style: final 키워드 추가 및 도메인 이해
    
    * feat: Line 도메인 관련 인수테스트 코드 작성

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,20 +4,48 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
     }
 
-    public LineResponse saveLine(LineRequest request) {
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllStations() {
+        return lineRepository.findAll().stream()
+                .map(LineResponse::of).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findById(final Long lineId) {
+        Line id = lineRepository.findById(lineId)
+                .orElseThrow(() -> new DataIntegrityViolationException("Not Found lineId" + lineId));
+        return LineResponse.of(id);
+    }
+
+    public LineResponse saveLine(final LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    public LineResponse updateById(final Long lineId,final LineRequest lineRequest) {
+        Line line = lineRepository.findById(lineId)
+                .orElseThrow(() -> new DataIntegrityViolationException("Not Found lineId" + lineId));
+        line.update(lineRequest.toLine());
+        return LineResponse.of(line);
+    }
+
+    public void deleteLineById(final Long id) {
+        lineRepository.deleteById(id);
     }
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -3,13 +3,12 @@ package nextstep.subway.line.ui;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -24,5 +23,34 @@ public class LineController {
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok().body(lineService.findAllStations());
+    }
+
+    @GetMapping("line/{lineId}")
+    public ResponseEntity<LineResponse> showLine(@PathVariable Long lineId) {
+        return ResponseEntity.ok().body(lineService.findById(lineId));
+    }
+
+    @PutMapping("line/{lineId}")
+    public ResponseEntity<LineResponse> updateLine(@PathVariable Long lineId,
+                                     @RequestBody LineRequest lineRequest
+    ) {
+        LineResponse line = lineService.updateById(lineId, lineRequest);
+        return ResponseEntity.ok().body(line);
+    }
+
+    @DeleteMapping("{lineId}")
+    public ResponseEntity<LineResponse> deleteLine(@PathVariable Long lineId) {
+        lineService.deleteLineById(lineId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<LineResponse> handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
