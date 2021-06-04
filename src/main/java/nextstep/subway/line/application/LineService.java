2eb0e3d50e4da6e2b6ee4d267851bd6b8bf76c04
hJJANG <dacapolife87@gmail.com>
2021-06-04 16:10:49 +0900

    feat : 노선저장시 구간별 정보생성 및 저장기능 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,6 +4,9 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,15 +18,32 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class LineService {
     private LineRepository lineRepository;
+    private StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
     @Transactional
     public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
+
+        Section section = createSection(request);
+        Line line = request.toLine();
+        line.addSection(section);
+        Line persistLine = lineRepository.save(line);
         return LineResponse.of(persistLine);
+    }
+
+    private Section createSection(LineRequest request) {
+        Station upStation = findStationById(request.getUpStationId());
+        Station downStation = findStationById(request.getDownStationId());
+
+        return Section.create(upStation, downStation, request.getDistance());
+    }
+
+    private Station findStationById(long stationId) {
+        return stationRepository.findById(stationId).orElseThrow(NoSuchElementException::new);
     }
 
     public List<LineResponse> findLines() {
