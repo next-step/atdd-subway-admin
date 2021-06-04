938ddd0cfe44304dcebe3e6f81f20dc6e242697d
hJJANG <dacapolife87@gmail.com>
2021-06-04 16:05:53 +0900

    feat : 노선 생성 Response 규격수정

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,29 +1,49 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<Station> stations;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate, List<Station> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
+        this.stations = stations;
     }
 
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
+        List<Section> sections = line.getSections();
+
+        List<Station> stations = new ArrayList<>();
+        for (Section section : sections) {
+            addStation(stations, section);
+        }
+
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate(), stations);
+    }
+
+    private static void addStation(List<Station> stations, Section section) {
+        if (!stations.contains(section.getUpStation())) {
+            stations.add(section.getUpStation());
+        }
+        stations.add(section.getDownStation());
     }
 
     public Long getId() {
@@ -36,6 +56,10 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<Station> getStations() {
+        return stations;
     }
 
     public LocalDateTime getCreatedDate() {
