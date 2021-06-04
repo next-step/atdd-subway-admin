f2d67bc71d348d4047b53ef39cfe2b49e3e47110
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-06-05 00:52:19 +0900

    test(LineAcceptanceTest): 역 생성 방법 변경 및 지하철 노선 조회 테스트 방법 수정

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -2,34 +2,53 @@ package nextstep.subway.line;
 
 import io.restassured.response.ExtractableResponse;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static nextstep.subway.line.LineHelper.*;
-import static nextstep.subway.station.StationHelper.가짜_역_생성;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
 
+    @Autowired
+    StationRepository stationRepository;
+
+    @Autowired
+    LineRepository lineRepository;
+
+    @Autowired
+    LineService lineService;
+
     ExtractableResponse createResponse;
 
+    Station 신사역;
     Station 강남역;
     Station 양재역;
+    Station 판교역;
 
     @BeforeEach
     void 미리_노선_및_역_생성() {
 
-        강남역 = 가짜_역_생성("강남역", 1L);
-        양재역 = 가짜_역_생성("양재역", 2L);
+        신사역 = stationRepository.save(new Station("신사역"));
+        강남역 = stationRepository.save(new Station("강남역"));
+        양재역 = stationRepository.save(new Station("양재역"));
+        판교역 = stationRepository.save(new Station("판교역"));
 
         LineRequest lineRequest = 노선_요청_정보("신분당선", "bg-red-600", 강남역.getId(), 양재역.getId(), 10);
 
@@ -54,6 +73,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // when
         // 지하철_노선_생성_요청
         ExtractableResponse response = 노선_생성(lineRequest);
+
         // then
         // 지하철_노선_생성_실패됨
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
@@ -87,6 +107,10 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // given
         // 지하철_노선_등록되어_있음
         Long id = 생성_노선_아이디(createResponse);
+
+        lineService.addSection(id, 양재역.getId(), 판교역.getId());
+        lineService.addSection(id, 신사역.getId(), 강남역.getId());
+
         // when
         // 지하철_노선_조회_요청
         ExtractableResponse response = 노선_조회(id);
@@ -95,6 +119,9 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_응답됨
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.jsonPath().getObject(".", LineResponse.class).getName()).isEqualTo("신분당선");
+        assertThat(response.jsonPath().getObject(".", LineResponse.class)
+                .getStations().stream().map(StationResponse::getName).collect(Collectors.toList()))
+                .containsExactly("신사역", "강남역", "양재역", "판교역");
     }
 
     @DisplayName("지하철 노선을 수정한다.")
