49afd9a2b96dbcf237dda2a1df6adf8e7caa9d02
Donghui-Lee <ehdgml3206@naver.com>
2021-06-04 23:16:55 +0900

    feat: LineResponse Station List 추가

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,29 +1,44 @@
 package nextstep.subway.line.dto;
 
-import nextstep.subway.line.domain.Line;
-
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<Station> stations;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<Station> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.stations = stations;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
 
+    public LineResponse(Line line) {
+        this.id = line.getId();
+        this.name = line.getName();
+        this.color = line.getColor();
+        this.stations = line.toStations();
+        this.createdDate = line.getCreatedDate();
+        this.modifiedDate = line.getModifiedDate();
+    }
+
+
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.toStations(), line.getCreatedDate(), line.getModifiedDate());
     }
 
     public Long getId() {
@@ -36,6 +51,10 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<Station> getStations() {
+        return Collections.unmodifiableList(stations);
     }
 
     public LocalDateTime getCreatedDate() {
