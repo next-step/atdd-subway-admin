0f38272a74f4a0f9995b210aacee4ba0156acadd
hJJANG <dacapolife87@gmail.com>
2021-06-04 15:45:50 +0900

    feat : 상하행구간관리를 위한 SectionEntity 및 Repository 추가

---
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -0,0 +1,53 @@
+package nextstep.subway.section.domain;
+
+import nextstep.subway.common.BaseEntity;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Section extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    private int distance;
+
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    protected Section() {
+    }
+
+    private Section(Station upStation, Station downStation, int distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public static Section create(Station upStation, Station downStation, int distance) {
+        return new Section(upStation, downStation, distance);
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public void setLine(Line line) {
+        this.line = line;
+    }
+}
