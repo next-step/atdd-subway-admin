1a70b7760c1437643271a9c00f26aad12262949e
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-06-05 14:10:35 +0900

    feat : Section (구간) 도메인클래스 신규작성

---
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -0,0 +1,52 @@
+package nextstep.subway.section.domain;
+
+import nextstep.subway.common.BaseEntity;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Section extends BaseEntity {
+
+    private static final int START_STOP_DISTANCE = 0;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Line line;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Station station;
+
+    private int distance;
+
+    protected Section() {
+    }
+
+    public Section(Line line, Station station, int distance) {
+        this.line = line;
+        this.station = station;
+        this.distance = distance;
+    }
+
+    public static Section startStop(Line line, Station station) {
+        return new Section(line, station, START_STOP_DISTANCE);
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
