0fba5a7179b0c6877ae9136bc048e33a00a15b97
lenkim <joenggyu0@gmail.com>
2021-06-05 19:21:43 +0900

    feat: 노선 조회 시 응답 결과에 역 목록 추가하기

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -2,22 +2,29 @@ package nextstep.subway.line.application;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.line.domain.Section;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Transactional
 public class LineService {
     private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
     @Transactional(readOnly = true)
@@ -30,12 +37,30 @@ public class LineService {
     public LineResponse findById(final Long lineId) {
         Line id = lineRepository.findById(lineId)
                 .orElseThrow(() -> new DataIntegrityViolationException("Not Found lineId" + lineId));
-        return LineResponse.of(id);
+
+        List<Station> stations = id.getSections().getValues().stream()
+                .map(a -> Stream.of(a.getDownStation(), a.getUpStation()))
+                .flatMap(Stream::distinct).collect(Collectors.toList());
+
+        return LineResponse.of(id, stations);
     }
 
     public LineResponse saveLine(final LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
+        Station downStation = stationRepository.findById(request.getDownStationId()).orElseThrow(() -> new DataIntegrityViolationException("Not Fount downStationId" + request.getDownStationId()));
+        Station upStation = stationRepository.findById(request.getUpStationId()).orElseThrow(() -> new DataIntegrityViolationException("Not Fount downStationId" + request.getUpStationId()));
+        int distance = request.getDistance();
+
+        Line line = request.toLine();
+        Section section = new Section(line, upStation, downStation, distance);
+        line.addSection(section);
+
+        Line persistLine = lineRepository.save(line);
+
+        List<Station> stations = persistLine.getSections().getValues().stream()
+                .map(a -> Stream.of(a.getDownStation(), a.getUpStation()))
+                .flatMap(Stream::distinct).collect(Collectors.toList());
+
+        return LineResponse.of(persistLine, stations);
     }
 
     public LineResponse updateById(final Long lineId,final LineRequest lineRequest) {
@@ -48,4 +73,5 @@ public class LineService {
     public void deleteLineById(final Long id) {
         lineRepository.deleteById(id);
     }
+
 }
