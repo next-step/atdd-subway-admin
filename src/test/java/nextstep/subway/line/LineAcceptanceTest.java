8972ccfcc7bcd7d72b73c2c4e0276f428e1fc902
ttuop <plintcsz@gmail.com>
2021-06-06 08:13:30 +0900

    test: 인수테스트 작성 (지하철 노션을 등록한다)

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,19 +1,58 @@
 package nextstep.subway.line;
 
-import nextstep.subway.AcceptanceTest;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
-    void createLine() {
+    void given_NoExisingLine_when_CreateLine_then_ReturnLine() {
         // when
         // 지하철_노선_생성_요청
+        final ExtractableResponse<Response> response = 지하철_노선_등록되어_있음("1호선", "bg-blue-600");
 
         // then
         // 지하철_노선_생성됨
+        final JsonPath jsonPath = response.jsonPath();
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+            () -> assertThat(response.header("Location")).isNotBlank(),
+            () -> assertThat(jsonPath.getString("id")).isNotNull(),
+            () -> assertThat(jsonPath.getString("name")).isNotNull(),
+            () -> assertThat(jsonPath.getString("color")).isNotNull()
+        );
+    }
+
+    private ExtractableResponse<Response> 지하철_노선_등록되어_있음(final String name, final String color) {
+        // given
+        final Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+
+        // when
+        return RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
