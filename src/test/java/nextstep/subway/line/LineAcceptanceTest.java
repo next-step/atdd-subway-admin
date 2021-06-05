14df25a7c5ea54ee8137b8f6b81345ec1d569bcc
Jiseon Lee <leejoyc9310@gmail.com>
2021-06-05 13:41:03 +0900

    test : 지하철 노선 신규생성 & 중복생성 테스트

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,85 +1,114 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.domain.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
+    private static final Line fourthLine = new Line("4호선", "blue");
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
-        // when
-        // 지하철_노선_생성_요청
+        // when : 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_등록되어_있음(fourthLine);
 
-        // then
-        // 지하철_노선_생성됨
+        // then : 지하철_노선_생성됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
     void createLine2() {
-        // given
-        // 지하철_노선_등록되어_있음
+        // given : 지하철_노선_등록되어_있음
+        지하철_노선_등록되어_있음(fourthLine);
 
-        // when
-        // 지하철_노선_생성_요청
+        // when : 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_등록되어_있음(fourthLine);
 
-        // then
-        // 지하철_노선_생성_실패됨
+        // then : 지하철_노선_생성_실패됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
-    @DisplayName("지하철 노선 목록을 조회한다.")
-    @Test
-    void getLines() {
-        // given
-        // 지하철_노선_등록되어_있음
-        // 지하철_노선_등록되어_있음
+//    @DisplayName("지하철 노선 목록을 조회한다.")
+//    @Test
+//    void getLines() {
+//        // given
+//        // 지하철_노선_등록되어_있음
+//        // 지하철_노선_등록되어_있음
+//
+//        // when
+//        // 지하철_노선_목록_조회_요청
+//
+//        // then
+//        // 지하철_노선_목록_응답됨
+//        // 지하철_노선_목록_포함됨
+//    }
+//
+//    @DisplayName("지하철 노선을 조회한다.")
+//    @Test
+//    void getLine() {
+//        // given
+//        // 지하철_노선_등록되어_있음
+//
+//        // when
+//        // 지하철_노선_조회_요청
+//
+//        // then
+//        // 지하철_노선_응답됨
+//    }
+//
+//    @DisplayName("지하철 노선을 수정한다.")
+//    @Test
+//    void updateLine() {
+//        // given
+//        // 지하철_노선_등록되어_있음
+//
+//        // when
+//        // 지하철_노선_수정_요청
+//
+//        // then
+//        // 지하철_노선_수정됨
+//    }
+//
+//    @DisplayName("지하철 노선을 제거한다.")
+//    @Test
+//    void deleteLine() {
+//        // given
+//        // 지하철_노선_등록되어_있음
+//
+//        // when
+//        // 지하철_노선_제거_요청
+//
+//        // then
+//        // 지하철_노선_삭제됨
+//    }
 
-        // when
-        // 지하철_노선_목록_조회_요청
+    private ExtractableResponse<Response> 지하철_노선_등록되어_있음(Line line){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", line.getName());
+        params.put("color",line.getColor());
 
-        // then
-        // 지하철_노선_목록_응답됨
-        // 지하철_노선_목록_포함됨
-    }
-
-    @DisplayName("지하철 노선을 조회한다.")
-    @Test
-    void getLine() {
-        // given
-        // 지하철_노선_등록되어_있음
-
-        // when
-        // 지하철_노선_조회_요청
-
-        // then
-        // 지하철_노선_응답됨
-    }
-
-    @DisplayName("지하철 노선을 수정한다.")
-    @Test
-    void updateLine() {
-        // given
-        // 지하철_노선_등록되어_있음
-
-        // when
-        // 지하철_노선_수정_요청
-
-        // then
-        // 지하철_노선_수정됨
-    }
-
-    @DisplayName("지하철 노선을 제거한다.")
-    @Test
-    void deleteLine() {
-        // given
-        // 지하철_노선_등록되어_있음
-
-        // when
-        // 지하철_노선_제거_요청
-
-        // then
-        // 지하철_노선_삭제됨
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
     }
 }
