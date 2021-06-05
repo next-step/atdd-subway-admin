cd68d795168eff29d36bcdbb1cea09781d9579af
bbbnam <bbbnam@naver.com>
2021-06-05 14:08:36 +0900

    test : 지하철역 인수테스트 정리, step 분리

---
+++ src/test/java/nextstep/subway/station/StationSteps.java
@@ -0,0 +1,44 @@
+package nextstep.subway.station;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.station.dto.StationRequest;
+import org.springframework.http.MediaType;
+
+public class StationSteps {
+
+    public static ExtractableResponse<Response> 지하철역_등록_되어있음(String name) {
+        return 지하철역_생성_요청(name);
+    }
+
+    public static ExtractableResponse<Response> 지하철역_생성_요청(String name) {
+        StationRequest stationRequest = new StationRequest(name);
+
+        return RestAssured.given().log().all()
+                .body(stationRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철역_조회_요청() {
+        return RestAssured.given().log().all()
+                .when()
+                .get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철역_삭제_요청(ExtractableResponse<Response> createResponse) {
+        String uri = createResponse.header("Location");
+
+        return RestAssured.given().log().all()
+                .when()
+                .delete(uri)
+                .then().log().all()
+                .extract();
+    }
+}
