64c04a40b8d96ca5d7de0804cab0a5291b7fc32c
byunahakjang1230 <byunghakjang1230@gmail.com>
2021-06-05 23:14:00 +0900

    refactor(StationAcceptanceTest.java): 인수테스트 리팩터링
    - 메소드 분리
    - 예외발생 인수테스트 케이스 추가
    - LineAcceptanceTest에서 사용할 수 있는 public 메소드 생성
    
    feat(StationController.java): 일괄등록 처리 메소드 추가 및 예외처리 핸들러 메소드 추가
    
    feat(StationService.java): 일괄등록 처리 서비스 구현
    
    refactor(StationService.java): 중복등록 및 미등록 역 삭제 시도 시 예외처리 로직 추가
    
    test(StationServiceTest.java): StationService 클래스 기능 검증 테스트 코드 작성
    
    test(StationRepositoryTest.java): StationRepository 기능 검증 테스트 코드 작성
    
    feat(StationRequest.java): 기본 생성자 및 역 이름 초기화 생성자 추가

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -1,14 +1,18 @@
 package nextstep.subway.station.application;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -20,8 +24,22 @@ public class StationService {
     }
 
     public StationResponse saveStation(StationRequest stationRequest) {
-        Station persistStation = stationRepository.save(stationRequest.toStation());
-        return StationResponse.of(persistStation);
+        try {
+            Station persistStation = stationRepository.save(stationRequest.toStation());
+            return StationResponse.of(persistStation);
+        } catch (DataIntegrityViolationException exception) {
+            throw new DuplicateKeyException("역 생성에 실패했습니다. 이미 존재하는 역입니다.");
+        }
+    }
+
+    public List<StationResponse> saveAllStations(List<StationRequest> stationRequests) {
+        List<Station> stations = stationRequests.stream()
+                .map(req -> req.toStation())
+                .collect(Collectors.toList());
+        return this.stationRepository.saveAll(stations)
+                .stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
@@ -34,6 +52,11 @@ public class StationService {
     }
 
     public void deleteStationById(Long id) {
-        stationRepository.deleteById(id);
+        Station station = findStationByIdOrThrow(id, "삭제 대상 역이 존재하지 않습니다.");
+        stationRepository.delete(station);
+    }
+
+    private Station findStationByIdOrThrow(Long id, String throwMessage) {
+        return stationRepository.findById(id).orElseThrow(() -> new NoSuchElementException(throwMessage));
     }
 }
--- src/main/java/nextstep/subway/station/ui/StationController.java
+++ src/main/java/nextstep/subway/station/ui/StationController.java
@@ -1,15 +1,23 @@
 package nextstep.subway.station.ui;
 
-import nextstep.subway.station.application.StationService;
-import nextstep.subway.station.dto.StationRequest;
-import nextstep.subway.station.dto.StationResponse;
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.util.List;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.dto.StationRequest;
+import nextstep.subway.station.dto.StationResponse;
 
 @RestController
 public class StationController {
@@ -23,6 +31,16 @@ public class StationController {
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         StationResponse station = stationService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
+    }
+
+    @PostMapping("/stations/all")
+    public ResponseEntity<List<StationResponse>> createStations(@RequestBody List<StationRequest> stationRequests) {
+        try {
+            List<StationResponse> stations = stationService.saveAllStations(stationRequests);
+            return ResponseEntity.created(URI.create("/stations")).body(stations);
+        } catch (DataIntegrityViolationException exception) {
+            throw new DuplicateKeyException("중복된 지하철 역 이름은 등록될 수 없습니다.");
+        }
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -39,5 +57,23 @@ public class StationController {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity handleDuplicateKeyException(DuplicateKeyException e) {
+        return ResponseEntity.badRequest().body(makeErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.badRequest().body(makeErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
+
+    private Map<String, Object> makeErrorMessage(HttpStatus status, String errorMessage) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+        errors.put("status", status.value());
+        errors.put("errorMessage", errorMessage);
+        return errors;
     }
 }
