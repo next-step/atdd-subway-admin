dde42e03ad5af05e100b08505905aae9d39f0129
5minho <5minhominho@gmail.com>
2021-06-07 01:54:04 +0900

    chore: 노선 단건 조회 쿼리 조건 빠진것 추가함

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -4,10 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 	boolean existsByName(String name);
 
-	@Query("select l from Line l join fetch l.sections.sections")
-	Optional<Line> findWithSectionsById(Long aLong);
+	@Query("select l from Line l join fetch l.sections.sections where l.id = :lineId")
+	Optional<Line> findWithSectionsById(@Param("lineId") Long lineId);
 }
