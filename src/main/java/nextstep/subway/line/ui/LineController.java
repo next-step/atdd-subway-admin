bc9c12aba9de1ceb48c7ca18eb4992575f8d7883
younghoon <bblnotbb@yahho.com>
2021-06-06 16:52:32 +0900

    feat : add exception logic in line controller

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -2,7 +2,9 @@ package nextstep.subway.line.ui;
 
 import java.net.URI;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,10 @@ public class LineController {
 	public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
 		LineResponse line = lineService.saveLine(lineRequest);
 		return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity handleIllegalArgsExcception(DataIntegrityViolationException e) {
+		return ResponseEntity.badRequest().build();
 	}
 }
