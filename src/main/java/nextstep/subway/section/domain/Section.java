81a78221bd4dda67c5a53a13c2b9f695bcda73e5
oper912 <oper912@naver.com>
2021-06-06 22:39:49 +0900

    feat(section): create entity

---
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -0,0 +1,78 @@
+package nextstep.subway.section.domain;
+
+import nextstep.subway.common.BaseEntity;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Table(name = "section")
+@Entity
+public class Section extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    @Column(name = "distance", nullable = false)
+    private int distance;
+
+    protected Section() {
+    }
+
+    public Section(Line line, Station upStation, Station downStation) {
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+    }
+
+    public List<Station> getStation() {
+        return Arrays.asList(upStation, downStation);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Section section = (Section) o;
+        return Objects.equals(id, section.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", upStation=" + upStation +
+                ", downStation=" + downStation +
+                ", distance=" + distance +
+                '}';
+    }
+}
