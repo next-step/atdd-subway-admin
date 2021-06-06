00a5dfea1104b72ced719d67eca5573d91d91ec6
mskangg <funnycoding91@gmail.com>
2021-06-07 02:45:22 +0900

    test: 인수 조건 정의 후 스텝 구현

---
+++ src/test/java/nextstep/subway/line/LineAcceptanceStep.java
@@ -0,0 +1,39 @@
+package nextstep.subway.line;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LineAcceptanceStep {
+
+    public static ExtractableResponse<Response> 지하철_노선_생성_요청(String name, String color) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선_등록되어_있음(String name, String color) {
+        return 지하철_노선_생성_요청(name, color);
+    }
+
+    public static void 지하철_노선_생성됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    public static void 지하철_노선_생성_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,35 +1,42 @@
 package nextstep.subway.line;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("지하철 노선 관련 기능")
+import static nextstep.subway.line.LineAcceptanceStep.*;
+
+@DisplayName("지하철 노선 관련 기능") //Feature
 public class LineAcceptanceTest extends AcceptanceTest {
-    @DisplayName("지하철 노선을 생성한다.")
+
+    //Background
+
+    @DisplayName("지하철 노선을 생성한다.") //Scenario
     @Test
     void createLine() {
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청("1호선", "blue");
 
         // then
-        // 지하철_노선_생성됨
+        지하철_노선_생성됨(response);
     }
 
-    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
+    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.") //Scenario
     @Test
     void createLine2() {
         // given
-        // 지하철_노선_등록되어_있음
+        지하철_노선_등록되어_있음("1호선", "blue");
 
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청("1호선", "blue");
 
         // then
-        // 지하철_노선_생성_실패됨
+        지하철_노선_생성_실패됨(response);
     }
 
-    @DisplayName("지하철 노선 목록을 조회한다.")
+    @DisplayName("지하철 노선 목록을 조회한다.") //Scenario
     @Test
     void getLines() {
         // given
@@ -44,7 +51,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_목록_포함됨
     }
 
-    @DisplayName("지하철 노선을 조회한다.")
+    @DisplayName("지하철 노선을 조회한다.") //Scenario
     @Test
     void getLine() {
         // given
@@ -57,7 +64,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_응답됨
     }
 
-    @DisplayName("지하철 노선을 수정한다.")
+    @DisplayName("지하철 노선을 수정한다.") //Scenario
     @Test
     void updateLine() {
         // given
@@ -70,7 +77,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_수정됨
     }
 
-    @DisplayName("지하철 노선을 제거한다.")
+    @DisplayName("지하철 노선을 제거한다.") //Scenario
     @Test
     void deleteLine() {
         // given
