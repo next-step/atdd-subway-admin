ea7f85eb87d73873aae4fa3df60075f6acc6e47b
itdar <thesockerrr@gmail.com>
2021-06-06 15:37:22 +0900

    test: lineRepository의 update 테스트케이스 추가

--- src/test/java/nextstep/subway/line/LineRepositoryTest.java
+++ src/test/java/nextstep/subway/line/LineRepositoryTest.java
@@ -6,6 +6,7 @@ import nextstep.subway.section.domain.Section;
 import nextstep.subway.section.domain.SectionRepository;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,14 +32,24 @@ public class LineRepositoryTest {
     @Autowired
     private StationRepository stationRepository;
 
+    private Station upStation;
+    private Station downStation;
+    private int distance;
+    private Section section;
+    private Line line2;
+    private Line line6;
+
+    @BeforeEach
+    void setup() {
+        upStation = stationRepository.save(Station.of("강남역"));
+        downStation = stationRepository.save(Station.of("역삼역"));
+        distance = 5;
+
+        section = sectionRepository.save(Section.of(upStation, downStation, distance));
+    }
+
     @Test
     void create() {
-        Station upStation = stationRepository.save(Station.of("강남역"));
-        Station downStation = stationRepository.save(Station.of("역삼역"));
-        int distance = 5;
-
-        Section section = sectionRepository.save(Section.of(upStation, downStation, distance));
-
         Line persistLine = lineRepository.save(Line.of("2호선", "다크그린", section));
 
         assertThat(persistLine.getStations()).contains(upStation, downStation);
@@ -46,22 +57,34 @@ public class LineRepositoryTest {
 
     @Test
     void findById() {
-        Station upStation = stationRepository.save(Station.of("강남역"));
-        Station downStation = stationRepository.save(Station.of("역삼역"));
-        int distance = 5;
-
-        Section section = sectionRepository.save(Section.of(upStation, downStation, distance));
-
-        Line expected = lineRepository.save(Line.of("2호선", "다크그린", section));
+        line2 = lineRepository.save(Line.of("2호선", "다크그린", section));
 
         testEntityManager.flush();
         testEntityManager.clear();
 
-        Line persistLine = lineRepository.findById(expected.getId()).orElseThrow(NoSuchElementException::new);
+        Line persistLine = lineRepository.findById(line2.getId()).orElseThrow(NoSuchElementException::new);
 
         List<String> names = persistLine.getStations().stream()
                 .map(station -> station.getName())
                 .collect(Collectors.toList());
         assertThat(names).contains("강남역", "역삼역");
     }
+
+    @Test
+    void update() {
+        line2 = lineRepository.save(Line.of("2호선", "다크그린", section));
+
+        Line updateLine = Line.of("name", "color");
+
+        line2.update(updateLine);
+
+        testEntityManager.flush();
+        testEntityManager.clear();
+
+        Line actualLine = lineRepository.findById(line2.getId()).orElseThrow(NoSuchElementException::new);
+
+        assertThat(updateLine.getName()).isEqualTo(actualLine.getName());
+        assertThat(updateLine.getColor()).isEqualTo(actualLine.getColor());
+    }
+
 }
