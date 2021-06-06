70d828ff0d8887de58c3bc04ccb5eb31db1d13bc
Insup Jung <31408300+jis1218@users.noreply.github.com>
2021-06-06 17:08:35 +0900

    feat(Section): Section 기능 수정
    - Section이 Comparator를 구현하도록 수정
    - Sections에서 add할 때 자동으로 정렬됨
    - 관련하여 임시 테스트 생성
    - Section Request 생성
    - Section Acceptance 테스트 코드 추가

---
+++ src/main/java/nextstep/subway/line/dto/SectionRequest.java
@@ -0,0 +1,26 @@
+package nextstep.subway.line.dto;
+
+public class SectionRequest {
+
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
+
+    public SectionRequest(Long upStationId, Long downStationId, Integer distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+}
