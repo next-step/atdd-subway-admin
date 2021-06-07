61fbb29eb27d61b3deb0bf618f872b0627ed6502
itdar <thesockerrr@gmail.com>
2021-06-08 01:25:12 +0900

    test: Section 추가 인수테스트 수정

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -1,23 +1,24 @@
 package nextstep.subway.section;
 
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.LineAcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.section.domain.Section;
 import nextstep.subway.station.StationAcceptanceTest;
-import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 import nextstep.subway.utils.RestAssuredCRUD;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static nextstep.subway.utils.RestAssuredCRUD.get;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Section에 대한 인수테스트")
@@ -48,14 +49,6 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @DisplayName("노선에 구간을 등록한다.")
     @Test
     void addSection() {
-        /**
-         * POST /lines/1/sections HTTP/1.1
-         * {
-         *"downStationId":"4",
-         *"upStationId":"2",
-         *"distance":10
-         * }
-         */
         // when
         // 지하철_노선에_지하철역_등록_요청
         StationResponse 강남역과_광교역_사이의_역 =
@@ -67,11 +60,16 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         createParams.put("downStationId", 광교역.getId() + "");
         createParams.put("distance", 5 + "");
 
-        RestAssuredCRUD.postRequest("/lines/"+신분당선.getId()+"/sections", createParams);
+        ExtractableResponse<Response> response = RestAssuredCRUD
+                .postRequest("/lines/"+신분당선.getId()+"/sections", createParams);
 
         // then
         // 지하철_노선에_지하철역_등록됨
-        List<String> stations = 신분당선.getStations().stream()
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        LineResponse 구간등록_후_신분당선 = get("/lines/"+신분당선.getId())
+                .body()
+                .as(LineResponse.class);
+        List<String> stations = 구간등록_후_신분당선.getStations().stream()
                 .map(stationResponse -> stationResponse.getName())
                 .collect(Collectors.toList());
         assertThat(stations).containsExactly("강남역", "강남역과_광교역_사이의_역", "광교역");
