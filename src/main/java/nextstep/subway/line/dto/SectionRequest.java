75987d9066ca91a48cd00757e8bef26088b8d823
bbbnam <bbbnam@naver.com>
2021-06-07 22:07:04 +0900

    feat : SectionRequest 객체 생성

---
+++ src/main/java/nextstep/subway/line/dto/SectionRequest.java
@@ -0,0 +1,16 @@
+package nextstep.subway.line.dto;
+
+public class SectionRequest {
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public SectionRequest() {
+    }
+
+    public SectionRequest(Long upStationId, Long downStationId, int distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+}
