9480af0e67f8a7b60ec7605c12e8bf6dc566f201
prodo-developer <wjdals300@gmail.com>
2021-06-08 10:53:29 +0900

    feat: 원시값 랩핑 & 컨버터 적용

---
+++ src/main/java/nextstep/subway/line/domain/DistanceToLongConverter.java
@@ -0,0 +1,18 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class DistanceToLongConverter implements AttributeConverter<Distance, Long> {
+
+    @Override
+    public Long convertToDatabaseColumn(Distance attribute) {
+        return null;
+    }
+
+    @Override
+    public Distance convertToEntityAttribute(Long dbData) {
+        return null;
+    }
+}
