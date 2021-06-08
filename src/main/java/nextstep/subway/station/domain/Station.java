e9c5f632328dfb29a93403e31f8027f8e19ca03c
Lee-Chungsun <cndtjs0218@naver.com>
2021-06-08 18:08:59 +0900

    refator : id 공통 컬럼 추출

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -6,9 +6,6 @@ import javax.persistence.*;
 
 @Entity
 public class Station extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     @Column(unique = true)
     private String name;
 
@@ -17,10 +14,6 @@ public class Station extends BaseEntity {
 
     public Station(String name) {
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public String getName() {
