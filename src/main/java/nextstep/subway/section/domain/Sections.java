1cf095ce844a049cfddec8eb0ee19227f0fb9460
itdar <thesockerrr@gmail.com>
2021-06-09 01:35:56 +0900

    feat: Sections 에서 전체 상행~ 하행 정렬된 Section 리스트 가져오는 기능 추가

--- src/main/java/nextstep/subway/section/domain/Sections.java
+++ src/main/java/nextstep/subway/section/domain/Sections.java
@@ -1,10 +1,13 @@
 package nextstep.subway.section.domain;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Embeddable;
-import javax.persistence.OneToMany;
+import nextstep.subway.station.domain.Station;
+import org.apache.commons.lang3.NotImplementedException;
+
+import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 @Embeddable
@@ -28,7 +31,66 @@ public class Sections {
         }
     }
 
+    public List<Section> get() {
+        return sections;
+    }
+
     public boolean contains(Section section) {
         return sections.contains(section);
+    }
+
+    public List<Station> getOrderedStations() {
+        throw new NotImplementedException();
+    }
+
+    public List<Section> getOrderedSections() {
+        Section topSection = findTopSection();
+
+        List<Section> orderedSections = makeOrderedSectionsFrom(topSection);
+
+        return orderedSections;
+    }
+
+    public Section findTopSection() {
+        return sections.stream()
+                .filter(section -> isTop(section))
+                .findFirst()
+                .orElseThrow(NullPointerException::new);
+    }
+
+    private boolean isTop(Section section) {
+        return !sections.stream()
+                .filter(it -> it.getDownStation().getName()
+                        .equals(section.getUpStation().getName()))
+                .findAny()
+                .isPresent();
+    }
+
+    protected List<Section> makeOrderedSectionsFrom(Section topSection) {
+        List<Section> orderedSections = new ArrayList<>();
+        orderedSections.add(topSection);
+
+        Map<String, Section> upMap = new HashMap<>();
+        Map<String, Section> downMap = new HashMap<>();
+
+        for (Section it : this.sections) {
+            upMap.put(it.getUpStation().getName(), it);
+            downMap.put(it.getDownStation().getName(), it);
+        }
+
+        Section prevSection = null;
+        while (topSection != null) {
+            prevSection = topSection;
+
+            topSection = upMap.get(topSection.getDownStation().getName());
+
+            if (topSection != null) {
+                orderedSections.add(topSection);
+            }
+        }
+
+        orderedSections.add(prevSection);
+
+        return orderedSections;
     }
 }
