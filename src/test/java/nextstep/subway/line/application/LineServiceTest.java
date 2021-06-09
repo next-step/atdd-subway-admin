6ec06abd53e28e91f3cb87cf969127cdd9f61bde
jimin.joo <wnwlals22@naver.com>
2021-06-09 22:43:58 +0900

    test(Section 정렬 테스트)

--- src/test/java/nextstep/subway/line/application/LineServiceTest.java
+++ src/test/java/nextstep/subway/line/application/LineServiceTest.java
@@ -5,7 +5,9 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.section.application.SectionService;
 import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -31,9 +33,10 @@ class LineServiceTest {
 
 	@Mock
 	private LineRepository lines;
-
 	@Mock
-	private StationRepository stations;
+	private StationService stationService;
+	@Mock
+	private SectionService sectionService;
 
 	@InjectMocks
 	private LineService lineService;
@@ -48,8 +51,8 @@ class LineServiceTest {
 		신분당선.addSection(Section.of(강남역, 역삼역, 10));
 
 		LineRequest request = new LineRequest("신분당선", "bg-red-600", 1l, 2l, 10);
-		Mockito.when(stations.findById(1l)).thenReturn(Optional.of(강남역));
-		Mockito.when(stations.findById(2l)).thenReturn(Optional.of(역삼역));
+		Mockito.when(stationService.findStationById(1l)).thenReturn(강남역);
+		Mockito.when(stationService.findStationById(2l)).thenReturn(역삼역);
 		Mockito.when(lines.save(any(Line.class))).thenReturn(신분당선);
 
 		// when
@@ -63,29 +66,9 @@ class LineServiceTest {
 			assertThat(response.getStations().get(1).getName()).isEqualTo(역삼역.getName());
 		});
 
-		Mockito.verify(stations).findById(1l);
-		Mockito.verify(stations).findById(2l);
+		Mockito.verify(stationService).findStationById(1l);
+		Mockito.verify(stationService).findStationById(2l);
 		Mockito.verify(lines).save(any(Line.class));
-	}
-
-	@Test
-	@DisplayName("노선 등록 테스트, 지하철역 정보가 없는 경우")
-	void saveLineTestWithOutStation() {
-		// given
-		Station 강남역 = new Station("강남역");
-		Station 역삼역 = new Station("역삼역");
-		Line 신분당선 = new Line("신분당선", "bg-red-600");
-		신분당선.addSection(Section.of(강남역, 역삼역, 10));
-
-		LineRequest request = new LineRequest("신분당선", "bg-red-600", 1l, 2l, 10);
-		Mockito.when(stations.findById(1l)).thenReturn(Optional.of(강남역));
-		Mockito.when(stations.findById(2l)).thenReturn(Optional.empty());
-
-		// when
-		assertThatThrownBy(() -> lineService.saveLine(request))
-			.isInstanceOf(ApiException.class)
-			.hasMessageContaining(NOT_FOUND_STATION.message());
-
-		Mockito.verify(lines, never()).save(any(Line.class));
+		Mockito.verify(sectionService).registerSection(신분당선, 강남역, 역삼역, 10);
 	}
 }
---
+++ src/test/java/nextstep/subway/section/application/SortedSectionUtilTest.java
@@ -0,0 +1,51 @@
+package nextstep.subway.section.application;
+
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SortedSectionUtilTest {
+
+	private Station 강남역;
+	private Station 구디역;
+	private Station 사당역;
+	private Station 판교역;
+
+
+	@BeforeEach
+	void setUp() {
+		강남역 = new Station("강남역");
+		구디역 = new Station("구디역");
+		사당역 = new Station("사당역");
+		판교역 = new Station("판교역");
+	}
+
+
+	@Test
+	@DisplayName("등록된 지하철 구간 정렬 테스트")
+	void sortedTest() {
+		// given
+		List<Section> sections = new ArrayList<>();
+		sections.add(Section.of(강남역, 구디역, 10));
+		sections.add(Section.of(판교역, 사당역, 3));
+		sections.add(Section.of(구디역, 판교역, 5));
+		SortedSectionUtil sortedSectionUtil = SortedSectionUtil.of(sections);
+
+		// when
+		List<Station> sortedStations = sortedSectionUtil.sorted();
+
+		// then
+		assertThat(sortedStations)
+			.isNotEmpty()
+			.containsExactly(강남역, 구디역, 판교역, 사당역);
+
+	}
+
+}
