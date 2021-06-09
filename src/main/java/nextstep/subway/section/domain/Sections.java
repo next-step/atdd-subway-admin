b35a68d61b64dd9a06d36e8fad1b9285352bab21
hJJANG <dacapolife87@gmail.com>
2021-06-09 17:48:07 +0900

    feat : 노선에서 역삭제 기능 추가 - 노선에서 역사이일때 구간 합치는 기능 - 구간삭제기능 - 이전역, 다음역 구하는 공통기능 추출 - 역리스트 출력 메소드 추가

--- src/main/java/nextstep/subway/section/domain/Sections.java
+++ src/main/java/nextstep/subway/section/domain/Sections.java
@@ -56,9 +56,7 @@ public class Sections {
     }
 
     private void addIfMatchUpStation(Section addSection) {
-        Optional<Section> matchUpstation = sections.stream()
-                .filter(section -> section.equalsUpStation(addSection))
-                .findFirst();
+        Optional<Section> matchUpstation = getNextSection(addSection.getUpStation());
 
         if (matchUpstation.isPresent()) {
             Section section = matchUpstation.get();
@@ -68,14 +66,65 @@ public class Sections {
     }
 
     private void addIfMatchDownStation(Section addSection) {
-        Optional<Section> matchDownStation = sections.stream()
-                .filter(section -> section.equalsDownStation(addSection))
-                .findFirst();
+        Optional<Section> matchDownStation = getPrevSection(addSection.getDownStation());
 
         if (matchDownStation.isPresent()) {
             Section section = matchDownStation.get();
 
             section.changeDownSection(addSection);
         }
+    }
+
+    public void delete(Station station) {
+        if (sections.size() < 2 ) {
+            throw new IndexOutOfBoundsException("노선에 역이 2개 이하 일때는 구간 삭제가 불가능합니다.");
+        }
+
+        Optional<Section> prevSection = getPrevSection(station);
+        Optional<Section> nextSection = getNextSection(station);
+
+        mergeIfBetweenSection(prevSection, nextSection);
+        deleteSections(prevSection, nextSection);
+    }
+
+    private void mergeIfBetweenSection(Optional<Section> optionalPrevSection, Optional<Section> optionalNextSection) {
+        if(optionalPrevSection.isPresent() && optionalNextSection.isPresent()) {
+            Section prevSection = optionalPrevSection.get();
+            Section nextSection = optionalNextSection.get();
+            prevSection.mergeSection(nextSection);
+        }
+    }
+
+    private void deleteSections(Optional<Section> optionalPrevSection, Optional<Section> optionalNextSection) {
+        if(optionalNextSection.isPresent()) {
+            sections.remove(optionalNextSection.get());
+        }
+
+        if(optionalPrevSection.isPresent() && !optionalNextSection.isPresent()) {
+            sections.remove(optionalPrevSection.get());
+        }
+    }
+
+    private Optional<Section> getNextSection(Station station) {
+        return sections.stream()
+                .filter(section -> section.equalsUpStation(station))
+                .findFirst();
+    }
+
+    private Optional<Section> getPrevSection(Station station) {
+        return sections.stream()
+                .filter(section -> section.equalsDownStation(station))
+                .findFirst();
+    }
+
+    public List<Station> getStations() {
+        List<Station> stations = new ArrayList<>();
+        stations.add(sections.first().getUpStation());
+
+        for (Section section : sections) {
+            stations.add(section.getDownStation());
+        }
+
+        return stations;
     }
 }
