4d63d7700fc1fb3673089f68fc5fb0c172093af1
tmkwak <tmkwak@hmm21.com>
2021-06-09 16:59:13 +0900

    fix : Distance 0보다 작을때 valid

--- src/main/java/nextstep/subway/section/domain/Distance.java
+++ src/main/java/nextstep/subway/section/domain/Distance.java
@@ -6,18 +6,26 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Distance {
 
+	private static final int MIN_DISTANCE = 0;
+
 	@Column
 	private int distance;
 
 	protected Distance() {
-		
+
 	}
 
 	public Distance(int distance) {
+		validate(distance);
 		this.distance = distance;
 	}
 
 	public int getDistance() {
 		return this.distance;
+	}
+
+	private void validate(int distance) {
+		if(MIN_DISTANCE >= distance)
+			throw new IllegalArgumentException("거리는 0보다 작을 수 없습니다.");
 	}
 }
