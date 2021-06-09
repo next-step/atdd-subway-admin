732026a845cde7adf3bb5e1dc72a7712f3bd5d21
김석진 <7271kim@naver.com>
2021-06-09 23:07:43 +0900

    feature : 예외 케이스를 고려한 인수테스트 작성

---
+++ src/main/java/nextstep/subway/section/dto/SectionRequest.java
@@ -0,0 +1,15 @@
+package nextstep.subway.section.dto;
+
+public class SectionRequest {
+
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
+
+    public SectionRequest(Long upStationId, Long downStationId, Integer distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+}
