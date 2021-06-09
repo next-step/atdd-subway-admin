caacd203a85c739a4d2f3101adc0dde0d8951c21
jeongminkyo <redwonder@naver.com>
2021-06-09 10:31:01 +0900

    Step1 리뷰 요청드립니다 (#219)
    * feat(LineAcceptanceTest.java) : 지하철 노선 관리 인수 테스트를 작성한다
    
    * feat(LineController.java) : 지하철 노선 관리 기능을 구현한다

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,9 +1,15 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +25,30 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+            .map(LineResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    public LineResponse getLine(Long id) {
+        Line line = lineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 라인이 존재하지 않습니다."));
+
+        return LineResponse.of(line);
+    }
+
+    public LineResponse updateLine(Long id, LineRequest lineRequest) {
+        Line line = lineRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 라인이 존재하지 않습니다."));
+        line.update(lineRequest.toLine());
+        return LineResponse.of(line);
+    }
+
+    public void deleteLineById(Long id) {
+        lineRepository.deleteById(id);
     }
 }
