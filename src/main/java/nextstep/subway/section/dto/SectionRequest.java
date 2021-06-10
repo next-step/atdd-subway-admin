39dcab8d7ef89381afd626efaadb6bab9ecdff9f
jimin.joo <wnwlals22@naver.com>
2021-06-10 10:14:46 +0900

    refactor: Request default 생성자

--- src/main/java/nextstep/subway/section/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/section/dto/SectionRequest.java
@@ -5,6 +5,10 @@ public class SectionRequest {
 	private Long downStationId;       // 하행역 아이디
 	private int distance;
 
+	public SectionRequest() {
+		// empty
+	}
+
 	public SectionRequest(final Long upStationId, final Long downStationId, final int distance) {
 		this.upStationId = upStationId;
 		this.downStationId = downStationId;
