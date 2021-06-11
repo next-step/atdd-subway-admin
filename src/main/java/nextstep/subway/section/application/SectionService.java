5236c7d9c16343b2efcba4752c93eb608aa1cde0
yjs2952 <yjs2952@naver.com>
2021-06-11 18:59:57 +0900

    feat: 구간 목록 조회 기능 추가

---
+++ src/main/java/nextstep/subway/section/application/SectionService.java
@@ -0,0 +1,29 @@
+package nextstep.subway.section.application;
+
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.section.domain.SectionRepository;
+import nextstep.subway.section.dto.SectionResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SectionService {
+
+    private final SectionRepository sectionRepository;
+
+    public SectionService(SectionRepository sectionRepository) {
+        this.sectionRepository = sectionRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<SectionResponse> getSectionsByLineId(Long lineId) {
+        List<Section> lines = sectionRepository.findAllByLine_Id(lineId);
+        return lines.stream()
+                .map(SectionResponse::of)
+                .collect(Collectors.toList());
+    }
+}
+
--- src/main/java/nextstep/subway/section/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/section/domain/SectionRepository.java
@@ -2,6 +2,9 @@ package nextstep.subway.section.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SectionRepository extends JpaRepository<Section, Long> {
 
+    List<Section> findAllByLine_Id(Long lineId);
 }
