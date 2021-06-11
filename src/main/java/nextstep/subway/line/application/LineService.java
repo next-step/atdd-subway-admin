74fa2956be12c067fb443be6a9d74ac0533f3971
hun <ini8262@itnomads.com>
2021-06-11 10:09:19 +0900

    refac : 같은 계층에서 서로 참조하는 코드 수정

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -6,7 +6,13 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.section.application.SectionService;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.section.domain.SectionRepository;
+import nextstep.subway.section.dto.SectionRequest;
+import nextstep.subway.section.dto.SectionResponse;
+import nextstep.subway.station.StationNotFoundException;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -17,15 +23,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class LineService {
     private final LineRepository lineRepository;
-
-    private final SectionService sectionService;
+    private final SectionRepository sectionRepository;
+    private final StationRepository stationRepository;
 
     public LineResponse saveLine(final LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
 
-        sectionService.registerSection(request.toSectionRequest(), persistLine);
+        registerSection(request.toSectionRequest(), persistLine);
 
         return LineResponse.of(persistLine);
+    }
+
+    private void registerSection(final SectionRequest request, final Line line) {
+        Station upStation = stationRepository.findById(request.getUpStationId())
+                .orElseThrow(()->new StationNotFoundException());
+
+        Station downStation = stationRepository.findById(request.getDownStationId())
+                .orElseThrow(()->new StationNotFoundException());
+
+        sectionRepository.save(request.toSection(line, upStation, downStation));
     }
 
     @Transactional(readOnly = true)
