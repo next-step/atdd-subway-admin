bc0f45acf539ddf05835700104d07b5fc17cd148
Donghui-Lee <ehdgml3206@naver.com>
2021-06-11 23:06:33 +0900

    refactor: 테스트 DisplayName 수정 - 무엇을 테스트 하는지 명확화

--- src/test/java/nextstep/subway/section/domain/DistanceTest.java
+++ src/test/java/nextstep/subway/section/domain/DistanceTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DistanceTest {
 
@@ -17,31 +20,30 @@ public class DistanceTest {
     }
 
     @Test
-    @DisplayName("유효하지 않은 거리(0보다 작은) 생성 테스트")
+    @DisplayName("유효하지 않은 거리(0보다 작은) 생성 하는 경우 실패 테스트")
     void validate() {
         // given & when & then
         Assertions.assertThrows(IllegalArgumentException.class, () -> Distance.of(-1));
         Assertions.assertThrows(IllegalArgumentException.class, () -> Distance.of(0));
     }
 
-    @Test
-    @DisplayName("현재 거리보다 짧은 거리 수정 테스트")
-    void updateDiffDistance() {
+    @ParameterizedTest(name = "현재 거리보다 짧은 거리인 경우 수정 가능 테스트")
+    @CsvSource(value = {"1:9", "3:7", "5:5", "9:1"}, delimiter = ':')
+    void updateDiffDistance(int input, int expected) {
         // given
-        Distance distance = Distance.of(5);
+        Distance distance = Distance.of(10);
         // when
-        distance.subtractDiffDistance(3);
+        distance.subtractDiffDistance(input);
         // then
-        assertThat(distance.toNumber()).isEqualTo(2);
+        assertThat(distance.toNumber()).isEqualTo(expected);
     }
 
-    @Test
-    @DisplayName("현재 거리보다 같거나 긴 거리 수정 테스트")
-    void updateDiffDistanceException() {
+    @ParameterizedTest(name = "현재 거리보다 같거나 긴 경우 수정 불가능 테스트")
+    @ValueSource(strings = {"5", "10", "15"})
+    void updateDiffDistanceException(int input) {
         // given
         Distance distance = Distance.of(5);
         // when & then
-        Assertions.assertThrows(IllegalArgumentException.class, () -> distance.subtractDiffDistance(10));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> distance.subtractDiffDistance(5));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> distance.subtractDiffDistance(input));
     }
 }
