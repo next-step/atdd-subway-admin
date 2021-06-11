d4ccd680b33459ee81cf9b13764ab34eb998a0be
lkimilhol <lkimilhol@gmail.com>
2021-06-12 00:24:39 +0900

    refactor(LineService): 리팩토링
    - 중복 로직 메서드 추출
    - 에러 메시지 상수처리

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -21,6 +21,9 @@ import nextstep.subway.line.dto.LineResponse;
 @Service
 @Transactional
 public class LineService {
+    public static final String NOT_FOUND_LINE = "노선을 찾을 수 없습니다.";
+    public static final String NOT_FOUND_STATION = "존재하지 않는 역입니다";
+
     private LineRepository lineRepository;
     private SectionRepository sectionRepository;
     private StationRepository stationRepository;
@@ -32,8 +35,8 @@ public class LineService {
     }
 
     public LineResponse saveLine(LineRequest request) {
-        Station upStation = stationRepository.findById(request.getUpStationId()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 역입니다"));
-        Station downStation = stationRepository.findById(request.getDownStationId()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 역입니다"));
+        Station upStation = getStation(request.getUpStationId());
+        Station downStation = getStation(request.getDownStationId());
 
         Line persistLine = lineRepository.save(request.toLine());
 
@@ -60,10 +63,11 @@ public class LineService {
 
     @Transactional(readOnly = true)
     public LineResponse findLine(Long id) {
-        Line line = lineRepository.findById(id).orElseThrow(() -> new RuntimeException("노선을 찾을 수 없습니다."));
+        Line line = getLine(id);
 
         List<Section> sections = sectionRepository.findByLineId(id);
         List<StationResponse> stationResponses = new ArrayList<>();
+
         for (Section section : sections) {
             addStationResponse(stationResponses, section);
         }
@@ -73,24 +77,21 @@ public class LineService {
 
     public LineResponse updateLine(Long id, LineRequest lineRequest) {
         Line line = getLine(id);
-
         line.update(lineRequest.toLine());
 
         return LineResponse.of(line);
     }
 
     public void deleteLineById(Long id) {
-        Line line = lineRepository.findById(id).orElseThrow(() -> new RuntimeException("노선을 찾을 수 없습니다."));
+        Line line = getLine(id);
         Sections sections = line.getSections();
-        sections.getSections().forEach(section -> {
-            sectionRepository.delete(section);
-        });
+        sections.getSections().forEach(section -> sectionRepository.delete(section));
 
         lineRepository.deleteById(id);
     }
 
     private Line getLine(Long id) {
-        return lineRepository.findById(id).orElseThrow(() -> new RuntimeException("노선을 찾을 수 없습니다."));
+        return lineRepository.findById(id).orElseThrow(() -> new RuntimeException(NOT_FOUND_LINE));
     }
 
     private void addStationResponse(List<StationResponse> stationResponses, Section section) {
@@ -102,5 +103,9 @@ public class LineService {
         if (!stationResponses.contains(stationResponse)) {
             stationResponses.add(stationResponse);
         }
+    }
+
+    private Station getStation(Long stationId) {
+        return stationRepository.findById(stationId).orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_STATION));
     }
 }
