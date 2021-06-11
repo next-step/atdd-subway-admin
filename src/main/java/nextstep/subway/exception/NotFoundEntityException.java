0fe202a0285919d314e277d8148b89491bb0295b
lenkim <joenggyu0@gmail.com>
2021-06-12 03:55:25 +0900

    refactor: 예외 코드 중 사용되지 않은 메서드 제거

--- src/main/java/nextstep/subway/exception/NotFoundEntityException.java
+++ src/main/java/nextstep/subway/exception/NotFoundEntityException.java
@@ -2,23 +2,8 @@ package nextstep.subway.exception;
 
 public class NotFoundEntityException extends RuntimeException {
 
-    public NotFoundEntityException() {
-        super();
-    }
-
     public NotFoundEntityException(String message) {
         super(message);
     }
 
-    public NotFoundEntityException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NotFoundEntityException(Throwable cause) {
-        super(cause);
-    }
-
-    protected NotFoundEntityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
