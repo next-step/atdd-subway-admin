597e8759180c62045c19b940692bc2eaa45b9e53
cw-seungkwon <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>
2021-06-11 20:00:54 +0900

    refactor: 중복된 annotation 제거

--- src/main/java/nextstep/subway/exception/ConflictException.java
+++ src/main/java/nextstep/subway/exception/ConflictException.java
@@ -1,9 +1,5 @@
 package nextstep.subway.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Line의 이름은 겹칠 수 없습니다")
 public class ConflictException extends RuntimeException {
 
     public ConflictException() {
