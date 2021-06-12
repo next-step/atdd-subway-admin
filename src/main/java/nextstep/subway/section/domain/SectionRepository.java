9601020e708c3c372f7a2ee76a903f92cc068923
seokjin <7271kim@naver.com>
2021-06-12 17:08:07 +0900

    feature : [Step4] 인수테스트 성공하도록 최종 반영

--- src/main/java/nextstep/subway/section/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/section/domain/SectionRepository.java
@@ -1,5 +1,9 @@
 package nextstep.subway.section.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SectionRepository extends JpaRepository<Section, Long> {}
+public interface SectionRepository extends JpaRepository<Section, Long> {
+    Optional<Section> findByUpStationId(Long stationId);
+}
