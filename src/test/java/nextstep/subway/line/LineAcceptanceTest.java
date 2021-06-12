1d8d414e12cec77ae76f96a2974ed7f8cfa23493
fDevJc <fDevJc@gmail.com>
2021-06-12 23:33:12 +0900

    test: 중복코드 제거 리팩토링

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -7,6 +7,7 @@ import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -28,36 +29,39 @@ public class LineAcceptanceTest extends AcceptanceTest {
     private static final String RED_LINE_COLOR = "red";
     private static final String LINE_REQUEST_PATH = "/lines";
 
-    @DisplayName("지하철 노선을 생성한다.")
-    @Test
-    void createLine() {
-        //given && when 지하철_노선_생성
+    private ExtractableResponse<Response> savedLineResponse1;
+    private ExtractableResponse<Response> savedLineResponse2;
+    private long savedUpStationId;
+    private long savedDownStationId;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        //given 상행선&&하행선을 포함한 노선1,2 생성
         ExtractableResponse<Response> responseUpStation = StationAcceptanceTest.requestCreateStation("상행선");
         ExtractableResponse<Response> responseDownStation = StationAcceptanceTest.requestCreateStation("하행선");
 
-        long upStationId = responseUpStation.jsonPath().getObject(".", StationResponse.class).getId();
-        long downStationId = responseDownStation.jsonPath().getObject(".", StationResponse.class).getId();
+        savedUpStationId = responseUpStation.jsonPath().getObject(".", StationResponse.class).getId();
+        savedDownStationId = responseDownStation.jsonPath().getObject(".", StationResponse.class).getId();
 
-        ExtractableResponse<Response> response = requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR, upStationId, downStationId, 10);
+        savedLineResponse1 = requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR, savedUpStationId, savedDownStationId, 10);
+        savedLineResponse2 = requestCreateLine(SECOND_LINE_NAME, RED_LINE_COLOR, savedUpStationId, savedDownStationId, 5);
+    }
+
+    @DisplayName("지하철 노선을 생성한다.")
+    @Test
+    void createLine() {
         // then 지하철_노선_생성됨
-        assertHttpStatus(response, HttpStatus.CREATED);
-        assertThat(response.header("Location")).isNotBlank();
+        assertHttpStatus(savedLineResponse1, HttpStatus.CREATED);
+        assertThat(savedLineResponse1.header("Location")).isNotBlank();
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
     void createLineWithDuplication() {
-        //given && when 지하철_노선_생성
-        ExtractableResponse<Response> responseUpStation = StationAcceptanceTest.requestCreateStation("상행선");
-        ExtractableResponse<Response> responseDownStation = StationAcceptanceTest.requestCreateStation("하행선");
-
-        long upStationId = responseUpStation.jsonPath().getObject(".", StationResponse.class).getId();
-        long downStationId = responseDownStation.jsonPath().getObject(".", StationResponse.class).getId();
-
-        // given 지하철_노선_등록되어_있음
-        requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR, upStationId, downStationId, 10);
         // when 지하철_동일이름_노선_생성_요청
-        ExtractableResponse<Response> response = requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR, upStationId, downStationId, 10);
+        ExtractableResponse<Response> response = requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR, savedUpStationId, savedDownStationId, 10);
         // then 지하철_노선_생성_실패됨
         assertHttpStatus(response, HttpStatus.BAD_REQUEST);
     }
@@ -65,16 +69,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선을 조회한다.")
     @Test
     void getLine() {
-        // given && when 지하철_노선_생성
-        // given 지하철_노선_등록되어_있음
-        ExtractableResponse<Response> responseUpStation = StationAcceptanceTest.requestCreateStation("상행선");
-        ExtractableResponse<Response> responseDownStation = StationAcceptanceTest.requestCreateStation("하행선");
-
-        long upStationId = responseUpStation.jsonPath().getObject(".", StationResponse.class).getId();
-        long downStationId = responseDownStation.jsonPath().getObject(".", StationResponse.class).getId();
-
-        requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR, upStationId, downStationId, 10);
-
         // when 지하철_노선_조회_요청
         ExtractableResponse<Response> response = RestAssured
                 .given().log().all()
@@ -83,17 +77,13 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // then 지하철_노선_역이포함되어_응답됨
         assertHttpStatus(response, HttpStatus.OK);
         assertThat(response.jsonPath().getObject(".", LineResponse.class)).isNotNull();
-
-        assertThat(response.jsonPath().getList("stations").size()).isEqualTo(1);
+        assertThat(response.jsonPath().getList("stations").size()).isEqualTo(2);
 
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
     @Test
     void getLines() {
-        // given 지하철_노선_2개_등록되어_있음
-        ExtractableResponse<Response> createdResponse1 = requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR);
-        ExtractableResponse<Response> createdResponse2 = requestCreateLine(SECOND_LINE_NAME, RED_LINE_COLOR);
         // when 지하철_노선_목록_조회_요청
         ExtractableResponse<Response> response = RestAssured
                 .given().log().all()
@@ -102,7 +92,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // then 지하철_노선_목록_응답됨
         assertHttpStatus(response, HttpStatus.OK);
         // 지하철_노선_목록_포함됨
-        List<Long> expectedLineIds = Stream.of(createdResponse1, createdResponse2)
+        List<Long> expectedLineIds = Stream.of(savedLineResponse1, savedLineResponse2)
                 .map(expectedResponse -> expectedResponse.jsonPath().getObject(".", LineResponse.class).getId())
                 .collect(Collectors.toList());
         List<Long> actualLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
@@ -114,8 +104,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선을 수정한다.")
     @Test
     void updateLine() {
-        // given 지하철_노선_등록되어_있음
-        requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR);
         // when 지하철_노선_수정_요청
         ExtractableResponse<Response> response = requestUpdateLine(makeParams(FIRST_LINE_NAME, RED_LINE_COLOR), 1L);
         // then 지하철_노선_수정됨
@@ -126,10 +114,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("존재하지 않는 지하철 노선을 수정한다.")
     @Test
     void updateNonExistentLine() {
-        // given 지하철_노선_등록되어_있음
-        requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR);
         // when 지하철_노선_수정_요청
-        ExtractableResponse<Response> response = requestUpdateLine(makeParams(FIRST_LINE_NAME, RED_LINE_COLOR), 2L);
+        ExtractableResponse<Response> response = requestUpdateLine(makeParams(FIRST_LINE_NAME, RED_LINE_COLOR), 3L);
         // then 지하철_노선_수정_실패됨
         assertHttpStatus(response, HttpStatus.BAD_REQUEST);
     }
@@ -137,8 +123,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선을 제거한다.")
     @Test
     void deleteLine() {
-        // given 지하철_노선_등록되어_있음
-        requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR);
         // when 지하철_노선_제거_요청
         ExtractableResponse<Response> response = requestDeleteLine(1L);
         // then 지하철_노선_삭제됨
@@ -148,10 +132,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("존재하지 않는 지하철 노선을 제거한다.")
     @Test
     void deleteNonExistentLine() {
-        // given 지하철_노선_등록되어_있음
-        requestCreateLine(FIRST_LINE_NAME, BLACK_LINE_COLOR);
         // when 지하철_노선_제거_요청
-        ExtractableResponse<Response> response = requestDeleteLine(2L);
+        ExtractableResponse<Response> response = requestDeleteLine(3L);
         // then 지하철_노선_삭제됨
         assertHttpStatus(response, HttpStatus.BAD_REQUEST);
     }
