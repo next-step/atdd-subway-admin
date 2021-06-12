ea18789eb68aef3c7c2b40e619affbd11d0a71b8
yjs2952 <yjs2952@naver.com>
2021-06-12 15:04:54 +0900

    refactor: fetch join 을 위해 @EntityGraph 사용

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -1,13 +1,13 @@
 package nextstep.subway.line.domain;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
-    @Query("select l from Line l join fetch l.sections.sections where l.id = :id")
-    Optional<Line> findByIdFetch(@Param("id") Long id);
+    @EntityGraph(attributePaths = {"sections.sections"})
+    Optional<Line> findById(@Param("id") Long id);
 }
