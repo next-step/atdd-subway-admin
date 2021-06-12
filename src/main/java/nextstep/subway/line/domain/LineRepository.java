3fdb4687344cd52a5829ed269caf74380932efba
yjs2952 <yjs2952@naver.com>
2021-06-12 21:27:27 +0900

    refactor: 기본 findById 사용

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -1,13 +1,6 @@
 package nextstep.subway.line.domain;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-
-    @EntityGraph(attributePaths = {"sections.sections"})
-    Optional<Line> findById(@Param("id") Long id);
 }
