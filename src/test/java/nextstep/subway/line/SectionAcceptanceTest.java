2551e0bce424c37fff713c90869847207adcc06f
younghoon <bblnotbb@yahho.com>
2021-06-14 02:55:01 +0900

    feat : write section test code for adding section

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -1,9 +1,20 @@
 package nextstep.subway.line;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.StationAcceptanceMethod;
 import nextstep.subway.station.dto.StationRequest;
 
@@ -12,13 +23,28 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 	Long stationId1;
 	Long stationId2;
 	int distance;
+	String lineId;
+
+	@BeforeEach
+	public void setUp() {
+		super.setUp();
+
+		setUpStations();
+		setUpLine();
+	}
 
 	private void setUpStations() {
 		stationId1 = StationAcceptanceMethod.getStationID(StationAcceptanceMethod.createStations(
 			new StationRequest("교대역")));
 		stationId2 = StationAcceptanceMethod.getStationID(StationAcceptanceMethod.createStations(
 			new StationRequest("역삼역")));
-		distance = 10;
+		distance = 7;
+	}
+
+	private void setUpLine() {
+		ExtractableResponse<Response> createResponse = LineAcceptanceMethod.createLine(
+			new LineRequest("2호선", "bg-green-600", stationId1, stationId2, distance));
+		lineId = LineAcceptanceMethod.getLineID(createResponse);
 	}
 
 	@DisplayName("기존에 존재하던 역 사이에 새로운 역을 등록한다.")
@@ -26,13 +52,28 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 	void addSectionBetweenStations() {
 		// given
 		// 지하철_역_등록되어_있음
-		setUpStations();
+		// 지하철_노선_등록되어_있음
 
 		// when
 		// 지하철_구간_추가_요청
+		Long stationId3 = StationAcceptanceMethod.getStationID(StationAcceptanceMethod.createStations(
+			new StationRequest("강남역")));
+
+		ExtractableResponse<Response> addResponse = RestAssured
+			.given().log().all()
+			.body(new SectionRequest(stationId1, stationId3, 4))
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when()
+			.post("/lines" + "/" + lineId)
+			.then().log().all()
+			.extract();
 
 		// then
 		// 지하철_구간_생성됨
+		assertThat(addResponse.statusCode()).isEqualTo(HttpStatus.OK);
+		// 지하철_역_추가_조회
+		ExtractableResponse<Response> findResponse = LineAcceptanceMethod.findLine(lineId);
+		assertThat(findResponse.jsonPath().getList(".", LineResponse.class).size()).isEqualTo(3);
 	}
 
 	@DisplayName("새로운 역을 상행 종점으로 등록한다.")
