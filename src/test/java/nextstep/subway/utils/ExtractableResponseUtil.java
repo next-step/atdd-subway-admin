2948c03a9a0d63c595adfeba7fd59c11f2df277b
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-13 10:30:59 +0900

    feat: ExtractableResponse 클래스의 layout 구현

---
+++ src/test/java/nextstep/subway/utils/ExtractableResponseUtil.java
@@ -0,0 +1,21 @@
+package nextstep.subway.utils;
+
+import io.restassured.response.ExtractableResponse;
+
+import java.util.List;
+
+public class ExtractableResponseUtil {
+
+    public static Long extractIdInResponse(ExtractableResponse response) {
+        return null;
+    }
+    public static List<Long> extractIdInResponses(ExtractableResponse response) {
+        return null;
+    }
+    public static <T> T extractObjectInResponse(ExtractableResponse response) {
+        return null;
+    }
+    public static <T> List<T> extractObjectsInResponse(ExtractableResponse response) {
+        return null;
+    }
+}
