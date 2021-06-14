ffb9747c630416026cf41219a94e11a4d765d75c
sanggu <tkdrn8578@naver.com>
2021-06-14 21:43:59 +0900

    test: 지하철 노선 목록을 조회한다.에 대한 인수테스트 추가

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -5,20 +5,27 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
+    public static final LineRequest 일호선 = new LineRequest("1호선", "red");
 
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
         // when
-        ExtractableResponse<Response> response = 지하철_노선_생성_요청(new LineRequest("1호선", "red"));
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(일호선);
 
         // then
         지하철_노선_생성됨(response);
@@ -29,14 +36,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine2() {
         // given
         // 지하철_노선_등록되어_있음
-        지하철_노선_생성_요청(new LineRequest("1호선", "red"));
+        지하철_노선_생성_요청(일호선);
 
         // when
         // 지하철_노선_생성_요청
-        ExtractableResponse<Response> response = 지하철_노선_생성_요청(new LineRequest("1호선", "red"));
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(일호선);
 
         // then
         // 지하철_노선_생성_실패됨
+        지하철_노선_생성_실패됨(response);
+    }
+
+    private void 지하철_노선_생성_실패됨(ExtractableResponse<Response> response) {
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
@@ -45,14 +56,26 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLines() {
         // given
         // 지하철_노선_등록되어_있음
+        지하철_노선_생성_요청(일호선);
         // 지하철_노선_등록되어_있음
+        지하철_노선_생성_요청(new LineRequest("2호선", "blue"));
 
         // when
         // 지하철_노선_목록_조회_요청
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .when().get("/lines")
+                .then().log().all().extract();
 
         // then
+        List<LineResponse> lines = response.body().as(List.class);
         // 지하철_노선_목록_응답됨
         // 지하철_노선_목록_포함됨
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        Assertions.assertThat(lines).containsExactlyInAnyOrder(
+                new LineResponse(1L, "1호선", "red", LocalDateTime.now(), LocalDateTime.now()),
+                new LineResponse(2L, "2호선", "blue", LocalDateTime.now(), LocalDateTime.now())
+        );
     }
 
     @DisplayName("지하철 노선을 조회한다.")
