a19c4eb5c119d16d05455da1aa05d4946347a1b9
accidentlywoo <accidentlywoo@gmail.com>
2021-06-15 00:11:13 +0900

    feat : section 로직을 section service로 분리

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -5,9 +5,7 @@ import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.exception.AlreadyExistLineException;
-import nextstep.subway.line.exception.NoneExistLineException;
-import nextstep.subway.section.domain.Section;
-import nextstep.subway.station.domain.Station;
+import nextstep.subway.section.application.SectionService;
 import nextstep.subway.station.domain.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,81 +18,71 @@ import java.util.Optional;
 @Service
 @Transactional
 public class LineService {
-    @Autowired
-    private LineRepository lineRepository;
+	@Autowired
+	private LineRepository lineRepository;
 
-    @Autowired
-    private StationRepository stationRepository;
+	@Autowired
+	private StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
-        this.lineRepository = lineRepository;
-    }
+	@Autowired
+	private SectionService sectionService;
 
-    public LineResponse saveLine(LineRequest request) {
-        Optional<Line> findLine = lineRepository.findByName(request.name());
-        if (findLine.isPresent()) {
-            throw new AlreadyExistLineException("이미 등록된 노선 정보입니다.");
-        }
+	public LineService(LineRepository lineRepository) {
+		this.lineRepository = lineRepository;
+	}
 
-        Line line = Line.create(request.name(), request.color());
+	public LineResponse saveLine(LineRequest request) {
+		Optional<Line> findLine = lineRepository.findByName(request.getName());
+		if (findLine.isPresent()) {
+			throw new AlreadyExistLineException("이미 등록된 노선 정보입니다.");
+		}
+		Line line = Line.create(request.getName(), request.getColor());
 
-        Optional<Station> upStation = stationRepository.findById(request.upStationId());
-        if (!upStation.isPresent()) {
-            throw new NoneExistLineException("잘못된 상행선 정보입니다.");
-        }
+		sectionService.create(line, request.getUpStationId(), request.getDownStationId(), request.getDistance());
 
-        Optional<Station> downStation = stationRepository.findById(request.downStationId());
-        if (!downStation.isPresent()) {
-            throw new NoneExistLineException("잘못된 하행선 정보입니다.");
-        }
+		Line newLine = lineRepository.save(line);
 
-        Section newSection = Section.create(upStation.get(), downStation.get(), request.distance());
-        line.setSection(newSection);
+		return LineResponse.of(newLine);
+	}
 
-        Line persistLine = lineRepository.save(line);
-        return LineResponse.of(persistLine);
-    }
+	@Transactional(readOnly = true)
+	public List<LineResponse> findAll() {
+		List<Line> lines = lineRepository.findAll();
 
-    @Transactional(readOnly = true)
-    public List<LineResponse> findAll() {
-        List<Line> lines = lineRepository.findAll();
+		List<LineResponse> lineResponses = new ArrayList<>();
+		for (Line line : lines) {
+			lineResponses.add(LineResponse.of(line));
+		}
 
-        List<LineResponse> lineResponses = new ArrayList<>();
-        for (Line line: lines) {
-            lineResponses.add(LineResponse.of(line));
-        }
+		return lineResponses;
+	}
 
-        return lineResponses;
-    }
+	public LineResponse findByName(String name) {
+		Line line = checkExistLine(name);
 
-    public LineResponse findByName(String name) {
-        Line line = checkExistLine(name);
+		return LineResponse.of(line);
+	}
 
-        return LineResponse.of(line);
-    }
+	public LineResponse updateByName(LineRequest req) {
+		Line line = checkExistLine(req.getName());
 
-    public LineResponse updateByName(LineRequest req) {
-        Line line = checkExistLine(req.name());
+		line.updateColor(req.getColor());
 
-        line.updateColor(req.color());
+		return LineResponse.of(line);
+	}
 
-        return LineResponse.of(line);
-    }
+	public void deleteByName(String name) {
+		Optional<Line> line = lineRepository.findByName(name);
 
-    public void deleteByName(String name) {
-        Optional<Line> line = lineRepository.findByName(name);
+		line.ifPresent(value -> lineRepository.delete(value));
+	}
 
-        line.ifPresent(value -> lineRepository.delete(value));
-    }
+	private Line checkExistLine(String name) {
+		Optional<Line> byName = lineRepository.findByName(name);
+		if (!byName.isPresent()) {
+			throw new IllegalArgumentException("해당 노선을 찾을 수 없습니다.");
+		}
 
-    private Line checkExistLine(String name) {
-        Optional<Line> byName = lineRepository.findByName(name);
-        if (!byName.isPresent()) {
-            throw new IllegalArgumentException("해당 노선을 찾을 수 없습니다.");
-        }
-
-        return byName.get();
-    }
-
-
+		return byName.get();
+	}
 }
