441aa3c4faa3868f603ee12919ae997269acc394
Junhee Ko <jko@rainist.com>
2021-06-15 00:28:39 +0900

    Add ATDD Step1 (#276)
    * refactor: LineService class to add final keyword to instance field
    
    * test: createLine test method in LineAcceptanceTest class
    
    * test: createLine test method for same line info in LineAcceptanceTest class
    
    * test: get all line test method in LineAcceptanceTest class
    
    * feat: add controller method for getting all lines in LineController class
    
    * feat: add service method for getting all lines in LineService class
    
    * test: add get line method in LineAcceptanceTest class
    
    * feat: add get line controller method in LineController class
    
    * feat: add get line service method in LineService class
    
    * test: add update line tes method in LineAcceptanceTest class
    
    * feat: add update line test method in LineController class
    
    * feat: add update line service method in LineService class
    
    * feat: add update line domain logic method in Line class
    
    * test: add delete line method in LineAcceptanceTest class
    
    * test: add delete line method in LineController class
    
    * test: add delete line method in LineService class
    
    * refactor: controller method name change in LineController class
    
    * refactor: remove unnecessary comments in LineAcceptanceTest class
    
    * refactor: extract createLine method in LineAcceptanceTest class

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,16 +1,22 @@
 package nextstep.subway.line.application;
 
+import javax.persistence.EntityNotFoundException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
@@ -19,5 +25,35 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        return lineRepository.findAll()
+            .stream()
+            .map(LineResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findLineBy(Long id) {
+        Line line = lineRepository
+            .findById(id)
+            .orElseThrow(EntityNotFoundException::new);
+
+        return LineResponse.of(line);
+    }
+
+    public LineResponse updateLineBy(Long id, LineRequest lineRequest) {
+        Line line = lineRepository
+            .findById(id)
+            .orElseThrow(EntityNotFoundException::new)
+            .getUpdatedLineBy(lineRequest);
+
+        return LineResponse.of(line);
+    }
+
+    public void deleteStationBy(Long id) {
+        lineRepository.deleteById(id);
     }
 }
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,85 +1,160 @@
 package nextstep.subway.line;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = createLine01();
 
         // then
-        // 지하철_노선_생성됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
     void createLine2() {
         // given
-        // 지하철_노선_등록되어_있음
+        createLine01();
 
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = createLine01();
 
         // then
-        // 지하철_노선_생성_실패됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
     @Test
     void getLines() {
         // given
-        // 지하철_노선_등록되어_있음
-        // 지하철_노선_등록되어_있음
+        ExtractableResponse<Response> createResponse01 = createLine01();
+        ExtractableResponse<Response> createResponse02 = createLine02();
 
         // when
-        // 지하철_노선_목록_조회_요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .when()
+            .get("/lines")
+            .then().log().all()
+            .extract();
 
         // then
-        // 지하철_노선_목록_응답됨
-        // 지하철_노선_목록_포함됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        List<Long> expectedLineIds = Stream.of(createResponse01, createResponse02)
+            .map(it -> Long.parseLong(it.header("Location").split("/")[2]))
+            .collect(Collectors.toList());
+        List<Long> resultLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
+            .map(LineResponse::getId)
+            .collect(Collectors.toList());
+        assertThat(resultLineIds).containsAll(expectedLineIds);
     }
 
     @DisplayName("지하철 노선을 조회한다.")
     @Test
     void getLine() {
         // given
-        // 지하철_노선_등록되어_있음
+        ExtractableResponse<Response> createResponse = createLine01();
 
         // when
-        // 지하철_노선_조회_요청
+        String expectLineId = createResponse.header("Location").split("/")[2];
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .when()
+            .get("/lines/" + expectLineId)
+            .then().log().all()
+            .extract();
+        String actualLineId = response.jsonPath().get("id").toString();
 
         // then
-        // 지하철_노선_응답됨
+        assertEquals(expectLineId, actualLineId);
     }
 
     @DisplayName("지하철 노선을 수정한다.")
     @Test
     void updateLine() {
         // given
-        // 지하철_노선_등록되어_있음
+        ExtractableResponse<Response> createResponse = createLine01();
 
         // when
-        // 지하철_노선_수정_요청
+        String updateLineId = createResponse.header("Location").split("/")[2];
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "1호선");
+        params.put("color", "#FF0000");
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .put("/lines/" + updateLineId)
+            .then().log().all()
+            .extract();
 
         // then
-        // 지하철_노선_수정됨
+        assertEquals(params.get("color"), response.jsonPath().get("color"));
     }
 
     @DisplayName("지하철 노선을 제거한다.")
     @Test
     void deleteLine() {
         // given
-        // 지하철_노선_등록되어_있음
+        ExtractableResponse<Response> createResponse = createLine01();
 
         // when
-        // 지하철_노선_제거_요청
+        String uri = createResponse.header("Location");
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .when()
+            .delete(uri)
+            .then().log().all()
+            .extract();
 
         // then
-        // 지하철_노선_삭제됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    private ExtractableResponse<Response> createLine01() {
+        return RestAssured.given().log().all()
+            .body(new HashMap<String, String>() {{
+                      put("name", "1호선");
+                      put("color", "#0000FF");
+                  }}
+            )
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all().extract();
+    }
+
+    private ExtractableResponse<Response> createLine02() {
+        return RestAssured.given().log().all()
+            .body(new HashMap<String, String>() {{
+                      put("name", "2호선");
+                      put("color", "#008000");
+                  }}
+            )
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all().extract();
     }
 }
