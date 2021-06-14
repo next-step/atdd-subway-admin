499a1c2a5e37c89d5a8b763ffe826d75e3cfae23
jung635 <sunju635@naver.com>
2021-06-14 09:23:29 +0900

    1단계 - 지하철 노선 관리 리뷰 요청 (#267)
    * docs($step1): update readme for step1
    
    * test($step1): add test cases to LineAcceptanceTest
    
    * fix($step1): fix bugs for LineAcceptanceTest
    
    * feat($step1): implements LineAcceptanceTest
    
    * docs($step1): add directory tree

--- README.md
+++ README.md
@@ -38,17 +38,208 @@ npm run dev
 ```
 <br>
 
-## ✏️ Code Review Process
-[텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/next-step/nextstep-docs/tree/master/codereview)
+## 🚀 1단계 - 지하철 노선 관리
 
-<br>
+### 요구사항
+#### 기능 목록
+1. 노선 생성
+2. 노선 목록 조회
+3. 노선 조회
+4. 노선 수정
+5. 노선 삭제
 
-## 🐞 Bug Report
+#### 요구사항 설명
+1. 지하철 노선 관련 기능의 인수 테스트 작성
+2. 지하철 노선 관련 기능 구현
+3. 인수 테스트 리팩터링
 
-버그를 발견한다면, [Issues](https://github.com/next-step/atdd-subway-admin/issues) 에 등록해주세요 :)
+#### 노선 관리 API Request / Response
+1. 지하철 노선 생성
+    1. Request
+       ```
+        POST /lines HTTP/1.1
+        accept: */*
+        content-type: application/json; charset=UTF-8
+        
+        {
+        "color": "bg-red-600",
+        "name": "신분당선"
+        }
+        ```
+    2. Response
+       ```
+        {
+            "id": 1,
+            "name": "신분당선",
+            "color": "bg-red-600",
+            "createdDate": "2020-11-13T09:11:51.997",
+            "modifiedDate": "2020-11-13T09:11:51.997"
+        }
+        ```
 
-<br>
+2. 지하철 노선 목록 조회
+    1. Request
+       ```
+        GET /lines HTTP/1.1
+        accept: application/json
+        host: localhost:49468
+        ```
+    2. Response
+       ```
+        HTTP/1.1 200
+        Content-Type: application/json
+        Date: Fri, 13 Nov 2020 00:11:51 GMT
+        
+        [
+        {
+        "id": 1,
+        "name": "신분당선",
+        "color": "bg-red-600",
+        "stations": [
+        
+                ],
+                "createdDate": "2020-11-13T09:11:52.084",
+                "modifiedDate": "2020-11-13T09:11:52.084"
+            },
+            {
+                "id": 2,
+                "name": "2호선",
+                "color": "bg-green-600",
+                "stations": [
+                    
+                ],
+                "createdDate": "2020-11-13T09:11:52.098",
+                "modifiedDate": "2020-11-13T09:11:52.098"
+            }
+        ]
+        ```
+       
+3. 지하철 노선 조회
+   1. Request
+      ```
+        GET /lines/1 HTTP/1.1
+        accept: application/json
+        host: localhost:49468
+       ```
+   2. Response
+      ```
+        HTTP/1.1 200
+        Content-Type: application/json
+        Date: Fri, 13 Nov 2020 00:11:51 GMT
+        
+        {
+        "id": 1,
+        "name": "신분당선",
+        "color": "bg-red-600",
+        "stations": [
+        
+            ],
+            "createdDate": "2020-11-13T09:11:51.866",
+            "modifiedDate": "2020-11-13T09:11:51.866"
+        }
+       ```
 
-## 📝 License
+4. 지하철 노선 수정
+    1. Request
+       ```
+        PUT /lines/1 HTTP/1.1
+        accept: */*
+        content-type: application/json; charset=UTF-8
+        content-length: 45
+        host: localhost:49468
+        
+        {
+        "color": "bg-blue-600",
+        "name": "구분당선"
+        }
+        ```
+    2. Response
+       ```
+        HTTP/1.1 200
+        Date: Fri, 13 Nov 2020 00:11:51 GMT
+        ```
 
-This project is [MIT](https://github.com/next-step/atdd-subway-admin/blob/master/LICENSE.md) licensed.
+5. 지하철 노선 삭제
+    1. Request
+       ```
+        DELETE /lines/1 HTTP/1.1
+        accept: */*
+        host: localhost:49468
+        ```
+    2. Response
+       ```
+        HTTP/1.1 204
+        Date: Fri, 13 Nov 2020 00:11:51 GMT
+       ```
+
+## 디렉토리 구조
+```
+│  ├─java
+│  │  └─nextstep
+│  │      └─subway
+│  │          │  PageController.java
+│  │          │  SubwayApplication.java
+│  │          │
+│  │          ├─common
+│  │          │      BaseEntity.java              공통 컬럼 관리 엔티티
+│  │          │
+│  │          ├─line                              지하철 노선 관리
+│  │          │  ├─application
+│  │          │  │      LineService.java
+│  │          │  │
+│  │          │  ├─domain
+│  │          │  │      Line.java
+│  │          │  │      LineRepository.java
+│  │          │  │
+│  │          │  ├─dto
+│  │          │  │      LineRequest.java
+│  │          │  │      LineResponse.java
+│  │          │  │
+│  │          │  └─ui
+│  │          │          LineController.java
+│  │          │
+│  │          └─station                           지하철 역 관리
+│  │              ├─application
+│  │              │      StationService.java
+│  │              │
+│  │              ├─domain
+│  │              │      Station.java
+│  │              │      StationRepository.java
+│  │              │
+│  │              ├─dto
+│  │              │      StationRequest.java
+│  │              │      StationResponse.java
+│  │              │
+│  │              └─ui
+│  │                      StationController.java
+│  │
+│  └─resources
+│      │  application.properties
+│      │  logback-access.xml
+│      │
+│      ├─static
+│      │  └─images
+│      │          logo_small.png
+│      │          main_logo.png
+│      │
+│      └─templates
+│              index.html
+│
+└─test
+   │  test.iml
+   │
+   └─java
+       └─nextstep
+           └─subway
+               │  AcceptanceTest.java
+               │  SubwayApplicationTests.java
+               │
+               ├─line
+               │      LineAcceptanceTest.java
+               │
+               ├─station
+               │      StationAcceptanceTest.java
+               │
+               └─utils
+                       DatabaseCleanup.java
+```
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,19 +1,42 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
         // when
         // 지하철_노선_생성_요청
+        LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600");
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(lineRequest);
 
         // then
         // 지하철_노선_생성됨
+        LineResponse lineResponse = response.as(LineResponse.class);
+        assertThat(lineResponse.getName()).isEqualTo(lineRequest.getName());
+        assertThat(lineResponse.getColor()).isEqualTo(lineRequest.getColor());
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
@@ -21,12 +44,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine2() {
         // given
         // 지하철_노선_등록되어_있음
+        지하철_노선_생성_요청(new LineRequest("신분당선", "bg-red-600"));
 
         // when
         // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(new LineRequest("신분당선", "bg-red-600"));
 
         // then
         // 지하철_노선_생성_실패됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
@@ -34,14 +60,29 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLines() {
         // given
         // 지하철_노선_등록되어_있음
-        // 지하철_노선_등록되어_있음
+        LineResponse createResponse1 = 지하철_노선_생성_요청(new LineRequest("신분당선", "bg-red-600")).as(LineResponse.class);
+        LineResponse createResponse2 = 지하철_노선_생성_요청(new LineRequest("2호선", "bg-green-600")).as(LineResponse.class);
 
         // when
         // 지하철_노선_목록_조회_요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/lines")
+                .then().log().all()
+                .extract();
 
         // then
         // 지하철_노선_목록_응답됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
         // 지하철_노선_목록_포함됨
+        List<Long> expectedLineIds = Arrays.asList(createResponse1.getId(), createResponse2.getId());
+        List<Long> resultLineIds = response.jsonPath().getList(".", LineResponse.class).stream()
+                .map(lineResponse -> lineResponse.getId())
+                .collect(Collectors.toList());
+
+        assertThat(resultLineIds).containsAll(expectedLineIds);
     }
 
     @DisplayName("지하철 노선을 조회한다.")
@@ -49,12 +90,17 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLine() {
         // given
         // 지하철_노선_등록되어_있음
+        LineResponse createResponse = 지하철_노선_생성_요청(new LineRequest("신분당선", "bg-red-600")).as(LineResponse.class);
 
         // when
         // 지하철_노선_조회_요청
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(createResponse.getId());
+        지하철_노선_조회_요청(createResponse.getId());
 
         // then
         // 지하철_노선_응답됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.as(LineResponse.class)).isEqualTo(createResponse);
     }
 
     @DisplayName("지하철 노선을 수정한다.")
@@ -62,12 +108,26 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void updateLine() {
         // given
         // 지하철_노선_등록되어_있음
+        LineResponse createResponse = 지하철_노선_생성_요청(new LineRequest("신분당선", "bg-red-600")).as(LineResponse.class);
 
         // when
         // 지하철_노선_수정_요청
+        LineRequest lineRequest = new LineRequest("구분당선", "bg-blue-600");
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put("/lines/" + createResponse.getId())
+                .then().log().all()
+                .extract();
+
+        LineResponse lineResponse = response.as(LineResponse.class);
 
         // then
         // 지하철_노선_수정됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(lineResponse.getName()).isEqualTo(lineRequest.getName());
+        assertThat(lineResponse.getColor()).isEqualTo(lineRequest.getColor());
     }
 
     @DisplayName("지하철 노선을 제거한다.")
@@ -75,11 +135,42 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void deleteLine() {
         // given
         // 지하철_노선_등록되어_있음
+        LineResponse createResponse = 지하철_노선_생성_요청(new LineRequest("신분당선", "bg-red-600")).as(LineResponse.class);
 
         // when
         // 지하철_노선_제거_요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .delete("/lines/" + createResponse.getId())
+                .then().log().all()
+                .extract();
+
 
         // then
         // 지하철_노선_삭제됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        assertThat(지하철_노선_조회_요청(createResponse.getId()).statusCode())
+                .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
+
+    ExtractableResponse<Response> 지하철_노선_생성_요청(LineRequest lineRequest) {
+        return RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    ExtractableResponse<Response> 지하철_노선_조회_요청(Long id) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/lines/" + id)
+                .then().log().all()
+                .extract();
+    }
+
 }
