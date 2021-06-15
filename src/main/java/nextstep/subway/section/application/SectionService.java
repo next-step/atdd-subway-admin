67700102044f7874ba8649ab7f0b9571f5475689
Donghui-Lee <ehdgml3206@naver.com>
2021-06-15 23:56:43 +0900

    refactor: 메서드명 변경 (누락 적용) - getOrderLineSections -> getOrderedLineSections

--- src/main/java/nextstep/subway/section/application/SectionService.java
+++ src/main/java/nextstep/subway/section/application/SectionService.java
@@ -29,7 +29,7 @@ public class SectionService {
     public void addSection(Long lineId, SectionRequest sectionRequest) {
         Line line = lineRepository.findById(lineId)
             .orElseThrow(EntityNotFoundException::new);
-        List<Section> sections = line.getOrderLineSections();
+        List<Section> sections = line.getOrderedLineSections();
         Section newSection = getSection(sectionRequest);
         Optional<Section> containSection = getContainsSection(sections, newSection);
 
