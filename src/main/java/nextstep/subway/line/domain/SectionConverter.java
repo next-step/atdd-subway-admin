b1bed53c44921a4883ca79f27b0c7b66f2fc28dd
wrallee <wrallee@naver.com>
2021-06-15 21:54:42 +0900

    chore(section): 구간 기능 세부내용 TODO 추가

--- src/main/java/nextstep/subway/line/domain/SectionConverter.java
+++ src/main/java/nextstep/subway/line/domain/SectionConverter.java
@@ -12,6 +12,14 @@ import nextstep.subway.station.domain.Station;
 
 public class SectionConverter {
 
+    // TODO 이미 등록된 구간인지 판별
+
+    // TODO 상/하행역이 둘 다 없는지 판별
+
+    // TODO 구간 길이가 적절한지 검증
+    
+    // TODO 새로운 역을 연결하는 방법에 대해 고민
+
     public static List<Station> extractStationsInAscending(List<Section> sections) {
         Map<Station, Station> relations = getRelations(sections);
 
