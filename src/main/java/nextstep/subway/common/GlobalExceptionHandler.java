84c1757721f5046e92df9830fd0a71bc14fc0169
dohchoi91 <dohchoi91@gmail.com>
2021-06-16 00:00:58 +0900

    refactor: 전역 에러 처리 개편

---
+++ src/main/java/nextstep/subway/common/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package nextstep.subway.common;
+
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import nextstep.subway.common.dto.ErrorResponse;
+import nextstep.subway.exception.NotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<ErrorResponse> handleIllegalArgsException(DataIntegrityViolationException e) {
+		return ResponseEntity.badRequest().body(ErrorResponse.of(BAD_REQUEST.value(), e.getMessage()));
+	}
+
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+		return ResponseEntity.badRequest().body(ErrorResponse.of(BAD_REQUEST.value(), NotFoundException.MESSAGE));
+	}
+}
--- src/main/java/nextstep/subway/exception/NotFoundException.java
+++ src/main/java/nextstep/subway/exception/NotFoundException.java
@@ -1,4 +1,5 @@
 package nextstep.subway.exception;
 
 public class NotFoundException extends RuntimeException {
+    public static final String MESSAGE = "조회 할 수 없습니다.";
 }
