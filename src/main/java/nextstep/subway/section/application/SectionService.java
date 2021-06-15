44c222e711214db817694cd70e1d4fec7da1a3c0
Jiseon Lee <leejoyc9310@gmail.com>
2021-06-16 00:21:59 +0900

    fix : 쿼리메소드명 수정

--- src/main/java/nextstep/subway/section/application/SectionService.java
+++ src/main/java/nextstep/subway/section/application/SectionService.java
@@ -4,6 +4,8 @@ import nextstep.subway.exception.DataNotFoundException;
 import nextstep.subway.section.domain.Section;
 import nextstep.subway.section.domain.SectionRepository;
 import nextstep.subway.section.dto.SectionResponse;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class SectionService {
 
     private final SectionRepository sectionRepository;
+    private final StationRepository stationRepository;
 
-    public SectionService(SectionRepository sectionRepository) {
+    public SectionService(SectionRepository sectionRepository, StationRepository stationRepository) {
         this.sectionRepository = sectionRepository;
+        this.stationRepository = stationRepository;
     }
 
     public SectionResponse findSectionById(Long id) {
@@ -27,11 +31,12 @@ public class SectionService {
     }
 
     public SectionResponse findSectionByUpStationId(Long upStationId) {
-        return SectionResponse.of(findByUpStationId(upStationId));
+        Station station = stationRepository.findById(upStationId).get();
+        return SectionResponse.of(findByUpStationId(station));
     }
 
-    public Section findByUpStationId(Long id) {
-        return sectionRepository.findByUpStationId(id)
+    public Section findByUpStationId(Station station) {
+        return sectionRepository.findByUpStation(station)
                 .orElseThrow(() -> new DataNotFoundException("일치하는 구간을 찾을 수 없습니다."));
     }
 }
--- src/main/java/nextstep/subway/section/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/section/domain/SectionRepository.java
@@ -1,11 +1,12 @@
 package nextstep.subway.section.domain;
 
+import nextstep.subway.station.domain.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
 
-    Optional<Section> findByUpStationId(Long upStationId);
+    Optional<Section> findByUpStation(Station upStation);
 }
 
