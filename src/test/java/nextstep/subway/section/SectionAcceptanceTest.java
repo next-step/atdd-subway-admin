133f97d8733acb3e9ff5656ce8afd58a368a69ef
jaewanLee <ianlee9180@gmail.com>
2021-06-15 21:27:45 +0900

    feat(SectionAcceptanceTest): @BeforeEach 적용 - 지하철역 등록 - 지하철 노선 등록

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -1,10 +1,30 @@
 package nextstep.subway.section;
 
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static nextstep.subway.line.LineAcceptanceTest.지하철_노선_등록되어_있음;
+import static nextstep.subway.station.StationAcceptanceTest.지하철역_등록;
 
 @DisplayName("지하철 구간 관련 기능")
-public class SectionAcceptanceTest {
+public class SectionAcceptanceTest extends AcceptanceTest {
+    private StationResponse 강남역;
+    private StationResponse 당산역;
+    private LineResponse 이호선;
+
+    @BeforeEach
+    public void setUp(){
+        super.setUp();
+        강남역 = 지하철역_등록("강남역").as(StationResponse.class);
+        당산역 = 지하철역_등록("당산역").as(StationResponse.class);
+
+        LineRequest lineRequest = new LineRequest("2호선", "bg-green-600", 강남역.getId(), 당산역.getId(), 10);
+        이호선 = 지하철_노선_등록되어_있음(lineRequest).as(LineResponse.class);
+    }
 
     @DisplayName("노선에 구간을 추가한다 - 역사이에 새로운 역을 등록한다")
     @Test
