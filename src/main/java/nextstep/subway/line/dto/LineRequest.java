23e9b1c09021e91dbc0ea95a7509fa958d2974b9
kimsanggu <tkdrn8578@paymint.co.kr>
2021-06-16 23:42:18 +0900

    feat: 노선 생성 시 하행 및 상행역에 대한 요청 값 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -5,6 +5,9 @@ import nextstep.subway.line.domain.Line;
 public class LineRequest {
     private String name;
     private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Long distance;
 
     public LineRequest() {
     }
@@ -14,12 +17,32 @@ public class LineRequest {
         this.color = color;
     }
 
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, Long distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
     public String getName() {
         return name;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Long getDistance() {
+        return distance;
     }
 
     public Line toLine() {
