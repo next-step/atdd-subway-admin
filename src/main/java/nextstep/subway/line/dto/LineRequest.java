7c1d8cc14df022f4e19e5d568a55404c390d7397
tinyhhj <krwsim@gmail.com>
2021-06-17 02:32:44 +0900

    feat: 인수테스트 실습

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -6,12 +6,16 @@ public class LineRequest {
     private String name;
     private String color;
 
-    public LineRequest() {
+    private LineRequest() {
     }
 
-    public LineRequest(String name, String color) {
+    private LineRequest(String name, String color) {
         this.name = name;
         this.color = color;
+    }
+
+    public static LineRequest of(String name, String color) {
+        return new LineRequest(name,color);
     }
 
     public String getName() {
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,8 +1,16 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
@@ -11,14 +19,51 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine() {
         // when
         // 지하철_노선_생성_요청
+        String name = "1호선";
+        String color = "빨간색";
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(LineRequest.of(name, color))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all()
+                .extract();
 
         // then
         // 지하철_노선_생성됨
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.body().jsonPath().getString("name")).isEqualTo(name);
+        assertThat(response.body().jsonPath().getString("color")).isEqualTo(color);
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
     void createLine2() {
+        
+        // when
+        String name = "1호선";
+        String color = "빨강";
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .body(LineRequest.of(name,color))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract();
+        
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.body().jsonPath().getString("name")).isEqualTo(name);
+        assertThat(response.body().jsonPath().getString("color")).isEqualTo(color);
+
+        response = RestAssured
+                .given().log().all()
+                .body(LineRequest.of(name,color))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
         // given
         // 지하철_노선_등록되어_있음
 
