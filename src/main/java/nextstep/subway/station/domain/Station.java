340f4761ca04d381161fc6ff37cbaa519d4fd2b6
cw-seungkwon <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>
2021-06-17 00:22:01 +0900

    feat: sorting 을 위한 comparable 구현

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -1,14 +1,24 @@
 package nextstep.subway.station.domain;
 
+import java.util.Comparator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import nextstep.subway.common.BaseEntity;
 
-import javax.persistence.*;
-
 @Entity
-public class Station extends BaseEntity {
+public class Station extends BaseEntity implements Comparable<Station> {
+
+    private static final Comparator<Station> COMPARATOR = Comparator.comparingLong((station) -> station.getId());
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
 
@@ -25,5 +35,10 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public int compareTo(Station o) {
+        return COMPARATOR.compare(this, o);
     }
 }
