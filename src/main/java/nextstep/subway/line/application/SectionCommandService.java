ad869a67b568f0ac4e096d080ce247548d95c750
mskangg <funnycoding91@gmail.com>
2021-06-17 04:39:28 +0900

    feat: 구간 삭제 UseCase 추가

--- src/main/java/nextstep/subway/line/application/SectionCommandService.java
+++ src/main/java/nextstep/subway/line/application/SectionCommandService.java
@@ -2,6 +2,7 @@ package nextstep.subway.line.application;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.SectionRepository;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationQueryUseCase;
 import nextstep.subway.station.domain.Station;
@@ -13,10 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class SectionCommandService implements SectionCommandUseCase {
     private final LineQueryUseCase lineQueryUseCase;
     private final StationQueryUseCase stationQueryUseCase;
+    private final SectionRepository sectionRepository;
 
-    public SectionCommandService(LineQueryUseCase lineQueryUseCase, StationQueryUseCase stationQueryUseCase) {
+    public SectionCommandService(LineQueryUseCase lineQueryUseCase, StationQueryUseCase stationQueryUseCase, SectionRepository sectionRepository) {
         this.lineQueryUseCase = lineQueryUseCase;
         this.stationQueryUseCase = stationQueryUseCase;
+        this.sectionRepository = sectionRepository;
     }
 
     @Override
@@ -25,5 +28,13 @@ public class SectionCommandService implements SectionCommandUseCase {
         Station upStation = stationQueryUseCase.findById(sectionRequest.getUpStationId());
         Station downStation = stationQueryUseCase.findById(sectionRequest.getDownStationId());
         line.addSection(Section.of(upStation, downStation, sectionRequest.getDistance()));
+    }
+
+    @Override
+    public void removeSectionByStationId(Long lineId, Long stationId) {
+        Line line = lineQueryUseCase.findById(lineId);
+        Station station = stationQueryUseCase.findById(stationId);
+        Section section = line.removeSection(station);
+        sectionRepository.delete(section);
     }
 }
