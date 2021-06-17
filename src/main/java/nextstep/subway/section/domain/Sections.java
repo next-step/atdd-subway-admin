82caae93a0503343703e9e63dac39c7419e37101
fDevJc <fDevJc@gmail.com>
2021-06-18 02:31:35 +0900

    refactor: 구간 추가 코드 리팩토링

--- src/main/java/nextstep/subway/section/domain/Sections.java
+++ src/main/java/nextstep/subway/section/domain/Sections.java
@@ -5,74 +5,54 @@ import nextstep.subway.station.domain.Station;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Embeddable
 public class Sections {
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL)
-    private List<Section> sections = new LinkedList<>();
+    private List<Section> sections = new ArrayList<>();
 
-    //TODO (3단계) 구간 추가시 순서대로 넣기
     public void add(Section newSection) {
-        boolean isAdded = false;
-        isAdded = addNewUpStation(newSection);
+        if (sections.size() == 0) {
+            sections.add(newSection);
+            return;
+        }
+        validateExistUpAndDownStation(newSection);
+        validateIncludeSameSection(newSection);
+        addSectionInMiddleWhenEqualToUpStation(newSection);
+        sections.add(newSection);
+    }
 
-        addEqualUpStation(newSection);
-        addEqualDownStation(newSection);
-
-        if (!isAdded) {
-            this.sections.add(newSection);
+    private void validateExistUpAndDownStation(Section newSection) {
+        Set<Station> stationSet = new HashSet<>();
+        for (Section section : sections) {
+            stationSet.add(section.getUpStation());
+            stationSet.add(section.getDownStation());
+        }
+        if (!(stationSet.contains(newSection.getUpStation()) || stationSet.contains(newSection.getDownStation()))) {
+            throw new IllegalArgumentException("상행선 하행선 모두 등록되어있지 않습니다.");
         }
     }
 
-    private boolean addNewUpStation(Section newSection) {
-        if (sections.size() != 0 && getFirstStation().equals(newSection.getDownStation())) {
-            sections.add(0, newSection);
-            return true;
+    private void validateIncludeSameSection(Section newSection) {
+        if (sections.contains(newSection)) {
+            throw new IllegalArgumentException("동일한 구간은 등록할 수 없습니다.");
         }
-        return false;
     }
 
-    private void addEqualDownStation(Section newSection) {
+    private void addSectionInMiddleWhenEqualToUpStation(Section newSection) {
         sections.stream()
-                .filter(section -> section.getDownStation().equals(newSection.getDownStation()))
-                .findFirst()
-                .ifPresent(section -> section.changeDownStation(newSection.getUpStation()));
-    }
-
-    private void addEqualUpStation(Section newSection) {
-        sections.stream()
-                .filter(section -> section.getUpStation().equals(newSection.getUpStation()))
+                .filter(section -> section.equalsUpStation(newSection))
                 .findFirst()
                 .ifPresent(section -> {
-                    Station tmpStation = section.getDownStation();
-                    section.changeDownStation(newSection.getDownStation());
-                    newSection.changeUpStation(section.getDownStation());
-                    newSection.changeDownStation(tmpStation);
+                    section.validateSectionDistance(newSection);
+                    section.changeUpStation(newSection.getDownStation());
+                    section.calculateDistance(newSection);
                 });
-        //this.sections.add(newSection);
-    }
-
-    private void addEqualUpStation_ref(Section newSection) {
-        //상행역에 동일 역이 존재하는경우
-        for (Section section : sections) {
-            if (section.getUpStation().equals(newSection.getUpStation())) {
-                if (newSection.getDistance() >= section.getDistance()) {
-                    throw new IllegalArgumentException();
-                } else {
-                    Station tmpStation = section.getDownStation();
-                    section.changeDownStation(newSection.getDownStation());
-                    newSection.changeUpStation(section.getDownStation());
-                    newSection.changeDownStation(tmpStation);
-                }
-            }
-        }
-        this.sections.add(newSection);
     }
 
     public List<Station> getSortedStations() {
+        Collections.sort(sections);
         List<Station> stationList = new ArrayList<>();
         stationList.add(getFirstStation());
         for (Section section : sections) {
@@ -83,43 +63,5 @@ public class Sections {
 
     private Station getFirstStation() {
         return sections.get(0).getUpStation();
-    }
-
-    public void add_refactor(Section newSection) {
-        제일처음_구간_추가(newSection);
-        새로운_역을_상행_종점으로_등록할_경우(newSection);
-        새로운_역을_하행_종점으로_등록할_경우(newSection);
-        역사이에_새로운_역을_등록할_경우(newSection);
-    }
-
-    private void 역사이에_새로운_역을_등록할_경우(Section newSection) {
-        sections.stream()
-                .filter(section -> section.getUpStation().equals(newSection.getUpStation()))
-                .findFirst()
-                .ifPresent(section -> {
-                    Station tmpStation = section.getDownStation();
-                    section.changeDownStation(newSection.getDownStation());
-                    newSection.changeUpStation(newSection.getDownStation());
-                    newSection.changeDownStation(tmpStation);
-                    this.sections.add(newSection);
-                });
-    }
-
-    private void 새로운_역을_하행_종점으로_등록할_경우(Section newSection) {
-        if (sections.get(sections.size() - 1).getDownStation().equals(newSection.getUpStation())) {
-            this.sections.add(newSection);
-        }
-    }
-
-    private void 새로운_역을_상행_종점으로_등록할_경우(Section newSection) {
-        if (sections.get(0).getUpStation().equals(newSection.getDownStation())) {
-            this.sections.add(0, newSection);
-        }
-    }
-
-    private void 제일처음_구간_추가(Section newSection) {
-        if (sections.size() == 0) {
-            this.sections.add(newSection);
-        }
     }
 }
