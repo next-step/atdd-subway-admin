d1c9269e0fe214efad1676e6941606877e86bd46
fDevJc <fDevJc@gmail.com>
2021-06-18 02:30:44 +0900

    test: 구간관련 테스트 추가

--- src/test/java/nextstep/subway/section/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/section/domain/SectionsTest.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SectionsTest {
     private Sections sections;
@@ -19,6 +18,7 @@ class SectionsTest {
     private Station station2;
     private Station station3;
     private Station station4;
+    private Section section1;
 
     @BeforeEach
     void setUp() {
@@ -28,34 +28,47 @@ class SectionsTest {
         station2 = new Station("2번역");
         station3 = new Station("3번역");
         station4 = new Station("4번역");
-        sections.add_refactor(Section.of(station1, station3, 7));
+        section1 = Section.of(station1, station3, 7);
+        sections.add(section1);
     }
 
     @Test
     void 역사이에_새로운_역을_등록할_경우() {
-        Section section = Section.of(station1, station2, 4);
-        sections.add_refactor(section);
+        Section section2 = Section.of(station1, station2, 4);
+        sections.add(section2);
         추가된_역순서와_비교(Arrays.asList(station1, station2, station3));
+        assertThat(section1.getDistance()).isEqualTo(3);
+        assertThat(section2.getDistance()).isEqualTo(4);
+
     }
 
     @Test
     void 새로운_역을_상행_종점으로_등록할_경우() {
-        Section section = Section.of(station0, station1, 4);
-        sections.add_refactor(section);
+        sections.add(Section.of(station0, station1, 4));
         추가된_역순서와_비교(Arrays.asList(station0, station1, station3));
     }
 
     @Test
     void 새로운_역을_하행_종점으로_등록할_경우() {
-        Section section = Section.of(station3, station4, 4);
-        sections.add_refactor(section);
+        sections.add(Section.of(station3, station4, 4));
         추가된_역순서와_비교(Arrays.asList(station1, station3, station4));
     }
 
     @Test
     void 역_사이에_새로운_역을_등록할_경우_기존_역_사이_길이보다_크거나_같으면_등록을_할_수_없음() {
-        Section section = Section.of(station1, station2, 7);
-        assertThatThrownBy(() -> sections.add_refactor(section))
+        assertThatThrownBy(() -> sections.add(Section.of(station1, station2, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상행역과_하행역이_이미_노선에_모두_등록되어_있다면_추가할_수_없음() {
+        assertThatThrownBy(() -> sections.add(Section.of(station1, station3, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상행역과_하행역_둘_중_하나도_포함되어있지_않으면_추가할_수_없음() {
+        assertThatThrownBy(() -> sections.add(Section.of(station2, station4, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
