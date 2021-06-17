db9695c46675af86348d17fcbe46c387495bb724
Jun <p_illusion@naver.com>
2021-06-17 17:14:20 +0900

    test: step1 라인 추가/조회/수정/삭제 기능 구현

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,5 +1,8 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
@@ -19,5 +22,31 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    public List<LineResponse> getLines() {
+        return lineRepository.findAll()
+            .stream().map(LineResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    public LineResponse getLine(Long id) {
+        Line findLine = lineRepository.findById(id).orElseThrow(NoSuchElementException::new);
+
+        return LineResponse.of(findLine);
+    }
+
+    public LineResponse updateLine(Long id, LineRequest lineRequest) {
+        Line findLine = lineRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        findLine.update(
+            new Line(lineRequest.getName(),
+                lineRequest.getColor())
+        );
+
+        return LineResponse.of(findLine);
+    }
+
+    public void deleteLine(Long id) {
+        lineRepository.deleteById(id);
     }
 }
--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.domain;
 
+import java.util.Objects;
 import nextstep.subway.common.BaseEntity;
 
 import javax.persistence.*;
@@ -36,5 +37,29 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(id, line.id) &&
+            Objects.equals(name, line.name) &&
+            Objects.equals(color, line.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", color='" + color + '\'' +
+            '}';
     }
 }
