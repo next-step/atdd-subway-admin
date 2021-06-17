9879c2c39d74c29cd1cc50ef0f355a9e799b577c
wrallee <wrallee@naver.com>
2021-06-17 20:24:29 +0900

    refactor(section): 사용하지 않는 임포트 제거

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -5,7 +5,6 @@ import static nextstep.subway.line.domain.SectionConverter.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
