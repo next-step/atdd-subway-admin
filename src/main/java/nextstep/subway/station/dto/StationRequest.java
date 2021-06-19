6121934c3d9f1a1060809cd434c359a8d56cb216
sanggu <tkdrn8578@naver.com>
2021-06-19 13:54:20 +0900

    feat: Controller에서 RequestDto mapping 시 ObjectMapper에 필요한 빈 생성자 추가

--- src/main/java/nextstep/subway/station/dto/StationRequest.java
+++ src/main/java/nextstep/subway/station/dto/StationRequest.java
@@ -5,6 +5,8 @@ import nextstep.subway.station.domain.Station;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {
+    }
 
     public StationRequest(String name) {
         this.name = name;
