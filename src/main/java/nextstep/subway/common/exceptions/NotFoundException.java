c724221e6767d8fa8cf44d1ff7c113f0372763f8
tinyhhj <krwsim@gmail.com>
2021-06-20 03:28:46 +0900

    feat: 노석 삭제 추가

---
+++ src/main/java/nextstep/subway/common/exceptions/NotFoundException.java
@@ -0,0 +1,12 @@
+package nextstep.subway.common.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
