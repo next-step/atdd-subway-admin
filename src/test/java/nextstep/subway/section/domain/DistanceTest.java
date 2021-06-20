1b6146cfebd10fcfc8be6413178fc3969ab07443
lkimilhol <lkimilhol@gmail.com>
2021-06-21 00:37:37 +0900

    test(DistanceTest): 거리 구하는 Distance 로직 테스트

--- src/test/java/nextstep/subway/section/domain/DistanceTest.java
+++ src/test/java/nextstep/subway/section/domain/DistanceTest.java
@@ -1,7 +1,9 @@
 package nextstep.subway.section.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import nextstep.subway.common.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,39 @@ class DistanceTest {
         Distance distance = new Distance(7);
         //then
         assertThat(distance).isNotNull();
+    }
+
+    @DisplayName("거리 빼기")
+    @Test
+    void sub() {
+        //given
+        Distance distance = new Distance(7);
+        //when
+        Distance add = distance.sub(new Distance(4));
+        //then
+        assertThat(add.equals(new Distance(3))).isTrue();
+    }
+
+    @DisplayName("거리 더하기")
+    @Test
+    void add() {
+        //given
+        Distance distance = new Distance(7);
+        //when
+        Distance add = distance.add(new Distance(3));
+        //then
+        assertThat(add.equals(new Distance(10))).isTrue();
+    }
+
+    @DisplayName("음수 에러")
+    @Test
+    void createFailed() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            new Distance(-1);
+        }).isInstanceOf(RuntimeException.class)
+                .hasMessage(ErrorMessage.DISTANCE_TOO_LONG);
     }
 }
