4348506864549b887608ccd5e51f2bec7c289793
dohchoi91 <dohchoi91@gmail.com>
2021-06-22 23:20:05 +0900

    feat: 구간 제거 기능 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -11,11 +12,15 @@ import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 
 import nextstep.subway.exception.CanNotAddSectionException;
+import nextstep.subway.exception.CanNotDeleteStateException;
+import nextstep.subway.exception.NotFoundException;
 import nextstep.subway.exception.RegisteredSectionException;
 import nextstep.subway.station.domain.Station;
 
 @Embeddable
 public class Sections {
+
+    public static final int MIN_SIZE = 1;
 
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Section> sections = new ArrayList<>();
@@ -64,5 +69,38 @@ public class Sections {
 
     public boolean contains(Station station) {
         return sections.stream().anyMatch(s -> s.containStation(station));
+    }
+
+    public void delete(Station station) {
+        if (canNotDelete()) {
+            throw new CanNotDeleteStateException();
+        }
+        if (!contains(station)) {
+            throw new NotFoundException();
+        }
+
+        Section upSection = sections.stream().filter(s -> s.getDownStation() == station)
+                                             .findFirst().map(this::remove).orElse(null);
+        Section downSection = sections.stream().filter(s -> s.getUpStation() == station)
+                                               .findFirst().map(this::remove).orElse(null);
+
+        if (existSections(upSection, downSection)) {
+            Section section = new Section(upSection.getUpStation(), downSection.getDownStation(),
+                upSection.getDistance() + downSection.getDistance());
+            sections.add(section);
+        }
+    }
+
+    private boolean existSections(Section ... sections) {
+        return Arrays.stream(sections).noneMatch(Objects::isNull);
+    }
+
+    private Section remove(Section section) {
+        sections.remove(section);
+        return section;
+    }
+
+    private boolean canNotDelete() {
+        return sections.size() == MIN_SIZE;
     }
 }
