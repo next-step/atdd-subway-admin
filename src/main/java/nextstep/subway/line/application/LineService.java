409ea1194af749f956521b1c791f5f9837bf6b4b
KyungSik9870 <rudtlr9870@gmail.com>
2021-06-23 04:29:20 +0900

    feat: 지하철 노선 목록 조회

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,5 +1,8 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
@@ -21,6 +24,12 @@ public class LineService {
 		checkDuplicated(request);
 		Line persistLine = lineRepository.save(request.toLine());
 		return LineResponse.of(persistLine);
+	}
+
+	public List<LineResponse> findAllLines() {
+		List<Line> lines = lineRepository.findAll();
+
+		return lines.stream().map(LineResponse::of).collect(Collectors.toList());
 	}
 
 	private void checkDuplicated(LineRequest request) {
