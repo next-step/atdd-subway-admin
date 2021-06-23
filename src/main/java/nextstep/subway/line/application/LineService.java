7348c8dd061aac7c9a9c95d68e0202c6c2c169c7
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-24 06:12:52 +0900

    refactor: 서비스 내 리포지토리 참조 변수에 final 키워드 붙이기

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -16,8 +16,8 @@ import java.util.Optional;
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
-    private StationRepository stationRepository;
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
     public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
