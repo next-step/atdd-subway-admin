3bf27cc8ea453a01957c564ccf87bc26a4ff2307
백승열 <beck33333@naver.com>
2021-06-24 00:21:27 +0900

    test: Sections 인수 테스트 추가

---
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -0,0 +1,167 @@
+package nextstep.subway.section;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.LineAcceptanceTest;
+import nextstep.subway.line.domain.LineRepository;
+import nextstep.subway.section.dto.SectionRequest;
+import nextstep.subway.section.dto.SectionResponse;
+import nextstep.subway.station.StationAcceptanceTest;
+import nextstep.subway.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SectionAcceptanceTest extends AcceptanceTest {
+
+    @Autowired
+    LineRepository lineRepository;
+
+    private static int LINE_DISTANCE = 10;
+    private StationResponse 문래역;
+    private StationResponse 잠실역;
+    private StationResponse 사당역;
+    private String lineURL;
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        문래역 = StationAcceptanceTest.지하철역_생성_요청("문래역").as(StationResponse.class);
+        잠실역 = StationAcceptanceTest.지하철역_생성_요청("잠실역").as(StationResponse.class);
+        사당역 = StationAcceptanceTest.지하철역_생성_요청("사당역").as(StationResponse.class);
+        lineURL = LineAcceptanceTest.지하철_노선_등록되어_있음("2호선", "green", 문래역.getId(), 잠실역.getId(), LINE_DISTANCE);
+
+    }
+
+
+    @DisplayName("역 사이에 새로운 역을 등록할 경우")
+    @Test
+    void addSectionInMiddleBased() {
+
+        int newSectionDistance = LINE_DISTANCE - 1;
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(lineURL, 문래역.getId(), 사당역.getId(), newSectionDistance);
+
+
+        List<Long> expectedStations = Arrays.asList(문래역.getId(), 사당역.getId(), 잠실역.getId());
+        List<Integer> expectedDistances = Arrays.asList(0, newSectionDistance, LINE_DISTANCE - newSectionDistance);
+
+        지하철_노선에_지하철역_등록됨(response);
+        지하철_노선에_등록한_구간_포함됨(response, expectedStations);
+        지하철_노선_구간_거리_계산됨(response, expectedDistances);
+    }
+
+    @DisplayName("새로운 역을 상행 종점으로 등록할 경우")
+    @Test
+    void addSectionAtFirstBased() {
+
+        int newSectionDistance = LINE_DISTANCE - 1;
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(lineURL, 사당역.getId(), 문래역.getId(), newSectionDistance);
+
+        List<Long> expectedStations = Arrays.asList(사당역.getId(), 문래역.getId(), 잠실역.getId());
+        List<Integer> expectedDistances = Arrays.asList(0, newSectionDistance, LINE_DISTANCE);
+
+
+        지하철_노선에_지하철역_등록됨(response);
+        지하철_노선에_등록한_구간_포함됨(response, expectedStations);
+        지하철_노선_구간_거리_계산됨(response, expectedDistances);
+    }
+
+    @DisplayName("새로운 역을 하행 종점으로 등록할 경우")
+    @Test
+    void addSectionAtLastBased() {
+        int newSectionDistance = LINE_DISTANCE - 1;
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(lineURL, 잠실역.getId(), 사당역.getId(), newSectionDistance);
+
+        List<Long> expectedStations = Arrays.asList(문래역.getId(), 잠실역.getId(), 사당역.getId());
+        List<Integer> expectedDistances = Arrays.asList(0, LINE_DISTANCE, newSectionDistance);
+
+
+        지하철_노선에_지하철역_등록됨(response);
+        지하철_노선에_등록한_구간_포함됨(response, expectedStations);
+        지하철_노선_구간_거리_계산됨(response, expectedDistances);
+
+    }
+
+    @DisplayName("노선에 구간의 길이보다 긴 거리로 등록하면 등록할 수 없는 테스트")
+    @Test
+    void addSectionGreaterDistance() {
+
+        int newSectionDistance = LINE_DISTANCE + 1;
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(lineURL, 문래역.getId(), 사당역.getId(), newSectionDistance);
+
+        지하철_노선에_유효하지않은_구간정보(response);
+    }
+
+
+    @DisplayName("노선에 이미 등록된 역들로 구간을 등록할 수 없는 테스트")
+    @Test
+    void addAlreadyExistStations() {
+
+        int newSectionDistance = LINE_DISTANCE + 1;
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(lineURL, 문래역.getId(), 잠실역.getId(), newSectionDistance);
+
+        지하철_노선에_유효하지않은_구간정보(response);
+    }
+
+    @DisplayName("노선에 등록된 역이 하나도 없는 구간은 등록할 수 없는 테스트")
+    @Test
+    void addNoExistStations() {
+        int newSectionDistance = LINE_DISTANCE - 1;
+        StationResponse 신사역 = StationAcceptanceTest.지하철역_생성_요청("신사역").as(StationResponse.class);
+        StationResponse 용산역 = StationAcceptanceTest.지하철역_생성_요청("용산역").as(StationResponse.class);
+
+        ExtractableResponse<Response> response =
+                지하철_노선에_지하철역_등록_요청(lineURL, 신사역.getId(), 용산역.getId(), newSectionDistance);
+
+        지하철_노선에_유효하지않은_구간정보(response);
+    }
+
+    private void 지하철_노선에_유효하지않은_구간정보(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private void 지하철_노선_구간_거리_계산됨(ExtractableResponse<Response> response, List<Integer> expectedDistances) {
+        List<Integer> resultStationIds = response.jsonPath().getList("stations", SectionResponse.class).stream()
+                .map(SectionResponse::getDistance)
+                .collect(Collectors.toList());
+        assertThat(resultStationIds).isEqualTo(expectedDistances);
+    }
+
+    private void 지하철_노선에_등록한_구간_포함됨(ExtractableResponse<Response> response, List<Long> expectedStationIDds) {
+        List<Long> resultStationIds = response.jsonPath().getList("stations", SectionResponse.class).stream()
+                .map(SectionResponse::getId)
+                .collect(Collectors.toList());
+        assertThat(resultStationIds).isEqualTo(expectedStationIDds);
+    }
+
+    private void 지하철_노선에_지하철역_등록됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선에_지하철역_등록_요청(String lineUri, Long upStationId, Long downStationId, int distance) {
+        SectionRequest request = createSectionRequest(upStationId, downStationId, distance);
+
+        return RestAssured.given().log().all()
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post(lineUri + "/sections")
+                .then().log().all().extract();
+    }
+
+    private static SectionRequest createSectionRequest(Long upStationId, Long downStationId, int distance) {
+        return new SectionRequest(upStationId, downStationId, distance);
+    }
+
+}
