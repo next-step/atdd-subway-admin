4e334ed87ed634643ff832b06c3d95aadba38344
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-25 00:27:02 +0900

    feat: 구간 요청 DTO 구현

---
+++ src/main/java/nextstep/subway/section/dto/SectionRequest.java
@@ -0,0 +1,41 @@
+package nextstep.subway.section.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class SectionRequest {
+
+    @NotBlank
+    private Long lineId;
+
+    @NotBlank
+    private Long upStationId;
+
+    @NotBlank
+    private Long downStationId;
+
+    @NotBlank
+    private int distance;
+
+    public SectionRequest(Long lineId, Long upStationId, Long downStationId, int distance) {
+        this.lineId = lineId;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
