eb2591212757a4d267892bb5807d15974606ca78
kimsanggu <tkdrn8578@paymint.co.kr>
2021-06-24 18:25:12 +0900

    feat: line에 구간 추가하는 서비스 로직 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,7 @@ import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.LinesResponse;
 import nextstep.subway.section.domain.Section;
+import nextstep.subway.section.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 import org.springframework.stereotype.Service;
@@ -35,6 +36,7 @@ public class LineService {
         return LineResponse.of(lineRepository.save(request.toLine(section)));
     }
 
+    // 기존에 있는 Section에 새로운 Section을 추가한다.
     public LinesResponse findAll() {
         List<Line> lines = lineRepository.findAll();
         return LinesResponse.of(lines);
@@ -59,4 +61,44 @@ public class LineService {
     public void deleteById(Long id) {
         lineRepository.deleteById(id);
     }
+
+    public LineResponse addSections(long lineId, SectionRequest request) {
+        // line 을 조회한다.
+        // 조회한 line에 구간을 가져와 상 또는 하 중 있는 곳에 연결한다.
+        // 상이나 하 중 일치하는 곳이 하나 존재한ㄷ.ㅏ
+        // 만약 둘 다 없는 경우 예외처리 한다.
+        // 만약 둘 다 존재하는 경우 예외처리 한다.
+        Line line = findByIdOrThrow(lineId);
+
+
+//        Station upStation = stationService.findById(request.getUpStationId());
+//        Station downStation = stationService.findById(request.getDownStationId());
+
+        // line에 존재하는 Section을 가져온다.
+
+        List<Section> sections = line.getSections();
+
+        for (int i = 0; i < sections.size(); i++) {
+            // 요청된 것 중에 statinos 가 일치하지 않는 것이 있는지 확인한다
+            Section section = sections.get(i);
+            long downStationId = request.getDownStationId();
+            long upStationId = request.getUpStationId();
+
+            if (section.getDownStation().getId() == downStationId && section.getUpStation().getId() == upStationId) {
+                throw new IllegalArgumentException();
+            }
+            // sections가
+
+            if (section.getDownStation().getId() == downStationId) {
+                // 상행을 요청한 것으로 변경한다.
+                // 역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음
+                if ()
+            }
+
+            if (section.getUpStation().getId() == upStationId) {
+
+            }
+        }
+    }
+
 }
