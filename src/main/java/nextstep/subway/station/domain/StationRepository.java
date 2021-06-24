d9010ab75c903f9223bf4aa9164d2e8914a52be3
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-25 00:39:51 +0900

    fix: import가 되어 있지 않던 예외 임포트

--- src/main/java/nextstep/subway/station/domain/StationRepository.java
+++ src/main/java/nextstep/subway/station/domain/StationRepository.java
@@ -1,5 +1,6 @@
 package nextstep.subway.station.domain;
 
+import nextstep.subway.station.exception.StationNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
