4e0bd8b508f849f99afb30e1b507b2e82cfe3f3a
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-24 22:51:45 +0900

    refactor: HttpStatus 를 import static으로 변경, @ResponseBody를 클래스에 붙이기

--- src/main/java/nextstep/subway/line/ui/LineControllerAdvice.java
+++ src/main/java/nextstep/subway/line/ui/LineControllerAdvice.java
@@ -2,9 +2,8 @@ package nextstep.subway.line.ui;
 
 import nextstep.subway.line.dto.LineExceptionResponse;
 import nextstep.subway.line.exception.LineNotFoundException;
-import nextstep.subway.station.domain.StationNotFoundException;
+import nextstep.subway.station.exception.StationNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,25 +12,28 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ResponseBody
 @ControllerAdvice(basePackages = "nextstep.subway.line.ui")
 public class LineControllerAdvice {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
+    @ResponseStatus(BAD_REQUEST)
     public LineExceptionResponse dataIntegrityViolationException(HttpServletRequest request, DataIntegrityViolationException e) {
         e.printStackTrace();
+
         String exceptionMessage = e.getMessage();
         LineExceptionResponse response = null;
         if (exceptionMessage != null && exceptionMessage.contains("Unique index or primary key violation")) {
-            response = new LineExceptionResponse("요청하신 이름이 이미 존재하여 처리할 수 없습니다.");
+            response = new LineExceptionResponse("요청 하신 이름이 이미 존재 하여 처리할 수 없습니다.");
         }
         return response;
     }
 
     @ExceptionHandler({LineNotFoundException.class, StationNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
+    @ResponseStatus(NOT_FOUND)
     public void entityNotFoundException(EntityNotFoundException e) {
         e.printStackTrace();
     }
