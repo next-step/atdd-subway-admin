638044ca4529cf4604c4008d1c9d576ab16f34fc
kimsanggu <tkdrn8578@paymint.co.kr>
2021-06-25 18:06:00 +0900

    refactor: 변수명을 가독성 높이도록 수정

--- src/main/java/nextstep/subway/exception/DuplicateSectionException.java
+++ src/main/java/nextstep/subway/exception/DuplicateSectionException.java
@@ -2,9 +2,9 @@ package nextstep.subway.exception;
 
 public class DuplicateSectionException extends RuntimeException {
 
-    private static final String DUPLICATE_SECTION_EXCEPTION = "중복되는 구간은 추가할 수 없습니다.";
+    private static final String DUPLICATE_SECTION_EXCEPTION_MESSAGE = "중복되는 구간은 추가할 수 없습니다.";
 
     public DuplicateSectionException() {
-        super(DUPLICATE_SECTION_EXCEPTION);
+        super(DUPLICATE_SECTION_EXCEPTION_MESSAGE);
     }
 }
