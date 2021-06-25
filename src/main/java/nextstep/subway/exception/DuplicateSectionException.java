bba9760f034ff9fbec2c5aee9a437df49126cca4
kimsanggu <tkdrn8578@paymint.co.kr>
2021-06-25 17:07:09 +0900

    feat: 구간이 중복되는 경우에 대한 custom 예외 추가

---
+++ src/main/java/nextstep/subway/exception/DuplicateSectionException.java
@@ -0,0 +1,10 @@
+package nextstep.subway.exception;
+
+public class DuplicateSectionException extends RuntimeException {
+
+    private static final String DUPLICATE_SECTION_EXCEPTION = "중복되는 구간은 추가할 수 없습니다.";
+
+    public DuplicateSectionException() {
+        super(DUPLICATE_SECTION_EXCEPTION);
+    }
+}
