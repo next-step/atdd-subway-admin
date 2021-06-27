24b5e6b9a74649483ba4c50ff19313bd645b2c7f
junhee-ko <junheee.ko@gmail.com>
2021-06-28 00:39:33 +0900

    feat: add stations field in LineResponse class

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,13 +1,18 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<Station> stations = new ArrayList<>();
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
@@ -22,8 +27,18 @@ public class LineResponse {
         this.modifiedDate = modifiedDate;
     }
 
+    public LineResponse(Long id, String name, String color, Section section, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.stations.add(section.getUpStation());
+        this.stations.add(section.getDownStation());
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getSections().get(0), line.getCreatedDate(), line.getModifiedDate());
     }
 
     public Long getId() {
@@ -36,6 +51,10 @@ public class LineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<Station> getStations() {
+        return stations;
     }
 
     public LocalDateTime getCreatedDate() {
