7a9756d3996e11a71e6da51d09a6b086f84dc9d3
sanggu <tkdrn8578@naver.com>
2021-06-27 23:53:22 +0900

    test: extractStationsResponse에 대한 테스트 코드 추가

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -2,6 +2,7 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.section.domain.Section;
 import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationsResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,10 +15,8 @@ class LineTest {
     void addSections_test() {
         //given
         Line line = new Line("신붇당선", "red");
-
         Station 판교역 = new Station("판교역");
         Station 이매역 = new Station("이매역");
-
         Section 구간 = new Section(판교역, 이매역, 10);
 
         //when
@@ -27,5 +26,22 @@ class LineTest {
         assertThat(addedSectionLine.getSections()).containsExactly(
                 구간
         );
+    }
+
+    @Test
+    @DisplayName("노선에 존재하는 역들을 추출한다.")
+    void extractStationsResponse_test() {
+        //given
+        Line line = new Line("신붇당선", "red");
+        Station 판교역 = new Station("판교역");
+        Station 이매역 = new Station("이매역");
+        Section 구간 = new Section(판교역, 이매역, 10);
+        Line addedSectionLine = line.addSection(구간);
+
+        //when
+        StationsResponse stationsResponse = addedSectionLine.extractStationsResponse();
+
+        //then
+        assertThat(stationsResponse.getStations()).hasSize(2);
     }
 }
