03b5e858e034f5cc22969a7aa55eb997012ac3fa
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-30 05:50:47 +0900

    feat: 거리 도메인에 equals, hashcode 구현

--- src/main/java/nextstep/subway/section/domain/Distance.java
+++ src/main/java/nextstep/subway/section/domain/Distance.java
@@ -4,6 +4,7 @@ import nextstep.subway.section.exception.BelowZeroDistanceException;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Distance {
@@ -35,5 +36,18 @@ public class Distance {
         if(this.value - distance.value <= 0) {
             throw new BelowZeroDistanceException();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance = (Distance) o;
+        return value == distance.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
