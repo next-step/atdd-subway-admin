a3491f4c40b0fbefa2ed9c1fbd315269b52dd27b
jordy-torvalds <jordy-torvalds@kakao.com>
2021-06-30 06:27:24 +0900

    feat: Stations의 equals문 구현

--- src/main/java/nextstep/subway/station/domain/Stations.java
+++ src/main/java/nextstep/subway/station/domain/Stations.java
@@ -2,6 +2,7 @@ package nextstep.subway.station.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Stations {
     private List<Station> values = new ArrayList<>();
@@ -31,5 +32,18 @@ public class Stations {
 
     public void addAll(Stations stations) {
         this.values.addAll(stations.values);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stations stations = (Stations) o;
+        return Objects.equals(values, stations.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(values);
     }
 }
