85cc333570e0188568eec1334254d51dc1f3e47e
chajs226 <chajs226@gmail.com>
2021-07-01 07:10:07 +0900

    refactor: 주석제거

--- src/main/java/nextstep/subway/section/domain/Section.java
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -48,7 +48,6 @@ public class Section extends BaseEntity {
 
     public void toLine(Line line) {
         this.line = line;
-        //line.getSections().initSections(this);
     }
 
     public void addLine(Line line) {
