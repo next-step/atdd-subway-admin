4ce7b7a7ca6fc046b9f6f55056222c84ffa82a21
ascii92der <ascii92der@gmail.com>
2021-06-30 20:47:13 +0900

    test: Add Line Controller Create Test

---
+++ src/test/java/nextstep/subway/line/ui/LineControllerTest.java
@@ -0,0 +1,55 @@
+package nextstep.subway.line.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
+class LineControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private LineService lineService;
+
+    @Test
+    void createLine() throws Exception {
+
+        // given
+        LineRequest lineRequest = new LineRequest("2호선", "green");
+        LineResponse lineResponse = LineResponse.of(new Line("2호선", "green"));
+
+        // when
+        when(lineService.saveLine(any(LineRequest.class)))
+                .thenReturn(lineResponse);
+
+        // then
+        mockMvc.perform(
+                post("/lines")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(lineRequest))
+        ).andExpect(status().isCreated());
+    }
+
+}
