ac9921f57e0b7cf91e865d03b6162b3bb7d619f3
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-02 17:24:24 +0900

    refactor: 불필요한 테스트 목적의 코드 삭

--- src/test/java/nextstep/subway/SubwayApplicationTests.java
+++ src/test/java/nextstep/subway/SubwayApplicationTests.java
@@ -13,31 +13,4 @@ class SubwayApplicationTests {
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void test() {
-        List<Sample> list = asList(new Sample(5, 4));
-        int target = 5;
-        list.sort((left, right) -> left.b == target ? -1 : 1);
-        System.out.println(list);
-
-    }
-}
-
-class Sample {
-    int a;
-    int b;
-
-    public Sample(int a, int b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    @Override
-    public String toString() {
-        return "Sample{" +
-                "a=" + a +
-                ", b=" + b +
-                '}';
-    }
 }
