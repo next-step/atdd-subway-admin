f3029518ab2f2ec3a7a59a14da9b7b101382f16a
junhee-ko <junheee.ko@gmail.com>
2021-07-04 22:52:50 +0900

    feat: add Section save logic in Section class

--- src/main/java/nextstep/subway/line/domain/Section.java
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -24,12 +24,13 @@ public class Section extends BaseEntity {
     @JoinColumn(name = "down_station_id")
     private Station downStation;
 
-    private Integer distance;
+    @Embedded
+    private SectionDistance distance;
 
     public Section() {
     }
 
-    public Section(Station upStation, Station downStation, int distance) {
+    public Section(Station upStation, Station downStation, SectionDistance distance) {
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
@@ -43,11 +44,39 @@ public class Section extends BaseEntity {
         return downStation;
     }
 
-    public Integer getDistance() {
+    public SectionDistance getDistance() {
         return distance;
     }
 
     public void add(Line line) {
         this.line = line;
+    }
+
+    public boolean isBetweenAndUpStationSameWith(Section oldSection) {
+        return upStation.getId().equals(oldSection.getUpStation().getId()) &&
+            !downStation.getId().equals(oldSection.getDownStation().getId());
+    }
+
+    public boolean isBetweenAndDownStationSameWith(Section oldSection) {
+        return downStation.getId().equals(oldSection.getDownStation().getId()) &&
+            !upStation.getId().equals(oldSection.getUpStation().getId());
+    }
+
+    public boolean isAtTheEndWith(Section oldSection) {
+        return downStation.getId().equals(oldSection.getUpStation().getId()) ||
+            upStation.getId().equals(oldSection.getDownStation().getId());
+    }
+
+    public void updateBy(Station newDownStation, SectionDistance newDistance) {
+        updateDownStationTo(newDownStation);
+        updateDistanceTo(newDistance);
+    }
+
+    private void updateDownStationTo(Station newDownStation) {
+        this.downStation = newDownStation;
+    }
+
+    private void updateDistanceTo(SectionDistance newDistance) {
+        this.distance = newDistance;
     }
 }
