4b5133f2457204983bc30c98db630027a675e9af
junhee-ko <junheee.ko@gmail.com>
2021-07-04 22:52:57 +0900

    feat: add Section save logic in Sections class

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.Stations;
 
 import javax.persistence.CascadeType;
@@ -18,7 +19,42 @@ public class Sections {
     }
 
     public void add(Section section) {
-        this.sections.add(section);
+        if (sections.isEmpty()) {
+            this.sections.add(section);
+            return;
+        }
+
+        addNewSectionToOldSection(section);
+    }
+
+    public void addNewSectionToOldSection(Section newSection) {
+        Station newUpStation = newSection.getUpStation();
+        Station newDownStation = newSection.getDownStation();
+        SectionDistance newDistance = newSection.getDistance();
+
+        for (Section oldSection : sections) {
+            Station oldDownStation = oldSection.getDownStation();
+            SectionDistance oldDistance = oldSection.getDistance();
+
+            if (newSection.isBetweenAndUpStationSameWith(oldSection)) {
+                oldDistance.checkDistanceWith(newDistance);
+                oldSection.updateBy(newDownStation, newDistance);
+                this.sections.add(new Section(newDownStation, oldDownStation, oldDistance.minus(newDistance)));
+                return;
+            }
+
+            if (newSection.isBetweenAndDownStationSameWith(oldSection)) {
+                oldDistance.checkDistanceWith(newDistance);
+                oldSection.updateBy(newUpStation, oldDistance.minus(newDistance));
+                this.sections.add(new Section(newUpStation, newDownStation, newDistance));
+                return;
+            }
+
+            if (newSection.isAtTheEndWith(oldSection)) {
+                this.sections.add(new Section(newUpStation, newDownStation, newDistance));
+                return;
+            }
+        }
     }
 
     public List<Section> getSections() {
