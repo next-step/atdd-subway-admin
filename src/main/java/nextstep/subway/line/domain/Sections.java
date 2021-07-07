4cc2178468f7c485b438ba5e4496292fb2e74264
junhee-ko <junheee.ko@gmail.com>
2021-07-07 23:34:30 +0900

    feat: add section logic in Sections class

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -5,6 +5,7 @@ import nextstep.subway.station.domain.Stations;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,62 +13,63 @@ import java.util.List;
 @Embeddable
 public class Sections {
 
-    @OneToMany(mappedBy = "line", cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "line", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     private final List<Section> sections = new ArrayList<>();
 
     public Sections() {
     }
 
-    public void add(Section section) {
+    public void add(Section section, Line line) {
         if (sections.isEmpty()) {
+            section.add(line);
             this.sections.add(section);
             return;
         }
 
-        addNewSectionToOldSection(section);
+        addNewSectionToOldSection(section, line);
     }
 
-    public void addNewSectionToOldSection(Section newSection) {
+    public void addNewSectionToOldSection(Section newSection, Line line) {
         Station newUpStation = newSection.getUpStation();
         Station newDownStation = newSection.getDownStation();
         SectionDistance newDistance = newSection.getDistance();
 
-        addSectionIfBetweenAndUpStationSame(newSection, newDownStation, newDistance);
-        addSectionIfBetweenAndDownStationSame(newSection, newUpStation, newDownStation, newDistance);
-        addSectionIfAtTheEnd(newSection, newUpStation, newDownStation, newDistance);
+        addSectionIfBetweenAndUpStationSame(newSection, newDownStation, newDistance, line);
+        addSectionIfBetweenAndDownStationSame(newSection, newUpStation, newDownStation, newDistance, line);
+        addSectionIfAtTheEnd(newSection, newUpStation, newDownStation, newDistance, line);
     }
 
-    private void addSectionIfBetweenAndUpStationSame(Section newSection, Station newDownStation, SectionDistance newDistance) {
+    private void addSectionIfBetweenAndUpStationSame(Section newSection, Station newDownStation, SectionDistance newDistance, Line line) {
         sections.stream()
             .filter(newSection::isBetweenAndUpStationSameWith)
             .findFirst()
             .ifPresent(section -> {
                 section.getDistance().checkDistanceWith(newDistance);
-                sections.add(createSectionFrom(newDownStation, section.getDownStation(), section.getDistance().minus(newDistance)));
+                sections.add(createSectionFrom(newDownStation, section.getDownStation(), section.getDistance().minus(newDistance), line));
                 section.updateBy(newDownStation, newDistance);
             });
     }
 
-    private void addSectionIfBetweenAndDownStationSame(Section newSection, Station newUpStation, Station newDownStation, SectionDistance newDistance) {
+    private void addSectionIfBetweenAndDownStationSame(Section newSection, Station newUpStation, Station newDownStation, SectionDistance newDistance, Line line) {
         sections.stream()
             .filter(newSection::isBetweenAndDownStationSameWith)
             .findFirst()
             .ifPresent(section -> {
                 section.getDistance().checkDistanceWith(newDistance);
-                sections.add(createSectionFrom(newUpStation, newDownStation, newDistance));
+                sections.add(createSectionFrom(newUpStation, newDownStation, newDistance, line));
                 section.updateBy(newUpStation, section.getDistance().minus(newDistance));
             });
     }
 
-    private void addSectionIfAtTheEnd(Section newSection, Station newUpStation, Station newDownStation, SectionDistance newDistance) {
+    private void addSectionIfAtTheEnd(Section newSection, Station newUpStation, Station newDownStation, SectionDistance newDistance, Line line) {
         sections.stream()
             .filter(newSection::isAtTheEndWith)
             .findFirst()
-            .ifPresent(section -> sections.add(createSectionFrom(newUpStation, newDownStation, newDistance)));
+            .ifPresent(section -> sections.add(createSectionFrom(newUpStation, newDownStation, newDistance, line)));
     }
 
-    private Section createSectionFrom(Station upStation, Station downStation, SectionDistance distance) {
-        return new Section(upStation, downStation, distance);
+    private Section createSectionFrom(Station upStation, Station downStation, SectionDistance distance, Line line) {
+        return new Section(upStation, downStation, distance, line);
     }
 
     public List<Section> getSections() {
@@ -76,5 +78,56 @@ public class Sections {
 
     public Stations getAllStations() {
         return new Stations(this);
+    }
+
+    public void removeSectionBy(Station station) {
+        if (isSizeOne()) {
+            throw new RuntimeException("Can't' remove if section size is 1");
+        }
+
+        remove(station);
+    }
+
+    private void remove(Station station) {
+        for (int i = 0; i < sections.size(); i++) {
+            Section section = sections.get(i);
+
+            if (isLeftMostStation(station, section)) {
+                sections.remove(section);
+                return;
+            }
+
+            if (isRightMostStation(station, i, section)) {
+                sections.remove(section);
+                return;
+            }
+
+            Section nextSection = sections.get(i + 1);
+            if (isMiddleStation(station, section, nextSection)) {
+                sections.remove(nextSection);
+                section.updateBy(nextSection.getDownStation(), section.getDistance().plus(nextSection.getDistance()));
+                return;
+            }
+        }
+    }
+
+    private boolean isSizeOne() {
+        return sections.size() == 1;
+    }
+
+    private boolean isLeftMostStation(Station station, Section section) {
+        return section.getUpStation().equals(station);
+    }
+
+    private boolean isRightMostStation(Station station, int i, Section section) {
+        return isLastSection(i) && section.getDownStation().equals(station);
+    }
+
+    private boolean isMiddleStation(Station station, Section section, Section nextSection) {
+        return section.getDownStation().equals(station) && nextSection.getUpStation().equals(station);
+    }
+
+    private boolean isLastSection(int i) {
+        return i + 1 == sections.size();
     }
 }
