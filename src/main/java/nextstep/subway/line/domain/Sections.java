0496b4e457f410ca7b1138816e2d451c98c3a548
Jeong Minkyo <redwonder@naver.com>
2021-07-15 01:00:44 +0900

    feat(Section) : 도메인 테스트 코드를 추가하다

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -25,6 +25,10 @@ public class Sections {
 	public Sections() {
 	}
 
+	public Sections(List<Section> sections) {
+		this.sections = sections;
+	}
+
 	public List<Station> getStations() {
 		return sections.stream()
 			.map(Section::getStations)
