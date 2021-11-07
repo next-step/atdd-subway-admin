99252a0bf5dcba566e6baade5eea95ecf8d3844b
devyonghee <devyonghee@gmail.com>
2021-11-07 18:09:12 +0900

    test(RestExceptionHandler) : 객체를 못찾으면 404 에러 반환 처리

--- src/main/java/nextstep/subway/configuration/RestExceptionHandler.java
+++ src/main/java/nextstep/subway/configuration/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package nextstep.subway.configuration;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +13,10 @@ public class RestExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Object> handleIllegalArgsException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleIllegalArgsException(NoSuchElementException e) {
+        return ResponseEntity.notFound().build();
     }
 }
