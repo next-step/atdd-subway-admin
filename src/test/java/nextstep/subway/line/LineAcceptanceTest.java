09db2e1d999413b67c375d94c56daa856514bad5
devyonghee <devyonghee@gmail.com>
2021-11-07 17:00:28 +0900

    test(LineAcceptanceTest) : 노선 중복 이름 생성 테스트 추가

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -54,13 +54,21 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void createLine2() {
         // given
-        // 지하철_노선_등록되어_있음
+        Map<String, String> params = lineParams("1호선", "blue");
+        givenLine(params);
 
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract();
 
         // then
-        // 지하철_노선_생성_실패됨
+        assertThat(response.statusCode())
+            .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
