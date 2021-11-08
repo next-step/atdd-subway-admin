1554fc483e879d35c5c2255cf76d655bc889b805
tyanotyrano <tyrano09@gmail.com>
2021-11-09 01:59:07 +0900

    tes(LineAcceptanceTest.java): 지하철 노선 생성과 관련된 인수 테스트 작성
    지하철 노선 생성과 관련된 인수 테스트를 작성한다.
    - LineAcceptanceTest : 지하철 노선 생성과 관련된 인수 테스트 작성
    - README.md : 완료된 기능 요구사항 체크

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,32 +1,47 @@
 package nextstep.subway.line;
 
-import nextstep.subway.AcceptanceTest;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineResponse;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선을 생성한다.")
-    @Test
-    void createLine() {
+    @ParameterizedTest
+    @CsvSource(value = {"신분당선,RED", "분당선,YELLOW", "1호선,BLUE", "2호선,GREEN"})
+    void createLine(String name, String color) {
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(name, color);
 
         // then
-        // 지하철_노선_생성됨
+        지하철_노선_생성됨(response);
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
-    @Test
-    void createLine2() {
+    @ParameterizedTest
+    @CsvSource(value = {"신분당선,RED", "분당선,YELLOW", "1호선,BLUE", "2호선,GREEN"})
+    void createLine2(String name, String color) {
         // given
-        // 지하철_노선_등록되어_있음
+        지하철_노선_등록되어_있음(name, color);
 
         // when
-        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = 지하철_노선_생성_요청(name, color);
 
         // then
-        // 지하철_노선_생성_실패됨
+        지하철_노선_생성_실패됨(response);
     }
 
     @DisplayName("지하철 노선 목록을 조회한다.")
@@ -81,5 +96,45 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         // 지하철_노선_삭제됨
+    }
+
+    private ExtractableResponse<Response> 지하철_노선_생성_요청(String name, String color) {
+        Map<String, String> params = createLineParams(name, color);
+
+        return RestAssured.given().log().all()
+                          .body(params)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .when()
+                          .post("/lines")
+                          .then().log().all()
+                          .extract();
+    }
+
+    private LineResponse 지하철_노선_등록되어_있음(String name, String color) {
+        Map<String, String> params = createLineParams(name, color);
+
+        return RestAssured.given().log().all()
+                          .body(params)
+                          .contentType(MediaType.APPLICATION_JSON_VALUE)
+                          .when()
+                          .post("/lines")
+                          .then().log().all()
+                          .extract()
+                          .as(LineResponse.class);
+    }
+
+    private void 지하철_노선_생성됨(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private void 지하철_노선_생성_실패됨(ExtractableResponse<Response> response) {
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    private Map<String, String> createLineParams(String name, String color) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        return params;
     }
 }
