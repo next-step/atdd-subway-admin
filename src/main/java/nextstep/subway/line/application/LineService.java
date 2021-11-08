5fb3a48d78425cd91b6e5d3a3a77ec327abd0e07
tyanotyrano <tyrano09@gmail.com>
2021-11-09 02:34:15 +0900

    tes(LineAcceptanceTest.java): 지하철 노선 조회와 관련된 인수 테스트 작성
    지하철 노선 조회와 관련된 인수 테스트를 작성한다.
    - LineAcceptanceTest : 지하철 노선 조회와 관련된 인수 테스트 작성
    - LineController : 노선 목록 및 개별 노선 조회 API 작성
    - LineService : 노선 목록 및 개별 노선 조회 처리
    - README.md : 완료된 기능 요구사항 체크

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,16 +1,22 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
@@ -19,5 +25,21 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> readLines() {
+        List<Line> lines = lineRepository.findAll();
+        return lines.stream()
+                    .map(LineResponse::of)
+                    .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse readLine(Long id) {
+        Line line = lineRepository.findById(id)
+                                  .orElseThrow(() -> new NoSuchElementException(
+                                      String.format("id=%s 에 해당하는 노선이 존재하지 않습니다.", id)));
+        return LineResponse.of(line);
     }
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -4,12 +4,15 @@ import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -21,8 +24,18 @@ public class LineController {
     }
 
     @PostMapping
-    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LineResponse>> readLines() {
+        return ResponseEntity.ok().body(lineService.readLines());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LineResponse> readLine(@PathVariable Long id) {
+        return ResponseEntity.ok().body(lineService.readLine(id));
     }
 }
