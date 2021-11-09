b7e223ef4316e4d7db174ea23851f19f7ba703c4
devyonghee <devyonghee@gmail.com>
2021-11-09 23:51:05 +0900

    feat(LineRequest) : 요청 조건 추가

--- src/main/java/nextstep/subway/line/dto/SectionRequest.java
+++ src/main/java/nextstep/subway/line/dto/SectionRequest.java
@@ -1,7 +1,6 @@
 package nextstep.subway.line.dto;
 
 import javax.validation.constraints.NotNull;
-import nextstep.subway.line.domain.Distance;
 
 public class SectionRequest {
 
@@ -14,15 +13,6 @@ public class SectionRequest {
     @NotNull
     private Integer distance;
 
-    private SectionRequest() {
-    }
-
-    public SectionRequest(Long upStationId, Long downStationId, Integer distance) {
-        this.upStationId = upStationId;
-        this.downStationId = downStationId;
-        this.distance = distance;
-    }
-
     public Long getUpStationId() {
         return upStationId;
     }
@@ -33,9 +23,5 @@ public class SectionRequest {
 
     public Integer getDistance() {
         return distance;
-    }
-
-    public Distance distance() {
-        return Distance.from(distance);
     }
 }
