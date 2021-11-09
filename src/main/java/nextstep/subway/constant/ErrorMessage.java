68aab3c9b27458429950e6ed60c43d59e375e731
tyanotyrano <tyrano09@gmail.com>
2021-11-10 04:16:06 +0900

    refactor(ErrorMessage.java): 생성자 접근제어자 public -> private 변경
    생성자 접근제어자 public -> private 로 변경한다.
    - ErrorMessage : 생성자 접근제어자 public -> private 변경

--- src/main/java/nextstep/subway/constant/ErrorMessage.java
+++ src/main/java/nextstep/subway/constant/ErrorMessage.java
@@ -3,5 +3,5 @@ package nextstep.subway.constant;
 public class ErrorMessage {
 	public static String NOT_EXIST_LINE = "id=%s 에 해당하는 노선이 존재하지 않습니다.";
 
-	public ErrorMessage() {}
+	private ErrorMessage() {}
 }
