7d63a9e7bfb3dd1c4108de37f942747943eb4e83
devyonghee <devyonghee@gmail.com>
2021-11-10 04:32:20 +0900

    test(Section) : 지하철 역 가져오기 테스트 추가

--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -1,10 +1,15 @@
 package nextstep.subway.line.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.mockito.Mockito.mock;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import nextstep.subway.common.domain.Name;
+import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +30,38 @@ class SectionsTest {
             .isThrownBy(() -> Sections.from(Collections.emptyList()))
             .withMessage("section list must not be empty");
     }
+
+
+    @Test
+    @DisplayName("지하철 역들")
+    void stations() {
+        //given
+        Name firstStation = Name.from("강남");
+        Name secondStation = Name.from("역삼");
+        Name thirdStation = Name.from("잠실");
+
+        Sections sections = Sections.from(Arrays.asList(
+            section(firstStation, secondStation),
+            section(secondStation, thirdStation)
+        ));
+
+        //when
+        List<Station> stations = sections.stations();
+
+        //then
+        assertThat(stations)
+            .hasSize(3)
+            .doesNotHaveDuplicates()
+            .extracting(Station::name)
+            .containsExactly(firstStation, secondStation, thirdStation);
+    }
+
+
+    private Section section(Name upStation, Name downStation) {
+        return Section.of(Station.from(upStation),
+            Station.from(downStation),
+            Distance.from(Integer.MAX_VALUE));
+    }
+
 
 }
