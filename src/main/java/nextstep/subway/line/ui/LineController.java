d8bb55c764e15135a63c7700e195177d542380e0
peter-alethio <peter@alethio.io>
2021-11-10 21:28:54 +0900

    feat: 지하철 노선 목록 조회 구현

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -24,6 +24,11 @@ public class LineController {
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
+    @GetMapping
+    public ResponseEntity findLines() {
+        return ResponseEntity.ok(lineService.findLines());
+    }
+
     @ExceptionHandler
     public ResponseEntity handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         return ResponseEntity.badRequest().build();
