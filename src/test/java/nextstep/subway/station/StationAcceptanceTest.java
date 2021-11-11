6155d742c023a5fca1f83a92e95e0f95ed16ab21
kelicia91 <kelicia91@gmail.com>
2021-11-12 00:10:12 +0900

    refactor: gardening StationAcceptanceTest.createStationWithDuplicatedName()

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -34,7 +34,7 @@ public class StationAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
     @Test
-    void createStationWithDuplicateName() {
+    void createStation_with_duplicated_name() {
         // given
         final Map<String, String> params = params("강남역");
         post(params);
