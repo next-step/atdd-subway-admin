f14d6001c629a2d79bbc7e821eba2fb27669721f
tyanotyrano <tyrano09@gmail.com>
2021-11-12 02:00:21 +0900

    test(LineNameTest.java): LineName 테스트 코드 추가
    LineName 테스트 코드 추가
    - LineName : 유효성 체크 함수 추가
    - LineNameTest : LineName 테스트 코드 추가

--- src/main/java/nextstep/subway/line/domain/LineName.java
+++ src/main/java/nextstep/subway/line/domain/LineName.java
@@ -1,12 +1,13 @@
 package nextstep.subway.line.domain;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+import org.springframework.util.StringUtils;
+
 @Embeddable
 public class LineName {
+    private static final String EMPTY_NAME_ERROR_MESSAGE = "노선이름이 비어있습니다. name=%s";
 
     @Column(name = "name", unique = true, nullable = false)
     private String name;
@@ -18,10 +19,17 @@ public class LineName {
     }
 
     public static LineName from(String name) {
+        validateLineName(name);
         return new LineName(name);
     }
 
     public String getValue() {
         return name;
+    }
+
+    private static void validateLineName(String name) {
+        if (!StringUtils.hasLength(name)) {
+            throw new IllegalArgumentException(String.format(EMPTY_NAME_ERROR_MESSAGE, name));
+        }
     }
 }
