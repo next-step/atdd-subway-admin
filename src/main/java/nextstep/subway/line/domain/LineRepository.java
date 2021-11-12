111e6d97c32754605444649fad7a1b570ddf0f61
HeeSeok Noh <nhs0912@nate.com>
2021-11-12 17:45:46 +0900

    refactor : LineRepository 필요없는 메서드 제거
    - 이름으로 노선찾기 메서드 삭제

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -2,10 +2,6 @@ package nextstep.subway.line.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface LineRepository extends JpaRepository<Line, Long> {
-    List<Line> findLineByName(String name);
-
+public interface LineRepository extends JpaRepository<Line, Long> {    
     boolean existsByName(String name);
 }
