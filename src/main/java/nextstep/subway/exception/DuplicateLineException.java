0f5d1b2c5f7d9f4529f63551c38a85dd42b00ad0
peter-alethio <peter@alethio.io>
2021-11-12 17:23:40 +0900

    fix: 유효하지 않은 에러 메세지 수정

--- src/main/java/nextstep/subway/exception/DuplicateLineException.java
+++ src/main/java/nextstep/subway/exception/DuplicateLineException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class DuplicateLineException extends BusinessException {
 
-    private static final String ERROR_MESSAGE = "이미 존재하는 사용자 입니다";
+    private static final String ERROR_MESSAGE = "이미 존재하는 라인 입니다";
 
     public DuplicateLineException() {
         super(ERROR_MESSAGE);
--- src/main/java/nextstep/subway/exception/LineNotExistException.java
+++ src/main/java/nextstep/subway/exception/LineNotExistException.java
@@ -2,7 +2,7 @@ package nextstep.subway.exception;
 
 public class LineNotExistException extends BusinessException {
 
-    private static final String ERROR_MESSAGE = "사용자가 존재하지 않습니다.";
+    private static final String ERROR_MESSAGE = "라인이 존재하지 않습니다.";
 
     public LineNotExistException() {
         super(ERROR_MESSAGE);
