8edf2be597f2f4655f93cdc5004f21c9f2733c26
devyonghee <devyonghee@gmail.com>
2021-11-14 18:31:31 +0900

    chore(Sections) : 메소드 액세스 레벨 수정

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -57,7 +57,7 @@ public class Sections {
         }
     }
 
-    public void addSection(Section section) {
+    void addSection(Section section) {
         validateAdditionalSection(section);
         addValidatedSection(section);
         deleteSectionsCache();
