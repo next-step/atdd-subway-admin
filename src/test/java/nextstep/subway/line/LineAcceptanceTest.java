e90a08ddbeacb03048d4825637189784906afd4b
vicjed <vicjed@icloud.com>
2021-11-15 22:04:52 +0900

    style: 탭 스페이로 변경

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -18,94 +18,94 @@ import nextstep.subway.AcceptanceTest;
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
 
-	public static final String NAME = "name";
-	public static final String COLOR = "color";
+    public static final String NAME = "name";
+    public static final String COLOR = "color";
 
-	@DisplayName("지하철 노선을 생성한다.")
-	@Test
-	void createLine() {
+    @DisplayName("지하철 노선을 생성한다.")
+    @Test
+    void createLine() {
 
-		Map<String, String> params = new HashMap<>();
-		params.put(NAME, "1호선");
-		params.put(COLOR, "남색");
+        Map<String, String> params = new HashMap<>();
+        params.put(NAME, "1호선");
+        params.put(COLOR, "남색");
 
-		// when
-		// 지하철_노선_생성_요청
-		ExtractableResponse<Response> response = RestAssured
-			.given().log().all()
-			.body(params)
-			.contentType(APPLICATION_JSON_VALUE)
-			.when()
-			.post("lines")
-			.then().log().all()
-			.extract();
+        // when
+        // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = RestAssured
+            .given().log().all()
+            .body(params)
+            .contentType(APPLICATION_JSON_VALUE)
+            .when()
+            .post("lines")
+            .then().log().all()
+            .extract();
 
-		// then
-		// 지하철_노선_생성됨
-		assertThat(response.statusCode()).isEqualTo(CREATED.value());
-	}
+        // then
+        // 지하철_노선_생성됨
+        assertThat(response.statusCode()).isEqualTo(CREATED.value());
+    }
 
-	@DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
-	@Test
-	void createLine2() {
-		// given
-		// 지하철_노선_등록되어_있음
+    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
+    @Test
+    void createLine2() {
+        // given
+        // 지하철_노선_등록되어_있음
 
-		// then
-		// 지하철_노선_생성_실패됨
-	}
+        // then
+        // 지하철_노선_생성_실패됨
+    }
 
-	@DisplayName("지하철 노선 목록을 조회한다.")
-	@Test
-	void getLines() {
-		// given
-		// 지하철_노선_등록되어_있음
-		// 지하철_노선_등록되어_있음
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void getLines() {
+        // given
+        // 지하철_노선_등록되어_있음
+        // 지하철_노선_등록되어_있음
 
-		// when
-		// 지하철_노선_목록_조회_요청
+        // when
+        // 지하철_노선_목록_조회_요청
 
-		// then
-		// 지하철_노선_목록_응답됨
-		// 지하철_노선_목록_포함됨
-	}
+        // then
+        // 지하철_노선_목록_응답됨
+        // 지하철_노선_목록_포함됨
+    }
 
-	@DisplayName("지하철 노선을 조회한다.")
-	@Test
-	void getLine() {
-		// given
-		// 지하철_노선_등록되어_있음
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void getLine() {
+        // given
+        // 지하철_노선_등록되어_있음
 
-		// when
-		// 지하철_노선_조회_요청
+        // when
+        // 지하철_노선_조회_요청
 
-		// then
-		// 지하철_노선_응답됨
-	}
+        // then
+        // 지하철_노선_응답됨
+    }
 
-	@DisplayName("지하철 노선을 수정한다.")
-	@Test
-	void updateLine() {
-		// given
-		// 지하철_노선_등록되어_있음
+    @DisplayName("지하철 노선을 수정한다.")
+    @Test
+    void updateLine() {
+        // given
+        // 지하철_노선_등록되어_있음
 
-		// when
-		// 지하철_노선_수정_요청
+        // when
+        // 지하철_노선_수정_요청
 
-		// then
-		// 지하철_노선_수정됨
-	}
+        // then
+        // 지하철_노선_수정됨
+    }
 
-	@DisplayName("지하철 노선을 제거한다.")
-	@Test
-	void deleteLine() {
-		// given
-		// 지하철_노선_등록되어_있음
+    @DisplayName("지하철 노선을 제거한다.")
+    @Test
+    void deleteLine() {
+        // given
+        // 지하철_노선_등록되어_있음
 
-		// when
-		// 지하철_노선_제거_요청
+        // when
+        // 지하철_노선_제거_요청
 
-		// then
-		// 지하철_노선_삭제됨
-	}
+        // then
+        // 지하철_노선_삭제됨
+    }
 }
