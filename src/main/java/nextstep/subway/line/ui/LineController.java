188d18d52b35db84d6ea5c1466ad545da3f05b56
jerry92k <jens92.k@gmail.com>
2021-11-15 18:03:06 +0900

    feat(LineController,LineService): 지하철 노선 목록 조회

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -3,16 +3,20 @@ package nextstep.subway.line.ui;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -27,6 +31,11 @@ public class LineController {
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showStations() {
+        return ResponseEntity.ok().body(lineService.findAllLines());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
