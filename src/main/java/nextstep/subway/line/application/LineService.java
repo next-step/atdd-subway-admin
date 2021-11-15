00a6a95c61d6159d7960cf7af6ee02fb861689fa
ChangSubKwak <paradise1732@gmail.com>
2021-11-15 21:19:23 +0900

    feat: 지하철 노선(CRUD) DB접근을 위한 Service 로직 작성

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,13 +4,19 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+    private static final String LINE = "노선";
+
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
@@ -19,5 +25,35 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAll() {
+        return lineRepository.findAll()
+            .stream()
+            .map(LineResponse::of)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse getLineResponseById(Long id) {
+        Line line = lineRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException(LINE));
+
+        return LineResponse.of(line);
+    }
+
+    public void updateLineById(Long id, LineRequest lineRequest) {
+        Line line = getLineById(id);
+        line.update(lineRequest.toLine());
+    }
+
+    public void deleteLineById(Long id) {
+        lineRepository.deleteById(id);
+    }
+
+    private Line getLineById(Long id) {
+        return lineRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException(LINE));
     }
 }
