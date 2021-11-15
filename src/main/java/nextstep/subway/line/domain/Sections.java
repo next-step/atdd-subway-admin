c50b2c53f7b65b058d6b4ec92afeedb7f24f97f5
Jeongsu Yang <mycat83@gmail.com>
2021-11-15 14:47:16 +0900

    feat(section): 상행역과 하행역이 노선에 포함되어 있거나 포함되지 않은 경우 예외 발생

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -8,6 +8,7 @@ import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Embeddable
@@ -16,8 +17,28 @@ public class Sections {
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Section> sections = new ArrayList<>();
 
+    public Sections() {
+    }
+
+    public Sections(List<Section> sections) {
+        this.sections = sections;
+    }
+
     public void add(Section section) {
-        sections.add(section);
+        if (sections.size() == 0) {
+            sections.add(section);
+            return;
+        }
+
+        Optional<Section> sectionUpStationMatchedOptional = findByUpStation(section);
+        Optional<Section> sectionDownStationMatchedOptional = findByDownStation(section);
+
+        if (sectionUpStationMatchedOptional.isPresent() && sectionDownStationMatchedOptional.isPresent()) {
+            throw new SectionAddFailedException("상행역과 하행역이 노선에 포함되어 있는 구간은 등록할 수 없습니다.");
+        }
+        if (!sectionUpStationMatchedOptional.isPresent() && !sectionDownStationMatchedOptional.isPresent()) {
+            throw new SectionAddFailedException("상행역과 하행역중 1개는 노선에 포함되어야 합니다.");
+        }
     }
 
     public List<Station> toStations() {
@@ -37,6 +58,18 @@ public class Sections {
 
     private Station getLastStation() {
         return sections.get(sections.size() - 1).getDownStation();
+    }
+
+    private Optional<Section> findByUpStation(Section section) {
+        return sections.stream()
+                .filter(section::isUpStationEquals)
+                .findFirst();
+    }
+
+    private Optional<Section> findByDownStation(Section section) {
+        return sections.stream()
+                .filter(section::isDownStationEquals)
+                .findFirst();
     }
 
     @Override
--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -1,34 +1,66 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("구간 목록 테스트")
 class SectionsTest {
 
-    @Test
-    @DisplayName("지하철 구간 순서에 맞는 지하철역 목록을 반환한다.")
-    void toStations() {
-        // given
-        Station 강남역 = new Station("강남역");
-        Station 역삼역 = new Station("역삼역");
-        Station 사당역 = new Station("사당역");
-        Section section1 = new Section(강남역, 역삼역, new Distance(10));
-        Section section2 = new Section(역삼역, 사당역, new Distance(15));
+    private Station 강남역;
+    private Station 역삼역;
+    private Station 사당역;
+    private Station 방배역;
 
+    @BeforeEach
+    void setUp() {
+        강남역 = new Station("강남역");
+        역삼역 = new Station("역삼역");
+        사당역 = new Station("사당역");
+        방배역 = new Station("방배역");
+    }
+
+//    @Test
+//    @DisplayName("지하철 구간 순서에 맞는 지하철역 목록을 반환한다.")
+//    void toStations() {
+//        // given
+//        Sections sections = new Sections();
+//        sections.add(new Section(강남역, 역삼역, new Distance(10)));
+//        sections.add(new Section(역삼역, 사당역, new Distance(15)));
+//
+//        // when
+//        List<Station> stations = sections.toStations();
+//
+//        // then
+//        assertThat(stations).containsExactly(강남역, 역삼역, 사당역);
+//    }
+
+    @Test
+    @DisplayName("상행역과 하행역이 노선에 포함되어 있는 구간을 등록할 경우 예외가 발생한다.")
+    void addThrowException1() {
+        // given
         Sections sections = new Sections();
-        sections.add(section1);
-        sections.add(section2);
+        sections.add(new Section(강남역, 역삼역, new Distance(10)));
 
         // when
-        List<Station> stations = sections.toStations();
+        assertThatExceptionOfType(SectionAddFailedException.class)
+                .isThrownBy(() -> sections.add(new Section(강남역, 역삼역, new Distance(5))))
+                .withMessageMatching("상행역과 하행역이 노선에 포함되어 있는 구간은 등록할 수 없습니다.");
+    }
 
-        // then
-        assertThat(stations).containsExactly(강남역, 역삼역, 사당역);
+    @Test
+    @DisplayName("상행역과 하행역이 노선에 포함되어 있지 않은 구간을 등록할 경우 예외가 발생한다.")
+    void addThrowException2() {
+        // given
+        Sections sections = new Sections();
+        sections.add(new Section(강남역, 역삼역, new Distance(10)));
+
+        // when
+        assertThatExceptionOfType(SectionAddFailedException.class)
+                .isThrownBy(() -> sections.add(new Section(사당역, 방배역, new Distance(5))))
+                .withMessageMatching("상행역과 하행역중 1개는 노선에 포함되어야 합니다.");
     }
 }
