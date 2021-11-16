0082c38e3fb430bd17ccf51ad2a6db7708ec832e
cus4703 <cus4703@gmail.com>
2021-11-16 20:46:41 +0900

    feat: 지하철 구간 추가시 예외 케이스 추가

--- src/test/java/nextstep/subway/section/LineStationAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/LineStationAcceptanceTest.java
@@ -16,7 +16,7 @@ import static nextstep.subway.utils.Fixture.*;
 public class LineStationAcceptanceTest extends AcceptanceTest {
 
 
-    @DisplayName("지하철 노선을 생성한다.")
+    @DisplayName("지하철 노선의 구간을 추가한다.")
     @Test
     void addSection() {
         StationResponse 기점역 = 역_생성(강남역);
@@ -27,6 +27,20 @@ public class LineStationAcceptanceTest extends AcceptanceTest {
                 = 지하철_노선_구간_추가("/lines/{id}/lineStations", id, 구간_추가(기점역, 역_생성(판교역), 5));
 
         응답_확인_OK(actual);
+    }
+
+    @DisplayName("지하철 노선의 구간을 추가한다.(동일한 건에 대한 오류 발생)")
+    @Test
+    void addSection_동일역_추가() {
+        StationResponse 강남 = 역_생성(강남역);
+        StationResponse 광교 = 역_생성(광교역);
+        ExtractableResponse<Response> response = 신규_지하철_노선_생성_요청("/lines", 신분당선, 10, 강남, 광교);
+        Long id = response.as(LineResponse.class).getId();
+
+        ExtractableResponse<Response> actual
+                = 지하철_노선_구간_추가("/lines/{id}/lineStations", id, 구간_추가(강남, 광교, 5));
+
+        응답_확인_BAD_REQUEST(actual);
     }
 
     private ExtractableResponse<Response> 지하철_노선_구간_추가(String path, Long id, LineStationRequest 판교역) {
