960182be6a1b92ef16a367c8c86029bf918df46a
jerry92k <jens92.k@gmail.com>
2021-11-16 17:49:38 +0900

    feat(LineResponse): 지하철 노선 조회시 구간의 지하철역 정보도 함께 조회

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,29 +1,40 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<Station> stations;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
+
 
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate, List<Section> sections) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
+        this.stations= new ArrayList<>();
+        for(Section section : sections){
+            this.stations.add(section.getUpStation());
+            this.stations.add(section.getDownStation());
+        }
     }
 
     public static LineResponse of(Line line) {
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getCreatedDate(), line.getModifiedDate(),line.getSections());
     }
 
     public Long getId() {
@@ -44,5 +55,9 @@ public class LineResponse {
 
     public LocalDateTime getModifiedDate() {
         return modifiedDate;
+    }
+
+    public List<Station> getStations() {
+        return stations;
     }
 }
