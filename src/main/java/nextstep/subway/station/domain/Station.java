634f51376b65908a3dc383fd3891f7007c2815bd
kelicia91 <kelicia91@gmail.com>
2021-11-17 01:41:49 +0900

    refactor: gardening Station, StationRequest, StationResponse

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -2,9 +2,13 @@ package nextstep.subway.station.domain;
 
 import java.util.Objects;
 
-import nextstep.subway.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import javax.persistence.*;
+import nextstep.subway.common.BaseEntity;
 
 @Entity
 public class Station extends BaseEntity {
@@ -29,5 +33,20 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Station))
+            return false;
+        Station station = (Station)o;
+        return Objects.equals(id, station.id) && Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
