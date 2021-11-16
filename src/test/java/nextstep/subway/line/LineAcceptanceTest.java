3f3b18ae24ea5b0340aa236f83fefeb5704a9259
jerry92k <jens92.k@gmail.com>
2021-11-16 22:11:57 +0900

    chore(LineAcceptanceTest): 포맷팅, 오타 수정

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.transaction.annotation.Transactional;
 
 @DisplayName("지하철 노선 관련 기능")
 // @Transactional
@@ -27,8 +26,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
 	private static final String LINE_SERVICE_PATH = "/lines";
 
 	//	given
-	public static final Map<String, String> 신분당선_정보 = 지하철_노선_정보_정의("신분당선", "bg-red-600",1L,2L,10);
-	public static final Map<String, String> 이호선_정보 = 지하철_노선_정보_정의("2호", "bg-이호선_정보-600",1L,2L,10);
+	public static final Map<String, String> 신분당선_정보 = 지하철_노선_정보_정의("신분당선", "bg-red-600", 1L, 2L, 10);
+	public static final Map<String, String> 이호선_정보 = 지하철_노선_정보_정의("2호", "bg-green-600", 1L, 2L, 10);
 
 	@BeforeEach
 	public void setUpLineAcceptance() {
@@ -84,9 +83,9 @@ public class LineAcceptanceTest extends AcceptanceTest {
 		String 신분당선_요청_경로 = 신분당선_생성_되어있음.header("Location");
 
 		// when
-		ExtractableResponse<Response> 신분당선_조회_결과 = 지하철_노선_조회(신분당선_요청_경로);
+		ExtractableResponse<Response> 신분당선_조회_결과 = AcceptanceTestFactory.get(신분당선_요청_경로);
 
-		// then
+		//then
 		정상_처리_확인(신분당선_조회_결과);
 		생성된_지하철_노선이_응답에_포함되어있는_확인(신분당선_조회_결과, 신분당선_요청_경로);
 	}
@@ -112,7 +111,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 		String 신분당선_요청_경로 = 신분당선_생성_되어있음.header("Location");
 
 		// when
-		Map<String, String> 신분당선_수정정보 = 지하철_노선_정보_정의("구분당", "bg-blue-600",1L,2L,10);
+		Map<String, String> 신분당선_수정정보 = 지하철_노선_정보_정의("구분당", "bg-blue-600", 1L, 2L, 10);
 		ExtractableResponse<Response> 신분당선_수정_결과 = 지하철_노선_수정(신분당선_수정정보, 신분당선_요청_경로);
 
 		// then
@@ -130,7 +129,7 @@ public class LineAcceptanceTest extends AcceptanceTest {
 		String 미등록_노선_요청_경로 = LINE_SERVICE_PATH + "/1";
 
 		// when
-		Map<String, String> 미등록_노선_수정정보 = 지하철_노선_정보_정의("구분당", "bg-blue-600",1L,2L,10);
+		Map<String, String> 미등록_노선_수정정보 = 지하철_노선_정보_정의("구분당", "bg-blue-600", 1L, 2L, 10);
 		ExtractableResponse<Response> 미등록_노선_수정_결과 = 지하철_노선_수정(미등록_노선_수정정보, 미등록_노선_요청_경로);
 
 		// then
@@ -151,8 +150,9 @@ public class LineAcceptanceTest extends AcceptanceTest {
 		삭제_완료_확인(신분당선_삭제_결과);
 	}
 
-	private static Map<String, String> 지하철_노선_정보_정의(String name, String color,Long upStationId, Long downStationId, int distance) {
-		return AcceptanceTestFactory.getLineInfo(name, color,upStationId,downStationId,distance);
+	private static Map<String, String> 지하철_노선_정보_정의(String name, String color, Long upStationId, Long downStationId,
+		int distance) {
+		return AcceptanceTestFactory.getLineInfo(name, color, upStationId, downStationId, distance);
 	}
 
 	private ExtractableResponse<Response> 지하철_노선_생성(Map<String, String> params) {
