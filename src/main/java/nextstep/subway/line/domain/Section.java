e08780c2321561e971c274db461f61a53f3913bd
moon <hoopster@airi.kr>
2021-11-16 17:08:20 +0900

    feat (Section) : 구간 (Section) 도메인 추가

---
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -0,0 +1,70 @@
+package nextstep.subway.line.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import nextstep.subway.common.BaseEntity;
+import nextstep.subway.station.domain.Station;
+
+@Entity
+public class Section extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "line_id")
+	private Line line;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "up_station_id")
+	private Station upStation;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "down_station_id")
+	private Station downStation;
+
+	private int distance;
+
+	protected Section() {
+	}
+
+	public Section(Line line, Station upStation, Station downStation, int distance) {
+		this.line = line;
+		this.upStation = upStation;
+		this.downStation = downStation;
+		this.distance = distance;
+	}
+
+	public Line getLine() {
+		return line;
+	}
+
+	public Station getUpStation() {
+		return upStation;
+	}
+
+	public Station getDownStation() {
+		return downStation;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Section section = (Section)o;
+		return getId().equals(section.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(line, upStation, downStation, distance);
+	}
+}
