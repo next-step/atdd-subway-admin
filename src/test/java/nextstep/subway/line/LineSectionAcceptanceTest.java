ad82a59ffb20710806536f6692c7e7605124f771
peter-alethio <peter@alethio.io>
2021-11-17 23:52:55 +0900

    test: 새로운 역을 상행종점으로 등록 인수 테스트

--- src/test/java/nextstep/subway/line/LineSectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineSectionAcceptanceTest.java
@@ -73,4 +73,15 @@ public class LineSectionAcceptanceTest extends AcceptanceTest {
         지하철_노선에_지하철역_등록됨(response);
         지하철_노선에_지하철역_정렬됨(lineResponse, Arrays.asList(강남역, 역삼역, 삼성역));
     }
+
+    @Test
+    void addLineSection_새로운_역을_상행종점으로_등록한다() {
+        // when
+        ExtractableResponse<Response> response = 지하철_노선에_지하철역_등록_요청(이호선, 역삼역, 강남역, 3);
+
+        // then
+        LineResponse lineResponse = LineStep.지하철_노선_조회되어_있음(이호선);
+        지하철_노선에_지하철역_등록됨(response);
+        지하철_노선에_지하철역_정렬됨(lineResponse, Arrays.asList(역삼역, 강남역, 삼성역));
+    }
 }
