5cea1fcaeb24738e273435fe66252a9a5e737880
신민준 <mjshin@lfpartners.co.kr>
2021-11-17 13:33:54 +0900

    refactor: StationController에서 중복사용되는 url은 클래스 어노테이션으로 이동

--- src/main/java/nextstep/subway/station/ui/StationController.java
+++ src/main/java/nextstep/subway/station/ui/StationController.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/stations")
 public class StationController {
     private StationService stationService;
 
@@ -19,25 +20,38 @@ public class StationController {
         this.stationService = stationService;
     }
 
-    @PostMapping("/stations")
+    /**
+     * 역 생성
+     *
+     * @param stationRequest
+     * @return
+     */
+    @PostMapping
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         StationResponse station = stationService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
     }
 
-    @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     * 전체목록 조회
+     *
+     * @return
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
         return ResponseEntity.ok().body(stationService.findAllStations());
     }
 
-    @DeleteMapping("/stations/{id}")
+    /**
+     * 역 삭제
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
         stationService.deleteStationById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
-        return ResponseEntity.badRequest().build();
-    }
 }
