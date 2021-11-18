d548cbf10c03836da85f51b76e0ebfc741882972
donkey <rlaehdzlsla@gmail.com>
2021-11-19 01:54:19 +0900

    test: 지하철 노선을 생성한다.

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,24 +1,44 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
-    @DisplayName("지하철 노선을 생성한다.")
+
     @Test
-    void createLine() {
+    void 지하철_노선을_생성한다() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "red");
+
         // when
         // 지하철_노선_생성_요청
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all().extract();
 
         // then
         // 지하철_노선_생성됨
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
-    void createLine2() {
+    void 기존에_존재하는_지하철_노선_이름으로_지하철_노선을_생성한다() {
         // given
         // 지하철_노선_등록되어_있음
 
@@ -29,9 +49,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_생성_실패됨
     }
 
-    @DisplayName("지하철 노선 목록을 조회한다.")
     @Test
-    void getLines() {
+    void 지하철_노선_목록을_조회한다() {
         // given
         // 지하철_노선_등록되어_있음
         // 지하철_노선_등록되어_있음
@@ -44,9 +63,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_목록_포함됨
     }
 
-    @DisplayName("지하철 노선을 조회한다.")
     @Test
-    void getLine() {
+    void 지하철_노선을_조회한다() {
         // given
         // 지하철_노선_등록되어_있음
 
@@ -57,9 +75,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_응답됨
     }
 
-    @DisplayName("지하철 노선을 수정한다.")
     @Test
-    void updateLine() {
+    void 지하철_노선을_수정한다() {
         // given
         // 지하철_노선_등록되어_있음
 
@@ -70,9 +87,8 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // 지하철_노선_수정됨
     }
 
-    @DisplayName("지하철 노선을 제거한다.")
     @Test
-    void deleteLine() {
+    void 지하철_노선을_제거한다() {
         // given
         // 지하철_노선_등록되어_있음
 
