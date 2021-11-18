5e1ed13fc2c0864b23100053e90c83053f6eeaad
moon <hoopster@airi.kr>
2021-11-18 14:58:32 +0900

    feat (Sections) : 섹션리스트에서 역(Station) 리스트를 반환하는 기능 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,11 +1,17 @@
 package nextstep.subway.line.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+
+import nextstep.subway.station.domain.Station;
 
 @Embeddable
 public class Sections {
@@ -26,5 +32,13 @@ public class Sections {
 
 	public void add(Section section) {
 		sectionList.add(section);
+	}
+
+	public List<Station> getStations() {
+		Set<Station> upStations = sectionList.stream().map(Section::getUpStation).collect(Collectors.toSet());
+		Set<Station> downStations = sectionList.stream().map(Section::getDownStation).collect(Collectors.toSet());
+
+		return Stream.of(upStations, downStations)
+			.flatMap(Collection::stream).distinct().collect(Collectors.toList());
 	}
 }
