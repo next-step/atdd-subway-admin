f8d55ee12d8db90e26edbb33b4d430edf348aa4e
shryu <>
2021-11-19 20:19:57 +0900

    feat : LineRequest Dto 객체 값 validate 메소드 구현

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -3,6 +3,7 @@ package nextstep.subway.line.dto;
 import nextstep.subway.line.domain.Line;
 
 public class LineRequest {
+
     private String name;
     private String color;
 
@@ -10,8 +11,18 @@ public class LineRequest {
     }
 
     public LineRequest(String name, String color) {
+        validateLineRequest(name, color);
         this.name = name;
         this.color = color;
+    }
+
+    private void validateLineRequest(String name, String color) {
+        if (name == null || color == null) {
+            throw new NullPointerException("입력값은 null 값을 넣으면 안됩니다.");
+        }
+        if (name.isEmpty() || color.isEmpty()) {
+            throw new IllegalArgumentException("입력값은 빈값을 넣으면 안됩니다.");
+        }
     }
 
     public String getName() {
