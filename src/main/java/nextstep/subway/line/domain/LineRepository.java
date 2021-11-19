758905a285ef3d11fe1d42d7e5c780f988cdc200
keepbang <rlqud1125@naver.com>
2021-11-19 19:42:26 +0900

    feat: findById 찾는 도메인 없을 경우 exception발생

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -1,10 +1,10 @@
 package nextstep.subway.line.domain;
 
-import nextstep.subway.exception.NotFoundDataException;
+import nextstep.subway.exception.NotFoundLineException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
     default Line findByIdElseThrow(Long id) {
-        return this.findById(id).orElseThrow(NotFoundDataException::new);
+        return this.findById(id).orElseThrow(NotFoundLineException::new);
     }
 }
