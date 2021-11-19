a0802a23ded738a3dda2568bc4bb9afcb2776d7d
songseongbin <ppsosqq@gmail.com>
2021-11-19 19:46:31 +0900

    fix(LineController): ResponseEntity 타입 명시

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -20,32 +20,32 @@ public class LineController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
+    public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
 
     @GetMapping
-    public ResponseEntity readAllLines() {
+    public ResponseEntity<List<LineResponse>> readAllLines() {
         List<LineResponse> lines = lineService.readAllLines();
         return ResponseEntity.ok(lines);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity readLine(@PathVariable Long id) {
+    public ResponseEntity<LineResponse> readLine(@PathVariable Long id) {
         LineResponse line = lineService.readLine(id);
         return ResponseEntity.ok(line);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity updateLine(@PathVariable Long id,
+    public ResponseEntity<LineResponse> updateLine(@PathVariable Long id,
                                      @RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.updateLine(id, lineRequest);
         return ResponseEntity.ok(line);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteLine(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
         lineService.deleteLine(id);
         return ResponseEntity.ok().build();
     }
