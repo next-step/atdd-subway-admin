eb172434e0aa91ca632b6031507d90a2f6d9538d
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-20 01:17:07 +0900

    feat: 지하철 노선 목록, 노선 조회 기능 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,9 +7,14 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class LineService {
+    public static final String LINE_NOT_FOUND_MESSAGE = "노선이 없습니다.";
     private LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
@@ -19,5 +24,19 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(line -> LineResponse.of(line))
+                .collect(Collectors.toList());
+    }
+
+    public LineResponse findById(long id) {
+        Line line = lineRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(LINE_NOT_FOUND_MESSAGE));
+        return LineResponse.of(line);
     }
 }
