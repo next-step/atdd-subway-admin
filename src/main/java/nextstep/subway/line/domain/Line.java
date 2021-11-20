4109be779aa548775f2a02dde04e4cc3f08a7dba
신민준 <jxin95@gmail.com>
2021-11-20 17:04:38 +0900

    feat: 노선에 구간을 추가할 수 있는 로직 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,29 +1,45 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.common.BaseEntity;
+import nextstep.subway.section.domain.Distance;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.section.domain.Sections;
+import nextstep.subway.station.domain.Station;
 
 import javax.persistence.*;
 
 @Entity
 public class Line extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     private String color;
+
+    @Embedded
+    private Sections sections = new Sections();
 
     public Line() {
     }
 
-    public Line(String name, String color) {
+    public Line(final String name, final String color, final Section section) {
         this.name = name;
         this.color = color;
+        section.setLine(this);
+        this.sections.add(section);
     }
 
-    public void update(Line line) {
-        this.name = line.getName();
-        this.color = line.getColor();
+    public void update(final String name, final String color, final Station upStation, final Station downStation, final Distance distance) {
+        this.name = name;
+        this.color = color;
+        this.sections.getSections()
+                .get(0)
+                .update(upStation, downStation, distance);
+        this.updateModifiedDate();
     }
 
     public Long getId() {
@@ -37,4 +53,9 @@ public class Line extends BaseEntity {
     public String getColor() {
         return color;
     }
+
+    public Sections getSections() {
+        return sections;
+    }
+
 }
