216393c7a72c88df6a8ee06b9397ae1df3142345
신민준 <jxin95@gmail.com>
2021-11-20 17:05:32 +0900

    feat: 노선관련 비지니스 로직에 구간을 추가할 수 있는 로직 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -5,7 +5,11 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.line.dto.LineResponseList;
+import nextstep.subway.line.dto.LineResponses;
+import nextstep.subway.section.domain.Distance;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.domain.StationRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,9 +22,11 @@ import java.util.Optional;
 public class LineService {
 
     private LineRepository lineRepository;
+    private StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
     @Transactional(readOnly = true)
@@ -30,14 +36,17 @@ public class LineService {
     }
 
     @Transactional(readOnly = true)
-    public LineResponseList findAll() {
+    public LineResponses findAll() {
         List<Line> persistLines = lineRepository.findAll();
-        return new LineResponseList(persistLines);
+        return new LineResponses(persistLines);
     }
 
     public LineResponse saveLine(LineRequest request) {
+        Station upStation = this.findSection(request.getUpStationId());
+        Station downStation = this.findSection(request.getDownStationId());
+        Section section = new Section(upStation, downStation, new Distance(request.getDistance()));
         validateDuplicatedLine(request.getName());
-        Line persistLine = lineRepository.save(request.toLine());
+        Line persistLine = lineRepository.save(request.toLine(section));
         return LineResponse.of(persistLine);
     }
 
@@ -49,10 +58,17 @@ public class LineService {
     }
 
     public LineResponse updateLine(Long id, LineRequest request) {
+        Station upStation = this.findSection(request.getUpStationId());
+        Station downStation = this.findSection(request.getDownStationId());
         Line line = this.findById(id);
-        line.update(request.toLine());
+        line.update(request.getName(), request.getColor(), upStation, downStation, new Distance(request.getDistance()));
         Line persistLine = lineRepository.save(line);
         return LineResponse.of(persistLine);
+    }
+
+    private Station findSection(Long sectionId) {
+        return stationRepository.findById(sectionId)
+                .orElseThrow(() -> new NotFoundException("찾을 수 없는 구간입니다."));
     }
 
     private Line findById(Long id) {
