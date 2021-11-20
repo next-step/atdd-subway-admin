487e5e93609f43ba211fb5ec1e764ddc33b5b09f
keepbang <rlqud1125@naver.com>
2021-11-20 21:18:04 +0900

    test: StationResponses로 역할 바꿔서 테스트 추가

---
+++ src/test/java/nextstep/subway/station/dto/StationResponsesTest.java
@@ -0,0 +1,25 @@
+package nextstep.subway.station.dto;
+
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.domain.Sections;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nextstep.subway.station.domain.StationTest.강남역;
+import static nextstep.subway.station.domain.StationTest.역삼역;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StationResponsesTest {
+    @Test
+    @DisplayName("노선 구간을 지하철 역 리스트로 나열")
+    void ofByLineSectionsTest(){
+        Line line = new Line("2호선", "green");
+        Section section = new Section(강남역, 역삼역, line, 10);
+        Sections sections = line.getSections();
+
+        StationResponses stationResponses = StationResponses.ofByLineSections(sections);
+
+        assertThat(stationResponses.getStationResponses()).hasSize(2);
+    }
+}
