3e7fbfe16a2cc238cbf780cc089d54e98ce6db3c
lovethefeel <lovethefeel@gmail.com>
2021-11-21 04:43:52 +0900

    feat: 지하철 노선 API 작성

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,11 +1,15 @@
 package nextstep.subway.line.application;
 
+import nextstep.subway.common.NotFoundException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,6 +22,32 @@ public class LineService {
 
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
+        return LineResponse.ofCreation(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+        return LineResponse.ofList(lines);
+    }
+
+    public void deleteLineById(Long id) {
+        lineRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findByLineId(Long id) {
+        LineResponse lineResponse = new LineResponse();
+        Line line = lineRepository.findById(id).orElseThrow(NotFoundException::new);
+        BeanUtils.copyProperties(line, lineResponse);
+        return lineResponse;
+    }
+
+    public LineResponse updateLine(Long id, LineRequest lineRequest) {
+        LineResponse lineResponse = new LineResponse();
+        Line line = lineRepository.findById(id).orElseThrow(NotFoundException::new);
+        line.update(new Line(lineRequest.getName(), lineRequest.getColor()));
+        BeanUtils.copyProperties(line, lineResponse);
+        return lineResponse;
     }
 }
