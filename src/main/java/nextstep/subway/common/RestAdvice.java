b9df42fce8319d340ce47d4fc2d5b1521f7f37db
lovethefeel <lovethefeel@gmail.com>
2021-11-21 04:48:14 +0900

    feat: 공통 예외 처리 메소드 추가

---
+++ src/main/java/nextstep/subway/common/RestAdvice.java
@@ -0,0 +1,15 @@
+package nextstep.subway.common;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestAdvice {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
+    }
+}
