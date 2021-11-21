05ecb06c56b85b5780545b940d348f9756f5610d
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-21 18:13:43 +0900

    refactor: 예외 발생 시 리턴 메시지 상수로 변경

--- src/main/java/nextstep/subway/line/ui/exception/LineExceptionAdvice.java
+++ src/main/java/nextstep/subway/line/ui/exception/LineExceptionAdvice.java
@@ -1,6 +1,6 @@
 package nextstep.subway.line.ui.exception;
 
-import nextstep.subway.line.ui.exception.LineNotFoundException;
+import nextstep.subway.line.ui.LineController;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -8,13 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = LineController.class)
 public class LineExceptionAdvice {
+
+    private static final String LINE_ALREADY_REGISTERED = "이미 등록되어 있는 노선입니다.";
+    private static final String LINE_NOT_EXISTS = "존재하지 않는 노선입니다.";
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     protected ResponseEntity handleDataIntegrityViolationException(
         DataIntegrityViolationException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 등록되어 있는 노선입니다.");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(LINE_ALREADY_REGISTERED);
     }
 
     @ExceptionHandler(LineNotFoundException.class)
@@ -24,6 +27,6 @@ public class LineExceptionAdvice {
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     protected ResponseEntity handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 노선입니다.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(LINE_NOT_EXISTS);
     }
 }
