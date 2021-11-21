84c8a678b5d1eac79c969d80f176b4648cde9468
Wangki Lee <lwk3007@gmail.com>
2021-11-21 10:14:37 +0900

    feature: section controller addSection

--- src/main/java/nextstep/subway/line/application/SectionService.java
+++ src/main/java/nextstep/subway/line/application/SectionService.java
@@ -5,7 +5,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import nextstep.subway.common.exception.NotFoundException;
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.domain.Section;
+import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
@@ -13,9 +15,12 @@ import nextstep.subway.station.domain.StationRepository;
 @Service
 public class SectionService {
     private final StationRepository stationRepository;
+    private final LineRepository lineRepository;
 
-    public SectionService(StationRepository stationRepository) {
+    public SectionService(StationRepository stationRepository,
+        LineRepository lineRepository) {
         this.stationRepository = stationRepository;
+        this.lineRepository = lineRepository;
     }
 
     @Transactional
@@ -24,6 +29,11 @@ public class SectionService {
         Station downStation = findStationById(request.getDownStationId());
         Section section = request.toSection(upStation, downStation);
         section.changeLine(line);
+    }
+
+    @Transactional
+    public LineResponse addSection(SectionRequest request, Long lineId) {
+        return null;
     }
 
     private Station findStationById(Long stationId) {
