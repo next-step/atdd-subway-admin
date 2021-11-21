2eff473b09b9b6d2cd4008ef684237bc0e2cca33
lovethefeel <lovethefeel@gmail.com>
2021-11-21 13:04:09 +0900

    feat: 공통 에러 응답 객체 생성

---
+++ src/main/java/nextstep/subway/common/ErrorResponse.java
@@ -0,0 +1,23 @@
+package nextstep.subway.common;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+
+    private HttpStatus status;
+    private String message;
+
+    public ErrorResponse(HttpStatus status, String message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
