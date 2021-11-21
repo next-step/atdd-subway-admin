dfa1e7b7b474d45b3f31554f9006e9cbca36ff87
coder013 <wnssh607@gmail.com>
2021-11-22 01:54:32 +0900

    feat: 지하철 노선 관리 기능 구현

---
+++ src/main/java/nextstep/subway/common/exception/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.common.exception;
+
+public class ElementNotFoundException extends RuntimeException {
+
+    public ElementNotFoundException() {
+    }
+}
--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,5 +1,6 @@
 package nextstep.subway.line.application;
 
+import nextstep.subway.common.exception.ElementNotFoundException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
@@ -7,10 +8,14 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
@@ -19,5 +24,38 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    public void updateLine(Long id, LineRequest request) {
+        Optional<Line> optionalLine = lineRepository.findById(id);
+        if (!optionalLine.isPresent()) {
+            throw new ElementNotFoundException();
+        }
+        Line line = optionalLine.get();
+        line.update(request.toLine());
+        lineRepository.save(line);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(line -> LineResponse.of(line))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findLineById(Long id) {
+        Optional<Line> optionalLine = lineRepository.findById(id);
+        if (!optionalLine.isPresent()) {
+            throw new ElementNotFoundException();
+        }
+
+        return LineResponse.of(optionalLine.get());
+    }
+
+    public void deleteLineById(Long id) {
+        lineRepository.deleteById(id);
     }
 }
