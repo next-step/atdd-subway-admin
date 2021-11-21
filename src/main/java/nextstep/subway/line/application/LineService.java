384812f7b6f550fe0d5ef53c8f10aff2d38424f4
Chaeyun Lee <chaeyunn@iochord.com>
2021-11-21 18:47:51 +0900

    feat: 노선 생성 구현

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,23 +1,34 @@
 package nextstep.subway.line.application;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import nextstep.subway.exception.AppException;
+import nextstep.subway.exception.ErrorCode;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+	private LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
-        this.lineRepository = lineRepository;
-    }
+	public LineService(LineRepository lineRepository) {
+		this.lineRepository = lineRepository;
+	}
 
-    public LineResponse saveLine(LineRequest request) {
-        Line persistLine = lineRepository.save(request.toLine());
-        return LineResponse.of(persistLine);
-    }
+	public LineResponse saveLine(LineRequest request) {
+		validateDuplication(request.getName());
+		Line persistLine = lineRepository.save(request.toLine());
+		return LineResponse.of(persistLine);
+	}
+
+	private void validateDuplication(String name) {
+		if (lineRepository.existsByName(name)) {
+			throw new AppException(ErrorCode.DUPLICATE_INPUT, name + "은 중복입니다");
+		}
+	}
+
 }
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,85 +1,135 @@
 package nextstep.subway.line;
 
-import nextstep.subway.AcceptanceTest;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.AcceptanceTest;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
-    @DisplayName("지하철 노선을 생성한다.")
-    @Test
-    void createLine() {
-        // when
-        // 지하철_노선_생성_요청
+	@DisplayName("지하철 노선을 생성한다.")
+	@Test
+	void createLine() {
+		// given
+		Map<String, String> params = new HashMap<>();
+		params.put("color", "bg-red-600");
+		params.put("name", "신분당선");
 
-        // then
-        // 지하철_노선_생성됨
-    }
+		// when
+		// 지하철_노선_생성_요청
+		ExtractableResponse<Response> response = RestAssured
+			.given().log().all()
+			.body(params)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when()
+			.post("/lines")
+			.then().log().all()
+			.extract();
 
-    @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
-    @Test
-    void createLine2() {
-        // given
-        // 지하철_노선_등록되어_있음
+		// then
+		// 지하철_노선_생성됨
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+		assertThat(response.header("Location")).isNotBlank();
+		assertThat(response.body().jsonPath().getLong("id")).isPositive();
+		assertThat(response.body().jsonPath().getString("name")).isEqualTo("신분당선");
+		assertThat(response.body().jsonPath().getString("color")).isEqualTo("bg-red-600");
+		assertThat(response.body().jsonPath().getString("createdDate")).isNotNull();
+		assertThat(response.body().jsonPath().getString("modifiedDate")).isNotNull();
+	}
 
-        // when
-        // 지하철_노선_생성_요청
+	@DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
+	@Test
+	void createLine2() {
+		// given
+		// 지하철_노선_등록되어_있음
+		Map<String, String> params = new HashMap<>();
+		params.put("color", "bg-red-600");
+		params.put("name", "신분당선");
+		RestAssured.given().log().all()
+			.body(params)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when()
+			.post("/lines")
+			.then().log().all()
+			.extract();
 
-        // then
-        // 지하철_노선_생성_실패됨
-    }
+		// when
+		// 지하철_노선_생성_요청
+		ExtractableResponse<Response> response = RestAssured
+			.given().log().all()
+			.body(params)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.when()
+			.post("/lines")
+			.then().log().all()
+			.extract();
 
-    @DisplayName("지하철 노선 목록을 조회한다.")
-    @Test
-    void getLines() {
-        // given
-        // 지하철_노선_등록되어_있음
-        // 지하철_노선_등록되어_있음
+		// then
+		// 지하철_노선_생성_실패됨
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+	}
 
-        // when
-        // 지하철_노선_목록_조회_요청
+	@DisplayName("지하철 노선 목록을 조회한다.")
+	@Test
+	void getLines() {
+		// given
+		// 지하철_노선_등록되어_있음
+		// 지하철_노선_등록되어_있음
 
-        // then
-        // 지하철_노선_목록_응답됨
-        // 지하철_노선_목록_포함됨
-    }
+		// when
+		// 지하철_노선_목록_조회_요청
 
-    @DisplayName("지하철 노선을 조회한다.")
-    @Test
-    void getLine() {
-        // given
-        // 지하철_노선_등록되어_있음
+		// then
+		// 지하철_노선_목록_응답됨
+		// 지하철_노선_목록_포함됨
+	}
 
-        // when
-        // 지하철_노선_조회_요청
+	@DisplayName("지하철 노선을 조회한다.")
+	@Test
+	void getLine() {
+		// given
+		// 지하철_노선_등록되어_있음
 
-        // then
-        // 지하철_노선_응답됨
-    }
+		// when
+		// 지하철_노선_조회_요청
 
-    @DisplayName("지하철 노선을 수정한다.")
-    @Test
-    void updateLine() {
-        // given
-        // 지하철_노선_등록되어_있음
+		// then
+		// 지하철_노선_응답됨
+	}
 
-        // when
-        // 지하철_노선_수정_요청
+	@DisplayName("지하철 노선을 수정한다.")
+	@Test
+	void updateLine() {
+		// given
+		// 지하철_노선_등록되어_있음
 
-        // then
-        // 지하철_노선_수정됨
-    }
+		// when
+		// 지하철_노선_수정_요청
 
-    @DisplayName("지하철 노선을 제거한다.")
-    @Test
-    void deleteLine() {
-        // given
-        // 지하철_노선_등록되어_있음
+		// then
+		// 지하철_노선_수정됨
+	}
 
-        // when
-        // 지하철_노선_제거_요청
+	@DisplayName("지하철 노선을 제거한다.")
+	@Test
+	void deleteLine() {
+		// given
+		// 지하철_노선_등록되어_있음
 
-        // then
-        // 지하철_노선_삭제됨
-    }
+		// when
+		// 지하철_노선_제거_요청
+
+		// then
+		// 지하철_노선_삭제됨
+	}
 }
