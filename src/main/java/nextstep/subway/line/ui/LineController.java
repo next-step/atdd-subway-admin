18131eebb0fc663f6b112696161f7fe0018e8818
condingknowjam <hanwoodww@gmail.com>
2021-11-21 23:32:07 +0900

    test: 지하철 노선 전체 목록 조회 테스트

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,18 +1,22 @@
 package nextstep.subway.line.ui;
 
 import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -21,6 +25,11 @@ public class LineController {
 
     public LineController(final LineService lineService) {
         this.lineService = lineService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok(lineService.findAllLines());
     }
 
     @PostMapping
--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -2,12 +2,17 @@ package nextstep.subway.line;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineResponse;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -15,6 +20,7 @@ import org.springframework.http.MediaType;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
@@ -26,19 +32,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // when
         // 지하철_노선_생성_요청
-        Response response = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(params)
-            .when()
-            .post("/lines")
-            .then().log().all()
-            .extract().response();
+        Response response = requestCreateLines(params);
 
         // then
         // 지하철_노선_생성됨
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.header("Location")).isNotBlank();
     }
+
+
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
     @Test
@@ -49,23 +51,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
         params.put("name", "천안역");
         params.put("color", "blue");
 
-        RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(params)
-            .when()
-            .post("/lines")
-            .then().log().all()
-            .extract();
+        requestCreateLines(params);
 
         // when
-        // 지하철_노선_생성_요청
-        Response response = RestAssured.given().log().all()
-            .body(params)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/lines")
-            .then().log().all()
-            .extract().response();
+        // 지하철_노선_중복생성_요청
+        Response response = requestCreateLines(params);
 
         // then
         // 지하철_노선_생성_실패됨
@@ -78,13 +68,41 @@ public class LineAcceptanceTest extends AcceptanceTest {
         // given
         // 지하철_노선_등록되어_있음
         // 지하철_노선_등록되어_있음
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "천안역");
+        params.put("color", "blue");
+        Map<String, String> otherParams = new HashMap<>();
+        otherParams.put("name", "서울역");
+        otherParams.put("color", "blue");
+
+        Response createResponse = requestCreateLines(params);
+        Response otherCreateResponse = requestCreateLines(otherParams);
 
         // when
         // 지하철_노선_목록_조회_요청
+        Response response = requestFindAllLines();
 
         // then
         // 지하철_노선_목록_응답됨
         // 지하철_노선_목록_포함됨
+        List<Long> findIds = response.jsonPath().getList(".", LineResponse.class)
+            .stream()
+            .map(lineResponse -> lineResponse.getId())
+            .collect(Collectors.toList());
+
+        List<Long> createIds = Arrays.asList(createResponse, otherCreateResponse).stream()
+            .map(res -> Long.parseLong(res.getHeader("Location").split("/")[2]))
+            .collect(Collectors.toList());
+
+        assertThat(findIds).containsAll(createIds);
+    }
+
+    private Response requestFindAllLines() {
+        return RestAssured.given().log().all()
+            .when()
+            .get("/lines")
+            .then().log().all()
+            .extract().response();
     }
 
     @DisplayName("지하철 노선을 조회한다.")
@@ -124,5 +142,17 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
         // then
         // 지하철_노선_삭제됨
+    }
+
+    private Response requestCreateLines(Map<String, String> params) {
+        Response response = RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(params)
+            .when()
+            .post("/lines")
+            .then().log().all()
+            .extract()
+            .response();
+        return response;
     }
 }
