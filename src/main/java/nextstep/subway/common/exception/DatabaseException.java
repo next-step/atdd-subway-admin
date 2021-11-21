d8f54487dff3fd094352ffaaef5d15a568a70981
wooobo <irooori89@gmail.com>
2021-11-21 23:45:12 +0900

    feat : `DatabaseException` 생성자 추가

--- src/main/java/nextstep/subway/common/exception/DatabaseException.java
+++ src/main/java/nextstep/subway/common/exception/DatabaseException.java
@@ -12,4 +12,17 @@ public class DatabaseException extends RuntimeException {
     public DatabaseException(String message) {
         super(message);
     }
+
+    public DatabaseException(String message, Throwable cause, boolean enableSuppression,
+        boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DatabaseException(Throwable cause) {
+        super(cause);
+    }
 }
