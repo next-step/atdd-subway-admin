43fa5d2b2684a8000d58986ad1e92ad969ddc9e6
youngrock.choi <tbvjdudfhr@naver.com>
2021-11-22 00:44:26 +0900

    feat: 노선의 역 목록을 조회하는 기능 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,26 +1,42 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.common.BaseEntity;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Entity
 public class Line extends BaseEntity {
     private static final String ERROR_MESSAGE = "빈 값을 입력하였습니다.";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     private String color;
 
-    public Line() {
+    @OneToMany(mappedBy = "line")
+    private List<Section> sections = new ArrayList<>();
+
+    protected Line() {
     }
 
     public Line(String name, String color) {
         validate(name, color);
         this.name = name;
         this.color = color;
+    }
+
+    public void addSection(Station upStation, Station downStation, int distance) {
+        this.sections.add(new Section(this, upStation, downStation, distance));
     }
 
     public void update(Line line) {
@@ -40,8 +56,25 @@ public class Line extends BaseEntity {
         return color;
     }
 
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public List<Station> getStations() {
+        return sections.stream()
+                .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<StationResponse> getStationResponses() {
+        return getStations().stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList());
+    }
+
     private void validate(String name, String color) {
-        if(isBlank(name) || isBlank(color)) {
+        if (isBlank(name) || isBlank(color)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
     }
