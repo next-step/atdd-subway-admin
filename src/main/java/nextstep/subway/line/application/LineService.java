415212d898697a12595d5a04df85489682aeb0f6
Lewis Seo <togokr@naver.com>
2021-11-21 09:55:40 +0900

    Step1 - 지하철 노선 관리 (#418)
    * feat(LineAcceptanceTest): 지하철 노선 인수 테스트 추가
    
    LineAcceptanceTest.java
    - 지하철 노선 생성 추가
    - 기존 존재하는 노선 추가 에러 확인 추가
    - 지하철 노선 목록 조회 추가
    - 지하철 노선 조회 추가
    - 지하철 노선 수정 추가
    - 지하철 노선 제거 추가
    LineController.java
    - showLines 노선 목록들 조회 추가
    - showLineById 노선 조회 추가
    - updateLineById 노선 수정 추가
    - deleteLineById 노선 삭제 추가
    LineResponse.java
    - equals, hashcode 추가 (id로 비교)
    LineFixture.java
    - requestCreateLine 노선 추가 메서드
    - requestUpdateLine 노선 수정 메서드
    - requestGetLines 노선들 조회 메서드
    - requestGetLineById 노선 조회 메서드
    - ofLineResponse 노선 parsing fixture
    - ofLineResponses 노선들 parsing fixture
    
    * feat(StationAcceptanceTest): 지하철 역 fixture 생성
    
    LineFixture.java
    - requestCreateStations 역 추가 메서드
    - requestGetStations 역 조회 메서드
    - requestDeleteStations 역 삭제 메서드
    - ofStationIds 역들 id parsing fixture
    
    * fix(LineAcceptanceTest): 지하철 역 예외 핸들러 추가
    
    LineController.java
    - 지하철 역 예외 핸들러 추가
    LineAcceptanceTest.java
    - Internal_Server_error -> Bad_Request로 변경

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,10 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class LineService {
@@ -19,5 +23,34 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(LineResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public LineResponse findLineById(Long id) {
+        Line line = lineRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new); // fixme : custom exception
+
+        return LineResponse.of(line);
+    }
+
+    @Transactional
+    public LineResponse update(Long id, LineRequest lineRequest) {
+        Line line = lineRepository.findById(id)
+                .orElseThrow(NoSuchElementException::new); // fixme : custom exception
+        line.update(lineRequest.toLine());
+        return LineResponse.of(line);
+    }
+
+    public void deleteLineById(Long id) {
+        lineRepository.deleteById(id);
     }
 }
--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,15 +1,25 @@
 package nextstep.subway.line.ui;
 
 import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -24,5 +34,32 @@ public class LineController {
     public ResponseEntity createLine(@RequestBody LineRequest lineRequest) {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok().body(lineService.findAllLines());
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity showLineById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(lineService.findLineById(id));
+    }
+
+    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateLineById(@PathVariable Long id, @RequestBody LineRequest lineRequest) {
+        LineResponse line = lineService.update(id, lineRequest);
+        return ResponseEntity.ok().body(line);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteLine(@PathVariable Long id) {
+        lineService.deleteLineById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
