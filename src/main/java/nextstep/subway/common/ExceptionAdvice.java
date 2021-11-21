0eb1d2d6083f0c589e8bce89a262b454510442aa
김도영 <95doyoung@gmail.com>
2021-11-21 14:58:00 +0900

    refactor (ExceptionAdvice) : @ControllerAdvice + @ControllerAdvice 에서  @RestControllerAdvice 로 변경

--- src/main/java/nextstep/subway/common/ExceptionAdvice.java
+++ src/main/java/nextstep/subway/common/ExceptionAdvice.java
@@ -1,13 +1,11 @@
 package nextstep.subway.common;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
 public class ExceptionAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
