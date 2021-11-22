d10474414c71e252102a349422c77ca68fd817c2
moon <hoopster@airi.kr>
2021-11-22 20:21:41 +0900

    refac (Sections, Station) : 지하철역 중복리스트 제거 리팩토링

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -1,5 +1,7 @@
 package nextstep.subway.station.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
@@ -20,5 +22,20 @@ public class Station extends BaseEntity {
 
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Station station = (Station)o;
+		return Objects.equals(getId(), station.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getId());
 	}
 }
