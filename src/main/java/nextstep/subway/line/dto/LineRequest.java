82715221d2074429ab22c1b6a4ddbb0a27430712
류성호 <>
2021-11-23 00:09:23 +0900

    feat : LineRequest 상행선 ID, 하행선ID, 거리 추가

--- src/main/java/nextstep/subway/line/dto/LineRequest.java
+++ src/main/java/nextstep/subway/line/dto/LineRequest.java
@@ -1,16 +1,30 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.section.application.dto.SectionSaveRequest;
 
 public class LineRequest {
 
     private String name;
     private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    protected LineRequest() {
+    }
 
     public LineRequest(String name, String color) {
+        this(name, color, null, null, 0);
+    }
+
+    public LineRequest(String name, String color, Long upStationId, Long downStationId, int distance) {
         validateLineRequest(name, color);
         this.name = name;
         this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
     }
 
     private void validateLineRequest(String name, String color) {
@@ -30,7 +44,23 @@ public class LineRequest {
         return color;
     }
 
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
     public Line toLine() {
         return new Line(name, color);
+    }
+
+    public SectionSaveRequest toSectionSaveRequest() {
+        return new SectionSaveRequest(upStationId, downStationId, distance);
     }
 }
