17d6d3ef624434eb03f09a9756649334173e0b63
류성호 <>
2021-11-22 18:10:10 +0900

    test : 역 미리 생성 메서드 범용 post 메소드 사용하게 수정

--- src/test/java/nextstep/subway/station/TestStationFactory.java
+++ src/test/java/nextstep/subway/station/TestStationFactory.java
@@ -1,22 +1,15 @@
 package nextstep.subway.station;
 
-import io.restassured.RestAssured;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
-import org.springframework.http.MediaType;
+import nextstep.subway.utils.TestPostRequestFactory;
 
 public class TestStationFactory {
 
     public static StationResponse 역_미리_생성(String name) {
         StationRequest stationRequest = new StationRequest(name);
 
-        return RestAssured.given().log().all()
-                .body(stationRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/stations")
-                .then().log().all()
-                .extract()
+        return TestPostRequestFactory.요청_post("/stations", stationRequest)
                 .body()
                 .as(StationResponse.class);
     }
