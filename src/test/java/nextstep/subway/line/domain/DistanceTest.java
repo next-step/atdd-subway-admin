0869646c217925d8a05c7106b27876ec15281ea9
vicjed <vicjed@icloud.com>
2021-11-22 16:42:19 +0900

    test(Distance): 테스트 추가

---
+++ src/test/java/nextstep/subway/line/domain/DistanceTest.java
@@ -0,0 +1,16 @@
+package nextstep.subway.line.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DistanceTest {
+
+    @Test
+    void 거리는_0보다_작을_수_없다() {
+        // then
+        Assertions.assertThatThrownBy(() -> {
+                      Distance distance = new Distance(-1);
+                  }).isInstanceOf(IllegalArgumentException.class)
+                  .hasMessage("구간의 거리가 0 또는 0보다 작을 수 없습니다.");
+    }
+}
