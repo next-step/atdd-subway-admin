f11a5be8c1c18589f86df20afc6530d814a03f2f
ChangSubKwak <paradise1732@gmail.com>
2021-11-22 17:40:16 +0900

    feat: 구간의 일급콜렉션(Sections) 클래스 작성

---
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -0,0 +1,37 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Sections {
+    @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Section> sections;
+
+    protected Sections() {
+        sections = new ArrayList<>();
+    }
+
+    private Sections(List<Section> sections) {
+        this.sections = sections;
+    }
+
+    public static Sections from(List<Section> sections) {
+        return new Sections(sections);
+    }
+
+    public void add(Section section) {
+        sections.add(section);
+    }
+
+    public void remove(Section section) {
+        sections.remove(section);
+    }
+
+    public List<Section> sections() {
+        return sections;
+    }
+}
