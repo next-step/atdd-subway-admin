4a57d40c4be5924bf1b6e305b8b72e68f40c0b1d
songseongbin <ppsosqq@gmail.com>
2021-11-23 19:08:16 +0900

    style(Line, Section): indent 수정

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -11,63 +11,63 @@ import nextstep.subway.section.domain.Sections;
 
 @Entity
 public class Line extends BaseEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Embedded
-	private LineName name;
+    @Embedded
+    private LineName name;
 
-	@Embedded
-	private LineColor color;
+    @Embedded
+    private LineColor color;
 
-	@Embedded
-	private Sections sections = Sections.empty();
+    @Embedded
+    private Sections sections = Sections.empty();
 
-	protected Line() {
-	}
+    protected Line() {
+    }
 
-	private Line(String name, String color) {
-		this.name = LineName.from(name);
-		this.color = LineColor.from(color);
-	}
+    private Line(String name, String color) {
+        this.name = LineName.from(name);
+        this.color = LineColor.from(color);
+    }
 
-	private Line(String name, String color, Long upStationId, Long downStationId, int distance) {
-		this.name = LineName.from(name);
-		this.color = LineColor.from(color);
-		this.sections.connect(Section.of(this, upStationId, downStationId, distance));
-	}
+    private Line(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this.name = LineName.from(name);
+        this.color = LineColor.from(color);
+        this.sections.connect(Section.of(this, upStationId, downStationId, distance));
+    }
 
-	public static Line from(String name, String color) {
-		return new Line(name, color);
-	}
+    public static Line from(String name, String color) {
+        return new Line(name, color);
+    }
 
-	public static Line from(String name, String color, Long upStationId, Long downStationId, int distance) {
-		return new Line(name, color, upStationId, downStationId, distance);
-	}
+    public static Line from(String name, String color, Long upStationId, Long downStationId, int distance) {
+        return new Line(name, color, upStationId, downStationId, distance);
+    }
 
-	public void update(Line line) {
-		this.name = line.getName();
-		this.color = line.getColor();
-	}
+    public void update(Line line) {
+        this.name = line.getName();
+        this.color = line.getColor();
+    }
 
-	public void addSection(Section section) {
-		sections.connect(section);
-	}
+    public void addSection(Section section) {
+        sections.connect(section);
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public LineName getName() {
-		return name;
-	}
+    public LineName getName() {
+        return name;
+    }
 
-	public LineColor getColor() {
-		return color;
-	}
+    public LineColor getColor() {
+        return color;
+    }
 
-	public Sections getSections() {
-		return sections;
-	}
+    public Sections getSections() {
+        return sections;
+    }
 }
--- src/main/java/nextstep/subway/section/domain/Section.java
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -8,44 +8,44 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 
 @Entity
 public class Section {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "line_id", foreignKey = @ForeignKey(name = "fk_line_of_section"))
-	private Line line;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "up_station_id", foreignKey = @ForeignKey(name = "fk_up_station_of_section"))
-	private Station upStation;
+    @ManyToOne
+    @JoinColumn(name = "line_id", foreignKey = @ForeignKey(name = "fk_line_of_section"))
+    private Line line;
 
-	@ManyToOne
-	@JoinColumn(name = "down_station_id", foreignKey = @ForeignKey(name = "fk_down_station_of_section"))
-	private Station downStation;
+    @ManyToOne
+    @JoinColumn(name = "up_station_id", foreignKey = @ForeignKey(name = "fk_up_station_of_section"))
+    private Station upStation;
 
-	@Embedded
-	private Distance distance;
+    @ManyToOne
+    @JoinColumn(name = "down_station_id", foreignKey = @ForeignKey(name = "fk_down_station_of_section"))
+    private Station downStation;
 
-	protected Section() {
-	}
+    @Embedded
+    private Distance distance;
 
-	private Section(Line line, Station upStation, Station downStation, int distance) {
-		this.line = line;
-		this.upStation = upStation;
-		this.downStation = downStation;
-		this.distance = Distance.from(distance);
-	}
+    protected Section() {
+    }
 
-	public static Section of(Line line, Long upStationId, Long downStationId, Integer distance) {
-		return new Section(line, Station.of(upStationId), Station.of(downStationId), distance);
-	}
+    private Section(Line line, Station upStation, Station downStation, int distance) {
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = Distance.from(distance);
+    }
+
+    public static Section of(Line line, Long upStationId, Long downStationId, Integer distance) {
+        return new Section(line, Station.of(upStationId), Station.of(downStationId), distance);
+    }
 
     public void updateForInsertMiddle(Section section) {
         this.upStation = section.upStation;
