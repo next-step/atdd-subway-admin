3d6e4fb2005c045194010c159683ef688eb92370
eedys <eedys@ex-em.com>
2021-11-23 23:06:26 +0900

    feat 예외처리 인수테스트

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -3,8 +3,6 @@ package nextstep.subway.section;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
-import nextstep.subway.line.LineAcceptanceTest;
-import nextstep.subway.station.StationAcceptanceTest;
 import nextstep.subway.utils.ApiUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -79,5 +77,66 @@ public class SectionAcceptanceTest extends AcceptanceTest {
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
+
+    @DisplayName("구간 추가하기 - 기존 상행 하행 사이에 등록될 경우 예외처리")
+    @Test
+    void addSection_DistanceGraterEqualExceptionTest() {
+
+        ExtractableResponse<Response> lineResponse = 지하철_노선_등록("신분당선", "bg-red-600", 7);
+        ExtractableResponse<Response> stationResponse = 지하철역_등록("신대방");
+
+        long lineId = lineResponse.body().jsonPath().getLong("id");
+        long upStationId = stationResponse.body().jsonPath().getLong("id");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("upStationId", upStationId);
+        params.put("downStationId", 4);
+        params.put("distance", 7);
+
+        ExtractableResponse<Response> response = ApiUtils.post(String.format("/lines/%s/sections", lineId), params);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @DisplayName("구간 추가 시 동일한 상행, 하행 등록 예외처리")
+    @Test
+    void addSection_EqualSectionExceptionTest() {
+
+        ExtractableResponse<Response> lineResponse = 지하철_노선_등록("신분당선", "bg-red-600", 7);
+
+        long lineId = lineResponse.body().jsonPath().getLong("id");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("upStationId", 1);
+        params.put("downStationId", 2);
+        params.put("distance", 4);
+
+        ExtractableResponse<Response> response = ApiUtils.post(String.format("/lines/%s/sections", lineId), params);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
+    @DisplayName("구간 추가 시 상행과 하행역 둘 다 포함되어있지 않으면 예외처리")
+    @Test
+    void addSection_UpStationOrDownStation_NotContainsExceptionTest() {
+
+        ExtractableResponse<Response> lineResponse = 지하철_노선_등록("신분당선", "bg-red-600", 7);
+        ExtractableResponse<Response> upStationResponse = 지하철역_등록("신대방");
+        ExtractableResponse<Response> downStationResponse = 지하철역_등록("신림");
+
+        long lineId = lineResponse.body().jsonPath().getLong("id");
+        long upStationId = upStationResponse.body().jsonPath().getLong("id");
+        long downStationId = downStationResponse.body().jsonPath().getLong("id");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", 4);
+
+        ExtractableResponse<Response> response = ApiUtils.post(String.format("/lines/%s/sections", lineId), params);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+
 
 }
