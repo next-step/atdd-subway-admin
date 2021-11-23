9227e039a5fd34b6206463356184abad890db987
jongwoo <jongwoo.dev@gmail.com>
2021-11-24 02:23:06 +0900

    refactor: line controller에 에러 핸들러 추가 및 인수테스트 수정 - 500에러 -> 400에러

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -5,8 +5,10 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +52,10 @@ public class LineController {
         Line newLine = lineRequest.toLineWithId(id);
         lineService.updateLine(newLine);
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleIllegalArgsException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
