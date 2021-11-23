3d60d1c7e57a6df89076bba49534e287cf5b81ba
lewisseo91 <togokr@naver.com>
2021-11-23 15:48:24 +0900

    fix(DuplicateEntityException): DataIntegrityViolationException -> RuntimeException 변경
    DataIntegrityViolationException -> RuntimeException 변경
    DataIntegrityViolationException 은 spring data jpa에 종속적인 측면이 있어서
    변경 (springframework.dao 에 속함)

--- src/main/java/nextstep/subway/common/exception/DuplicateEntityException.java
+++ src/main/java/nextstep/subway/common/exception/DuplicateEntityException.java
@@ -1,8 +1,6 @@
 package nextstep.subway.common.exception;
 
-import org.springframework.dao.DataIntegrityViolationException;
-
-public class DuplicateEntityException extends DataIntegrityViolationException {
+public class DuplicateEntityException extends RuntimeException {
     private static final long serialVersionUID = 2L;
 
     public DuplicateEntityException(String message) {
