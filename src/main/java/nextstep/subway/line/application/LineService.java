88decfd85dba624dde5de1f833c8ac78bc9ecce1
sungwoo <ssw890418@gmail.com>
2021-11-23 23:58:49 +0900

    refactor:라인목록 조회와 라인 하나를 조회하는 서비스 구현

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,9 @@ import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Transactional
 public class LineService {
@@ -19,5 +22,24 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
+    }
+
+    public List<LineResponse> findAllLines() {
+        List<Line> findLines = lineRepository.findAll();
+
+        List<LineResponse> responses = new ArrayList<>();
+        for(Line line : findLines) {
+            responses.add(LineResponse.of(line));
+        }
+        return responses;
+
+    }
+
+    public LineResponse findById(Long lineId) {
+        System.out.println("findById start");
+        Line line = lineRepository.findById(lineId).orElseThrow(
+                () -> new IllegalArgumentException(String.format("존재하지 않는 노선입니다.[lineId: %s]", lineId))
+        );
+        return LineResponse.of(line);
     }
 }
