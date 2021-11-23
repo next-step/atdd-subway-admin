e88d352606e231d5cc8a9b58e36f6dbd7870df30
sungwoo <ssw890418@gmail.com>
2021-11-23 23:56:52 +0900

    refactor:라인목록과 라인하나를 조회하는 요청 추가

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -3,13 +3,12 @@ package nextstep.subway.line.ui;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lines")
@@ -25,4 +24,19 @@ public class LineController {
         LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
     }
+
+    @GetMapping
+    public ResponseEntity findAllLines() {
+        List<LineResponse> lines = lineService.findAllLines();
+        return ResponseEntity.ok().body(lines);
+    }
+
+    @GetMapping(value = "/{lineId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity findLine(@PathVariable Long lineId) {
+        System.out.println("findLine start");
+        LineResponse line = lineService.findById(lineId);
+        return ResponseEntity.ok().body(line);
+    }
+
+
 }
