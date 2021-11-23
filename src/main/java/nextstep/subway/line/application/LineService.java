842e5b689bf572cdb59f8836e728f156ed429d55
sungwoo <ssw890418@gmail.com>
2021-11-24 01:37:50 +0900

    refactor:노선 수정, 삭제 로직 구현 추가

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -4,6 +4,8 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.ui.exception.DuplicateLineNameException;
+import nextstep.subway.line.ui.exception.NotFoundLineException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,9 @@ public class LineService {
     }
 
     public LineResponse saveLine(LineRequest request) {
+        if(lineRepository.existsByName(request.getName())){
+            throw new DuplicateLineNameException(String.format("노선 이름이 이미 존재합니다.[%s]", request.getName()));
+        }
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
     }
@@ -32,13 +37,23 @@ public class LineService {
             responses.add(LineResponse.of(line));
         }
         return responses;
-
     }
 
     public LineResponse findById(Long lineId) {
         Line line = lineRepository.findById(lineId).orElseThrow(
-                () -> new IllegalArgumentException(String.format("존재하지 않는 노선입니다.[lineId: %s]", lineId))
+                () -> new NotFoundLineException(String.format("존재하지 않는 노선입니다.[lineId: %s]", lineId))
         );
         return LineResponse.of(line);
+    }
+
+    public void updateLine(Long lineId, LineRequest lineRequest) {
+        Line line = lineRepository.findById(lineId).orElseThrow(
+                () -> new NotFoundLineException(String.format("존재하지 않는 노선입니다.[lineId: %s]", lineId))
+        );
+        line.update(new Line(lineRequest.getName(), lineRequest.getColor()));
+    }
+
+    public void deleteLineById(Long lineId) {
+        lineRepository.deleteById(lineId);
     }
 }
