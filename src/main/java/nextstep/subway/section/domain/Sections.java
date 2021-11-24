58d1a62780f9f267ac67c039c85e03d53cd3ff7a
eedys <eedys@ex-em.com>
2021-11-24 21:53:42 +0900

    refactor(Sections) : MESSAGE REMOVE

--- src/main/java/nextstep/subway/section/domain/Sections.java
+++ src/main/java/nextstep/subway/section/domain/Sections.java
@@ -11,16 +11,12 @@ import java.util.function.Predicate;
 
 import static java.util.stream.Collectors.toList;
 
-import static java.util.stream.Collectors.toList;
-
 @Embeddable
 public class Sections {
 
     private static final String STATION_NOT_CONTAINS_MESSAGE = "상행과 하행 모두 포함되지 않습니다.";
     private static final String SECTION_NOT_CORRECT_MESSAGE = "동일한 구간을 추가할 수 없습니다.";
     private static final String DISTANCE_GREATER_OR_CORRECT_MESSAGE = "거리가 작아야합니다.";
-    private static final String UP_STATION_NOT_CORRECT_MESSAGE = "일치하는 상행역이 존재하지 않습니다.";
-    private static final String DOWN_STATION_NOT_CORRECT_MESSAGE = "일치하는 하행역이 존재하지 않습니다.";
     private static final String SECTION_NOT_EXIST_MESSAGE = "구간이 1개일 때 삭제할 수 없습니다.";
 
     @OneToMany(mappedBy = "line", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
