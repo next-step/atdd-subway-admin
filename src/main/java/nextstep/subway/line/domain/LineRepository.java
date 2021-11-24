314524719002f4bee6700c97a414fa91dc97e27e
kelicia91 <kelicia91@gmail.com>
2021-11-24 23:07:25 +0900

    style: JPQL

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -10,11 +10,11 @@ public interface LineRepository extends JpaRepository<Line, Long> {
 	// compare for (nextstep.subway.line 구간 추가 기능 7 tests)
 	// before(default findById, lazy-loading): 5 sec 789 ms ~ 6 sec 110 ms
 	// after(fetch join): 4 sec 215 ms ~ 4 sec 591 ms
-	@Query("SELECT DISTINCT l from Line l"
-		+ " join fetch l.sections w"
-		+ " join fetch w.sections sc"
-		+ " join fetch sc.upStation"
-		+ " join fetch sc.downStation"
+	@Query("SELECT DISTINCT l FROM Line l"
+		+ " JOIN FETCH l.sections w"
+		+ " JOIN FETCH w.sections sc"
+		+ " JOIN FETCH sc.upStation"
+		+ " JOIN FETCH sc.downStation"
 	)
 	Optional<Line> findById(Long id);
 }
