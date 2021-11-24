dce1d7158ffb81a62aad6bbc997378e27432e71e
박창혁 <changchang73@pionnet.co.kr>
2021-11-24 21:51:18 +0900

    style : 지하철역 -> 노선 명 으로 변경

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,6 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
-import nextstep.subway.line.dto.LineResponse;
 
 @DisplayName("지하철 노선 관련 기능")
 public class LineAcceptanceTest extends AcceptanceTest {
@@ -27,13 +25,13 @@ public class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void createLine() {
         //given
-        LineRequest 화곡역 = 지하철_노선_정보("화곡", "purple");
+        LineRequest 신분당선 = 지하철_노선_정보("신분당선", "red");
 
         // when
-        ExtractableResponse<Response> 노선_생성_응답 = 지하철_노선_생성_요청(화곡역);
+        ExtractableResponse<Response> 신분당선_생성_응답 = 지하철_노선_생성_요청(신분당선);
 
         // then
-        지하철_노선_생성됨(노선_생성_응답);
+        지하철_노선_생성됨(신분당선_생성_응답);
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
@@ -41,11 +39,11 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void createLine2() {
         // given
         // 지하철_노선_등록되어_있음
-        LineRequest 화곡역 = 지하철_노선_정보("화곡", "purple");
-        ExtractableResponse<Response> 화곡역_생성_응답 = 지하철_노선_생성_요청(화곡역);
+        LineRequest 신분당선 = 지하철_노선_정보("신분당선", "red");
+        ExtractableResponse<Response> 신분당선_생성_응답 = 지하철_노선_생성_요청(신분당선);
 
         // when
-        ExtractableResponse<Response> 중복_결과_응답 = 지하철_노선_생성_요청(화곡역);
+        ExtractableResponse<Response> 중복_결과_응답 = 지하철_노선_생성_요청(신분당선);
 
         // then
         지하철_노선_생성_실패됨(중복_결과_응답);
@@ -56,18 +54,18 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLines() {
         // given
         // 지하철_노선_등록되어_있음
-        LineRequest 화곡역 = 지하철_노선_정보("화곡", "purple");
-        ExtractableResponse<Response> 화곡역_생성_응답 = 지하철_노선_생성_요청(화곡역);
+        LineRequest 신분당선 = 지하철_노선_정보("신분당선", "red");
+        ExtractableResponse<Response> 신분당선_생성_응답 = 지하철_노선_생성_요청(신분당선);
         // 지하철_노선_등록되어_있음
-        LineRequest 사당역 = 지하철_노선_정보("사당", "blue");
-        ExtractableResponse<Response> 사당역_생성_응답 = 지하철_노선_생성_요청(사당역);
+        LineRequest 공항철도 = 지하철_노선_정보("공항철도", "blue");
+        ExtractableResponse<Response> 공항철도_생성_응답 = 지하철_노선_생성_요청(공항철도);
 
         // when
         ExtractableResponse<Response> 노선_목록_응답 = 지하철_노선_목록_조회();
 
         // then
         지하철_노선_응답됨(노선_목록_응답);
-        지하철_노선_목록_포함됨(노선_목록_응답, 화곡역_생성_응답, 사당역_생성_응답);
+        지하철_노선_목록_포함됨(노선_목록_응답, 신분당선_생성_응답, 공항철도_생성_응답);
     }
 
     @DisplayName("지하철 노선을 조회한다.")
@@ -75,14 +73,14 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void getLine() {
         // given
         // 지하철_노선_등록되어_있음
-        LineRequest 화곡역 = 지하철_노선_정보("화곡", "purple");
-        ExtractableResponse<Response> 화곡역_생성_응답 = 지하철_노선_생성_요청(화곡역);
+        LineRequest 신분당선 = 지하철_노선_정보("신분당선", "red");
+        ExtractableResponse<Response> 신분당선_생성_응답 = 지하철_노선_생성_요청(신분당선);
 
         // when
-        ExtractableResponse<Response> 화곡역_조회_응답 = 지하철_노선_조회_요청(화곡역_생성_응답);
+        ExtractableResponse<Response> 신분당선_조회_응답 = 지하철_노선_조회_요청(신분당선_생성_응답);
 
         // then
-        지하철_노선_응답됨(화곡역_조회_응답);
+        지하철_노선_응답됨(신분당선_조회_응답);
     }
 
     @DisplayName("지하철 노선을 수정한다.")
@@ -90,15 +88,15 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void updateLine() {
         // given
         // 지하철_노선_등록되어_있음
-        LineRequest 화곡역 = 지하철_노선_정보("화곡", "purple");
-        ExtractableResponse<Response> 화곡역_생성_응답 = 지하철_노선_생성_요청(화곡역);
-        LineRequest 까치산역 = 지하철_노선_정보("까치산", "purple");
+        LineRequest 신분당선 = 지하철_노선_정보("신분당선", "red");
+        ExtractableResponse<Response> 신분당선_생성_응답 = 지하철_노선_생성_요청(신분당선);
+        LineRequest 공항철도 = 지하철_노선_정보("공항철도", "blue");
 
         // when
-        ExtractableResponse<Response> 화곡역_수정_응답 = 지하철_노선_수정_요청(화곡역_생성_응답, 까치산역);
+        ExtractableResponse<Response> 신분당선_수정_응답 = 지하철_노선_수정_요청(신분당선_생성_응답, 공항철도);
 
         // then
-        지하철_노선_응답됨(화곡역_수정_응답);
+        지하철_노선_응답됨(신분당선_수정_응답);
     }
 
     @DisplayName("지하철 노선을 제거한다.")
@@ -106,14 +104,14 @@ public class LineAcceptanceTest extends AcceptanceTest {
     void deleteLine() {
         // given
         // 지하철_노선_등록되어_있음
-        LineRequest 화곡역 = 지하철_노선_정보("화곡", "purple");
-        ExtractableResponse<Response> 화곡역_생성_응답 = 지하철_노선_생성_요청(화곡역);
+        LineRequest 신분당선 = 지하철_노선_정보("신분당선", "red");
+        ExtractableResponse<Response> 신분당선_생성_응답 = 지하철_노선_생성_요청(신분당선);
 
         // when
-        ExtractableResponse<Response> 화곡역_제거_응답 = 지하철_노선_제거_요청(화곡역_생성_응답);
+        ExtractableResponse<Response> 신분당선_제거_응답 = 지하철_노선_제거_요청(신분당선_생성_응답);
 
         // then
-        지하철_노선_삭제됨(화곡역_제거_응답);
+        지하철_노선_삭제됨(신분당선_제거_응답);
     }
 
     private LineRequest 지하철_노선_정보(String name, String color) {
@@ -164,7 +162,6 @@ public class LineAcceptanceTest extends AcceptanceTest {
 
     private void 지하철_노선_목록_포함됨(ExtractableResponse<Response> resultResponse,
         ExtractableResponse<Response> response1, ExtractableResponse<Response> response2) {
-        response1.jsonPath().getObject("id", Long.class);
         List<Long> expectedLineIds = Arrays.asList(
             response1.jsonPath().getObject("id", Long.class),
             response2.jsonPath().getObject("id", Long.class)
