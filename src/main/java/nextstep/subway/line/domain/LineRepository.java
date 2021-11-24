81073a89e08eedd37269ade2aa9108d69579e7f1
kelicia91 <kelicia91@gmail.com>
2021-11-24 23:17:44 +0900

    fix: wrong query (missing where)

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -4,17 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
-	// compare for (nextstep.subway.line 구간 추가 기능 7 tests)
-	// before(default findById, lazy-loading): 5 sec 789 ms ~ 6 sec 110 ms
-	// after(fetch join): 4 sec 215 ms ~ 4 sec 591 ms
-	@Query("SELECT DISTINCT l FROM Line l"
+	@Query("SELECT l FROM Line l"
 		+ " JOIN FETCH l.sections w"
 		+ " JOIN FETCH w.sections sc"
 		+ " JOIN FETCH sc.upStation"
 		+ " JOIN FETCH sc.downStation"
+		+ " WHERE l.id = :id"
 	)
-	Optional<Line> findById(Long id);
+	Optional<Line> findById(@Param("id") Long id);
 }
