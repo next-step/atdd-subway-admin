83ee7a6aac10df8ad029f346ac6285ed349b275c
devsigner9920 <devsigner9920@icloud.com>
2021-11-25 16:55:15 +0900

    test: 지하철 노선 생성 인수 테스트작성

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,19 +1,56 @@
 package nextstep.subway.line;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.*;
 
 @DisplayName("지하철 노선 관련 기능")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class LineAcceptanceTest extends AcceptanceTest {
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
+        // given
+        // 지하철_노선_생성_요청_데이터
+        LineRequest lineRequest = new LineRequest("1호선", "blue");
+
         // when
         // 지하철_노선_생성_요청
-
+        ExtractableResponse<Response> createResponse = RestAssured.given().log().all()
+          .accept(MediaType.ALL_VALUE)
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
+          .body(lineRequest)
+          .when()
+          .post("/lines")
+          .then().log().all().extract();
         // then
         // 지하철_노선_생성됨
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        LineResponse lineResponse = createResponse.jsonPath().getObject(".", LineResponse.class);
+        assertThat(lineResponse.getName()).isEqualTo("1호선");
+        assertThat(lineResponse.getColor()).isEqualTo("blue");
     }
 
     @DisplayName("기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성한다.")
