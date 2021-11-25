f534e4622120873f802238f5b82d69f0afd630b2
sungwoo <ssw890418@gmail.com>
2021-11-26 01:25:32 +0900

    feat:구간을 표현하기 위한 엔티티 추가, 역 엔티티와 연관관계 매핑

---
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -0,0 +1,54 @@
+package nextstep.subway.line.domain;
+
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    @Embedded
+    private Distance distance;
+
+    public Section() {
+    }
+
+    public Section(Station upStation, Station downStation, int distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = Distance.of(distance);
+    }
+
+    public static Section of(Station upStation, Station downStation, int distance) {
+        return new Section(upStation, downStation, distance);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Distance getDistance() {
+        return distance;
+    }
+}
+
