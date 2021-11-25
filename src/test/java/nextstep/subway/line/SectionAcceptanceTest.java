4defb94c79e58218481510c513cc236cf1b9c9ce
koola97620 <koola97620@nate.com>
2021-11-25 17:52:30 +0900

    feat: 새로운 역을 상행 종점으로 등록할 경우 인스테스트 작성

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.StationApiRequests;
 import nextstep.subway.station.dto.StationResponse;
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,6 +43,25 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         StationResponse 삼성역 = StationApiRequests.지하철_역_생성됨("삼성역");
         SectionRequest sectionRequest = new SectionRequest(강남역.getId(), 삼성역.getId(), 4);
 
+        // when
+        // 노선 구간 등록 요청
+        ExtractableResponse<Response> response = SectionApiRequests.지하철_노선_구간_등록_요청(서울2호선, sectionRequest);
+
+        // then
+        // 노선에 구간 등록됨.
+        지하철_노선_구간_등록됨(response);
+        지하철_노선에_등록된_역_확인(서울2호선,"강남역","삼성역","잠실역");
+    }
+
+    @DisplayName("새로운 구간의 역을 상행 종점으로 등록한다.")
+    @Test
+    void addUpSection() {
+        // given
+        // 지하철 역 생성됨
+        // 지하철 노선 생성됨
+        StationResponse 사당역 = StationApiRequests.지하철_역_생성됨("사당역");
+        SectionRequest sectionRequest = new SectionRequest(사당역.getId(), 강남역.getId(), 7);
+
 
         // when
         // 노선 구간 등록 요청
@@ -47,11 +70,21 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         // then
         // 노선에 구간 등록됨.
         지하철_노선_구간_등록됨(response);
+        지하철_노선에_등록된_역_확인(서울2호선,"사당역","강남역","잠실역");
     }
 
     private void 지하철_노선_구간_등록됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
+    private void 지하철_노선에_등록된_역_확인(Long lineId, String... names) {
+        ExtractableResponse<Response> response = LineApiRequests.지하철_노선_조회_요청(lineId);
+        LineResponse result = response.as(LineResponse.class);
+        List<String> savedStationNames = result.getStations()
+                .stream()
+                .map(stationResponse -> stationResponse.getName())
+                .collect(Collectors.toList());
+        assertThat(savedStationNames).containsExactly(names);
+    }
 
 }
