d6b118750556cf9a32d6d27b1322695b75c7861d
koola97620 <koola97620@nate.com>
2021-11-26 00:09:15 +0900

    test: 실패하는 테스트 수정

--- src/test/java/nextstep/subway/line/domain/LineTest.java
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -15,23 +15,16 @@ class LineTest {
     @DisplayName("지하철 노선 역 목록 조회")
     @Test
     void getStations() {
-        Line line = new Line("4호선", "blue");
         Station upStationSadang = new Station("사당역");
         Station downStationAnsan = new Station("안산역");
-        Section firstSection = new Section(upStationSadang, downStationAnsan, 10);
-        line.addSection(firstSection);
-
-        Station upStationAnsan = new Station("안산역");
-        Station downStationOido = new Station("오이도역");
-        Section secondSection = new Section(upStationAnsan, downStationOido, 15);
-        line.addSection(secondSection);
+        Line line = new Line("4호선", "blue",upStationSadang,downStationAnsan,10);
 
         List<String> stationNames = line.getStations()
                 .stream()
                 .map(StationResponse::getName)
                 .collect(Collectors.toList());
 
-        assertThat(stationNames).containsExactly(upStationSadang.getName(), upStationAnsan.getName(), downStationOido.getName());
+        assertThat(stationNames).containsExactly(upStationSadang.getName(), downStationAnsan.getName());
     }
 
 }
