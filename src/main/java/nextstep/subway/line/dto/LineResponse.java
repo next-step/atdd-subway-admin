bd3581dc21b6093a1a87444be366de1ce84d9e23
ChangSubKwak <paradise1732@gmail.com>
2021-11-27 05:14:08 +0900

    feat: 지하철 응답 DTO에 구간 데이터 추가 및 상행역 기준 조회 기능 추가

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,10 +1,12 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LineResponse {
@@ -12,17 +14,19 @@ public class LineResponse {
     private String name;
     private String color;
     private List<StationResponse> stations;
+    private Map<Long, SectionResponse> sections;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
     public LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations, Map<Long, SectionResponse> sections, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.stations = stations;
+        this.sections = sections;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
@@ -31,8 +35,19 @@ public class LineResponse {
         return new LineResponse(line.getId()
             , line.getName()
             , line.getColor()
-            , line.getStations().stream().map(StationResponse::of).collect(Collectors.toList())
-            , line.getCreatedDate(), line.getModifiedDate());
+            , line.getStations()
+                .stream()
+                .map(StationResponse::of)
+                .collect(Collectors.toList())
+            , line.getSections()
+                .stream()
+                .collect(Collectors.toMap(
+                    e -> e.getUpStation().getId(),
+                    e -> SectionResponse.of(e.getUpStation(), e.getDownStation(), e.getDistance()))
+                )
+            , line.getCreatedDate()
+            , line.getModifiedDate()
+        );
     }
 
     public Long getId() {
@@ -49,6 +64,14 @@ public class LineResponse {
 
     public List<StationResponse> getStations() {
         return stations;
+    }
+
+    public Map<Long, SectionResponse> getSections() {
+        return sections;
+    }
+
+    public SectionResponse getSectionFromUpStationId(Long upStationId) {
+        return sections.get(upStationId);
     }
 
     public LocalDateTime getCreatedDate() {
