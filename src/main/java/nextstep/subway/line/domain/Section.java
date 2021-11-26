376b94927d8942e539ae74890e0dac1867d9d798
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-26 15:44:03 +0900

    feat: Section 정의

---
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -0,0 +1,57 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import nextstep.subway.station.domain.Station;
+
+@Entity
+public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "upstation_id", foreignKey = @ForeignKey(name = "fk_section_to_upstation"))
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "downstation_id", foreignKey = @ForeignKey(name = "fk_section_to_downstation"))
+    private Station downStation;
+
+    private int distance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id", foreignKey = @ForeignKey(name = "fk_section_to_line"))
+    private Line line;
+
+    protected Section() {
+    }
+
+    public Section(final Station upStation, final Station downStation, final int distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void addLine(final Line line) {
+        this.line = line;
+    }
+}
