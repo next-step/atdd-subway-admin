34fa32dbb85001709f407f81f3e4bb2d2a4f4785
wooobo <irooori89@gmail.com>
2021-11-26 13:50:56 +0900

    test : `LineStation` 추가 실패 케이스 테스트 추가

--- src/test/java/nextstep/subway/line/LineStationAddAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineStationAddAcceptanceTest.java
@@ -5,16 +5,14 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.station.dto.StationResponse;
-import nextstep.subway.utils.LineAcceptanceTestUtil;
-import nextstep.subway.utils.StationAcceptanceTestUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
+import static nextstep.subway.utils.StationAcceptanceTestUtil.*;
 import static nextstep.subway.utils.LineStationAcceptanceTestUtil.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static nextstep.subway.utils.LineAcceptanceTestUtil.*;
 
 @DisplayName("지하철 노선에 역 등록 관련 기능")
 public class LineStationAddAcceptanceTest extends AcceptanceTest {
@@ -29,19 +27,12 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
         super.setUp();
 
         // given
-        ExtractableResponse<Response> createdStationResponse1 = StationAcceptanceTestUtil.지하철됨_역_생성_됨(
-            "잠실역");
-        ExtractableResponse<Response> createdStationResponse2 = StationAcceptanceTestUtil.지하철됨_역_생성_됨(
-            "몽촌토성역");
-        ExtractableResponse<Response> createdStationResponse3 = StationAcceptanceTestUtil.지하철됨_역_생성_됨(
-            "강동구청역");
+        잠실역ID = 지하철됨_역_생성_됨_toId("잠실역");
+        몽촌토성역ID = 지하철됨_역_생성_됨_toId("몽촌토성역");
+        강동구청역ID = 지하철됨_역_생성_됨_toId("강동구청역");
 
-        잠실역ID = createdStationResponse1.as(StationResponse.class).getId();
-        몽촌토성역ID = createdStationResponse2.as(StationResponse.class).getId();
-        강동구청역ID = createdStationResponse3.as(StationResponse.class).getId();
-
-        ExtractableResponse<Response> createLineResponse = LineAcceptanceTestUtil.지하철_노선_등록되어_있음(
-            "2호선", "RED", 잠실역ID, 몽촌토성역ID, 100);
+        ExtractableResponse<Response> createLineResponse = 지하철_노선_등록되어_있음("2호선", "RED", 잠실역ID,
+            몽촌토성역ID, 100);
 
         노선ID = createLineResponse.as(LineResponse.class).getId();
     }
@@ -52,21 +43,21 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     void 기존_구간_사이에_역추가() {
         // given
         // when
-        ExtractableResponse<Response> 노선_구간_추가_응답 = LineAcceptanceTestUtil.지하철_노선구간_추가_되어_있음(노선ID,
-            잠실역ID, 강동구청역ID, 50);
+        ExtractableResponse<Response> 노선_구간_추가_응답 = 지하철_노선구간_사이에_역_추가_되어_있음(노선ID, 강동구청역ID, 50);
 
         // then
         지하철_구간_요청_응답_검증(노선_구간_추가_응답, HttpStatus.CREATED);
     }
 
+
     @DisplayName("지하철 노선에 여러개의 역을 순서대로 등록한다.")
     @Test
     void 지하철노선_노회_역_정렬_등록() {
         // given
-        LineAcceptanceTestUtil.지하철_노선구간_추가_되어_있음(노선ID, 강동구청역ID, 몽촌토성역ID, 50);
+        지하철_노선구간_사이에_역_추가_되어_있음(노선ID, 강동구청역ID, 50);
 
         // when
-        ExtractableResponse<Response> response = LineAcceptanceTestUtil.지하철_노선_조회_요청(노선ID);
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(노선ID);
 
         // then
         지하철_구간_요청_응답_검증(response, HttpStatus.OK);
@@ -77,10 +68,10 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     @Test
     void 상행_종점_역_등록() {
         // given
-        LineAcceptanceTestUtil.지하철_노선구간_추가_되어_있음(노선ID, 강동구청역ID, 잠실역ID, 50);
+        지하철_노선구간_상행_종점_역_추가_되어_있음(노선ID, 강동구청역ID, 50);
 
         // when
-        ExtractableResponse<Response> response = LineAcceptanceTestUtil.지하철_노선_조회_요청(노선ID);
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(노선ID);
 
         // then
         지하철_구간_요청_응답_검증(response, HttpStatus.OK);
@@ -91,14 +82,78 @@ public class LineStationAddAcceptanceTest extends AcceptanceTest {
     @Test
     void 하행_종점_역_등록() {
         // given
-        LineAcceptanceTestUtil.지하철_노선구간_추가_되어_있음(노선ID, 몽촌토성역ID, 강동구청역ID, 50);
+        지하철_노선구간_하행_종점_역_추가_되어_있음(노선ID, 강동구청역ID, 50);
 
         // when
-        ExtractableResponse<Response> response = LineAcceptanceTestUtil.지하철_노선_조회_요청(노선ID);
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(노선ID);
 
         // then
         지하철_구간_요청_응답_검증(response, HttpStatus.OK);
         지하철_노선에_지하척역_순서_정렬됨(response, Lists.newArrayList(잠실역ID, 몽촌토성역ID, 강동구청역ID));
     }
 
+    @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이와 같으면 등록을 할 수 없음")
+    @Test
+    void 추가_역길이_기존_역사이_길이와_같으면_실패() {
+        // given
+        // when
+        ExtractableResponse<Response> 노선_구간_추가_응답 = 지하철_노선구간_사이에_역_추가_되어_있음(노선ID, 강동구청역ID, 100);
+
+        // then
+        지하철_구간_요청_응답_검증(노선_구간_추가_응답, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크면 등록을 할 수 없음")
+    @Test
+    void 추가_역길이_기존_역사이_길이_이상이면_실패() {
+        // given
+        // when
+        ExtractableResponse<Response> 노선_구간_추가_응답 = 지하철_노선구간_사이에_역_추가_되어_있음(노선ID, 강동구청역ID, 200);
+
+        // then
+        지하철_구간_요청_응답_검증(노선_구간_추가_응답, HttpStatus.BAD_REQUEST);
+    }
+
+    @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+    @Test
+    void 상행_하행_이미_모두_등록시_실패() {
+        // given
+        // when
+        ExtractableResponse<Response> 노선_구간_추가_응답 = 지하철_노선구간_사이에_역_추가_되어_있음(노선ID, 잠실역ID, 200);
+
+        // then
+        지하철_구간_요청_응답_검증(노선_구간_추가_응답, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
+    @Test
+    void 상행_하행_일치하는_역_없으면_실패() {
+        // given
+        long 없는역ID1 = 지하철됨_역_생성_됨_toId("없는역1");
+        long 없는역ID2 = 지하철됨_역_생성_됨_toId("없는역2");
+
+        // when
+        ExtractableResponse<Response> 노선_구간_추가_응답 = 지하철_노선구간_추가_되어_있음(노선ID, 없는역ID1, 없는역ID2, 200);
+
+        // then
+        지하철_구간_요청_응답_검증(노선_구간_추가_응답, HttpStatus.BAD_REQUEST);
+    }
+
+
+    private ExtractableResponse<Response> 지하철_노선구간_사이에_역_추가_되어_있음(Long lineId, Long stationId,
+        int distance) {
+        return 지하철_노선구간_추가_되어_있음(lineId, stationId, 몽촌토성역ID, distance);
+    }
+
+    private ExtractableResponse<Response> 지하철_노선구간_상행_종점_역_추가_되어_있음(Long lineId, Long stationId,
+        int distance) {
+        return 지하철_노선구간_추가_되어_있음(lineId, stationId, 잠실역ID, distance);
+    }
+
+    private ExtractableResponse<Response> 지하철_노선구간_하행_종점_역_추가_되어_있음(Long lineId, Long stationId,
+        int distance) {
+        return 지하철_노선구간_추가_되어_있음(lineId, 몽촌토성역ID, stationId, distance);
+    }
 }
