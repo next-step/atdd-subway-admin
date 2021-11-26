7ffc9dbbb8b82d74ac39375e7d6f47ca3522ee10
jennie267 <jennieee267@gmail.com>
2021-11-26 21:47:52 +0900

    refactor : StationResponse List를 만드는 역할을 StationResponse에게 할당함

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -2,7 +2,6 @@ package nextstep.subway.line.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.dto.StationResponse;
@@ -21,10 +20,10 @@ public class LineResponse {
     private LocalDateTime modifiedDate;
 
 
-    public LineResponse() {
+    private LineResponse() {
     }
 
-    public LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+    private LineResponse(Long id, String name, String color, List<StationResponse> stations, LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -34,11 +33,7 @@ public class LineResponse {
     }
 
     public static LineResponse of(Line line) {
-        List<StationResponse> stations = line.getStations()
-                .stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations, line.getCreatedDate(), line.getModifiedDate());
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), StationResponse.listOf(line.getStations()), line.getCreatedDate(), line.getModifiedDate());
     }
 
     public Long getId() {
