9d48d1df3b101597b61a343a34d8a2159bac693d
jennie267 <jennieee267@gmail.com>
2021-11-26 22:37:08 +0900

    test : equals, hashCode 테스트 케이스 추가

--- src/test/java/nextstep/subway/line/domain/SectionTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionTest.java
@@ -28,5 +28,44 @@ public class SectionTest {
                 () -> assertThat(section.getDownStation().equals(downStation)).isTrue()
                 );
     }
+    
+    @Test
+    @DisplayName("같은 구간인지 확인")
+    void 같은_구간_구별() {
+        // given
+        Station upStation = Station.from("강남역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 15);
+        
+        Section section = Section.of(line, upStation, downStation, 15);
+        Section comparedSection = Section.of(line, upStation, downStation, 15);
+        
+        // when, then
+        // then
+        assertAll(
+                () -> assertThat(section.equals(comparedSection)).isTrue(),
+                () -> assertThat(section.hashCode() == comparedSection.hashCode()).isTrue()
+                );
+    }
+    
+    @Test
+    @DisplayName("다른 구간인지 확인")
+    void 다른_구간_구별() {
+        // given
+        Station upStation = Station.from("강남역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 15);
+        Section section = Section.of(line, upStation, downStation, 15);
+        
+        Line comparedLine = Line.of("3호선", "빨간색", upStation, downStation, 17);
+        Section comparedSection = Section.of(comparedLine, upStation, downStation, 17);
+        
+        // when, then
+        // then
+        assertAll(
+                () -> assertThat(section.equals(comparedSection)).isFalse(),
+                () -> assertThat(section.hashCode() == comparedSection.hashCode()).isFalse()
+                );
+    }
 
 }
