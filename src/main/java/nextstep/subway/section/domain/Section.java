d8e15dbf4882eef30730ee921a1f08d441aac1f4
이승민 <sml7179@naver.com>
2021-11-26 21:20:57 +0900

    feat: 구역 생성

---
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -0,0 +1,38 @@
+package nextstep.subway.section.domain;
+
+import nextstep.subway.common.BaseEntity;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Section extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Line line;
+
+    @ManyToOne
+    private Station upStation;
+
+    @ManyToOne
+    private Station downStation;
+
+    @Column
+    private int distance;
+
+    protected Section(){
+
+    }
+
+    public Section(Line line, Station upStation, Station downStation, int distance) {
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+}
