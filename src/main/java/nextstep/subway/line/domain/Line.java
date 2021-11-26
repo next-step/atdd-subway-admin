fa1028c7f06019d45b7433330813f829a7d7ac68
Junhwan Kim <junhwan.tech@gmail.com>
2021-11-26 16:38:50 +0900

    refactor: Line 생성 시 Section을 전달 받도록 수정

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,24 +1,40 @@
 package nextstep.subway.line.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import nextstep.subway.common.BaseEntity;
-
-import javax.persistence.*;
 
 @Entity
 public class Line extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
     private String color;
+
+    @OneToMany(mappedBy = "line", fetch = FetchType.LAZY)
+    private List<Section> sections = new ArrayList<>();
 
     public Line() {
     }
 
-    public Line(String name, String color) {
+    public Line(String name, String color, Section section) {
         this.name = name;
         this.color = color;
+
+        section.addLine(this);
+        this.sections.add(section);
     }
 
     public void update(Line line) {
@@ -36,5 +52,9 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
+    }
+
+    public List<Section> getSections() {
+        return sections;
     }
 }
