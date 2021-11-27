f0388a8cf70b9855fe03191ba6c945bd1b968af7
jennie267 <jennieee267@gmail.com>
2021-11-27 18:26:34 +0900

    test : 구간 등록 인수 테스트 작성

--- README.md
+++ README.md
@@ -113,7 +113,7 @@ This project is [MIT](https://github.com/next-step/atdd-subway-admin/blob/master
     
 
 ## 구현목록
-- [ ] 구간 등록 인수 테스트 작성
+- [x] 구간 등록 인수 테스트 작성
 - [ ] 역 사이에 새로운 역을 등록
     - [ ] 새로운 길이를 뺀 나머지를 새롭게 추가된 역과의 길이로 설정
     - [ ] 새로운 역을 상행 종점으로 등록할 경우
--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -10,6 +10,7 @@ import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
 
@@ -70,5 +71,10 @@ public class LineService {
         Station downStation = stationService.findById(request.getDownStationId());
         
         return request.toLine(upStation, downStation);
+    }
+
+    public void addSection(Long id, SectionRequest sectionRequest) {
+        // TODO Auto-generated method stub
+        
     }
 }
