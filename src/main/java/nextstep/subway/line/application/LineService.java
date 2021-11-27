708c7066a71451ae34d7289460d607f93798c3a1
devsigner9920 <devsigner9920@icloud.com>
2021-11-27 20:11:15 +0900

    refactor: 1단계 미션 피드백 예외 처리 리팩터링

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,49 +1,54 @@
 package nextstep.subway.line.application;
 
-import javassist.NotFoundException;
-import nextstep.subway.exceptions.DuplicateLineException;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.exception.LineDuplicateException;
+import nextstep.subway.line.exception.LineNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
 
     public LineService(LineRepository lineRepository) {
         this.lineRepository = lineRepository;
     }
 
     public LineResponse saveLine(LineRequest request) {
-        if (lineRepository.findByName(request.getName()).isPresent()) {
-            throw new DuplicateLineException("중복된 지하철 노선 이름이 있습니다.");
-        }
+        checkDuplicateLine(request);
+
         Line persistLine = lineRepository.save(request.toLine());
         return LineResponse.of(persistLine);
     }
 
-    public List<LineResponse> findAllLines() {
-        return lineRepository.findAll()
-          .stream()
-          .map(LineResponse::of)
-          .collect(Collectors.toList());
+    private void checkDuplicateLine(LineRequest request) {
+        if (lineRepository.findByName(request.getName()).isPresent()) {
+            throw new LineDuplicateException();
+        }
     }
 
+    @Transactional(readOnly = true)
+    public List<LineResponse> findAllLines() {
+        List<Line> persistLines = lineRepository.findAll();
+        return LineResponse.ofList(persistLines);
+    }
+
+    @Transactional(readOnly = true)
     public LineResponse findById(Long id) {
-        Line line = lineRepository.findById(id).orElseThrow(() -> new NoSuchElementException("노선을 찾을 수 없습니다."));
+        Line line = lineRepository.findById(id)
+          .orElseThrow(LineNotFoundException::new);
         return LineResponse.of(line);
     }
 
     public void update(Long id, LineRequest lineRequest) {
-        Line line = lineRepository.findById(id).orElseThrow(() -> new NoSuchElementException("노선을 찾을 수 없습니다."));
+        Line line = lineRepository.findById(id)
+          .orElseThrow(LineNotFoundException::new);
         line.update(lineRequest.toLine());
     }
 
---
+++ src/main/java/nextstep/subway/line/exception/LineDuplicateException.java
@@ -0,0 +1,13 @@
+package nextstep.subway.line.exception;
+
+import nextstep.subway.common.ServiceException;
+import org.springframework.http.HttpStatus;
+
+public class LineDuplicateException extends ServiceException {
+
+  private static final String MESSAGE = "이미 등록된 노선이 존재합니다.";
+
+  public LineDuplicateException() {
+    super(HttpStatus.BAD_REQUEST, MESSAGE);
+  }
+}
