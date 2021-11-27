609906ae45252c1b2d245065f0579851425affe2
lovethefeel <lovethefeel@gmail.com>
2021-11-28 02:16:28 +0900

    refactor: - CascadeType 추가 - 정적 메소드 추가

--- src/main/java/nextstep/subway/section/domain/Section.java
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -1,6 +1,7 @@
 package nextstep.subway.section.domain;
 
-import nextstep.subway.common.BaseEntity;
+import nextstep.subway.common.Message;
+import nextstep.subway.common.domain.BaseEntity;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.station.domain.Station;
 
@@ -13,16 +14,16 @@ public class Section extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "line_id", foreignKey = @ForeignKey(name = "fk_section_to_line"))
     private Line line;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "up_station_id", foreignKey = @ForeignKey(name = "fk_up_station_to_line"))
     private Station upStation;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "donw_station_id", foreignKey = @ForeignKey(name = "fk_donw_station_to_line"))
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "down_station_id", foreignKey = @ForeignKey(name = "fk_down_station_to_line"))
     private Station downStation;
 
     private int distance;
@@ -30,16 +31,22 @@ public class Section extends BaseEntity {
     protected Section() {
     }
 
-    public Section(final Line line, final Station upStation, final Station downStation, final int distance) {
-        this(null, line, upStation, downStation, distance);
-    }
-
-    private Section(final Long id, final Line line, final Station upStation, final Station downStation, final int distance) {
-        this.id = id;
+    private Section(final Station upStation, final Station downStation, final int distance) {
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
-        setLine(line);
+    }
+
+    public static Section of(final Station upStation, final Station downStation, final int distance){
+        return new Section(upStation, downStation, distance);
+    }
+
+    public Section(final Long id, final Line line, final Station upStation, final Station downStation, final int distance){
+        this.id = id;
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
     }
 
     public Long getId() {
@@ -54,12 +61,68 @@ public class Section extends BaseEntity {
         return line;
     }
 
-    public void setLine(final Line line) {
-        if (Objects.nonNull(this.line)) {
-            this.line.removeLine(this);
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void changeUpStation(Station upStation) {
+        this.upStation = upStation;
+    }
+
+    public void changeDownStation(Station downStation) {
+        this.downStation = downStation;
+    }
+
+    public void minusDistance(int distance){
+        if (this.getDistance() <= distance){
+            throw new IllegalArgumentException(Message.SECTION_DISTANCE_REGISTER.getMessage());
         }
+        this.distance = this.distance - distance;
+    }
+
+    public boolean isUpStationEquals(Section section){
+        return this.getUpStation().equals(section.getUpStation());
+    }
+
+    public boolean isDownStationEquals(Section section){
+        return this.getDownStation().equals(section.getDownStation());
+    }
+
+    public boolean isUpStationAndTargetDownStationEquals(Section section){
+        return this.getUpStation().equals(section.getDownStation());
+    }
+
+    public boolean isDownStationAndTargetUpStationEquals(Section section){
+        return this.getDownStation().equals(section.getUpStation());
+    }
+
+    public void setLine(Line line) {
         this.line = line;
-        line.getSections().add(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Section section = (Section) o;
+        return distance == section.distance
+                && Objects.equals(id, section.id)
+                && Objects.equals(line, section.line)
+                && Objects.equals(upStation, section.upStation)
+                && Objects.equals(downStation, section.downStation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, line, upStation, downStation, distance);
     }
 
     @Override
