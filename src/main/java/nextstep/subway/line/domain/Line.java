e1e48d2b864cda14a946043144940d1d61f87fa8
lovethefeel <lovethefeel@gmail.com>
2021-11-28 02:09:08 +0900

    refactor: - 정적메소드 추가 - 노선의 속한 구간 찾는 메소드 추가

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,11 +1,17 @@
 package nextstep.subway.line.domain;
 
-import nextstep.subway.common.BaseEntity;
+import nextstep.subway.common.domain.BaseEntity;
 import nextstep.subway.section.domain.Section;
 import nextstep.subway.section.domain.Sections;
+import nextstep.subway.station.domain.Station;
+import nextstep.subway.station.dto.StationResponse;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Entity
 public class Line extends BaseEntity {
@@ -19,14 +25,28 @@ public class Line extends BaseEntity {
     private String color;
 
     @Embedded
-    private Sections sections = new Sections();
+    private Sections sections = Sections.of(new ArrayList<>());
 
     protected Line() {
     }
 
-    public Line(final String name, final String color) {
+    private Line(final String name, final String color, final Sections sections) {
         this.name = name;
         this.color = color;
+        this.sections = sections;
+    }
+
+    private Line(final String name, final String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public static Line of(final String name, final String color, final Sections sections) {
+        return new Line(name, color, sections);
+    }
+
+    public static Line of(final String name, final String color) {
+        return new Line(name, color);
     }
 
     public void update(final Line line) {
@@ -46,8 +66,8 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public Sections getSections() {
-        return sections;
+    public List<Section> getSections() {
+        return sections.getSections();
     }
 
     @Override
@@ -63,7 +83,24 @@ public class Line extends BaseEntity {
         return Objects.hash(name);
     }
 
-    public void removeLine(final Section section) {
-        this.sections.remove(section);
+    public void addSection(Section section) {
+        this.sections.add(section, this);
+    }
+
+    public List<Station> getStations() {
+        return sections.getSections().stream()
+                .flatMap(section -> Stream.of(section.getUpStation(), section.getDownStation()))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<StationResponse> getStationResponses() {
+        return getStations().stream()
+                .map(StationResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isContainingStation(Station station){
+        return getStations().contains(station);
     }
 }
