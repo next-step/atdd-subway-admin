21c77682864c29fc9c7a692f4a2ea4b3123d6d25
jennie267 <jennieee267@gmail.com>
2021-11-28 00:10:49 +0900

    test : 구간 등록 예외 인수 테스트 작성

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -38,6 +38,60 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
+    
+    @DisplayName("새로운 역 등록시 기존 역 사이 길이보다 크거나 같으면 등록 할 수 없음")
+    @Test
+    void 기존_길이보다_긴_구간_등록_불가() {
+        // given
+        StationResponse 봉천역 = 지하철역_등록되어_있음("봉천역");
+        StationResponse 낙성대역 = 지하철역_등록되어_있음("낙성대역");
+        LineResponse 이호선 = 지하철_노선_등록되어_있음(LineRequest.of("이호선", "bg-green-600", 봉천역.getId(), 낙성대역.getId(), 30));
+        
+        StationResponse 서울대입구역 = 지하철역_등록되어_있음("서울대입구역");
+        SectionRequest request = SectionRequest.of(봉천역.getId(), 서울대입구역.getId(), 50);
+
+        // when
+        ExtractableResponse<Response> response = 구간_추가_요청(이호선.getId(), request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    
+    @DisplayName("상행역, 하행역이 모두 등록되어있으면 추가할 수 없음")
+    @Test
+    void 등록된_구간_등록_불가() {
+        // given
+        StationResponse 서울대입구역 = 지하철역_등록되어_있음("서울대입구역");
+        StationResponse 낙성대역 = 지하철역_등록되어_있음("낙성대역");
+        LineResponse 이호선 = 지하철_노선_등록되어_있음(LineRequest.of("이호선", "bg-green-600", 서울대입구역.getId(), 낙성대역.getId(), 30));
+        
+        SectionRequest request = SectionRequest.of(서울대입구역.getId(), 낙성대역.getId(), 30);
+
+        // when
+        ExtractableResponse<Response> response = 구간_추가_요청(이호선.getId(), request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    
+    @DisplayName("상행역, 하행역이 모두 등록되어있지않으면 추가할 수 없음")
+    @Test
+    void 등록되지_않은_역_구간_등록_불가() {
+        // given
+        StationResponse 서울대입구역 = 지하철역_등록되어_있음("서울대입구역");
+        StationResponse 낙성대역 = 지하철역_등록되어_있음("낙성대역");
+        LineResponse 이호선 = 지하철_노선_등록되어_있음(LineRequest.of("이호선", "bg-green-600", 서울대입구역.getId(), 낙성대역.getId(), 30));
+        
+        StationResponse 신림역 = 지하철역_등록되어_있음("신림역");
+        StationResponse 봉천역 = 지하철역_등록되어_있음("봉천역");
+        SectionRequest request = SectionRequest.of(신림역.getId(), 봉천역.getId(), 20);
+
+        // when
+        ExtractableResponse<Response> response = 구간_추가_요청(이호선.getId(), request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 
     private ExtractableResponse<Response> 구간_추가_요청(Long lineId, SectionRequest request) {
         return RestAssured
