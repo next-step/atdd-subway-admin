04d8ed935a9fba9a125a66a2d9dab2a2f823ed59
jennie267 <jennieee267@gmail.com>
2021-11-28 11:11:35 +0900

    test : Section에 새로 추가된 메소드 테스트케이스 추가

--- src/test/java/nextstep/subway/line/domain/SectionTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionTest.java
@@ -2,9 +2,12 @@ package nextstep.subway.line.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import nextstep.subway.station.domain.Station;
 
@@ -41,7 +44,6 @@ public class SectionTest {
         Section comparedSection = Section.of(line, upStation, downStation, 15);
         
         // when, then
-        // then
         assertAll(
                 () -> assertThat(section.equals(comparedSection)).isTrue(),
                 () -> assertThat(section.hashCode() == comparedSection.hashCode()).isTrue()
@@ -61,10 +63,98 @@ public class SectionTest {
         Section comparedSection = Section.of(comparedLine, upStation, downStation, 17);
         
         // when, then
-        // then
         assertAll(
                 () -> assertThat(section.equals(comparedSection)).isFalse(),
                 () -> assertThat(section.hashCode() == comparedSection.hashCode()).isFalse()
+                );
+    }
+    
+    @Test
+    @DisplayName("길이가 짧은지 확인")
+    void 길이_확인() {
+        // given
+        Station upStation = Station.from("강남역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 30);
+        Section section = Section.of(line, upStation, downStation, 30);
+        
+        // when, then
+        assertThrows(IllegalArgumentException.class, ()->{
+                    section.checkShorter(25);
+                });
+    }
+    
+    @DisplayName("같은 상행역인지 확인")
+    @ParameterizedTest
+    @CsvSource(value = { "서울대입구역:true", "봉천역:false" }, delimiter = ':')
+    void 같은_상핵역_확인(String stationName, boolean expected) {
+        // given
+        Station upStation = Station.from("서울대입구역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 30);
+        Section section = Section.of(line, upStation, downStation, 30);
+        
+        Station newUpStation = Station.from(stationName);
+        
+        // when, then
+        assertThat(section.isSameUpStation(newUpStation)).isEqualTo(expected);
+    }
+    
+    @DisplayName("같은 하행역인지 확인")
+    @ParameterizedTest
+    @CsvSource(value = { "교대역:true", "봉천역:false" }, delimiter = ':')
+    void 같은_하행역_확인(String stationName, boolean expected) {
+        // given
+        Station upStation = Station.from("서울대입구역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 30);
+        Section section = Section.of(line, upStation, downStation, 30);
+        
+        Station newDownStation = Station.from(stationName);
+        
+        // when, then
+        assertThat(section.isSameDownStation(newDownStation)).isEqualTo(expected);
+    }
+    
+    @Test
+    @DisplayName("기존 구간이 잘변경되는지 확인 (상행역)")
+    void 기존_구간_변경_확인_상행역() {
+        // given
+        Station upStation = Station.from("서울대입구역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 30);
+        Section section = Section.of(line, upStation, downStation, 30);
+
+        Station newUpStation = Station.from("사당역");
+        
+        // when
+        section.moveUpStationTo(newUpStation, 10);
+        
+        // then
+        assertAll(
+                () -> assertThat(section.getUpStation()).isEqualTo(newUpStation),
+                () -> assertThat(section.getDistance()).isEqualTo(20)
+                );
+    }
+    
+    @Test
+    @DisplayName("기존 구간이 잘변경되는지 확인 (하행역)")
+    void 기존_구간_변경_확인_하행역() {
+        // given
+        Station upStation = Station.from("서울대입구역");
+        Station downStation = Station.from("교대역");
+        Line line = Line.of("2호선", "파란색", upStation, downStation, 30);
+        Section section = Section.of(line, upStation, downStation, 30);
+
+        Station newDownStation = Station.from("낙성대입구역");
+        
+        // when
+        section.moveDownStationTo(newDownStation, 20);
+        
+        // then
+        assertAll(
+                () -> assertThat(section.getDownStation()).isEqualTo(newDownStation),
+                () -> assertThat(section.getDistance()).isEqualTo(10)
                 );
     }
 
