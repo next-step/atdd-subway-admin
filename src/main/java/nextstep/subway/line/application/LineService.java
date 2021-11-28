577944753bcd9cd68eeb425bf9ec7f6d23ff0737
minho <lights8615@gmail.com>
2021-11-28 14:27:58 +0900

    refactor: 빈 주입되는 객체 final 사용

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -18,8 +18,8 @@ import nextstep.subway.station.domain.StationRepository;
 
 @Service
 public class LineService {
-    private LineRepository lineRepository;
-    private StationRepository stationRepository;
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
     public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
