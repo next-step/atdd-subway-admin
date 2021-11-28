92dccc19d1531fd659cd2f820fb7a82657ae630e
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-11-29 08:58:13 +0900

    refactor(line): 지하철 역 조회 쿼리 리팩토링

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,7 +1,9 @@
 package nextstep.subway.line.application;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
@@ -29,19 +31,32 @@ public class LineService {
     public LineResponse saveLine(final LineRequest request) {
         checkLineNameIsUnique(request.getName());
         final Line persistLine = lineRepository.save(request.toLine());
-        final Station upStation = stationRepository.findById(request.getUpStationId())
-            .orElseThrow(NoSuchElementException::new);
-        final Station downStation = stationRepository.findById(request.getDownStationId())
-            .orElseThrow(NoSuchElementException::new);
+        final List<Station> stations = getStations(
+            request.getUpStationId(),
+            request.getDownStationId()
+        );
+        final Station upStation = getStationById(stations, request.getUpStationId());
+        final Station downStation = getStationById(stations, request.getDownStationId());
         final Section section = new Section(upStation, downStation, request.getDistance());
         persistLine.addSection(section);
         return LineResponse.of(persistLine);
     }
 
-    private void checkLineNameIsUnique(final String requestedName) {
-        if (lineRepository.existsByName(requestedName)) {
-            throw new IllegalArgumentException("이미 존재하는 지하철 노선 이름으로 지하철 노선을 생성할 수 없습니다.");
+    private List<Station> getStations(final Long upStationId, final Long downStationId) {
+        final List<Station> stations = stationRepository.findAllById(
+            Arrays.asList(upStationId, downStationId)
+        );
+        if (stations.size() != 2) {
+            throw new NoSuchElementException();
         }
+        return stations;
+    }
+
+    private Station getStationById(final List<Station> stations, final Long id) {
+        return stations.stream()
+            .filter(s -> Objects.equals(s.getId(), id))
+            .findFirst()
+            .orElseThrow(NoSuchElementException::new);
     }
 
     public List<LineResponse> findLines() {
@@ -75,5 +90,11 @@ public class LineService {
     private Line getLineById(final Long id) {
         return lineRepository.findById(id)
             .orElseThrow(NoSuchElementException::new);
+    }
+
+    private void checkLineNameIsUnique(final String requestedName) {
+        if (lineRepository.existsByName(requestedName)) {
+            throw new IllegalArgumentException("이미 존재하는 지하철 노선 이름으로 지하철 노선을 생성할 수 없습니다.");
+        }
     }
 }
