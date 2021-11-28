28b5e42d62ad253d6ae6a846cb9a83370a2bce11
Chloe <page.prologue@gmail.com>
2021-11-28 10:59:13 +0900

    test : 노선 인수테스트 내용 수정

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -5,14 +5,15 @@ import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineEditRequest;
 import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.station.dto.StationRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import static nextstep.subway.line.LineScenarioMethod.*;
-import static nextstep.subway.station.StationScenarioMethod.등록되지_않은_지하철_역;
-import static nextstep.subway.station.StationScenarioMethod.지하철_역_정보;
+import static nextstep.subway.station.StationScenarioMethod.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -66,12 +67,13 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void findLines() {
         // given
-        Map<String, Long> stations = 지하철_역_정보("강남", "광교");
+        TreeMap<String, Long> stations = 지하철_역_여러개_등록되어_있음(new StationRequest("강남"), new StationRequest("광교"),
+                                                                 new StationRequest("신설동"), new StationRequest("까치산"));
         LineRequest 신분당선 = 지하철_노선_정보("신분당선", "bg-red-600", stations.get("강남"), stations.get("광교"), 13);
-        LineRequest 수인선 = 지하철_노선_정보("수인선", "bg-yellow-600", stations.get("강남"), stations.get("광교"), 14);
+        LineRequest 서울2호선 = 지하철_노선_정보("서울2호선", "bg-green-600", stations.get("신설동"), stations.get("까치산"), 51);
 
         지하철_노선_등록되어_있음(신분당선);
-        지하철_노선_등록되어_있음(수인선);
+        지하철_노선_등록되어_있음(서울2호선);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_조회_요청("/lines");
@@ -79,23 +81,23 @@ class LineAcceptanceTest extends AcceptanceTest {
         // then
         지하철_노선_조회_응답됨(response);
         지하철_노선_목록_조회_결과_포함됨(response, 신분당선);
-        지하철_노선_목록_조회_결과_포함됨(response, 수인선);
+        지하철_노선_목록_조회_결과_포함됨(response, 서울2호선);
     }
 
     @DisplayName("지하철 구간을 상행역 부터 하행역 순으로 정렬한 노선을 조회한다.")
     @Test
     void findLine() {
         // given
-        Map<String, Long> stations = 지하철_역_정보("오이도", "인천");
-        LineRequest 수인선 = 지하철_노선_정보("수인선", "bg-yellow-600", stations.get("오이도"), stations.get("인천"), 14);
-        String createdLocationUri = 지하철_노선_등록되어_있음(수인선);
+        Map<String, Long> stations = 지하철_역_정보("신설동", "까치산");
+        LineRequest 서울2호선 = 지하철_노선_정보("서울2호선", "bg-green-600", stations.get("신설동"), stations.get("까치산"), 3);
+        String createdLocationUri = 지하철_노선_등록되어_있음(서울2호선);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_조회_요청(createdLocationUri);
 
         // then
         지하철_노선_조회_응답됨(response);
-        지하철_노선_조회_결과_일치됨(response, 수인선);
+        지하철_노선_조회_결과_일치됨(response, 서울2호선);
         지하철_노선_구간_정렬됨(response, stations);
     }
 
@@ -148,9 +150,9 @@ class LineAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteLine() {
         // given
-        Map<String, Long> stations = 지하철_역_정보("오이도", "인천");
-        LineRequest 수인선 = 지하철_노선_정보("수인선", "bg-yellow-600", stations.get("오이도"), stations.get("인천"), 14);
-        String createdLocationUri = 지하철_노선_등록되어_있음(수인선);
+        Map<String, Long> stations = 지하철_역_정보("신설동", "까치산");
+        LineRequest 서울2호선 = 지하철_노선_정보("서울2호선", "bg-yellow-600", stations.get("신설동"), stations.get("까치산"), 51);
+        String createdLocationUri = 지하철_노선_등록되어_있음(서울2호선);
 
         // when
         ExtractableResponse<Response> response = 지하철_노선_제거_요청(createdLocationUri);
