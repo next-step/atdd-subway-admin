efe58fd300257f0448b838d260bba193ba790e2d
shryu <>
2021-11-30 02:49:46 +0900

    feat : sections return 메소드 수정

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -38,8 +38,8 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public List<Section> getSections() {
-        return sections.getSections();
+    public Sections getSections() {
+        return sections;
     }
 
     public void update(String name, String color) {
