d3429baeee073c179b5742de4960f234d8745f0e
shryu <>
2021-11-29 16:48:54 +0900

    feat : 역 사이 구간 추가 인수테스트 강화

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -4,8 +4,11 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.section.dto.SectionRequest;
+import nextstep.subway.section.dto.SectionResponse;
 import nextstep.subway.station.TestStationFactory;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.utils.TestRequestFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,27 +16,39 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.line.TestLineFactory.노선_생성;
+import static nextstep.subway.utils.TestRequestFactory.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("지하철 구간 관련 기능")
 public class SectionAcceptanceTest extends AcceptanceTest {
     Long DEFAULT_UP_STATION_ID = 1L;
-    Long DEFAULT_DOWN_STATION_IDd = 2L;
+    Long DEFAULT_DOWN_STATION_ID = 2L;
     Long DEFAULT_LINE_ID = 1L;
+    Long newStationId;
+    int DISTANCE = 3;
 
     @BeforeEach
     void setup() {
         노선_생성("강남역", "삼성역", "2호선", "green", 10);
+        newStationId = TestStationFactory.역_생성("역삼역").getId();
     }
 
-    @DisplayName("역 사이에 새로운 역 등록")
+    @DisplayName("상행역 사이에 새로운 역 등록")
     @Test
-    void addSection() {
-        // given
-        Long newStationId = TestStationFactory.역_생성("역삼역").getId();
-
+    void addUpStationSection() {
         // when
-        ExtractableResponse<Response> response = 지하철_구간_추가_요청(DEFAULT_UP_STATION_ID, newStationId, 3);
+        ExtractableResponse<Response> response = 지하철_구간_추가_요청(DEFAULT_UP_STATION_ID, newStationId, DISTANCE);
+
+        // then
+        지하철_구간_생성됨(response);
+    }
+
+    @DisplayName("하행역 사이에 새로운 역 등록")
+    @Test
+    void addDownStationSection() {
+        // when
+        ExtractableResponse<Response> response = 지하철_구간_추가_요청(newStationId, DEFAULT_DOWN_STATION_ID, DISTANCE);
 
         // then
         지하철_구간_생성됨(response);
@@ -42,10 +57,14 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     private ExtractableResponse<Response> 지하철_구간_추가_요청(Long upStationId, Long downStationId, int distance) {
         SectionRequest sectionRequest = new SectionRequest(upStationId, downStationId, distance);
         String path = "lines/" + DEFAULT_LINE_ID + "/sections";
-        return TestRequestFactory.요청(HttpMethod.POST, path, sectionRequest);
+        return 요청(HttpMethod.POST, path, sectionRequest);
     }
 
     private void 지하철_구간_생성됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.body().as(SectionResponse.class).getDistance()).isEqualTo(DISTANCE)
+        );
+
     }
 }
