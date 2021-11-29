eca6bd0c39e0b4c19391837dd455942c9cdd5098
shryu <>
2021-11-29 16:48:31 +0900

    feat : 구간 추가 로직 구현

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,12 +1,15 @@
 package nextstep.subway.line.domain;
 
 import nextstep.subway.section.domain.Section;
+import nextstep.subway.station.domain.Station;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Embeddable
 public class Sections {
@@ -17,11 +20,49 @@ public class Sections {
         return sections;
     }
 
-    public void add(Section section) {
-        sections.add(section);
+    public void add(Section addedSection) {
+        List<Station> stations = getStations();
+
+        if (stations.contains(addedSection.getUpStation())) {
+            Section foundSection = findSectionByUpstation(addedSection.getUpStation());
+            foundSection.update(addedSection.getDownStation(), foundSection.getDownStation(), foundSection.getDistance() - addedSection.getDistance());
+        }
+        if (stations.contains(addedSection.getDownStation())) {
+            Section foundSection = findSectionByDownStation(addedSection.getDownStation());
+            foundSection.update(foundSection.getUpStation(), foundSection.getUpStation(), foundSection.getDistance() - addedSection.getDistance());
+        }
+
+        sections.add(addedSection);
     }
 
     public void removeSection(Section section) {
         sections.remove(section);
+    }
+
+    public boolean isContainsSection(Section section) {
+        return sections.contains(section);
+    }
+
+    private List<Station> getStations() {
+        List<Station> stations = new LinkedList<>();
+        sections.forEach(section -> {
+            stations.add(section.getUpStation());
+            stations.add(section.getDownStation());
+        });
+        return stations.stream().distinct().collect(Collectors.toList());
+    }
+
+    private Section findSectionByDownStation(Station downStation) {
+        return sections.stream()
+                .filter(section -> section.getDownStation().equals(downStation))
+                .findFirst()
+                .get();
+    }
+
+    private Section findSectionByUpstation(Station upStation) {
+        return sections.stream()
+                .filter(section -> section.getUpStation().equals(upStation))
+                .findFirst()
+                .get();
     }
 }
