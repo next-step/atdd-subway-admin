2277415c6d6860dfb2ba65a93c3e25abcfd21aa1
lovethefeel <lovethefeel@gmail.com>
2021-11-30 01:43:07 +0900

    refactor: 정적 메소드 추가

--- src/main/java/nextstep/subway/station/dto/StationResponse.java
+++ src/main/java/nextstep/subway/station/dto/StationResponse.java
@@ -3,18 +3,28 @@ package nextstep.subway.station.dto;
 import nextstep.subway.station.domain.Station;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StationResponse {
     private Long id;
     private String name;
+
     private LocalDateTime createdDate;
+
     private LocalDateTime modifiedDate;
+
+    protected StationResponse() {
+    }
 
     public static StationResponse from(final Station station) {
         return new StationResponse(station.getId(), station.getName(), station.getCreatedDate(), station.getModifiedDate());
     }
 
-    public StationResponse() {
+    public static List<StationResponse> from(final List<Station> stations) {
+        return stations.stream()
+                .map(StationResponse::from)
+                .collect(Collectors.toList());
     }
 
     public StationResponse(final Long id, final String name, final LocalDateTime createdDate, final LocalDateTime modifiedDate) {
@@ -30,5 +40,13 @@ public class StationResponse {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
     }
 }
