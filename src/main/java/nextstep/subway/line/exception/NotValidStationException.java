bc5eeab25054c7f2cfb2e51144df57d3d0d9a0cc
shryu <>
2021-11-30 03:42:54 +0900

    feat : 구간 역 관련 exception 구현

---
+++ src/main/java/nextstep/subway/line/exception/NotValidStationException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.line.exception;
+
+public class NotValidStationException extends RuntimeException {
+    public NotValidStationException(String message) {
+        super(message);
+    }
+}
