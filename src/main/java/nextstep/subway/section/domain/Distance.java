ece9098bd3db31be7f46fb5cc998fc4bda943094
shryu <>
2021-11-30 03:19:51 +0900

    feat : Distance 값 객체 구현

---
+++ src/main/java/nextstep/subway/section/domain/Distance.java
@@ -0,0 +1,36 @@
+package nextstep.subway.section.domain;
+
+import nextstep.subway.section.exception.UnValidDistanceException;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Distance {
+    private static final int MINIMUM_DISTANCE = 1;
+    @Column(nullable = false)
+    private int distance;
+
+    protected Distance() {
+    }
+
+    public Distance(int distance) {
+        validateDistance(distance);
+        this.distance = distance;
+    }
+
+    public int getValue() {
+        return distance;
+    }
+
+    public void update(int distance) {
+        validateDistance(distance);
+        this.distance = distance;
+    }
+
+    private void validateDistance(int distance) {
+        if (distance < MINIMUM_DISTANCE) {
+            throw new UnValidDistanceException("구간 입력이 잘못되었습니다.");
+        }
+    }
+}
