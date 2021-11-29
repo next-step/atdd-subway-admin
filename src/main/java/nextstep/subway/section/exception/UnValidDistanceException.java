137d46eedc4fb9840b5db46dbaff82941c8cf0e8
shryu <>
2021-11-30 03:41:04 +0900

    style : UnValidException 이름 수정

--- src/main/java/nextstep/subway/section/exception/UnValidDistanceException.java
+++
@@ -1,7 +0,0 @@
-package nextstep.subway.section.exception;
-
-public class UnValidDistanceException extends RuntimeException {
-    public UnValidDistanceException(String message) {
-        super(message);
-    }
-}
