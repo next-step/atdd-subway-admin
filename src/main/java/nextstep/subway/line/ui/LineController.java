4d3001adf5e5137505bfc7e65238345fda2709a4
shryu <>
2021-11-29 16:45:37 +0900

    feat : 구간추가 controller 구현

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,10 +1,14 @@
 package nextstep.subway.line.ui;
 
 import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.application.dto.AddSectionRequest;
 import nextstep.subway.line.application.dto.LineUpdateRequest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.exception.ExistDuplicatedNameException;
+import nextstep.subway.section.domain.Section;
+import nextstep.subway.section.dto.SectionRequest;
+import nextstep.subway.section.dto.SectionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -50,6 +54,19 @@ public class LineController {
     public ResponseEntity deleteLine(@PathVariable Long lineId) {
         lineService.delete(lineId);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{lineId}/sections")
+    public ResponseEntity<SectionResponse> addSection(@PathVariable Long lineId,
+                                                      @RequestBody SectionRequest sectionRequest) {
+        Section section = lineService.addSection(new AddSectionRequest(lineId,
+                sectionRequest.getUpStationId(),
+                sectionRequest.getDownStationId(),
+                sectionRequest.getDistance()));
+
+        SectionResponse sectionResponse =
+                new SectionResponse(section.getId(), section.getUpStation().getId(), section.getDownStation().getId(), section.getDistance());
+        return ResponseEntity.created(URI.create("/lines/" + lineId + "/sections" + sectionResponse.getId())).body(sectionResponse);
     }
 
     @ExceptionHandler(ExistDuplicatedNameException.class)
