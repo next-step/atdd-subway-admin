e9b960258219f6a926ba5c3424b7df50253ba5f2
lovethefeel <lovethefeel@gmail.com>
2021-11-30 01:41:07 +0900

    refactor: 피드백 반영 부정 메세지

--- src/main/java/nextstep/subway/common/Message.java
+++ src/main/java/nextstep/subway/common/Message.java
@@ -2,9 +2,11 @@ package nextstep.subway.common;
 
 public enum Message {
 
-    SECTION_DISTANCE_REGISTER("등록할 수 없는 구간입니다."),
-    BOTH_UP_AND_DOWN("상행, 하행 역 모두가 포함되어있습니다."),
-    BOTH_UP_AND_DOWN_NOT("상행, 하행 역 모두가 포함되지 않았습니다.");
+    NOT_FIND_LINE("해당 노선은 존재하지 않습니다."),
+    NOT_FIND_STATION("해당 역은 존재하지 않습니다."),
+    NOT_REGISTER_SECTION_DISTANCE("등록할 수 없는 구간입니다."),
+    NOT_REGISTER_ALL_INCLUDE("상행, 하행 역 모두가 포함되어 있어 등록할 수 없습니다."),
+    NOT_REGISTER_NOT_ALL_INCLUDE("상행, 하행 역 모두가 포함되지 않아서 등록할 수 없습니다.");
 
     private final String message;
 
