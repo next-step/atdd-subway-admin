bac02b19969bd56f59fd3918a12c741237214c39
HeeSeok Noh <nhs0912@nate.com>
2021-11-30 23:30:51 +0900

    refactor: sortTest 리팩토링

--- src/test/java/nextstep/subway/line/LineTest.java
+++ src/test/java/nextstep/subway/line/LineTest.java
@@ -1,30 +1,25 @@
 package nextstep.subway.line;
 
-import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Distance;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@DisplayName("지하철 라인 테스트")
 public class LineTest {
 
     @Test
     @DisplayName("지하철 목록을 조회한다.")
     void searchStationsTest() {
-        StationRequest dangSanStationRequest = new StationRequest("당산역");
-        StationRequest hongDaeStationRequest = new StationRequest("홍대입구역");
+        Station dangSanStation = new Station("당산역");
+        Station hongDaeStation = new Station("홍대입구역");
+        Station chungJeoungRoStation = new Station("충정로역");
+        Line line = new Line("2호선", "green", dangSanStation, hongDaeStation, new Distance(10));
 
-        StationRequest chungJeoungRoStationRequest = new StationRequest("충정로역");
-        Station savedDangSanStation = dangSanStationRequest.toStation();
-        Station savedHongDaeStation = hongDaeStationRequest.toStation();
-        Station chungJeoungRoStation = chungJeoungRoStationRequest.toStation();
-        Line line = new Line("2호선", "green", savedDangSanStation, savedHongDaeStation, new Distance(10));
-
-        Section newSection = new Section(line, savedHongDaeStation, chungJeoungRoStation, new Distance(5));
+        Section newSection = new Section(line, hongDaeStation, chungJeoungRoStation, new Distance(5));
 
         line.addSection(newSection);
         assertThat(line.getStations().size()).isEqualTo(3);
--- src/test/java/nextstep/subway/line/SectionsTest.java
+++ src/test/java/nextstep/subway/line/SectionsTest.java
@@ -1,70 +1,77 @@
 package nextstep.subway.line;
 
-import nextstep.subway.exception.InputDataErrorCode;
 import nextstep.subway.exception.InputDataErrorException;
-import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Distance;
+import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
-import nextstep.subway.station.dto.StationRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("지하철 구간 기능 테스트")
 public class SectionsTest {
 
     @Test
     @DisplayName("상행종점 추가 충정로역- 당산역- 홍대입구역")
     public void sortTest() {
-        Line line = createLine("당산역", "홍대입구역");
 
-        Station upStation = findStation(line, "당산역");
-        Station createdStation = new StationRequest("충정로역").toStation();
-        Section newSection = new Section(line, createdStation, upStation, new Distance(3));
+        Station dangSanStation = new Station("당산역");
+        Station hongDaeStation = new Station("홍대입구역");
+        Station chungJeongRoStation = new Station("충정로역");
+
+        Line line = createLine(dangSanStation, hongDaeStation);
+        Section newSection = new Section(line, chungJeongRoStation, dangSanStation, new Distance(3));
         line.addSection(newSection);
 
-        checkStationNames(line,"충정로역", "당산역", "홍대입구역");
-        checkDistance(line,"충정로역", 3);
+        checkStationNames(line, "충정로역", "당산역", "홍대입구역");
+        checkDistance(line, "충정로역", 3);
     }
 
     @Test
     @DisplayName("구간 중간 추가 당산역- 충정로역- 홍대입구역")
     public void addMiddleTest() {
-        Line line = createLine("당산역", "홍대입구역");
+        Station dangSanStation = new Station("당산역");
+        Station hongDaeStation = new Station("홍대입구역");
+        Station chungJeongRoStation = new Station("충정로역");
 
-        Station createdStation = new StationRequest("충정로역").toStation();
-        Station upStation = findStation(line, "당산역");
-        Section newSection = new Section(line, upStation, createdStation, new Distance(2));
+        Line line = createLine(dangSanStation, hongDaeStation);
+
+        Section newSection = new Section(line, dangSanStation, chungJeongRoStation, new Distance(2));
         line.addSection(newSection);
 
-        checkStationNames(line,"당산역", "충정로역", "홍대입구역");
-        checkDistance(line,"홍대입구역", 8);
+        checkStationNames(line, "당산역", "충정로역", "홍대입구역");
+        checkDistance(line, "홍대입구역", 8);
     }
 
     @Test
     @DisplayName("하행종점 중간 추가 당산역- 홍대입구역- 충정로역")
     public void addEndTest() {
-        Line line = createLine("당산역", "홍대입구역");
+        Station dangSanStation = new Station("당산역");
+        Station hongDaeStation = new Station("홍대입구역");
+        Station chungJeongRoStation = new Station("충정로역");
 
-        Station createdStation = new StationRequest("충정로역").toStation();
-        Station downStation = findStation(line, "홍대입구역");
-        Section newSection = new Section(line, downStation, createdStation, new Distance(1));
+        Line line = createLine(dangSanStation, hongDaeStation);
+
+        Section newSection = new Section(line, hongDaeStation, chungJeongRoStation, new Distance(1));
         line.addSection(newSection);
 
-        checkStationNames(line,"당산역", "홍대입구역", "충정로역");
-        checkDistance(line,"충정로역", 1);
+        checkStationNames(line, "당산역", "홍대입구역", "충정로역");
+        checkDistance(line, "충정로역", 1);
     }
 
     @Test
     @DisplayName("구간 중간 추가 기존 구간보다 같을 경우 에러처리")
     public void addSameDistanceTest() {
-        Line line = createLine("당산역", "홍대입구역");
+        Station dangSanStation = new Station("당산역");
+        Station hongDaeStation = new Station("홍대입구역");
+        Station chungJeongRoStation = new Station("충정로역");
 
-        Station createdStation = new StationRequest("충정로역").toStation();
-        Station upStation = findStation(line, "당산역");
-        Section newSection = new Section(line, upStation, createdStation, new Distance(10));
+        Line line = createLine(dangSanStation, hongDaeStation);
+
+        Section newSection = new Section(line, dangSanStation, chungJeongRoStation, new Distance(10));
 
         assertThatThrownBy(() -> {
             line.addSection(newSection);
@@ -73,18 +80,8 @@ public class SectionsTest {
 
     }
 
-    private Line createLine(String upStationName, String downStationName) {
-        Station upStation = new StationRequest(upStationName).toStation();
-        Station downStation = new StationRequest(downStationName).toStation();
-
+    private Line createLine(Station upStation, Station downStation) {
         return new Line("2호선", "green", upStation, downStation, new Distance(10));
-    }
-
-    private Station findStation(Line line, String stationName) {
-        return line.getStations().stream()
-                .filter(it -> it.getName().equals(stationName))
-                .findFirst()
-                .orElseThrow(() -> new InputDataErrorException(InputDataErrorCode.THERE_IS_NOT_SEARCHED_STATION));
     }
 
     private void checkDistance(Line line, String sectionStationName, int exepectedDistance) {
