0ddeae479219f32ca8d48643d4edfebc1ce54cf3
jennie267 <jennieee267@gmail.com>
2021-12-01 00:05:27 +0900

    test : DB에 등록되지 않은 역으로 구간 추가할때 테스트

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -16,6 +16,7 @@ import nextstep.subway.AcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
+import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.dto.StationResponse;
 
 @DisplayName("지하철 구간 관련 기능")
@@ -89,6 +90,25 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         // when
         ExtractableResponse<Response> response = 구간_추가_요청(이호선.getId(), request);
 
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    
+    @DisplayName("역이 DB에 등록되어있지않으면 추가할 수 없음")
+    @Test
+    void DB에_등록되지_않은_역_구간_등록_불가() {
+        // given
+        StationResponse 서울대입구역 = 지하철역_등록되어_있음("서울대입구역");
+        StationResponse 낙성대역 = 지하철역_등록되어_있음("낙성대역");
+        LineResponse 이호선 = 지하철_노선_등록되어_있음(LineRequest.of("이호선", "bg-green-600", 서울대입구역.getId(), 낙성대역.getId(), 30));
+        
+        StationResponse 잠실역 = StationResponse.of(Station.from("잠실역"));
+        StationResponse 건대역 = StationResponse.of(Station.from("건대역"));
+        SectionRequest request = SectionRequest.of(잠실역.getId(), 건대역.getId(), 20);
+
+        // when
+        ExtractableResponse<Response> response = 구간_추가_요청(이호선.getId(), request);
+        
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
