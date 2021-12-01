2a93d19bc1300fe7bf0e5befb6cbd09196bff392
lovethefeel <lovethefeel@gmail.com>
2021-12-02 02:13:49 +0900

    test: 구간 제거 픽스쳐 메소드 추가

--- src/test/java/nextstep/subway/fixture/TestSectionFactory.java
+++ src/test/java/nextstep/subway/fixture/TestSectionFactory.java
@@ -31,10 +31,10 @@ public class TestSectionFactory {
         );
     }
 
-    public static void 지하철_노선에_지하철역_구간_목록_포함됨(ExtractableResponse<Response> response) {
+    public static void 지하철_노선에_지하철역_구간_목록_포함됨(ExtractableResponse<Response> response, int expected) {
         LineResponse lineResponse = response.as(LineResponse.class);
         assertAll(
-                () -> assertThat(lineResponse.getStations()).hasSize(2)
+                () -> assertThat(lineResponse.getStations()).hasSize(expected)
         );
     }
 
@@ -42,4 +42,22 @@ public class TestSectionFactory {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
+    public static ExtractableResponse<Response> 지하철_노선_구간_삭제_요청(LineResponse 이호선, StationResponse 삭제역) {
+        return RestAssured.given().log().all()
+                .queryParam("stationId", 삭제역.getId())
+                .when()
+                .delete("/lines/"+이호선.getId()+"/sections")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 지하철_노선_구간_삭제됨(ExtractableResponse<Response> response) {
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        );
+    }
+
+    public static void 지하철_노선_구간_삭제_실패됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 }
