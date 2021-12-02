53440f2aa112b2a47f9b4e69c627eb8d4e7ac654
vicjed <vicjed@icloud.com>
2021-12-03 01:14:27 +0900

    fix(Message) : 메시지 수정

--- src/main/java/nextstep/subway/common/Message.java
+++ src/main/java/nextstep/subway/common/Message.java
@@ -4,7 +4,7 @@ public enum Message {
     MESSAGE_SECTION_DISTANCE_NOT_LESS_THAN_ZERO("구간의 거리가 0과 같거나 작을 수 없습니다. 입력된 값[%d]"),
     MESSAGE_IS_NOT_EXISTS_STATION("기존 노선에 해당역이 존재하지 않습니다. 상행역[%s] 하행역[%s]"),
     MESSAGE_ALREADY_REGISTERED_SECTION("이미 등록되어 있는 구간입니다."),
-    MESSAGE_LIMIT_SECTION_SIZE("구간은 하나이상이거나 작아야합니다."),
+    MESSAGE_LIMIT_SECTION_SIZE("제거할 수 없습니다. \n 해당 라인의 기점역과 종점역입니다."),
     STATION_DUPLICATION("상행선과 하행선은 동일할 수 없습니다."),
     SECTION_NOT_FOUND_EXCEPTION("해당_구간_영역이_존재하지 않습니다."),
     STATION_NOTFOUND_EXCEPTION ("지하철역이 존재하지 않습니다.");
