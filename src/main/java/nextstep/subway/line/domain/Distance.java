3a9503c3e78e48e0d4f0b904568c75a9d31aaeb4
jennie267 <jennieee267@gmail.com>
2021-12-02 23:48:12 +0900

    refactor : Distance 불필요해진 메서드 정리 (checkShorter)

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -27,18 +27,11 @@ public class Distance {
     
     private void validateDistance(int distance) {
         if (distance <= 0) {
-            throw new IllegalArgumentException("구간의 길이는 0보다 커야합니다.");
-        }
-    }
-    
-    private void checkShorter(Distance distance) {
-        if (this.distance <= distance.getDistance()) {
-            throw new IllegalArgumentException(String.format("길이가 맞지 않는 노선입니다.(%d)", this.distance));
+            throw new IllegalArgumentException(String.format("길이가 유효하지 않습니다.(%d)", distance));
         }
     }
     
     public Distance minus(Distance distance) {
-        checkShorter(distance);
         return Distance.from(this.distance - distance.getDistance());
     }
 
