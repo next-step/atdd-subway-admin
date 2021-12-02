f8b20237634c5ff8f4124e8f42aa7971d36e2f25
condingknowjam <hanwoodww@gmail.com>
2021-12-02 18:04:06 +0900

    refactor: 피드백 반영

--- src/main/java/nextstep/subway/exception/SubwayExceptionHandler.java
+++ src/main/java/nextstep/subway/exception/SubwayExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class LineExceptionHandler {
+public class SubwayExceptionHandler {
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<Void> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
