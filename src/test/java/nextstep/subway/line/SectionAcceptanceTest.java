b6e8779afa254e56426c12eb4e38ed29ec547a11
jennie267 <jennieee267@gmail.com>
2021-12-04 03:29:17 +0900

    test : 구간 삭제 예외 인수 테스트 작성

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -131,6 +131,21 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
+    
+    @DisplayName("구간이 하나인 노선은 지하철역을 제거할 수 없음")
+    @Test
+    void 구간_하나짜리_노선_지하철역_제거() {
+        // given
+        StationResponse 서울대입구역 = 지하철역_등록되어_있음("서울대입구역");
+        StationResponse 낙성대역 = 지하철역_등록되어_있음("낙성대역");
+        LineResponse 이호선 = 지하철_노선_등록되어_있음(LineRequest.of("이호선", "bg-green-600", 서울대입구역.getId(), 낙성대역.getId(), 30));
+
+        // when
+        ExtractableResponse<Response> response = 구간_제거_요청(이호선.getId(), 서울대입구역.getId());
+        
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 
     private ExtractableResponse<Response> 구간_추가_요청(Long lineId, SectionRequest request) {
         return RestAssured
