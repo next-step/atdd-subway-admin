124b8c16747f53a05d5c81c681ee1fdf86e73f1a
lewisseo91 <togokr@naver.com>
2021-12-03 21:46:13 +0900

    refactor(Exception): serialNumber 추가
    serialNumber 필요한 예외들 추가, 줄 바꿈 정리

--- src/main/java/nextstep/subway/common/exception/NotFoundStationException.java
+++ src/main/java/nextstep/subway/common/exception/NotFoundStationException.java
@@ -2,6 +2,8 @@ package nextstep.subway.common.exception;
 
 public class NotFoundStationException extends RuntimeException {
     public static final String NOT_FOUND_STATION_MESSAGE = "역을 찾을 수 없습니다.";
+    private static final long serialVersionUID = 7L;
+
     public NotFoundStationException() {
         super(NOT_FOUND_STATION_MESSAGE);
     }
