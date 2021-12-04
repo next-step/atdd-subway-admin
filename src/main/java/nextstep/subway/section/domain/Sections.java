b3d52b58bf4f35a15a68274ff04607c387d6bd1c
daehyun0 <dd70296@gmail.com>
2021-12-04 09:51:32 +0900

    refactor: sections equals&hashcode 구현

--- src/main/java/nextstep/subway/section/domain/Sections.java
+++ src/main/java/nextstep/subway/section/domain/Sections.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
@@ -62,5 +63,20 @@ public class Sections {
 
 	public boolean contains(Section section) {
 		return sections.contains(section);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Sections sections1 = (Sections)o;
+		return Objects.equals(sections, sections1.sections);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(sections);
 	}
 }
