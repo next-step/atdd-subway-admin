9d04de8357709270e6d3883feea43533ee990f03
신민준 <jxin95@gmail.com>
2021-12-05 02:29:54 +0900

    feat: Distance 클래스에 deduct(빼기) 메서드 추가

--- src/main/java/nextstep/subway/section/domain/Distance.java
+++ src/main/java/nextstep/subway/section/domain/Distance.java
@@ -7,6 +7,8 @@ import javax.persistence.Embeddable;
 public class Distance {
 
     private static final int MIN_DISTANCE = 0;
+    private static final String TOO_LOW_DISTANCE = "거리는 0보다 커야 합니다.";
+    private static final String SHOULD_NEED_BIGGER_VALUE = "기존 구간보다 큰 수를 입력할 수 없습니다.";
 
     @Column
     private int distance;
@@ -15,18 +17,24 @@ public class Distance {
     }
 
     public Distance(final int distance) {
-        validate(distance);
+        validate(distance, TOO_LOW_DISTANCE);
         this.distance = distance;
+    }
+
+    public void deduct(final int value) {
+        int result = this.distance - value;
+        validate(result, SHOULD_NEED_BIGGER_VALUE);
+        this.distance = result;
+    }
+
+    private void validate(final int distance, final String errorMessage) {
+        if (MIN_DISTANCE >= distance) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 
     public int getDistance() {
         return distance;
-    }
-
-    private void validate(int distance) {
-        if (MIN_DISTANCE >= distance) {
-            throw new IllegalArgumentException("거리는 0보다 커야 합니다.");
-        }
     }
 
 }
