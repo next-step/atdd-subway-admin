319080438caf2234ab34030268b26df2f00c57da
류성호 <>
2021-12-04 19:08:02 +0900

    feat : 구간 제거 exception 클래스 구현

---
+++ src/main/java/nextstep/subway/section/exception/NotRemoveSectionException.java
@@ -0,0 +1,7 @@
+package nextstep.subway.section.exception;
+
+public class NotRemoveSectionException extends RuntimeException {
+    public NotRemoveSectionException(String message) {
+        super(message);
+    }
+}
