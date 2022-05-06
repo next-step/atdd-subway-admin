feb53802dd903fee916e77287eae221f6ddb4bc1
yyy96 <dlduswo1247@gmail.com>
2022-05-07 00:45:15 +0900

    feat: Section Entity 구현

---
+++ src/main/java/nextstep/subway/line/domain/Section.java
@@ -0,0 +1,44 @@
+package nextstep.subway.line.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id")
+    private Line line;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public Section() {
+    }
+
+    public Section(Line line, Long upStationId, Long downStationId, int distance) {
+        this.line = line;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
+
+
