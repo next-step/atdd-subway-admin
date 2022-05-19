6d59d3743fc26894a53239ef9f17805ce0e2a1a3
boorownie <probitanima11@gmail.com>
2022-05-19 19:59:54 +0900

    default code

---
+++ src/main/java/nextstep/subway/dto/StationRequest.java
@@ -0,0 +1,15 @@
+package nextstep.subway.dto;
+
+import nextstep.subway.domain.Station;
+
+public class StationRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Station toStation() {
+        return new Station(name);
+    }
+}
