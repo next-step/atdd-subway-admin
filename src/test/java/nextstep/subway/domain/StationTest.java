31d3e2f39e422acec85253758bc492fbfce8bcff
ssonsh <chosunci@gmail.com>
2022-05-20 22:35:32 +0900

    refactoring : Station 지하철역 도메인 엔티티의 name을 일급 객체로 Wrapping 한다.
    - 변경사항에 대해 Station 도메인과 StationName의 TDD

---
+++ src/test/java/nextstep/subway/domain/StationTest.java
@@ -0,0 +1,33 @@
+package nextstep.subway.domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StationTest {
+
+    @DisplayName("지하철 역 도메인을 생성한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"서울대입구역", "판교역", "수서역", "압구정역"})
+    void generate01(String name) {
+        // given & when
+        Station station = Station.from(name);
+
+        // then
+        assertEquals(StationName.from(name), station.getName());
+    }
+
+    @DisplayName("지하철 역 명이 null 이나 빈 문자열일 수 없다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void generate02(String name) {
+        // given & then & then
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> Station.from(name))
+            .withMessageMatching("지하철 역 명이 비어있습니다.");
+    }
+}
