fd0262843df6749305a871fda6cb53012241816e
KangSeongHyo <abc9023@naver.com>
2022-05-23 03:50:24 +0900

    refactor : 지하철역 관련 인수 테스트들을 하나의 인수테스로 통합 처리

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -35,92 +35,45 @@ class StationAcceptanceTest {
     }
 
     /**
-     * When 지하철역을 생성하면
-     * Then 지하철역이 생성된다
-     * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
-     */
-    @DisplayName("지하철역을 생성한다.")
-    @Test
-    void createStation() {
-        // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-        ExtractableResponse<Response> createResponse = requestCreateStation(params);
-
-        // then
-        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        // then
-        ExtractableResponse<Response> response = requestGetStations();
-        assertThat(response.jsonPath().getList("name")).containsAnyOf("강남역");
-    }
-
-    /**
-     * Given 지하철역을 생성하고
+     * When 지하철역을 2개를 생성하면
+     * Then 지하철역 2개가 생성된다
      * When 기존에 존재하는 지하철역 이름으로 지하철역을 생성하면
      * Then 지하철역 생성이 안된다
-     */
-    @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
-    @Test
-    void createStationWithDuplicateName() {
-        // given
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "강남역");
-        assertThat(requestCreateStation(params).statusCode()).isEqualTo(HttpStatus.CREATED.value());
-
-        // when
-        ExtractableResponse<Response> response = requestCreateStation(params);
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    /**
-     * Given 2개의 지하철역을 생성하고
-     * When 지하철역 목록을 조회하면
+     * When 지하철역 목록 조회시
      * Then 2개의 지하철역을 응답 받는다
-     */
-    @DisplayName("지하철역을 조회한다.")
+     * When 2개 중 1개 지하철역을 삭제하면
+     * Then 그 지하철역은 목록 조회 시 생성한 역을 찾을 수 없다
+     * */
+    @DisplayName("지하철역을 관리한다.")
     @Test
-    void getStations() {
-
-        //given
+    void manageStations(){
+        // when
         Map<String, String> params1 = new HashMap<>();
         Map<String, String> params2 = new HashMap<>();
         params1.put("name", "강남역");
         params2.put("name", "서울역");
-        for (ExtractableResponse<Response> response : requestCreateStations(Arrays.asList(params1, params2))) {
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // then
+        for (ExtractableResponse<Response> createResponse : requestCreateStations(Arrays.asList(params1, params2))) {
+            assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         }
 
-        //when
-        ExtractableResponse<Response> response = requestGetStations();
+        // when
+        ExtractableResponse<Response> createResponse = requestCreateStation(params1);
+
+        // then
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        // when
+        ExtractableResponse<Response> getAllResponse = requestGetStations();
+        assertThat(getAllResponse.jsonPath().getList("name")).contains("강남역");
 
         //then
         assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.jsonPath().getList("id")).contains(1, 2),
-                () -> assertThat(response.jsonPath().getList("name")).contains("강남역", "서울역")
+                () -> assertThat(getAllResponse.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(getAllResponse.jsonPath().getList("id")).contains(1, 2),
+                () -> assertThat(getAllResponse.jsonPath().getList("name")).contains("강남역", "서울역")
         );
-    }
-
-    /**
-     * Given 지하철역을 생성하고
-     * When 그 지하철역을 삭제하면
-     * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-     */
-    @DisplayName("지하철역을 제거한다.")
-    @Test
-    void deleteStation() {
-
-        //given
-        Map<String, String> params1 = new HashMap<>();
-        Map<String, String> params2 = new HashMap<>();
-        params1.put("name", "신림역");
-        params2.put("name", "서울역");
-        for (ExtractableResponse<Response> response : requestCreateStations(Arrays.asList(params1, params2))) {
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        }
 
         //when
         assertThat(requestDeleteStation(1L).statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
@@ -130,7 +83,7 @@ class StationAcceptanceTest {
         assertAll(
                 () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(response.jsonPath().getList("id")).doesNotContain(1),
-                () -> assertThat(response.jsonPath().getList("name")).doesNotContain("신림역"),
+                () -> assertThat(response.jsonPath().getList("name")).doesNotContain("강남역"),
 
                 () -> assertThat(response.jsonPath().getList("id")).contains(2),
                 () -> assertThat(response.jsonPath().getList("name")).contains("서울역")
