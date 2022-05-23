97c6f20b0dc9017343b0091b1b9687b4277d2c3d
ssonsh <chosunci@gmail.com>
2022-05-24 07:40:59 +0900

    refactor : 인수테스트 케이스간 테스트 격리시키기
    - @Sql 을 활용한 .sql 작성된 쿼리문서 수행
    
    - DELETE가 아닌 TRUNCATE 를 통한 테이블 데이터 초기화

--- src/test/java/nextstep/subway/AcceptanceTest.java
+++ src/test/java/nextstep/subway/AcceptanceTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("/truncate.sql")
 public class AcceptanceTest {
 
     @LocalServerPort
