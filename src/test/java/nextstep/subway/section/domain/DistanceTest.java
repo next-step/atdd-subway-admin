88515db5a91c8142fffb78717462e056b83ec5c3
ssonsh <chosunci@gmail.com>
2022-05-25 00:04:36 +0900

    test(Section) : 지하철 구간 도메인 엔티티 기능 개발에 대한 테스트 코드 작성
    - 상행, 하행, 거리로 구성된 지하철 구간을 생성할 수 있다.
    
    - 각 요소별 생성 불가한 케이스에 대한 테스트 코드 작성

---
+++ src/test/java/nextstep/subway/section/domain/DistanceTest.java
@@ -0,0 +1,30 @@
+package nextstep.subway.section.domain;
+
+import static nextstep.subway.constants.SectionExceptionMessage.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import nextstep.subway.constants.SectionExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DistanceTest {
+
+    @DisplayName("1 이상의 정수로 거리를 생성할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100, 1000})
+    void generate01(int distance) {
+        // given & when & then
+        assertThatNoException().isThrownBy(() -> Distance.from(distance));
+    }
+
+    @DisplayName("1보다 작은 정수로 거리를 생성할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -1, 0})
+    void generate02(int distance) {
+        // given & when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> Distance.from(distance))
+            .withMessageContaining(DISTANCE_IS_MUST_BE_GREATER_THAN_1);
+    }
+}
---
+++ src/test/java/nextstep/subway/section/domain/SectionTest.java
@@ -0,0 +1,72 @@
+package nextstep.subway.section.domain;
+
+import static nextstep.subway.constants.SectionExceptionMessage.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SectionTest {
+    private static final int DISTANCE = 5;
+
+    private Station 판교역;
+    private Station 강남역;
+    private Distance distance;
+
+    @BeforeEach
+    void setUp() {
+        판교역 = Station.of(1L, "판교역");
+        강남역 = Station.of(2L, "강남역");
+        distance = Distance.from(DISTANCE);
+    }
+
+    @DisplayName("지하철 구간을 상행역과 하행역, 거리를 통해 생성할 수 있다.")
+    @Test
+    void generate01() {
+        // given & when
+        Section section = Section.of(판교역, 강남역, distance);
+
+        // then
+        assertAll(
+            () -> assertEquals(section.getUpStation(), 판교역),
+            () -> assertEquals(section.getDownStation(), 강남역),
+            () -> assertEquals(section.getDistance(), distance)
+        );
+    }
+
+    @DisplayName("지하철 구간 생성 시 상행역 없이 생성될 수 없다.")
+    @Test
+    void generate02() {
+        // given & when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> Section.of(null, 강남역, distance))
+            .withMessageContaining(UP_STATION_IS_NOT_NULL);
+    }
+
+    @DisplayName("지하철 구간 생성 시 하행역 없이 생성될 수 없다.")
+    @Test
+    void generate03() {
+        // given & when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> Section.of(판교역, null, distance))
+            .withMessageContaining(DOWN_STATION_IS_NOT_NULL);
+    }
+
+    @DisplayName("지하철 구간 생성 시 구간 없이 생성될 수 없다.")
+    @Test
+    void generate04() {
+        // given & when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> Section.of(판교역, 강남역, null))
+            .withMessageContaining(DISTANCE_IS_NOT_NULL);
+    }
+
+    @DisplayName("지하철 구간 생성 시 상, 하행 구간이 같을 수 없다.")
+    @Test
+    void generate05() {
+        // given & when & then
+        assertThatIllegalArgumentException().isThrownBy(() -> Section.of(판교역, 판교역, distance))
+            .withMessageContaining(CANNOT_EQUALS_UP_STATION_WITH_DOWN_STATION);
+    }
+}
