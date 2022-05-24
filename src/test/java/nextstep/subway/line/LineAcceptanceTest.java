e3565a58683708481dd2a98e2b43de176182cde0
limwoobin <drogba02@naver.com>
2022-05-24 21:35:27 +0900

    test: 지하철 노선 목록조회에 대한 인수테스트를 작성하라

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -8,6 +8,7 @@ import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import nextstep.subway.dto.LineResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,7 +37,7 @@ class LineAcceptanceTest {
      * Then 지하철 노선이 생성된다
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
      */
-    @DisplayName("노선을 생성한다.")
+    @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createStation() {
         // when
@@ -66,7 +67,63 @@ class LineAcceptanceTest {
         assertThat(convertToFiledName(getResponse, "color")).containsAnyOf("yellow");
     }
 
+    /**
+     * given 2개의 지하철 노선을 생성하고
+     * when 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다
+     */
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void getLines() {
+        // given
+        지하철_노선_생성("2호선", "red");
+        지하철_노선_생성("1호선", "blue");
+
+        // when
+        ExtractableResponse<Response> response = 지하철_노선_조회();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(convertToDTO(response)).hasSize(2);
+    }
+
+    /**
+     * 전달받은 지하철역 목록을 저장한다
+     * @param name 노선 이름
+     * @param color 노선 색상
+     */
+    private ExtractableResponse<Response> 지하철_노선_생성(String name, String color) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+
+        return RestAssured.given().log().all()
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/lines")
+            .then().log().all()
+            .extract();
+    }
+
+    /**
+     * 지하철 노선 목록을 조회한다
+     */
+    private ExtractableResponse<Response> 지하철_노선_조회() {
+        return RestAssured.given().log().all()
+            .when().get("/lines")
+            .then().log().all()
+            .extract();
+    }
+
     private List<String> convertToFiledName(ExtractableResponse<Response> response, String field) {
+        return response.jsonPath().getList(field, String.class);
+    }
+
+    private List<LineResponse> convertToDTO(ExtractableResponse<Response> response) {
+        return response.jsonPath().getList("", LineResponse.class);
+    }
+
+    private List<String> convertToFieldNames(ExtractableResponse<Response> response, String field) {
         return response.jsonPath().getList(field, String.class);
     }
 }
