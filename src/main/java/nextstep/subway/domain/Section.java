41bb44d6f68067b0036e112532b2f76308efc655
heowc <heowc1992@gmail.com>
2022-05-24 21:01:40 +0900

    feat: 도메인 구현

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -13,10 +13,10 @@ public class Section extends BaseEntity {
 
     private Long lineId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private Station upStation;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     private Station downStation;
 
     @Embedded
@@ -34,6 +34,40 @@ public class Section extends BaseEntity {
     public void bindLine(Line line) {
         requireNonNull(line, "지하철 노선이 비었습니다");
         this.lineId = line.getId();
+    }
+
+    public boolean isConnectable(Section other) {
+        boolean upConnected = isConnectable(this.upStation, other);
+        boolean downConnected = isConnectable(this.downStation, other);
+        return !((upConnected && downConnected) || (!upConnected && !downConnected));
+    }
+
+    private boolean isConnectable(Station station, Section otherSection) {
+        return station.equals(otherSection.getUpStation()) || station.equals(otherSection.getDownStation());
+    }
+
+    public void relocate(Section section) {
+        if (this.upStation.equals(section.upStation) && !this.downStation.equals(section.downStation)) {
+            relocateUpStation(section);
+            return;
+        }
+        if (this.downStation.equals(section.downStation) && !this.upStation.equals(section.upStation)) {
+            relocateDownStation(section);
+            return;
+        }
+    }
+
+    private void relocateUpStation(Section section) {
+        Station tempStation = this.downStation;
+        this.downStation = section.downStation;
+        section.upStation = this.downStation;
+        section.downStation = tempStation;
+        distance.minus(section.distance);
+    }
+
+    private void relocateDownStation(Section section) {
+        this.downStation = section.upStation;
+        distance.minus(section.distance);
     }
 
     public Station getUpStation() {
--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -4,10 +4,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
@@ -25,14 +23,49 @@ public class Sections {
     protected Sections() {
     }
 
-    public void add(Section section) {
-        requireNonNull(section, "section");
-        sections.add(section);
+    public void add(Section other) {
+        requireNonNull(other, "section");
+        if (!sections.isEmpty()) {
+            Section section = getConnectableSection(other);
+            section.relocate(other);
+        }
+        sections.add(other);
+    }
+
+    private Section getConnectableSection(Section section) {
+        return sections.stream()
+                       .filter(s -> s.isConnectable(section))
+                       .findFirst()
+                       .orElseThrow(CanNotConnectSectionException::new);
     }
 
     public List<Station> getStations() {
-        return sections.stream()
-                       .flatMap(s -> Stream.of(s.getUpStation(), s.getDownStation()))
-                       .collect(Collectors.toList());
+        return getSectionsInOrder();
+    }
+
+    private List<Station> getSectionsInOrder() {
+        Map<Station, Station> map = sections.stream()
+                                            .collect(Collectors.toMap(Section::getUpStation, Section::getDownStation));
+        Station station = findUpStationOfHeadSection();
+        List<Station> stations = new ArrayList<>();
+        while (map.get(station) != null) {
+            stations.add(station);
+            station = map.get(station);
+        }
+        stations.add(station);
+        return stations;
+    }
+
+    private Station findUpStationOfHeadSection() {
+        Set<Station> upStations = sections.stream()
+                            .map(Section::getUpStation)
+                                           .collect(Collectors.toSet());
+        Set<Station> downStations = sections.stream()
+                                             .map(Section::getDownStation)
+                                             .collect(Collectors.toSet());
+        return upStations.stream()
+                         .filter(upStation -> !downStations.contains(upStation))
+                         .findFirst()
+                         .orElseThrow(IllegalArgumentException::new);
     }
 }
