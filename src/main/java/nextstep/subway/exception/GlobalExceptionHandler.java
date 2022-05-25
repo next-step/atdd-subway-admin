89f37abe08bed2bcd51ab85db0334a27233b72ee
KangSeongHyo <abc9023@naver.com>
2022-05-25 22:20:13 +0900

    feat : GlobalExceptionHandler 핸들러에 logger 추가

--- src/main/java/nextstep/subway/exception/GlobalExceptionHandler.java
+++ src/main/java/nextstep/subway/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package nextstep.subway.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler({IllegalArgumentException.class, DataIntegrityViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Void> badRequest(Exception e){
+    public ResponseEntity<Void> badRequest(Exception e) {
+        logger.error("Bad Request ",e);
         return ResponseEntity.badRequest().build();
     }
 
