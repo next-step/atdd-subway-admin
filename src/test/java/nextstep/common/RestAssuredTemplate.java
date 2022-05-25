5faa1a250faa1fa3ad19043afc1ac1b1f52e3773
Celina <luv.mins99@gmail.com>
2022-05-25 16:44:08 +0900

    test(RestAssuredTemplate): 공통 메서드 분리

---
+++ src/test/java/nextstep/common/RestAssuredTemplate.java
@@ -0,0 +1,33 @@
+package nextstep.common;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.Map;
+import org.springframework.http.MediaType;
+
+public class RestAssuredTemplate {
+    public static ExtractableResponse<Response> get(String path) {
+        return RestAssured.given().log().all()
+                .when().get(path)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> post(String path, Map<String, String> params) {
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post(path)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> delete(String path, Object param) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete(path, param)
+                .then().log().all()
+                .extract();
+    }
+}
