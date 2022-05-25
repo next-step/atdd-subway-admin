25b2de75c9f93535b264429ac0439abbe78ad7eb
kwonyongil <ksgkms23@likelion.org>
2022-05-25 21:16:28 +0900

    refactor(subway) 패키지 경로 변경

--- src/main/java/nextstep/subway/repository/StationRepository.java
+++ src/main/java/nextstep/subway/repository/StationRepository.java
@@ -1,8 +1,8 @@
-package nextstep.subway.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+package nextstep.subway.repository;
 
 import java.util.List;
+import nextstep.subway.domain.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
     @Override
