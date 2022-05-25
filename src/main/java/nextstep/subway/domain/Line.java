c9021faed7f21e3929cf58497862e10efa12c08b
tyakamyz <tyakamyz@naver.com>
2022-05-25 19:04:54 +0900

    feat: 지하철 노선, 구간 Entity 생성

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,7 +1,60 @@
 package nextstep.subway.domain;
 
-public class Line {
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Line extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "color")
+    private String color;
+
+    @OneToMany(mappedBy = "line")
+    private List<Section> Sections;
+
     public Long getId() {
-        return null;
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<Section> getSections() {
+        return Sections;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Line)) return false;
+        Line line = (Line) o;
+        if (getId() != null && Objects.equals(getId(), line.getId())) return true;
+        return Objects.equals(getId(), line.getId()) && Objects.equals(getName(), line.getName()) && Objects.equals(getColor(), line.getColor()) && Objects.equals(getSections(), line.getSections());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor(), getSections());
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
---
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -0,0 +1,68 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Section extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "distance")
+    private int distance;
+
+    @OneToOne
+    @JoinColumn(name = "STATION_ID")
+    private Station upStation;
+
+    @OneToOne
+    @JoinColumn(name = "STATION_ID")
+    private Station downStation;
+
+    @ManyToOne
+    @JoinColumn(name = "LINE_ID")
+    private Line line;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Section)) return false;
+        Section section = (Section) o;
+        if (getId() != null && Objects.equals(getId(), section.getId())) return true;
+        return getDistance() == section.getDistance() && Objects.equals(getId(), section.getId()) && Objects.equals(getUpStation(), section.getUpStation()) && Objects.equals(getDownStation(), section.getDownStation()) && Objects.equals(getLine(), section.getLine());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getDistance(), getUpStation(), getDownStation(), getLine());
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", distance=" + distance +
+                '}';
+    }
+}
