932de2877856472a2bb23ae0c6169f6d14c8c03a
kwonyongil <ksgkms23@likelion.org>
2022-05-26 00:59:24 +0900

    feat(subway) 지하철 노선 인수테스트 노선 생성 테스트 추가

---
+++ src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
@@ -0,0 +1,110 @@
+package nextstep.subway.acceptance.line;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import nextstep.subway.acceptance.base.BaseAcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+class LineAcceptanceTest extends BaseAcceptanceTest {
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @DisplayName("지하철노선 생성")
+    @Test
+    void createLine() {
+        // when
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "bg-red-600");
+        params.put("upStationId", 1);
+        params.put("downStationId", 2);
+        params.put("distance", 10);
+
+        지하철노선_생성_요청(params);
+
+        // when
+        ExtractableResponse<Response> showResponse = 지하철노선_목록_조회_요청();
+        List<String> lineNames = showResponse.jsonPath().getList("name", String.class);
+        List<String> colors = showResponse.jsonPath().getList("color", String.class);
+
+        // then
+        assertThat(lineNames).containsAnyOf("신분당선");
+        assertThat(colors).containsAnyOf("bg-red-600");
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철노선 목록 조회")
+    @Test
+    void showStation() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @DisplayName("지하철노선 조회")
+    @Test
+    void showStations() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @DisplayName("지하철노선 수정")
+    @Test
+    void updateStations() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @DisplayName("지하철노선 삭제")
+    @Test
+    void deleteStations() {
+
+    }
+
+    ExtractableResponse<Response> 지하철노선_생성_요청(Map<String, Object> params) {
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    ExtractableResponse<Response> 지하철노선_목록_조회_요청() {
+        return RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    ExtractableResponse<Response> 지하철노선_제거_요청(long id) {
+        return RestAssured.given().log().all()
+                .when().delete("/lines/" + id)
+                .then().log().all()
+                .extract();
+    }
+}
