4e95cca4af6a0e790659c8ff98d24437372b88da
Celina <luv.mins99@gmail.com>
2022-05-26 17:45:41 +0900

    feat(Line): 지하철 노선 도메인 추가
    - Station 과의 연관관계 추가

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,55 @@
+package nextstep.subway.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "line")
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "line")
+    private final List<Station> stations = new ArrayList<>();
+
+    public Line(String name) {
+        this.name = name;
+    }
+
+    protected Line() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public void addStation(Station station) {
+        stations.add(station);
+        station.setLine(this);
+    }
+}
--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,6 +1,13 @@
 package nextstep.subway.domain;
 
-import javax.persistence.*;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Station extends BaseEntity {
@@ -9,6 +16,9 @@ public class Station extends BaseEntity {
     private Long id;
     @Column(unique = true)
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
 
     public Station() {
     }
@@ -23,5 +33,13 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    public void setLine(final Line line) {
+        if (Objects.nonNull(this.line)) {
+            this.line.getStations().remove(this);
+        }
+        this.line = line;
+        line.getStations().add(this);
     }
 }
