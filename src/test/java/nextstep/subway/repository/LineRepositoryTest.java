2aaf6827a878a0ee34f1f75df416083d1409763a
kwonyongil <ksgkms23@likelion.org>
2022-05-26 18:36:52 +0900

    feat(subway) application.yml 설정파일 생성

---
+++ src/test/java/nextstep/subway/repository/LineRepositoryTest.java
@@ -0,0 +1,10 @@
+package nextstep.subway.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class LineRepositoryTest {
+
+}
