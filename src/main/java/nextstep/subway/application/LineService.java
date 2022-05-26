02fb3da0c85ee798626106022e5b028fe3ee4ae9
kwonyongil <ksgkms23@likelion.org>
2022-05-26 23:53:48 +0900

    feat(subway) LineRepositoryTest 저장 기능 테스트 추가

--- src/main/java/nextstep/subway/application/LineService.java
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -3,9 +3,12 @@ package nextstep.subway.application;
 import java.util.List;
 import java.util.stream.Collectors;
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
 import nextstep.subway.dto.LineRequest;
 import nextstep.subway.dto.LineResponse;
+import nextstep.subway.exception.NotFoundException;
 import nextstep.subway.repository.LineRepository;
+import nextstep.subway.repository.StationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,13 +17,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class LineService {
     LineRepository lineRepository;
 
-    public LineService(LineRepository lineRepository) {
+    StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
     @Transactional
     public LineResponse saveLine(LineRequest lineRequest) {
-        Line persistStation = lineRepository.save(lineRequest.toLine());
+        Station upStation = stationRepository.findById(lineRequest.getUpStationId())
+                .orElseThrow(() -> new NotFoundException("등록된 지하철역이 없습니다."));
+        Station downStation = stationRepository.findById(lineRequest.getDownStationId())
+                .orElseThrow(() -> new NotFoundException("등록된 지하철역이 없습니다."));
+        Line line = lineRequest.toLine();
+        line.addUpStation(upStation);
+        line.addDownStation(downStation);
+        Line persistStation = lineRepository.save(line);
         return LineResponse.of(persistStation);
     }
 
--- src/test/java/nextstep/subway/repository/LineRepositoryTest.java
+++ src/test/java/nextstep/subway/repository/LineRepositoryTest.java
@@ -1,10 +1,53 @@
 package nextstep.subway.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class LineRepositoryTest {
+class LineRepositoryTest {
+    @Autowired
+    private LineRepository lineRepository;
 
+    @Autowired
+    private StationRepository stationRepository;
+
+    Line line;
+    Station upStation;
+    Station downStation;
+
+    @BeforeEach
+    void setUp() {
+        upStation = Station.builder("지하철역")
+                .build();
+        downStation = Station.builder("새로운지하철역")
+                .build();
+        Station save1 = stationRepository.save(upStation);
+        Station save2 = stationRepository.save(downStation);
+        line = Line.builder("신분당선", "bg-red-600", 10)
+                .build()
+                .addUpStation(save1)
+                .addDownStation(save2);
+    }
+
+    @DisplayName("노선 저장 테스트")
+    @Test
+    void save() {
+        Line save = lineRepository.save(line);
+        assertAll(
+                () -> assertThat(save.getId()).isNotNull(),
+                () -> assertThat(save.getColor()).isEqualTo("bg-red-600"),
+                () -> assertThat(save.getName()).isEqualTo("신분당선"),
+                () -> assertThat(save.getUpStation().getName()).isEqualTo(upStation.getName())
+        );
+    }
 }
