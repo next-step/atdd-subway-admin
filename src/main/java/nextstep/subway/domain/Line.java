6ec62ae40e67a3a4aff58bfbef0c118f4be9b703
kwonyongil <ksgkms23@likelion.org>
2022-05-26 19:08:51 +0900

    feat(subway) Line 연관관계 매핑

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,10 +1,16 @@
 package nextstep.subway.domain;
 
+import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import org.apache.commons.lang3.StringUtils;
 
 @Entity
@@ -17,33 +23,53 @@ public class Line extends BaseEntity {
     @Column(nullable = false)
     private String color;
 
+    @ManyToOne(optional = false, cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "UP_STATION_ID", foreignKey = @ForeignKey(name = "fk_line_up_station"))
+    private Station upStation;
+
+    @ManyToOne(optional = false, cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "DOWN_STATION_ID", foreignKey = @ForeignKey(name = "fk_line_down_station"))
+    private Station downStation;
+
     protected Line() {
     }
 
     private Line(LineBuilder lineBuilder) {
         this.name = lineBuilder.name;
         this.color = lineBuilder.color;
+        this.upStation = lineBuilder.upStation;
+        this.downStation = lineBuilder.downStation;
     }
 
-    public static LineBuilder builder(String name, String color) {
-        return new LineBuilder(name, color);
+    public static LineBuilder builder(String name, String color, Station upStation, Station downStation) {
+        return new LineBuilder(name, color, upStation, downStation);
     }
 
     public static class LineBuilder {
         private Long id;
         private final String name;
         private final String color;
+        private final Station upStation;
+        private final Station downStation;
 
-        private LineBuilder(String name, String color) {
-            validateNameNotNull(name);
-            validateColorNotNull(color);
+        private LineBuilder(String name, String color, Station upStation, Station downStation) {
+            validateParameter(name, color, upStation, downStation);
             this.name = name;
             this.color = color;
+            this.upStation = upStation;
+            this.downStation = downStation;
         }
 
         public LineBuilder id(Long id) {
             this.id = id;
             return this;
+        }
+
+        private void validateParameter(String name, String color, Station upStation, Station downStation) {
+            validateNameNotNull(name);
+            validateColorNotNull(color);
+            validateUpStationNotNull(upStation);
+            validateDownStationNotNull(downStation);
         }
 
         private void validateNameNotNull(String name) {
@@ -55,6 +81,18 @@ public class Line extends BaseEntity {
         private void validateColorNotNull(String color) {
             if (StringUtils.isNotEmpty(color)) {
                 throw new IllegalArgumentException("칼라 정보가 없습니다.");
+            }
+        }
+
+        private void validateUpStationNotNull(Station upStation) {
+            if (Objects.isNull(upStation)) {
+                throw new IllegalArgumentException("상행역 정보가 없습니다.");
+            }
+        }
+
+        private void validateDownStationNotNull(Station downStation) {
+            if (Objects.isNull(downStation)) {
+                throw new IllegalArgumentException("하행역 정보가 없습니다.");
             }
         }
 
