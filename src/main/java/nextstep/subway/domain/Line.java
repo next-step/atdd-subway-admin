99818523a9a7deaab197c896b0090661cfc6e7b3
Donghee Han <handh0413@gmail.com>
2022-05-28 00:44:29 +0900

    feat: 지하철 노선(Line) 엔티티 추가

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,70 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Line extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    @Column
+    private String color;
+    @Column
+    private int distance;
+
+    protected Line() {
+    }
+
+    public Line(String name, String color, int distance) {
+        this.name = name;
+        this.color = color;
+        this.distance = distance;
+    }
+
+    public Line(Long id, String name, String color, int distance) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return distance == line.distance &&
+                Objects.equals(name, line.name) &&
+                Objects.equals(color, line.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, distance);
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
+}
