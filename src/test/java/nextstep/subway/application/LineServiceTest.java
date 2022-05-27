4de2c10a994f4e7b9f710f8e1517b2400da872d2
tasklet1579 <tasklet1579@gmail.com>
2022-05-27 20:27:19 +0900

    test : 지하철노선 기능 테스트 - 지하철노선 생성 - 지하철노선 목록 조회 - 지하철노선 조회

---
+++ src/test/java/nextstep/subway/application/LineServiceTest.java
@@ -0,0 +1,85 @@
+package nextstep.subway.application;
+
+import nextstep.subway.dto.LineRequest;
+import nextstep.subway.dto.LineResponse;
+import nextstep.subway.util.DatabaseCleaner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("지하철노선 서비스 관련 기능")
+@ActiveProfiles(value = "acceptance")
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.properties")
+class LineServiceTest {
+    @Autowired
+    LineService service;
+
+    @Autowired
+    DatabaseCleaner cleaner;
+
+    @BeforeEach
+    public void setUp() {
+        cleaner.execute();
+    }
+
+    @DisplayName("지하철 노선을 생성한다")
+    @Test
+    void createLine() {
+        // given
+        LineRequest request = new LineRequest("신분당선", "bg-red-600", 1L, 2L, 10);
+
+        // when
+        LineResponse response = service.createLine(request);
+
+        // then
+        assertThat(response).isNotNull();
+        assertThat(response.getName()).isEqualTo("신분당선");
+        assertThat(response.getColor()).isEqualTo("bg-red-600");
+    }
+
+    @DisplayName("지하철 노선 목록을 조회한다")
+    @Test
+    void getLines() {
+        // given
+        List<LineRequest> requests = Arrays.asList(
+                new LineRequest("신분당선", "bg-red-600", 1L, 2L, 10),
+                new LineRequest("2호선", "bg-blue-200", 3L, 4L, 80)
+        );
+
+        for (LineRequest request : requests) {
+            service.createLine(request);
+        }
+
+        // when
+        List<LineResponse> lines = service.getLines();
+
+        // then
+        assertThat(lines.size()).isEqualTo(2);
+    }
+
+    @DisplayName("지하철 노선을 조회한다")
+    @Test
+    void getLine() {
+        // given
+        LineRequest request = new LineRequest("신분당선", "bg-red-600", 1L, 2L, 10);
+        LineResponse response = service.createLine(request);
+
+        // when
+        LineResponse line = service.getLine(response.getId());
+
+        // then
+        assertThat(line).isNotNull();
+    }
+}
