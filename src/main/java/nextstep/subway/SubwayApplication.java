fe1074fb30095d6d708022d3e30ce773c19673f0
mond <revive29@gmail.com>
2022-05-27 22:49:32 +0900

    docs(step3): '구간 추가 기능' 기능 목록 정리 및 인수 조건 작성

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @SpringBootApplication
 public class SubwayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SubwayApplication.class, args);
     }
