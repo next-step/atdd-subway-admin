aa58e5c18716f8b3ad28be17be6d1b3568120bba
Celina <luv.mins99@gmail.com>
2022-05-27 15:22:53 +0900

    refactor(StationService): transaction 정리
    - Read에 해당하는 메서드는 readOnly = true
    - Create, Update, Delete에 해당하는 메서드는 Transactional

--- src/main/java/nextstep/subway/application/StationService.java
+++ src/main/java/nextstep/subway/application/StationService.java
@@ -1,5 +1,7 @@
 package nextstep.subway.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import nextstep.subway.dto.StationRequest;
@@ -7,11 +9,7 @@ import nextstep.subway.dto.StationResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
-@Transactional(readOnly = true)
 public class StationService {
     private final StationRepository stationRepository;
 
@@ -25,6 +23,7 @@ public class StationService {
         return StationResponse.of(persistStation);
     }
 
+    @Transactional(readOnly = true)
     public List<StationResponse> findAllStations() {
         List<Station> stations = stationRepository.findAll();
 
