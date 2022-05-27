d2986b327c24622f6ad64785ae4d151ccca9b75b
Celina <luv.mins99@gmail.com>
2022-05-27 15:40:48 +0900

    test(LineAcceptanceTest): 지하철 노선 인수 테스트 구현

--- src/test/java/nextstep/subway/station/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -1,23 +1,44 @@
 package nextstep.subway.station;
 
+import static nextstep.subway.station.StationAcceptanceTest.지하철역을_생성한다;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.List;
+import nextstep.common.RestAssuredTemplate;
+import nextstep.subway.application.DatabaseCleanup;
+import nextstep.subway.dto.LineRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
 @DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("acceptance")
 public class LineAcceptanceTest {
     @LocalServerPort
     int port;
+
+    @Autowired
+    private DatabaseCleanup dataBaseCleanUp;
 
     @BeforeEach
     public void setUp() {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
+            dataBaseCleanUp.afterPropertiesSet();
         }
+        dataBaseCleanUp.execute();
+
+        지하철역을_생성한다("강남역");
+        지하철역을_생성한다("잠실역");
     }
 
     /**
@@ -27,7 +48,12 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
+        // when
+        지하철_노선을_생성한다("2호선", "green lighten-3");
 
+        // then
+        List<String> lineNames = 모든_지하철_노선을_조회한다();
+        assertThat(lineNames).containsAnyOf("2호선");
     }
 
     /**
@@ -38,7 +64,15 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선의 목록을 조회한다.")
     @Test
     void getLines() {
+        // given
+        지하철_노선을_생성한다("2호선", "green lighten-3");
+        지하철_노선을_생성한다("8호선", "pink lighten-3");
 
+        // when
+        List<String> lineNames = 모든_지하철_노선을_조회한다();
+
+        // then
+        assertThat(lineNames).hasSize(2);
     }
 
     /**
@@ -49,7 +83,15 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선을 조회한다.")
     @Test
     void getLine() {
+        // given
+        ExtractableResponse<Response> createResponse = 지하철_노선을_생성한다("2호선", "green lighten-3");
+        long lineId = createResponse.body().jsonPath().getLong("id");
 
+        // when
+        String lineName = 지하철_노선을_조회한다(lineId);
+
+        // then
+        assertThat(lineName).isEqualTo("2호선");
     }
 
     /**
@@ -60,7 +102,15 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선을 수정한다.")
     @Test
     void updateLine() {
+        // given
+        ExtractableResponse<Response> createResponse = 지하철_노선을_생성한다("2호선", "green lighten-3");
+        long lineId = createResponse.body().jsonPath().getLong("id");
 
+        // when
+        ExtractableResponse<Response> response = 지하철_노선을_수정한다(lineId, "8호선", "pink lighten-3");
+
+        // then
+        지하철_노선_정보가_수정된다(response);
     }
 
     /**
@@ -71,6 +121,42 @@ public class LineAcceptanceTest {
     @DisplayName("지하철 노선을 삭제한다.")
     @Test
     void deleteLine() {
+        // given
+        ExtractableResponse<Response> createResponse = 지하철_노선을_생성한다("2호선", "green lighten-3");
+        long lineId = createResponse.body().jsonPath().getLong("id");
 
+        // when
+        ExtractableResponse<Response> response = 지하철_노선을_삭제한다(lineId);
+
+        // then
+        지하철_노선이_삭제된다(response);
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선을_생성한다(String name, String color) {
+        return RestAssuredTemplate.post("/lines", new LineRequest(name, color));
+    }
+
+    public static List<String> 모든_지하철_노선을_조회한다() {
+        return RestAssuredTemplate.get("/lines").jsonPath().getList("name", String.class);
+    }
+
+    public static String 지하철_노선을_조회한다(long lineId) {
+        return RestAssuredTemplate.get("/lines/" + lineId).jsonPath().getString("name");
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선을_수정한다(long lineId, String name, String color) {
+        return RestAssuredTemplate.put("/lines/" + lineId, new LineRequest(name, color));
+    }
+
+    public static ExtractableResponse<Response> 지하철_노선을_삭제한다(long lineId) {
+        return RestAssuredTemplate.delete("/lines/{id}", lineId);
+    }
+
+    private void 지하철_노선_정보가_수정된다(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private void 지하철_노선이_삭제된다(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
