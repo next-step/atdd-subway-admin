df218d517a084f8eef3ddb85f68a700c7e91fe60
jhsong2580 <thdwmdgns@naver.com>
2022-05-27 17:46:24 +0900

    feat: station 1개 상세정보 조회 기능 구현

--- src/main/java/nextstep/subway/application/StationService.java
+++ src/main/java/nextstep/subway/application/StationService.java
@@ -1,18 +1,19 @@
 package nextstep.subway.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import nextstep.subway.dto.StationRequest;
 import nextstep.subway.dto.StationResponse;
+import nextstep.subway.exception.StationNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class StationService {
+
     private StationRepository stationRepository;
 
     public StationService(StationRepository stationRepository) {
@@ -29,12 +30,18 @@ public class StationService {
         List<Station> stations = stationRepository.findAll();
 
         return stations.stream()
-                .map(station -> StationResponse.of(station))
-                .collect(Collectors.toList());
+            .map(station -> StationResponse.of(station))
+            .collect(Collectors.toList());
     }
 
     @Transactional
     public void deleteStationById(Long id) {
         stationRepository.deleteById(id);
+    }
+
+    public StationResponse findStationById(Long id) {
+        Station station = stationRepository.findById(id)
+            .orElseThrow(StationNotFoundException::new);
+        return StationResponse.of(station);
     }
 }
