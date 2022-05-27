e1310cb8a15eaefb424148c2d1020d90bfd17751
toughchb <ggang2000@gmail.com>
2022-05-28 00:24:32 +0900

    refactor(Line): up_station_id, down_station_id 매핑

--- src/main/java/nextstep/subway/line/domain/Line.java
+++ src/main/java/nextstep/subway/line/domain/Line.java
@@ -1,10 +1,14 @@
 package nextstep.subway.line.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import nextstep.subway.common.domain.BaseEntity;
+import nextstep.subway.station.domain.Station;
 
 @Entity
 public class Line extends BaseEntity {
@@ -17,16 +21,26 @@ public class Line extends BaseEntity {
 
     private String color;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
     protected Line() {
     }
 
-    private Line(String name, String color) {
+    private Line(String name, String color, Station upStation, Station downStation) {
         this.name = name;
         this.color = color;
+        this.upStation = upStation;
+        this.downStation = downStation;
     }
 
-    public static Line of(String name, String color) {
-        return new Line(name, color);
+    public static Line of(String name, String color, Station upStation, Station downStation) {
+        return new Line(name, color, upStation, downStation);
     }
 
     public Long getId() {
@@ -41,8 +55,16 @@ public class Line extends BaseEntity {
         return color;
     }
 
-    public void update(Line line) {
-        this.name = line.getName();
-        this.color = line.getColor();
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public void update(String name, String color) {
+        this.name = name;
+        this.color = color;
     }
 }
