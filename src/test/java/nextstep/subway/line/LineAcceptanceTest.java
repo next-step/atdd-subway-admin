4ddb01a857ba4c6f4ed28096d5c2dd73ec7773f1
toughchb <ggang2000@gmail.com>
2022-05-27 21:54:42 +0900

    test:지하철노선 단건 조회 실패 테스트

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import nextstep.subway.BaseAcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
+import org.assertj.core.api.AbstractStringAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -32,7 +33,9 @@ public class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 2개의 지하철 노선을 생성하고 When 지하철 노선 목록을 조회하면 Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
      */
     @DisplayName("지하철노선 목록 조회")
     @Test
@@ -45,6 +48,29 @@ public class LineAcceptanceTest extends BaseAcceptanceTest {
         노선이름_포함_확인(노선_이름_목록, "신분당선", "분당선");
     }
 
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @DisplayName("지하철노선 조회")
+    @Test
+    void findOneLine() {
+        LineRequest 신분당선 = LineRequest.of("신분당선", "bg-red-600", 1L, 2L, 10);
+        ExtractableResponse<Response> 노선_생성_요청_응답 = 노선_생성_요청(신분당선);
+        long id = 노선의_id_구한다(노선_생성_요청_응답);
+        ExtractableResponse<Response> 노선_조회_요청_응답 = 노선_조회_요청(id);
+        조회_노선_이름_확인(노선_조회_요청_응답, "신분당선");
+    }
+
+    private void 조회_노선_이름_확인(ExtractableResponse<Response> 노선_조회_요청_응답, String name) {
+        assertThat(노선_조회_요청_응답.jsonPath().getString("name")).isEqualTo(name);
+    }
+
+    private long 노선의_id_구한다(ExtractableResponse<Response> 노선_생성_요청_응답) {
+        return 노선_생성_요청_응답.jsonPath().getLong("id");
+    }
+
     private ExtractableResponse<Response> 노선_생성_요청(LineRequest line) {
         return RestAssured.given().log().all()
                 .body(line)
@@ -55,7 +81,7 @@ public class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     private List<String> 노선_이름_목록을_구한다() {
-        return 노선_조회_요청().jsonPath().getList("name", String.class);
+        return 노선_전체_조회_요청().jsonPath().getList("name", String.class);
     }
     private void 노선이름_포함_확인(List<String> lineNames, String... lineName) {
         assertThat(lineNames).containsAll(Arrays.asList(lineName));
@@ -65,7 +91,14 @@ public class LineAcceptanceTest extends BaseAcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    private ExtractableResponse<Response> 노선_조회_요청() {
+    private ExtractableResponse<Response> 노선_조회_요청(Long id) {
+        return RestAssured.given().log().all()
+                .when().get("/lines/" + id)
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 노선_전체_조회_요청() {
         return RestAssured.given().log().all()
                 .when().get("/lines")
                 .then().log().all()
