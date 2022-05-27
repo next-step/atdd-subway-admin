ea96be43685d613246da307de2a874cca7267fdc
toughchb <ggang2000@gmail.com>
2022-05-27 21:32:22 +0900

    feat(LineController): 노선 전체 조회 api 구현

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,10 +1,13 @@
 package nextstep.subway.line.ui;
 
 import java.net.URI;
+import java.util.List;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LineController {
 
-    public final LineService service;
+    public final LineService lineService;
 
     public LineController(LineService service) {
-        this.service = service;
+        this.lineService = service;
     }
 
     @PostMapping("/lines")
     public ResponseEntity<LineResponse> createLine(@RequestBody LineRequest lineRequest) {
-        LineResponse lineResponse = service.saveLine(lineRequest);
+        LineResponse lineResponse = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + lineResponse.getId())).body(lineResponse);
+    }
+
+    @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok().body(lineService.findAllLines());
     }
 }
