b16931266d6f01afec78d713b8024a5450fb6c4b
Celina <luv.mins99@gmail.com>
2022-05-27 15:14:51 +0900

    refactor(Station): 지하철 역 객체 수정
    - 노선과의 연관관계 삭제

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,24 +1,18 @@
 package nextstep.subway.domain;
 
-import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
-public class Station extends BaseEntity {
+public class Station {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String name;
-    @ManyToOne
-    @JoinColumn(name = "line_id")
-    private Line line;
 
     public Station() {
     }
@@ -33,13 +27,5 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
-    }
-
-    public void setLine(final Line line) {
-        if (Objects.nonNull(this.line)) {
-            this.line.getStations().remove(this);
-        }
-        this.line = line;
-        line.getStations().add(this);
     }
 }
