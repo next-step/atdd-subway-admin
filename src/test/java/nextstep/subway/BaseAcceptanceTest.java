079a26b2987f127b10bb65591ca5e4b9aadf9702
toughchb <ggang2000@gmail.com>
2022-05-27 22:27:43 +0900

    feat: 테스트 격리성 추가

--- src/test/java/nextstep/subway/BaseAcceptanceTest.java
+++ src/test/java/nextstep/subway/BaseAcceptanceTest.java
@@ -4,7 +4,9 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/truncate.sql")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BaseAcceptanceTest {
     @LocalServerPort
