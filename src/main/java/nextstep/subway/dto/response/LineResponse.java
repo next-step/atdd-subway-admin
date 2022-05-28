159be2584cba9bc35860f464afa2f045c2de6e6c
jhsong2580 <thdwmdgns@naver.com>
2022-05-28 10:16:36 +0900

    feat: getter추가

--- src/main/java/nextstep/subway/dto/response/LineResponse.java
+++ src/main/java/nextstep/subway/dto/response/LineResponse.java
@@ -3,6 +3,7 @@ package nextstep.subway.dto.response;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import nextstep.subway.domain.Line;
 import nextstep.subway.domain.Station;
 import nextstep.subway.dto.StationDTO;
 import nextstep.subway.enums.LineColor;
@@ -18,5 +19,26 @@ public class LineResponse {
         this.name = name;
         this.color = lineColor.getColorName();
         this.stations = Arrays.asList(upStation.toStationDTO(),downStation.toStationDTO());
+    }
+
+    public static LineResponse of(Line line){
+        return new LineResponse(line.getId(), line.getName(), line.getLineColor(),
+            line.getUpStation(), line.getDownStation());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public List<StationDTO> getStations() {
+        return stations;
     }
 }
