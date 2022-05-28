f72c4d2945e8a09cb50b6b3c4279482b73151757
toughchb <ggang2000@gmail.com>
2022-05-29 01:48:41 +0900

    test(SectionAcceptanceTest): 노선에 구간을 등록 실패하는 테스트 추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -2,17 +2,25 @@ package nextstep.subway.section;
 
 import static nextstep.subway.line.LineAssuredMethod.노선_생성_요청;
 import static nextstep.subway.station.StationAssuredMethod.지하철역_생성_요청;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.BaseAcceptanceTest;
 import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.section.dto.SectionRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-public class SectionAcceptanceTest extends BaseAcceptanceTest {
+class SectionAcceptanceTest extends BaseAcceptanceTest {
 
     private Long 강남역_id;
     private Long 광교역_id;
+    private Long 신분당선_id;
 
     @BeforeEach
     public void setUp() {
@@ -21,14 +29,34 @@ public class SectionAcceptanceTest extends BaseAcceptanceTest {
         강남역_id = 지하철역_생성_요청("강남역").jsonPath().getLong("id");
         광교역_id = 지하철역_생성_요청("광교역").jsonPath().getLong("id");
         LineRequest 신분당선 = LineRequest.of("신분당선", "bg-red-600", 강남역_id, 광교역_id, 10);
-        노선_생성_요청(신분당선);
+        신분당선_id = 노선_생성_요청(신분당선).jsonPath().getLong("id");
     }
 
     @DisplayName("노선에 구간을 등록한다.")
     @Test
     void addSection() {
-        // when 지하철_노선에_구간_등록_요청
+        // given 지하철 노선에 지하철역 등록 요청
+        long 양재역_id = 지하철역_생성_요청("양재역").jsonPath().getLong("id");
+
+        // when 신분당선 지하철 노선에 새로운 구간 등록 요청
+        SectionRequest 새로운_구간 = SectionRequest.of(광교역_id, 양재역_id, 5);
+        ExtractableResponse<Response> 구간_생성_요청_응답 = 구간_생성_요청(새로운_구간, 신분당선_id);
 
         // then 지하철_노선에_구간_등록됨
+        노선_생성_성공_확인(구간_생성_요청_응답);
     }
+
+    private void 노선_생성_성공_확인(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> 구간_생성_요청(SectionRequest sectionRequest, Long lindId) {
+        return RestAssured.given().log().all()
+                .body(sectionRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("lines/{lindId}/sections", lindId)
+                .then().log().all()
+                .extract();
+    }
+
 }
