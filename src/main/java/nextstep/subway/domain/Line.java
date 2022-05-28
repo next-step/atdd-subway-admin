1fae045586e4333f60849535c9abada45227e227
otakjong <tj3828@naver.com>
2022-05-28 22:19:27 +0900

    feat: step2 - 상행종점역과 하해종점역 동일 여부 체크 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -34,10 +34,18 @@ public class Line {
     }
 
     public Line(String name, String color, Station upStation, Station downStation) {
+        validateUpDownStation(upStation, downStation);
+
         this.name = name;
         this.color = color;
         this.upStation = upStation;
         this.downStation = downStation;
+    }
+
+    private void validateUpDownStation(Station upStation, Station downStation) {
+        if (upStation.equals(downStation)) {
+            throw new IllegalArgumentException("상행종점역과 하행종점역은 같을 수가 없습니다.");
+        }
     }
 
     public void update(LineRequest lineRequest) {
