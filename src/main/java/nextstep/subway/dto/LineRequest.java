a1457cf037f0ae262d562cd4ec795cd11957a26b
Woody <yuncheol92@naver.com>
2022-05-28 16:52:02 +0900

    feat : lineRequest, lineResponse 생성

---
+++ src/main/java/nextstep/subway/dto/LineRequest.java
@@ -0,0 +1,47 @@
+package nextstep.subway.dto;
+
+import nextstep.subway.domain.line.Line;
+
+public class LineRequest {
+
+    private String name;
+    private String color;
+    private int distance;
+    private Long upStationId;
+    private Long downStationId;
+
+    public LineRequest() {
+    }
+
+    public LineRequest(String name, String color, int distance, Long upStationId, Long downStationId) {
+        this.name = name;
+        this.color = color;
+        this.distance = distance;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Line toLine() {
+        return Line.create(name, color, distance);
+    }
+}
