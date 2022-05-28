2629f09ef53c2d0276e1467e301899e06cc00a65
jhsong2580 <thdwmdgns@naver.com>
2022-05-28 10:10:56 +0900

    feat: lineRequest -> line 변경 로직 추가

--- src/main/java/nextstep/subway/dto/request/LineRequest.java
+++ src/main/java/nextstep/subway/dto/request/LineRequest.java
@@ -1,11 +1,21 @@
 package nextstep.subway.dto.request;
 
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+import nextstep.subway.enums.LineColor;
+
 public class LineRequest {
+
     private Long id;
     private String name;
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private String color;
+
+    public Line toLine(Station upStation, Station downStation) {
+        return new Line(id, name, LineColor.getLineColorByName(color), upStation, downStation);
+    }
 
     public Long getId() {
         return id;
