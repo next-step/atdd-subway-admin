ef7447c96cf5bd023af782f5c1d03c67dfccda9c
jhsong2580 <thdwmdgns@naver.com>
2022-05-28 09:59:14 +0900

    fix: 기본생성자 protect로 변경

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -10,7 +10,9 @@ public class Station extends BaseEntity {
     @Column(unique = true)
     private String name;
 
-    public Station() {
+
+
+    protected Station() {
     }
 
     public Station(String name) {
