7dc8dc04ab006765836d87f6ea58445e32d0234a
kbh0581 <kbh0581@gmail.com>
2022-05-29 01:23:53 +0900

    test: 지하철노선 목록 조회 인수테스트 작성

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,6 +1,7 @@
 package nextstep.subway.line;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -23,12 +24,19 @@ import org.springframework.test.context.jdbc.Sql;
 public class LineAcceptanceTest {
     @LocalServerPort
     int port;
+    Long downStationId;
+    Long upStationId;
+
 
     @BeforeEach
     public void setUp() {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
+
+        downStationId = 지하철역을_생성_한다("주안역").jsonPath().getLong("id");
+        upStationId = 지하철역을_생성_한다("인천역").jsonPath().getLong("id");
+
     }
 
     /**
@@ -38,34 +46,15 @@ public class LineAcceptanceTest {
     @Test
     @DisplayName("노선역을 생성한다.")
     void createLine() {
-        Long downStationId = 지하철역을_생성_한다("주안역").jsonPath().getLong("id");
-        Long upStationId = 지하철역을_생성_한다("인천역").jsonPath().getLong("id");
-
+        //When 지하철 노선을 생성하면
         노선을_생성한다(new LineRequest("1호선", "bg-red-500", 10,
                 downStationId, upStationId));
-
+        //Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.
         final List<String> 전체노선이름들 = 전체_노선을_찾는다().jsonPath().getList("name");
 
         assertThat(전체노선이름들).contains("1호선");
 
     }
-
-    private void 노선을_생성한다(LineRequest lineRequest) {
-        RestAssured.given().log().all()
-                .body(lineRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/line")
-                .then().log().all()
-                .extract();
-    }
-
-    private ExtractableResponse<Response> 전체_노선을_찾는다() {
-        return RestAssured.given().log().all()
-                .when().get("/lines")
-                .then().log().all()
-                .extract();
-    }
-
 
     /**
          Given 2개의 지하철 노선을 생성하고
@@ -75,7 +64,21 @@ public class LineAcceptanceTest {
     @Test
     @DisplayName("지하철노선 목록 조회")
     void searchLines() {
+        //Given 2개의 지하철 노선을 생성하고
+        노선을_생성한다(new LineRequest("1호선", "bg-red-500", 10,
+                downStationId, upStationId));
 
+        노선을_생성한다(new LineRequest("2호선", "bg-red-500", 4,
+                downStationId, upStationId));
+
+        // When 지하철 노선 목록을 조회하면
+        final List<String> 전체노선이름들 = 전체_노선을_찾는다().jsonPath().getList("name");
+
+        //Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+        assertAll(
+                () -> assertThat(전체노선이름들).hasSize(2),
+                () -> assertThat(전체노선이름들).contains("1호선", "2호선")
+        );
     }
 
     /**
@@ -119,6 +122,22 @@ public class LineAcceptanceTest {
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private void 노선을_생성한다(LineRequest lineRequest) {
+        RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/line")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 전체_노선을_찾는다() {
+        return RestAssured.given().log().all()
+                .when().get("/lines")
                 .then().log().all()
                 .extract();
     }
