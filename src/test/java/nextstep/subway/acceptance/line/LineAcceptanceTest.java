1f7f749825ac79736b6f0acbeef17644ea00bd9a
kwonyongil <ksgkms23@likelion.org>
2022-05-28 23:20:38 +0900

    refactor(subway) beforeEach 애노테이션을 이용해서 지하철역을 생성하는 반복적인 로직을 추출하여 리팩토링

--- src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/line/LineAcceptanceTest.java
@@ -13,24 +13,33 @@ import nextstep.subway.acceptance.base.BaseAcceptanceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @DisplayName("지하철노선 관련 기능")
 class LineAcceptanceTest extends BaseAcceptanceTest {
+    @LocalServerPort
+    int port;
+    long upStationId;
+    long downStationId;
+    @BeforeEach
+    void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+        // Given
+        upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
+        downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
+    }
 
     /**
-     * Given 상행, 하행 지하철 역을 생성하고
      * When 지하철 노선을 생성하면
      * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
      */
     @DisplayName("지하철노선 생성")
     @Test
     void createLine() {
-        // Given
-        long upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
-        long downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
-
         // when
         지하철노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
 
@@ -47,7 +56,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 상행, 하행 지하철 역을 생성하고
      * Given 지하철 노선을 생성하고
      * When 기존에 존재하는 지하철 노선 이름으로 지하철 노선을 생성하면
      * Then 지하철 노선이 생성이 안된다
@@ -55,10 +63,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
     @Test
     void createLineWithDuplicateName() {
-        // Given
-        long upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
-        long downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
-
         // Given
         지하철노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
 
@@ -70,7 +74,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 상행, 하행 지하철 역을 생성하고
      * Given 2개의 지하철 노선을 생성하고
      * When 지하철 노선 목록을 조회하면
      * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
@@ -78,10 +81,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     @DisplayName("지하철노선 목록 조회")
     @Test
     void showLines() {
-        // Given
-        long upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
-        long downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
-
         // Given
         지하철노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
         지하철노선_생성_요청("5호선", "bg-blue-600", upStationId, downStationId, 10);
@@ -104,7 +103,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 상행, 하행 지하철 역을 생성하고
      * Given 지하철 노선을 생성하고
      * When 생성한 지하철 노선을 조회하면
      * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
@@ -112,10 +110,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     @DisplayName("지하철노선 조회")
     @Test
     void showLine() {
-        // Given
-        long upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
-        long downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
-
         // Given
         long id = 지하철노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10).jsonPath().getLong("id");
 
@@ -134,7 +128,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 상행, 하행 지하철 역을 생성하고
      * Given 지하철 노선을 생성하고
      * When 생성한 지하철 노선을 수정하면
      * Then 해당 지하철 노선 정보는 수정된다
@@ -142,10 +135,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     @DisplayName("지하철노선 수정")
     @Test
     void updateLine() {
-        // Given
-        long upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
-        long downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
-
         // Given
         지하철노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
 
@@ -164,7 +153,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     }
 
     /**
-     * Given 상행, 하행 지하철 역을 생성하고
      * Given 지하철 노선을 생성하고
      * When 생성한 지하철 노선을 삭제하면
      * Then 해당 지하철 노선 정보는 삭제된다
@@ -172,11 +160,6 @@ class LineAcceptanceTest extends BaseAcceptanceTest {
     @DisplayName("지하철노선 삭제")
     @Test
     void deleteLine() {
-        // Given
-        long upStationId = 지하철역_생성_요청("지하철역").jsonPath().getLong("id");
-        long downStationId = 지하철역_생성_요청("새로운지하철역").jsonPath().getLong("id");
-
-
         // Given
         ExtractableResponse<Response> createResponse = 지하철노선_생성_요청("신분당선", "bg-red-600", upStationId, downStationId, 10);
         long id = createResponse.jsonPath().getLong("id");
