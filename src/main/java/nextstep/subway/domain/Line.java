297c460bc60b73506244a1180474b44bb1f26785
seungin yang <rhfpdk92@naver.com>
2022-05-28 23:41:29 +0900

    fix(bug fix): Sections 초기화 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -15,7 +15,7 @@ public class Line extends BaseEntity {
     private String color;
 
     @Embedded
-    private Sections sections;
+    private Sections sections = new Sections();
 
     public Line() {
     }
