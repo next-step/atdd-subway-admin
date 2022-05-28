5fdf21f589a543926a225c3e47b4c737de5c6c95
jhsong2580 <thdwmdgns@naver.com>
2022-05-28 10:00:34 +0900

    feat: dto 및 entity -> dto 로직 생성

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,16 +1,24 @@
 package nextstep.subway.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import nextstep.subway.dto.StationDTO;
 
 @Entity
 public class Station extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String name;
 
-
+    public StationDTO toStationDTO() {
+        return new StationDTO(this.id, this.name);
+    }
 
     protected Station() {
     }
