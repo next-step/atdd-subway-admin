d072397b10df1043ec6b10696b66f48bf1432ba4
minzzang <minjjang1117@gmail.com>
2022-05-28 21:34:34 +0900

    feat(SectionMapper): findById 중복 제거

--- src/main/java/nextstep/subway/application/SectionMapper.java
+++ src/main/java/nextstep/subway/application/SectionMapper.java
@@ -2,28 +2,20 @@ package nextstep.subway.application;
 
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
-import nextstep.subway.domain.StationRepository;
 import nextstep.subway.dto.LineRequest;
 import org.springframework.stereotype.Component;
 
-import java.util.NoSuchElementException;
-
 @Component
 public class SectionMapper {
-    public static final String NOT_FOUND_LINE = "해당 지하철역을 찾을 수 없습니다";
+    private final StationService stationService;
 
-    private final StationRepository stationRepository;
-
-    public SectionMapper(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
+    public SectionMapper(StationService stationService) {
+        this.stationService = stationService;
     }
 
     public Section from(LineRequest lineRequest) {
-        Station upStation = stationRepository.findById(lineRequest.getUpStationId())
-                .orElseThrow(() -> new NoSuchElementException(NOT_FOUND_LINE));
-
-        Station downStation = stationRepository.findById(lineRequest.getDownStationId())
-                .orElseThrow(() -> new NoSuchElementException(NOT_FOUND_LINE));
+        Station upStation = stationService.findById(lineRequest.getUpStationId());
+        Station downStation = stationService.findById(lineRequest.getDownStationId());
 
         return new Section(lineRequest.getDistance(), upStation, downStation);
     }
