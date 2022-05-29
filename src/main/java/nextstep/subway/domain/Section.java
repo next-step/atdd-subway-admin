114683cb0b73df28c0c2cb0906d1149d6bb6c464
jhsong2580 <thdwmdgns@naver.com>
2022-05-29 23:06:35 +0900

    feat: 현재 section의 다음 추가 로직 구현

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -1,13 +1,15 @@
 package nextstep.subway.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import nextstep.subway.exception.SectionInvalidException;
+import org.springframework.util.ObjectUtils;
 
 @Entity
 public class Section {
@@ -27,10 +29,10 @@ public class Section {
 
     private Integer distance;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @OneToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Section nextSection;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @OneToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Section backSection;
 
     protected Section() {
@@ -44,5 +46,59 @@ public class Section {
         this.distance = distance;
         this.nextSection = nextSection;
         this.backSection = backSection;
+    }
+
+    public void setSectionAfterThis(Section appendSection) {
+        validDistance(appendSection);
+        appendSection.setBackNextSection(this, nextSection);
+
+        if (ObjectUtils.isEmpty(this.nextSection)) {//line 상향종착지, distance변경해야함.
+            this.nextSection = appendSection;
+            this.line.changeUpStation(appendSection);
+            return;
+        }
+
+        this.distance = this.distance - appendSection.getDistance();
+        this.nextSection.setBackNextSection(appendSection, null);
+        this.nextSection = appendSection;
+    }
+
+    public void setBackNextSection(Section backSection, Section nextSection) {
+        if (!ObjectUtils.isEmpty(backSection)) {
+            this.backSection = backSection;
+        }
+        if (!ObjectUtils.isEmpty(nextSection)) {
+            this.nextSection = nextSection;
+        }
+    }
+
+    private void validDistance(Section appendSection) {
+        if (this.distance <= appendSection.distance) { //현재 섹션보다 신규 섹션의 길이가 같거나 크면 넣지못한다.
+            throw new SectionInvalidException();
+        }
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public Section getNextSection() {
+        return nextSection;
+    }
+
+    public Section getBackSection() {
+        return backSection;
     }
 }
