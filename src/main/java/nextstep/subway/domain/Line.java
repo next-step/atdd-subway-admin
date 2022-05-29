cd828c64f2b182435eae29f355698608b3ef7055
mmtos <js223kr92@naver.com>
2022-05-29 16:09:13 +0900

    fix: 테스트 컨텍스트 빌드시 Persistence예외 발생 - Line 연관관계 매핑 및 조인테이블 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -3,11 +3,16 @@ package nextstep.subway.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Line extends BaseEntity{
@@ -18,6 +23,12 @@ public class Line extends BaseEntity{
     private String name;
     @Column(unique = true)
     private String color;
+
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+    @JoinTable(name = "line_station"
+        ,joinColumns = {@JoinColumn(name = "line_id",referencedColumnName = "id")}
+        , inverseJoinColumns = {@JoinColumn(name = "station_id",referencedColumnName = "id")}
+    )
     private List<Station> stations = new ArrayList<>();
 
     protected Line() {
