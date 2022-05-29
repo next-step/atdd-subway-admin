7e1f556cc671481b439e501d611a094569f50d52
jhsong2580 <thdwmdgns@naver.com>
2022-05-29 22:33:17 +0900

    feat: 검색조건 추가

--- src/main/java/nextstep/subway/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/domain/SectionRepository.java
@@ -1,7 +1,11 @@
 package nextstep.subway.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
+    Optional<Section> findAllByUpStationAndLine(Station upStation, Line line);
+    Optional<Section> findAllByDownStationAndLine(Station downStation, Line line);
 
+    Optional<Section> findAllByUpStationAndDownStationAndLine(Station upStation, Station downStation, Line line);
 }
