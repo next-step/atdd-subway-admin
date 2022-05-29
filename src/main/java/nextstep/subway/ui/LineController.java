59db111cba4eb07b985450059148a5f8213f63d6
KangSeongHyo <abc9023@naver.com>
2022-05-29 12:59:41 +0900

    refactor : 상단 requestMapping 에 path 처리하도록 변경

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/lines")
 @RestController
 public class LineController {
-
-    private static final String LINES_PATH = "/lines/";
 
     private final LineService lineService;
 
@@ -27,31 +27,31 @@ public class LineController {
         this.lineService = lineService;
     }
 
-    @PostMapping("/lines")
+    @PostMapping
     public ResponseEntity<LineResponseDTO> createLines(@RequestBody LineRequestDTO lineRequestDTO) {
         LineResponseDTO lineResponseDTO = lineService.saveLine(lineRequestDTO);
-        return ResponseEntity.created(URI.create(LINES_PATH + lineResponseDTO.getId())).body(lineResponseDTO);
+        return ResponseEntity.created(URI.create("/lines/" + lineResponseDTO.getId())).body(lineResponseDTO);
     }
 
-    @GetMapping("/lines")
+    @GetMapping
     public ResponseEntity<List<LineResponseDTO>> showLines() {
         LineResponsesDTO lineResponsesDTO = lineService.findAll();
         return ResponseEntity.ok(lineResponsesDTO.getLineResponses());
     }
 
-    @GetMapping("/lines/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<LineResponseDTO> showLine(@PathVariable Long id) {
         LineResponseDTO lineResponsesDTO = lineService.findOne(id);
         return ResponseEntity.ok(lineResponsesDTO);
     }
 
-    @PutMapping("/lines/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Void> updateLine(@PathVariable Long id, @RequestBody LineRequestDTO lineRequestDTO) {
         lineService.updateLineInfo(id, lineRequestDTO);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/lines/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLine(@PathVariable Long id) {
         lineService.deleteLine(id);
         return ResponseEntity.noContent().build();
