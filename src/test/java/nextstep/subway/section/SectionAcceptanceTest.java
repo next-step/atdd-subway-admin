1143be7d870d33a12d458810a043d85315975d6b
tyakamyz <tyakamyz@naver.com>
2022-05-29 13:21:53 +0900

    test: 지하철 구간 등록 인수 테스트 케이스 작성

---
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -0,0 +1,65 @@
+package nextstep.subway.section;
+
+import io.restassured.RestAssured;
+import nextstep.subway.util.DatabaseCleanup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@DisplayName("지하철 구간 관련 기능")
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SectionAcceptanceTest {
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+            databaseCleanup.afterPropertiesSet();
+        }
+        databaseCleanup.cleanUp();
+    }
+
+    @Test
+    void 역_사이에_새로운_역으로_지하철구간_생성() {
+
+    }
+
+    @Test
+    void 새로운_역을_상행_종점_지하철구간_생성() {
+
+    }
+
+    @Test
+    void 새로운_역을_하행_종점으로_지하철구간_생성() {
+
+    }
+
+    @Test
+    @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음")
+    void 지하철구간_생성_예외_1() {
+
+    }
+
+    @Test
+    @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
+    void 지하철구간_생성_예외_2() {
+
+    }
+
+    @Test
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
+    void 지하철구간_생성_예외_3() {
+
+    }
+}
