0d19fbd985469ee6d234c80ae6c6b2a9f0d3da1f
jhsong2580 <thdwmdgns@naver.com>
2022-05-29 17:36:57 +0900

    feat: line station many to many를 위한 테이블 구현

---
+++ src/main/java/nextstep/subway/domain/LineStation.java
@@ -0,0 +1,36 @@
+package nextstep.subway.domain;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class LineStation extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "LINE_ID",foreignKey = @ForeignKey(name = "fk_line_station_line"))
+    private Line line;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "STATION_ID",foreignKey = @ForeignKey(name = "fk_line_station_station"))
+    private Station station;
+
+    protected LineStation(){}
+
+    public LineStation(Line line, Station station) {
+        this.line = line;
+        this.station = station;
+    }
+
+
+}
