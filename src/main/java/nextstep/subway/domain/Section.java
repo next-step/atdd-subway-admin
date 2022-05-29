7c1a77bd330138e156b68a92356cf3969f7d8a96
tyakamyz <tyakamyz@naver.com>
2022-05-29 13:28:18 +0900

    feat: Section 엔티티 틀 구현

---
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -0,0 +1,68 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "LINE_ID")
+    private Line line;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "UP_STATION_ID")
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DOWN_STATION_ID")
+    private Station downStation;
+
+    @Column(name = "distance")
+    private int distance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Section)) return false;
+        Section section = (Section) o;
+        if (getId() != null && Objects.equals(getId(), section.getId())) return true;
+        return getDistance() == section.getDistance() && Objects.equals(getId(), section.getId()) && Objects.equals(getLine(), section.getLine()) && Objects.equals(getUpStation(), section.getUpStation()) && Objects.equals(getDownStation(), section.getDownStation());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getLine(), getUpStation(), getDownStation(), getDistance());
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", distance=" + distance +
+                '}';
+    }
+}
---
+++ src/test/java/nextstep/subway/section/SectionRepositoryTest.java
@@ -0,0 +1,6 @@
+package nextstep.subway.section;
+
+class SectionRepositoryTest {
+
+
+}
