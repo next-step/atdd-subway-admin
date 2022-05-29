918b73a1b81f8ff1f626c37443598788b6a21d98
KangSeongHyo <abc9023@naver.com>
2022-05-29 12:55:57 +0900

    test : 상행, 하행 종점 구간 등록 인수테스트 추가

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -41,7 +41,7 @@ class SectionAcceptanceTest {
     static void init() {
         이호선.put("name", "2호선");
         이호선.put("color", "bg-green-200");
-        이호선.put("distance", 30);
+        이호선.put("distance", 10);
     }
 
     @BeforeEach
@@ -57,8 +57,8 @@ class SectionAcceptanceTest {
         STATION_IDS.put("신대방역", ExtractUtils.extractId(StationAcceptanceTest.createStation("신대방역")));
         STATION_IDS.put("봉천역", ExtractUtils.extractId(StationAcceptanceTest.createStation("봉천역")));
         STATION_IDS.put("사당역", ExtractUtils.extractId(StationAcceptanceTest.createStation("사당역")));
-        이호선.put("upStationId", STATION_IDS.get("당산역"));
-        이호선.put("downStationId", STATION_IDS.get("사당역"));
+        이호선.put("upStationId", STATION_IDS.get("신도림역"));
+        이호선.put("downStationId", STATION_IDS.get("봉천역"));
 
         ExtractableResponse<Response> response = LineAcceptanceTest.creatLine(이호선);
         이호선.put("id", ExtractUtils.extractId(response));
@@ -76,6 +76,33 @@ class SectionAcceptanceTest {
 
         //when
         ExtractableResponse<Response> createResponse = createSection((Long) 이호선.get("id"),
+                STATION_IDS.get("신도림역"),
+                STATION_IDS.get("신대방역"),
+                3);
+        //then
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        //when
+        ExtractableResponse<Response> response = requestGetAll(LineAcceptanceTest.LINE_PATH);
+
+        //then
+        assertThat(ExtractUtils.extractNames(response)).containsExactly("신도림역", "신대방역", "봉천역");
+        assertThat(ExtractUtils.extract("stations.distance", response, Integer.class)).contains(0, 3, 7);
+
+    }
+
+   /**
+     * When 새로운 역을 상행 종점으로 등록하면
+     * Then 새로운 역이 상행 좀점으로 추가 생성 된다.
+     * When 노선 목록 조회 시
+     * Then 연결된 구간의 지하철역들을 확인할 수 있다.
+     */
+    @DisplayName("새로운 역을 상행 종점으로 등록한다.")
+    @Test
+    void addSection_up_station() {
+
+        //when
+        ExtractableResponse<Response> createResponse = createSection((Long) 이호선.get("id"),
                 STATION_IDS.get("당산역"),
                 STATION_IDS.get("신도림역"),
                 10);
@@ -86,8 +113,35 @@ class SectionAcceptanceTest {
         ExtractableResponse<Response> response = requestGetAll(LineAcceptanceTest.LINE_PATH);
 
         //then
-        assertThat(ExtractUtils.extractNames(response)).containsExactly("당산역", "신도림역", "사당역");
-        assertThat(ExtractUtils.extract("stations.distance", response, Integer.class)).contains(0, 10, 30);
+        assertThat(ExtractUtils.extractNames(response)).containsExactly("당산역", "신도림역", "봉천역");
+        assertThat(ExtractUtils.extract("stations.distance", response, Integer.class)).contains(0, 10, 10);
+
+    }
+
+   /**
+     * When 새로운 역을 하행 종점으로 등록하면
+     * Then 새로운 역이 하행 좀점으로 추가 생성 된다.
+     * When 노선 목록 조회 시
+     * Then 연결된 구간의 지하철역들을 확인할 수 있다.
+     */
+    @DisplayName("새로운 역을 상행 종점으로 등록한다.")
+    @Test
+    void addSection_down_station() {
+
+        //when
+        ExtractableResponse<Response> createResponse = createSection((Long) 이호선.get("id"),
+                STATION_IDS.get("봉천역"),
+                STATION_IDS.get("사당역"),
+                15);
+        //then
+        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        //when
+        ExtractableResponse<Response> response = requestGetAll(LineAcceptanceTest.LINE_PATH);
+
+        //then
+        assertThat(ExtractUtils.extractNames(response)).containsExactly("신도림역", "봉천역", "사당역");
+        assertThat(ExtractUtils.extract("stations.distance", response, Integer.class)).contains(0, 10, 15);
 
     }
 
