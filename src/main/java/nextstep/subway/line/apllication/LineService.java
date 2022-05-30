d3263c548793f604635911488acce9d43040be1e
wenodev <logicalstark1105@gmail.com>
2022-05-30 14:15:11 +0900

    fix(LineService.java): Transaction 설정한다

--- src/main/java/nextstep/subway/line/apllication/LineService.java
+++ src/main/java/nextstep/subway/line/apllication/LineService.java
@@ -5,6 +5,7 @@ import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
 import nextstep.subway.line.dto.LineResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class LineService {
@@ -14,6 +15,7 @@ public class LineService {
         this.lineRepository = lineRepository;
     }
 
+    @Transactional
     public LineResponse create(LineRequest lineRequest) {
         Line line = lineRepository.save(lineRequest.toLine());
         return LineResponse.of(line);
