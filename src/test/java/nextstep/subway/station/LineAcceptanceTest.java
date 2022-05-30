e7deac77989cdd179bfc941c7d0e22c4efe6dd16
Philip <pch4623@gmail.com>
2022-05-30 19:47:01 +0900

    test: 인수 테스트 레이아웃 작성

---
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -0,0 +1,100 @@
+package nextstep.subway.station;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@DisplayName("지하철역 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+    }
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선이 생성된다
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @DisplayName("지하철 노선을 생성하면 조회 시 해당 노선을 찾을 수 있다.")
+    @Test
+    void createLine() {
+        // when
+
+        // then
+
+        // then
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다
+     */
+    @DisplayName("생성된 전체 지하철 노선 목록을 조회할 수 있다.")
+    @Test
+    void getLines() {
+        // when
+
+        // then
+
+        // then
+    }
+
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @DisplayName("생성된 특정 지하철 노선을 조회할 수 있다.")
+    @Test
+    void getLine() {
+        // when
+
+        // then
+
+        // then
+    }
+
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @DisplayName("지하철 노선 정보를 수정할 수 있다.")
+    @Test
+    void updateLine() {
+        // when
+
+        // then
+
+        // then
+    }
+
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @DisplayName("지하철 노선 정보를 삭제할 수 있다.")
+    @Test
+    void deleteLine() {
+        // when
+
+        // then
+
+        // then
+    }
+}
