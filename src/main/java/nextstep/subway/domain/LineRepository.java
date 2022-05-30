341eba203f899b91f51d87c5c977489d4bc06b55
jhsong2580 <thdwmdgns@naver.com>
2022-05-30 22:13:37 +0900

    refactor: 효율적 query를 위한 entityGraph설정

--- src/main/java/nextstep/subway/domain/LineRepository.java
+++ src/main/java/nextstep/subway/domain/LineRepository.java
@@ -1,7 +1,11 @@
 package nextstep.subway.domain;
 
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
+    @EntityGraph(attributePaths = {"upStation", "downStation"})
+    Optional<Line> findById(Long id);
 }
