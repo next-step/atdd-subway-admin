befc45d1842bae633b683b4955fd9a48f514ffee
Philip <pch4623@gmail.com>
2022-05-30 22:12:01 +0900

    feat: 지하철 노선 관련 API 호출을 위한 DTO 추가

---
+++ src/main/java/nextstep/subway/dto/LineRequest.java
@@ -0,0 +1,28 @@
+package nextstep.subway.dto;
+
+public class LineRequest {
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Integer distance;
+
+    private LineRequest() {
+    }
+
+    private LineRequest(String name, String color, Long upStationId, Long downStationId, Integer distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public static LineRequest of(String name, String color) {
+        return LineRequest.of(name, color, null, null, null);
+    }
+
+    public static LineRequest of(String name, String color, Long upStationId, Long downStationId, Integer distance) {
+        return new LineRequest(name, color, upStationId, downStationId, distance);
+    }
+}
