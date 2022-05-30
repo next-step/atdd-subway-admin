e5ab02cdfcb3a98b9800778f8682d224d455d075
soob-forest <dltntnqo@naver.com>
2022-05-31 02:05:50 +0900

    feat(LineService): 지하철 노선 저장 기능 추가

---
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -0,0 +1,37 @@
+package nextstep.subway.application;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.dto.LineRequest;
+import nextstep.subway.dto.LineResponse;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    @Transactional
+    public LineResponse saveLine(LineRequest lineRequest) throws NotFoundException {
+
+        Station upStation = stationRepository.findById(lineRequest.getUpStationId())
+                .orElseThrow(NotFoundException::new);
+        Station downStation = stationRepository.findById(lineRequest.getDownStationId())
+                .orElseThrow(NotFoundException::new);
+
+        Line persistLine = lineRepository.save(lineRequest.toLine(upStation, downStation));
+        return LineResponse.of(persistLine);
+    }
+}
