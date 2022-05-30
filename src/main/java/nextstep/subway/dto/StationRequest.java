573ac6b0b6e568372b0a60b2253a97a2f575a26a
lcjltj <lcjltj@gmail.com>
2022-05-31 00:53:31 +0900

    refactor(StationRequest): 생성자 추가 및 final 추가

--- src/main/java/nextstep/subway/dto/StationRequest.java
+++ src/main/java/nextstep/subway/dto/StationRequest.java
@@ -2,8 +2,18 @@ package nextstep.subway.dto;
 
 import nextstep.subway.domain.Station;
 
+import java.util.Objects;
+
 public class StationRequest {
-    private String name;
+    private final String name;
+
+    private StationRequest() {
+        this.name = null;
+    }
+
+    public StationRequest(final String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
@@ -11,5 +21,25 @@ public class StationRequest {
 
     public Station toStation() {
         return new Station(name);
+    }
+
+    @Override
+    public String toString() {
+        return "StationRequest{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final StationRequest that = (StationRequest) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
