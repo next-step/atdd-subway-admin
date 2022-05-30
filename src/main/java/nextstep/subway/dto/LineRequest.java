d1e89b20d8a62c237e10321278388fb4c5b441b6
soob-forest <dltntnqo@naver.com>
2022-05-31 02:05:08 +0900

    feat(LineDto): LineRequest, LineResponse Dto 추가

---
+++ src/main/java/nextstep/subway/dto/LineRequest.java
@@ -0,0 +1,37 @@
+package nextstep.subway.dto;
+
+import nextstep.subway.domain.Distance;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
+public class LineRequest {
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public Line toLine(Station upStation, Station downStation) {
+        return new Line(name, color, upStation, downStation, Distance.from(distance));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
