830ff6ad6af365b7ae6b89af90309a4ee90b9448
soob-forest <dltntnqo@naver.com>
2022-05-30 23:51:40 +0900

    refactor(Station): Station에 생성자 추가 및 equals, hascode 추가

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import java.util.Objects;
 import javax.persistence.*;
 
 @Entity
@@ -13,6 +14,11 @@ public class Station extends BaseEntity {
     public Station() {
     }
 
+    public Station(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Station(String name) {
         this.name = name;
     }
@@ -23,5 +29,22 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Station station = (Station) o;
+        return Objects.equals(id, station.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
