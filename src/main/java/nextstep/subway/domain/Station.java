57cb9a1ed25b180212a8e3fcd1ab9070bb5e7b4d
14km <k.connor614@gmail.com>
2022-05-31 01:12:33 +0900

    refactor: Station delete 방식 수정

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,14 +1,20 @@
 package nextstep.subway.domain;
 
+import org.hibernate.annotations.Where;
+
 import javax.persistence.*;
 
 @Entity
+@Where(clause = "deleted=false")
 public class Station extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String name;
+
+    private boolean deleted = false;
 
     public Station() {
     }
@@ -23,5 +29,13 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void delete() {
+        this.deleted = true;
     }
 }
