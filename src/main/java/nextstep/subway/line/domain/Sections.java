cc813b3e2e72d60d875480489607128746b2a074
iamjunsulee <mellow0924@gmail.com>
2022-05-30 13:49:56 +0900

    feat(Sections): 상/하행역 모두 포함하지 않는 경우에 대한 체크 기능 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -11,6 +11,9 @@ import nextstep.subway.station.domain.Station;
 
 @Embeddable
 public class Sections {
+    private static final String ALREADY_REGISTERED_ERROR_MESSAGE = "상행 역과 하행 역이 이미 모두 등록되어 있어 구간 추가할 수 없습니다.";
+    private static final String NO_EXISTS_ERROR_MESSAGE = "상행 역과 하행 역 둘 중 하나라도 등록되어 있지 않으면 구간 추가할 수 없습니다.";
+
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
     private final List<Section> sections = new ArrayList<>();
 
@@ -19,18 +22,29 @@ public class Sections {
             this.sections.add(newSection);
             return;
         }
-
-        if (hasUpStationAndDownStation(newSection)) {
-            throw new IllegalArgumentException("상행 역과 하행 역이 이미 모두 등록되어 있어 구간 추가할 수 없습니다.");
-        }
-
+        validateSection(newSection);
         sections.forEach(section -> section.update(newSection));
         sections.add(newSection);
+    }
+
+    private void validateSection(Section newSection) {
+        if (hasUpStationAndDownStation(newSection)) {
+            throw new IllegalArgumentException(ALREADY_REGISTERED_ERROR_MESSAGE);
+        }
+
+        if(hasNotUpStationAndDownStation(newSection)) {
+            throw new IllegalArgumentException(NO_EXISTS_ERROR_MESSAGE);
+        }
     }
 
     private boolean hasUpStationAndDownStation(Section newSection) {
         List<Station> stations = findAllStations();
         return stations.contains(newSection.getUpStation()) && stations.contains(newSection.getDownStation());
+    }
+
+    private boolean hasNotUpStationAndDownStation(Section newSection) {
+        List<Station> stations = findAllStations();
+        return !stations.contains(newSection.getUpStation()) && !stations.contains(newSection.getDownStation());
     }
 
     private List<Station> findAllStations() {
