bc591f06d0b031599ff3664b8c58b8b3fb0f0d28
mannue <sppe12@gmail.com>
2022-05-31 23:55:32 +0900

    refactor(LineRequest, StationRequest ): 불필요한 import 제거

--- src/main/java/nextstep/subway/dto/LineRequest.java
+++ src/main/java/nextstep/subway/dto/LineRequest.java
@@ -1,14 +1,13 @@
 package nextstep.subway.dto;
 
 import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Station;
 
 public class LineRequest {
-    private String name;
-    private String color;
-    private Long upStationId;
-    private Long downStationId;
-    private Long distance;
+    private final String name;
+    private final String color;
+    private final Long upStationId;
+    private final Long downStationId;
+    private final Long distance;
 
     public LineRequest(String name, String color, Long upStationId, Long downStationId, Long distance) {
         this.name = name;
