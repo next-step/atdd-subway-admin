4bf5ab0cd776d4444415021d2a61febad82e0cab
Philip <pch4623@gmail.com>
2022-06-01 00:07:12 +0900

    test: 지하철 노선 생성 확인 인수 테스트 선개발

--- src/test/java/nextstep/subway/station/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -1,15 +1,27 @@
 package nextstep.subway.station;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.dto.LineRequest;
+import nextstep.subway.dto.LineResponse;
+import nextstep.subway.dto.StationRequest;
+import nextstep.subway.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LineAcceptanceTest {
+public class LineAcceptanceTest extends BaseAcceptanceTest {
     @LocalServerPort
     int port;
 
@@ -29,10 +41,44 @@ public class LineAcceptanceTest {
     @Test
     void createLine() {
         // when
+        StationResponse upStation = createStationRequest("강남역").jsonPath().getObject("", StationResponse.class);
+        StationResponse donwStation = createStationRequest("광교역").jsonPath().getObject("", StationResponse.class);
+
+        LineRequest lineRequest = LineRequest.of("신분당선", "bg-red-600", upStation.getId(), donwStation.getId(), 10);
+        ExtractableResponse<Response> response = createLineRequest(lineRequest);
 
         // then
+        assertThat(response.statusCode())
+                .isEqualTo(HttpStatus.CREATED.value());
 
         // then
+        LineResponse lineResponse = findLineRequest(1L).jsonPath().getObject("", LineResponse.class);
+        assertThat(lineResponse)
+                .satisfies(line -> {
+                    assertThat(line.getName())
+                            .isEqualTo(lineRequest.getName());
+                    assertThat(line.getColor())
+                            .isEqualTo(lineRequest.getColor());
+                    assertThat(line.getStations())
+                            .containsExactly(upStation, donwStation);
+                });
+    }
+
+
+    private ExtractableResponse<Response> createLineRequest(LineRequest lineRequest) {
+        return RestAssured.given().log().all()
+                .body(lineRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> findLineRequest(long lineId) {
+        return RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
     }
 
     /**
