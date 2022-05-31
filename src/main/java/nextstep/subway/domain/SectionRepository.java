bbc3dfb4b9b628e1b2872ce48773d39631d25199
Donghee Han <handh0413@gmail.com>
2022-05-31 19:47:48 +0900

    test: Section 조회 기능 추가

--- src/main/java/nextstep/subway/domain/SectionRepository.java
+++ src/main/java/nextstep/subway/domain/SectionRepository.java
@@ -3,7 +3,10 @@ package nextstep.subway.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
-    List<Section> findByLine_Id(Long id);
+    List<Section> findByLine(Line line);
+    Optional<Section> findByUpStationAndLine(Station station, Line line);
+    Optional<Section> findByDownStationAndLine(Station station, Line line);
 }
--- src/test/java/nextstep/subway/domain/SectionRepositoryTest.java
+++ src/test/java/nextstep/subway/domain/SectionRepositoryTest.java
@@ -1,5 +1,6 @@
 package nextstep.subway.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,6 +27,7 @@ class SectionRepositoryTest {
     @Autowired
     private SectionRepository sectionRepository;
 
+    @DisplayName("노선 내 구간 생성")
     @Test
     void createSection() {
         // given
@@ -39,8 +42,9 @@ class SectionRepositoryTest {
         assertThat(actual).isNotNull();
     }
 
+    @DisplayName("노선 ID로 구간 조회")
     @Test
-    void 노선내_연결정보_가져오기() {
+    void findLineById() {
         // given
         Station upStation = stationRepository.save(new Station("잠실역"));
         Station downStation = stationRepository.save(new Station("강남역"));
@@ -50,9 +54,45 @@ class SectionRepositoryTest {
         entityManager.clear();
 
         // when
-        List<Section> sections = sectionRepository.findByLine_Id(line.getId());
+        List<Section> sections = sectionRepository.findByLine(line);
 
         // then
         assertThat(sections).hasSize(1);
+    }
+
+    @DisplayName("상행역 ID로 구간 조회")
+    @Test
+    void findSectionByUpStation() {
+        // given
+        Station upStation = stationRepository.save(new Station("잠실역"));
+        Station downStation = stationRepository.save(new Station("강남역"));
+        Line line = lineRepository.save(new Line("2호선", "#009D3E", 100, upStation, downStation));
+
+        sectionRepository.save(new Section(100, upStation, downStation, line));
+        entityManager.clear();
+
+        // when
+        Optional<Section> section = sectionRepository.findByUpStationAndLine(upStation, line);
+
+        // then
+        assertThat(section).isNotEmpty();
+    }
+
+    @DisplayName("하행역 ID로 구간 조회")
+    @Test
+    void findSectionByDownStation() {
+        // given
+        Station upStation = stationRepository.save(new Station("잠실역"));
+        Station downStation = stationRepository.save(new Station("강남역"));
+        Line line = lineRepository.save(new Line("2호선", "#009D3E", 100, upStation, downStation));
+
+        sectionRepository.save(new Section(100, upStation, downStation, line));
+        entityManager.clear();
+
+        // when
+        Optional<Section> section = sectionRepository.findByDownStationAndLine(downStation, line);
+
+        // then
+        assertThat(section).isNotEmpty();
     }
 }
