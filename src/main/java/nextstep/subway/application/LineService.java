55beaa45b4d1f1449d5b68b77f3db6bcab948433
Philip <pch4623@gmail.com>
2022-06-01 10:51:13 +0900

    feat: 지하철 노선 생성 및 단건 조회 메서드 추가

---
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -0,0 +1,35 @@
+package nextstep.subway.application;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.dto.LineRequest;
+import nextstep.subway.dto.LineResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    @Transactional
+    public LineResponse saveLine(LineRequest lineRequest) {
+        Station upStation = stationRepository.getById(lineRequest.getUpStationId());
+        Station downStation = stationRepository.getById(lineRequest.getDownStationId());
+
+        Line persistLine = lineRepository.save(lineRequest.toLine(upStation, downStation));
+        return LineResponse.of(persistLine);
+    }
+
+    public LineResponse findLine(Long id) {
+        return  LineResponse.of(lineRepository.getById(id));
+    }
+}
