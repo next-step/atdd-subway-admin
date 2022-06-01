b5711f31391a9675bd7c7cf0eb7b0dbd94d31acb
soob-forest <dltntnqo@naver.com>
2022-06-01 15:24:26 +0900

    refactor(LineRepository): 필요 없는 함수 선언 제거

--- src/main/java/nextstep/subway/domain/LineRepository.java
+++ src/main/java/nextstep/subway/domain/LineRepository.java
@@ -4,6 +4,4 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-    @Override
-    List<Line> findAll();
 }
