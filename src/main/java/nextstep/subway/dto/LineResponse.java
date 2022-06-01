375e7628dc7448e50c168dd8afa17bc4411a9efd
Philip <pch4623@gmail.com>
2022-06-01 14:54:37 +0900

    feat: equals, hashCode 오버라이딩

--- src/main/java/nextstep/subway/dto/LineResponse.java
+++ src/main/java/nextstep/subway/dto/LineResponse.java
@@ -5,6 +5,7 @@ import nextstep.subway.domain.Line;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class LineResponse {
     private Long id;
@@ -44,5 +45,21 @@ public class LineResponse {
 
     public List<StationResponse> getStations() {
         return stations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineResponse that = (LineResponse) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(name, that.name)
+                && Objects.equals(color, that.color)
+                && Objects.equals(stations, that.stations);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color, stations);
     }
 }
