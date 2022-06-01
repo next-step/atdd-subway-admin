33731bce1b37b7957744595cefe8b9ee6a0a353f
Ttungga <torb2002@gmail.com>
2022-06-02 03:13:49 +0900

    feat: add LineController.showLines()

--- src/main/java/nextstep/subway/ui/LineController.java
+++ src/main/java/nextstep/subway/ui/LineController.java
@@ -1,10 +1,13 @@
 package nextstep.subway.ui;
 
 import java.net.URI;
+import java.util.List;
 import nextstep.subway.application.LineService;
 import nextstep.subway.dto.LineRequest;
 import nextstep.subway.dto.LineResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,10 @@ public class LineController {
     public ResponseEntity<LineResponse> createLine(@RequestBody final LineRequest lineRequest) {
         final LineResponse line = lineService.saveLine(lineRequest);
         return ResponseEntity.created(URI.create("/lines/" + line.getId())).body(line);
+    }
+
+    @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<LineResponse>> showLines() {
+        return ResponseEntity.ok().body(lineService.findAllLines());
     }
 }
