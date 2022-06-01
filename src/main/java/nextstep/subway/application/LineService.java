09324280ca8d0e7adff76e1e375600bad4f2eb27
millennion-jin <Jadestern2@>
2022-06-01 13:43:45 +0900

    feat(LineController, LineService) : 지하철 노선 생성 및 목록조회 기능 구현

---
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -0,0 +1,44 @@
+package nextstep.subway.application;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.dto.LineRequest;
+import nextstep.subway.dto.LineResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    @Transactional
+    public LineResponse saveLine(LineRequest lineRequest) {
+        Line persistLine = lineRepository.save(lineRequest.toLine());
+
+        Station upStation = stationRepository.getById(lineRequest.getUpStationId());
+        Station downStation = stationRepository.getById(lineRequest.getDownStationId());
+        persistLine.addStation(upStation);
+        persistLine.addStation(downStation);
+
+        return LineResponse.of(persistLine);
+    }
+
+    public List<LineResponse> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(LineResponse::of)
+                .collect(Collectors.toList());
+    }
+}
