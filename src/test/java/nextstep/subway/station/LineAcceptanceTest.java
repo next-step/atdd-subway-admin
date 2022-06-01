c3afd7cd90f465904c6e802a1dbd63478041d2c0
vvsungho <vvsungho@gmail.com>
2022-06-01 20:36:56 +0900

    refactor: 테스트케이스 중복코드 정리

--- src/test/java/nextstep/subway/station/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -9,6 +9,8 @@ import nextstep.subway.dto.LineRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -22,7 +24,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LineAcceptanceTest {
-
     @LocalServerPort
     int port;
 
@@ -37,22 +38,28 @@ public class LineAcceptanceTest {
         RestAssured.port = port;
         upStation = stationRepository.save(new Station("왕십리역"));
         downStation = stationRepository.save(new Station("수원역"));
+
+        final LineRequest 분당선_생성_Request = new LineRequest("분당선", upStation.getId(), downStation.getId(), "yellow", 1000L);
+        지하철노선_생성(분당선_생성_Request);
+        final LineRequest 이호선_생성_Request = new LineRequest("2호선", upStation.getId(), downStation.getId(), "green", 1000L);
+        지하철노선_생성(이호선_생성_Request);
     }
 
     /**
      * - when 지하철 노선을 생성하면
      * - then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.
      */
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"1호선__blue__1000"})
     @DisplayName("지하철 노선 생성 테스트")
-    void 지하철노선_생성_테스트() {
+    void 지하철노선_생성_테스트(String paramsText) {
+        String[] params = paramsText.split("__");
         // given
-        LineRequest 분당선_생성_Request = new LineRequest("분당선", upStation.getId(), downStation.getId(), "yellow", 1000L);
-
-        ExtractableResponse<Response> 지하철노선_생성_응답 = 지하철노선_생성(분당선_생성_Request);
+        final LineRequest 노선생성_Request = new LineRequest(params[0], upStation.getId(), downStation.getId(), params[1], Long.valueOf(params[2]));
+        final ExtractableResponse<Response> 지하철노선_생성_응답 = 지하철노선_생성(노선생성_Request);
 
         // when
-        ExtractableResponse<Response> 지하철노선_조회_응답 = 지하철노선_조회();
+        final ExtractableResponse<Response> 지하철노선_조회_응답 = 지하철노선_조회();
 
         // then
         assertThat(지하철노선_조회_응답.jsonPath().getList(".").contains(지하철노선_생성_응답.jsonPath().get())).isTrue();   // then
@@ -66,18 +73,14 @@ public class LineAcceptanceTest {
     @Test
     @DisplayName("지하철노선 목록 조회 테스트")
     void 지하철노선_목록_조회_테스트() {
-        LineRequest 분당선_생성_Request = new LineRequest("분당선", upStation.getId(), downStation.getId(), "yellow", 1000L);
-        LineRequest 이호선_생성_Request = new LineRequest("2호선", upStation.getId(), downStation.getId(), "yellow", 1000L);
-
-        지하철노선_생성(분당선_생성_Request);
-        지하철노선_생성(이호선_생성_Request);
-
-        ExtractableResponse<Response> 지하철목록_응답 = 지하철노선_조회();
+        // when
+        final ExtractableResponse<Response> 지하철목록_응답 = 지하철노선_조회();
+        // then
         assertThat(지하철목록_응답.jsonPath().getList(".").size()).isEqualTo(2);
     }
 
     private ExtractableResponse<Response> 지하철노선_생성(LineRequest lineRequest) {
-        ExtractableResponse<Response> 지하철노선_생성_응답 =
+        final ExtractableResponse<Response> 지하철노선_생성_응답 =
                 RestAssured.given().log().all()
                         .body(lineRequest)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -95,7 +98,7 @@ public class LineAcceptanceTest {
     }
 
     private ExtractableResponse<Response> 지하철노선_조회() {
-        ExtractableResponse<Response> 지하철노선_조회_응답 =
+        final ExtractableResponse<Response> 지하철노선_조회_응답 =
                 RestAssured.given().log().all()
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .when().get("line")
