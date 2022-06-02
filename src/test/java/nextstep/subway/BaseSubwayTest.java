c77d470beb3918eaa4aa8e65ff7c54bfae24816a
sangho lee <sangholee.dev@gmail.com>
2022-06-02 23:54:06 +0900

    refactor: 지하철 생성과 목록 조회 테스트 코드 위치 수정

--- src/test/java/nextstep/subway/BaseSubwayTest.java
+++ src/test/java/nextstep/subway/BaseSubwayTest.java
@@ -1,16 +1,9 @@
 package nextstep.subway;
 
-import static nextstep.subway.ui.UrlConstant.CREATE_STATIONS;
-import static nextstep.subway.ui.UrlConstant.SHOW_STATIONS;
-
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseSubwayTest {
@@ -24,21 +17,4 @@ public abstract class BaseSubwayTest {
         }
     }
 
-    protected ExtractableResponse<Response> 지하철_생성(final String name) {
-        return RestAssured.given().log().all()
-                .body(Collections.singletonMap("name", name))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post(CREATE_STATIONS)
-                .then().log().all()
-                .extract();
-    }
-
-    protected ExtractableResponse<Response> 지하철_목록_조회() {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get(SHOW_STATIONS)
-                .then().log().all()
-                .extract();
-    }
 }
