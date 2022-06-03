e1171d6f4a784c23b21b673c0357208c34bc2502
otakjong <tj3828@naver.com>
2022-06-03 15:55:00 +0900

    refactor(test): step3(피드백) - SectionsTest 리팩토링

--- src/main/java/nextstep/subway/section/domain/Section.java
+++ src/main/java/nextstep/subway/section/domain/Section.java
@@ -75,11 +75,11 @@ public class Section {
     }
 
     private void validateUpDownStation(Station upStation, Station downStation) {
-        if (upStation == null && downStation == null) {
+        if (upStation == null || downStation == null) {
             throw new IllegalArgumentException("상행종점역, 하행종점역이 존재하지 않습니다.");
         }
 
-        if (upStation != null && upStation.equals(downStation)) {
+        if (upStation.isSame(downStation)) {
             throw new IllegalArgumentException("상행종점역과 하행종점역은 같을 수가 없습니다.");
         }
     }
