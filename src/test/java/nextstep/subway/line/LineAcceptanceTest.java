2060da313488edb3f936ba207721429b1a770071
jaesungahn91 <jaesungahn91@gmail.com>
2022-06-04 10:43:05 +0900

    test: 지하철노선 인수 조건 작성

---
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,59 @@
+package nextstep.subway.line;
+
+import nextstep.subway.BaseAcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("지하철 노선 관련 기능")
+public class LineAcceptanceTest extends BaseAcceptanceTest {
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @Test
+    void 지하철노선_생성() {
+
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @Test
+    void 지하철노선_목록_조회() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @Test
+    void 지하철노선_조회() {
+        
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @Test
+    void 지하철노선_수정() {
+
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @Test
+    void 지하철노선_삭제() {
+
+    }
+
+}
