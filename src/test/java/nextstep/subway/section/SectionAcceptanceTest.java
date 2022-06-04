9eb925b54ae88c057bba47662eb1598e756d785e
Yul Jeon <notkhaki@gmail.com>
2022-06-04 20:41:37 +0900

    test(SectionAcceptanceTest): add cases that last station is replaced

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -32,18 +32,22 @@ public class SectionAcceptanceTest {
     @LocalServerPort
     int port;
 
+    StationResponse 청량리역;
     StationResponse 왕십리역;
-    StationResponse 선릉역;
     StationResponse 서울숲역;
+    StationResponse 선릉역;
+    StationResponse 도곡역;
     LineResponse 분당선;
 
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
 
+        청량리역 = StationAcceptanceTest.createStation(new StationRequest("청량리역")).as(StationResponse.class);
         왕십리역 = StationAcceptanceTest.createStation(new StationRequest("왕십리역")).as(StationResponse.class);
-        선릉역 = StationAcceptanceTest.createStation(new StationRequest("선릉역")).as(StationResponse.class);
         서울숲역 = StationAcceptanceTest.createStation(new StationRequest("서울숲역")).as(StationResponse.class);
+        선릉역 = StationAcceptanceTest.createStation(new StationRequest("선릉역")).as(StationResponse.class);
+        도곡역 = StationAcceptanceTest.createStation(new StationRequest("도곡역")).as(StationResponse.class);
         분당선 = LineAcceptanceTest.createLine(new LineRequest(
                         "분당선", "bg-yellow-600", 왕십리역.getId(), 선릉역.getId(), 7))
                 .as(LineResponse.class);
@@ -57,6 +61,9 @@ public class SectionAcceptanceTest {
     @DisplayName("상행이 같은 구간을 추가한다.")
     @Test
     void 역_사이_새로운_역_추가_상행_동일() {
+        // given
+        // 분당선(왕십리역-선릉역)
+
         // when
         addSection("/lines/" + 분당선.getId(), new SectionRequest(왕십리역.getId(), 서울숲역.getId(), 4));
 
@@ -74,6 +81,9 @@ public class SectionAcceptanceTest {
     @DisplayName("하행이 같은 구간을 추가한다.")
     @Test
     void 역_사이_새로운_역_추가_하행_동일() {
+        // given
+        // 분당선(왕십리역-선릉역)
+
         // when
         addSection("/lines/" + 분당선.getId(), new SectionRequest(서울숲역.getId(), 선릉역.getId(), 4));
 
@@ -81,6 +91,46 @@ public class SectionAcceptanceTest {
         List<String> stationNames = LineAcceptanceTest.getLine("/lines/" + 분당선.getId())
                 .jsonPath().getList("stations.name", String.class);
         assertThat(stationNames).containsExactly("왕십리역", "서울숲역", "선릉역");
+    }
+
+    /**
+     * Given 구간 하나를 가진 노선이 있을 때
+     * When 하행 역이 기존의 상행 종점과 같은 구간을 추가하면
+     * Then 추가된 구간의 상행 역이 새로운 상행 종점이 된다
+     */
+    @DisplayName("하행 역이 기존의 상행 종점과 같은 구간을 추가한다.")
+    @Test
+    void 새로운_상행_종점() {
+        // given
+        // 분당선(왕십리역-선릉역)
+
+        // when
+        addSection("/lines/" + 분당선.getId(), new SectionRequest(청량리역.getId(), 왕십리역.getId(), 4));
+
+        // then
+        List<String> stationNames = LineAcceptanceTest.getLine("/lines/" + 분당선.getId())
+                .jsonPath().getList("stations.name", String.class);
+        assertThat(stationNames).containsExactly("청량리역", "왕십리역", "선릉역");
+    }
+
+    /**
+     * Given 구간 하나를 가진 노선이 있을 때
+     * When 상행 역이 기존의 하행 종점과 같은 구간을 추가하면
+     * Then 추가된 구간의 하행 역이 새로운 하행 종점이 된다
+     */
+    @DisplayName("상행 역이 기존의 하행 종점과 같은 구간을 추가한다.")
+    @Test
+    void 새로운_하행_종점() {
+        // given
+        // 분당선(왕십리역-선릉역)
+
+        // when
+        addSection("/lines/" + 분당선.getId(), new SectionRequest(선릉역.getId(), 도곡역.getId(), 4));
+
+        // then
+        List<String> stationNames = LineAcceptanceTest.getLine("/lines/" + 분당선.getId())
+                .jsonPath().getList("stations.name", String.class);
+        assertThat(stationNames).containsExactly("왕십리역", "선릉역", "도곡역");
     }
 
     private ExtractableResponse<Response> addSection(String lineLocation, SectionRequest body) {
