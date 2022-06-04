fa88bf6824968acd37b7d84799ae17c99a19a062
Ttungga <torb2002@gmail.com>
2022-06-04 19:23:40 +0900

    refactor: remove LineStationRepository

--- src/main/java/nextstep/subway/domain/LineStationRepository.java
+++
@@ -1,9 +0,0 @@
-package nextstep.subway.domain;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LineStationRepository extends JpaRepository<LineStation, Long> {
-    @Override
-    List<LineStation> findAll();
-}
