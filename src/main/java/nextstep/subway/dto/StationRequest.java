81166c61a4d92b7529d60c2575fcbace69f90497
Ttungga <torb2002@gmail.com>
2022-06-04 22:23:46 +0900

    test: add tests for StationService

--- src/main/java/nextstep/subway/dto/StationRequest.java
+++ src/main/java/nextstep/subway/dto/StationRequest.java
@@ -5,6 +5,13 @@ import nextstep.subway.domain.Station;
 public class StationRequest {
     private String name;
 
+    public StationRequest() {
+    }
+
+    public StationRequest(final String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
