1ef6436a0260ea32bf674b9436390dfec5c299a6
Ttungga <torb2002@gmail.com>
2022-06-06 01:09:50 +0900

    test: fix incorrect sections

--- src/test/java/nextstep/subway/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/domain/SectionsTest.java
@@ -10,9 +10,9 @@ class SectionsTest {
     private final Line line = new Line("신분당선", "bg-red-600");
     private final Station finalUpStation = new Station("강남역");
     private final Station finalDownStation = new Station("정자역");
-    private final Long lineDistance = 30L;
-
     private final Station newStation = new Station("양재역");
+
+    private final Long lineDistance = 30L;
 
     @Test
     void 구간을_추가할_수_있어야_한다() {
@@ -31,8 +31,8 @@ class SectionsTest {
     void 구간_목록을_조회할_수_있어야_한다() {
         // given
         final Sections sections = new Sections();
-        final Section section1 = new Section(line, finalUpStation, finalDownStation, lineDistance);
-        final Section section2 = new Section(line, finalUpStation, newStation, 10L);
+        final Section section1 = new Section(line, finalUpStation, newStation, 10L);
+        final Section section2 = new Section(line, newStation, finalDownStation, lineDistance - 10L);
         sections.add(section1);
         sections.add(section2);
 
