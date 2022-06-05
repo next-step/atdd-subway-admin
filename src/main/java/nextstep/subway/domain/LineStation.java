8f080619d9c5d92d6665050aaba4291db00ae841
mannue <sppe12@gmail.com>
2022-06-05 11:24:14 +0900

    feat(LineStation): 메소드 추가 작업
    - 다음역을 알려주는 메소드
    - 출발점 인지 알려주는 메소드
    - 현재 노선 정보를 알려주는 메소드
    - 현재 역과 전역 간의 거리를
    알려주는 메소드
    - 같은 노선일 경우에만 역을 추가하는 메소드

--- src/main/java/nextstep/subway/domain/LineStation.java
+++ src/main/java/nextstep/subway/domain/LineStation.java
@@ -26,12 +26,50 @@ public class LineStation {
         this.section = section;
     }
 
+    public LineStation(final Line line, final Station station) {
+        this(line, new Section(null, station, new Distance(0L)));
+    }
+
     public Long getId() {
         return id;
     }
 
     public Line getLine() {
         return line;
+    }
+
+    public Station getPreStation() {
+        return this.section.getUpStation();
+    }
+
+    public boolean isStartStation() {
+        return Objects.isNull(getPreStation());
+    }
+
+    public Distance getDistance() {
+        return this.section.getDistance();
+    }
+
+    public LineStation addLineStation(final LineStation newLineStation) {
+        validationLine(newLineStation);
+        return addLineStation(newLineStation, this.section.updatable(newLineStation.section));
+    }
+
+    private LineStation addLineStation(LineStation newLineStation, Section updatedSection) {
+        if (newLineStation.isSameSection(updatedSection)) {
+            return newLineStation;
+        }
+        return new LineStation(this.line, updatedSection);
+    }
+
+    private void validationLine(LineStation newLineStation) {
+        if (!Objects.equals(this.line, newLineStation.getLine())) {
+            throw new IllegalArgumentException("Line is difference");   
+        }
+    }
+
+    private boolean isSameSection(final Section updatedSection) {
+        return Objects.equals(this.section, updatedSection);
     }
 
     private void updateLine(final Line line) {
--- src/test/java/nextstep/subway/line/LineStationTest.java
+++ src/test/java/nextstep/subway/line/LineStationTest.java
@@ -1,58 +1,96 @@
 package nextstep.subway.line;
 
 import nextstep.subway.domain.*;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import javax.persistence.EntityNotFoundException;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-@DataJpaTest
 public class LineStationTest {
-
-    @Autowired
-    private LineRepository lineRepository;
-    @Autowired
-    private StationRepository stationRepository;
-
-    @Autowired
-    private LineStationRepository lineStationRepository;
 
     private Station 강남역;
     private Station 양재역;
-    private Line 신분당선_강남_양재_10L;
+    private Station 수원역;
+    private Station 인천역;
+    private Line 신분당선;
 
     @BeforeEach
     void setUp() {
-        강남역 = stationRepository.save(new Station("강남역"));
-        양재역 = stationRepository.save(new Station("양재역"));
-        신분당선_강남_양재_10L = new Line("신분당선", "bg-red-600", 강남역, 양재역, new Distance(10L));
+        강남역 = new Station("강남역");
+        양재역 = new Station("양재역");
+        수원역 = new Station("수원역");
+        인천역 = new Station("인천역");
+        신분당선 = new Line("신분당선", "bg-red-600");
     }
 
     @DisplayName("line, station, preStation, distance 를 가진다.")
     @Test
     void createTest() {
-        assertThat(new LineStation(신분당선_강남_양재_10L, new Section(강남역, 양재역, new Distance(10L))))
-                .isEqualTo(new LineStation(신분당선_강남_양재_10L, new Section(강남역, 양재역, new Distance(10L))));
+        assertThat(new LineStation(신분당선, new Section(강남역, 양재역, new Distance(10L))))
+                .isEqualTo(new LineStation(신분당선, new Section(강남역, 양재역, new Distance(10L))));
     }
 
-    @DisplayName("동등성 비교")
+    @DisplayName("전역을 알려줄수 있다.")
     @Test
-    void identityTest() {
-        LineStation savedLineStation = lineStationRepository.save(new LineStation(신분당선_강남_양재_10L, new Section(강남역, 양재역, new Distance(10L))));
-        AssertionsForClassTypes.assertThat(lineStationRepository.findById(savedLineStation.getId()).orElseThrow(EntityNotFoundException::new)).isEqualTo(savedLineStation);
+    void getPreStationTest() {
+        assertThat((new LineStation(신분당선, new Section(강남역, 양재역, new Distance(10L))).getPreStation()))
+                .isEqualTo(강남역);
     }
 
-    @DisplayName("연관관계 테스트")
+    @DisplayName("출발역인지 알수 있다.")
+    @ParameterizedTest
+    @MethodSource("providePreStationAndExpectedResult")
+    void isStartStationTest(final Station preStation, final boolean expectedResult) {
+        final LineStation createLineStation = new LineStation(신분당선, new Section(preStation, 양재역, new Distance(10L)));
+        assertThat(createLineStation.isStartStation()).isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> providePreStationAndExpectedResult() {
+        return Stream.of(
+                Arguments.of(new Station("강남역"), false),
+                Arguments.of(null, true)
+        );
+    }
+
+    @DisplayName("현재노선을 알수 있다.")
     @Test
-    void mappingTest() {
-        LineStation savedLineStation = lineStationRepository.save(new LineStation(신분당선_강남_양재_10L, new Section(강남역, 양재역, new Distance(10L))));
-        Line line = lineRepository.findById(savedLineStation.getLine().getId()).orElseThrow(EntityNotFoundException::new);
-        assertThat(line.getLineStations().contains(savedLineStation)).isTrue();
+    void getLineTest() {
+        assertThat((new LineStation(신분당선, new Section(강남역, 양재역, new Distance(10L))).getLine()))
+                .isEqualTo(신분당선);
+    }
+
+    @DisplayName("전역과의 거리를 알수 있다.")
+    @Test
+    void getDistanceTest() {
+        assertThat(new LineStation(신분당선, 강남역).getDistance()).isEqualTo(new Distance(0L));
+    }
+
+    @DisplayName("같은 노선인 경우에만 역을 추가할수 있다.")
+    @Test
+    void addLineStationTest() {
+        final LineStation startStation = new LineStation(신분당선, 강남역);
+        LineStation 신분당선_양재역 = new LineStation(신분당선, new Section(강남역, 양재역, 3L));
+        assertThat(startStation.addLineStation(신분당선_양재역)).isEqualTo(신분당선_양재역);
+
+        Station 신논현역 = new Station("신논현역");
+        LineStation 신분당선_신논현역 = new LineStation(신분당선, new Section(강남역,신논현역, 2L));
+        LineStation updatedLineStation = 신분당선_양재역.addLineStation(신분당선_신논현역);
+        assertThat(updatedLineStation).isEqualTo(신분당선_신논현역);
+        assertThat(신분당선_양재역).isEqualTo(new LineStation(신분당선, new Section(신논현역,양재역, 1L)));
+    }
+
+    @DisplayName("같은 노선인 아닐 경우 역을 추가하면 에러를 발생한다.")
+    @Test
+    void invalidAddLineStationTest() {
+        LineStation addLineStation = new LineStation(new Line("1호선", "bg-blue-600"), new Section(강남역, 수원역, 10L));
+        assertThatThrownBy(() -> new LineStation(신분당선, 강남역).addLineStation(addLineStation))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
