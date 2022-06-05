361551cd24a74c9258a0fb5d20ecf40618f31775
Philip <pch4623@gmail.com>
2022-06-06 02:04:50 +0900

    feat: 구간 리스트 나타내기 위한 일급 컬렉션

---
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -0,0 +1,29 @@
+package nextstep.subway.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Embeddable
+public class Sections {
+    @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Section> sections = new LinkedList<>();
+
+    protected Sections() {
+    }
+
+    private Sections(List<Section> sections) {
+        this.sections = sections;
+    }
+
+    public static Sections from(List<Section> sections) {
+        return new Sections(sections);
+    }
+    
+    public List<Section> getSections() {
+        return sections;
+    }
+}
