d1c02fff6410f84a839a0f83a20c5b6286d8233f
songsimo <chy.song89@gmail.com>
2022-06-06 02:46:05 +0900

    test: DatabaseCleanup 추가

--- src/test/java/nextstep/subway/station/AcceptanceTest.java
+++ src/test/java/nextstep/subway/station/AcceptanceTest.java
@@ -2,8 +2,10 @@ package nextstep.subway.station;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.DatabaseCleanup;
 
 @DisplayName("ATDD 테스트 공통")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,6 +26,11 @@ public class AcceptanceTest {
 		if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
 			RestAssured.port = port;
 		}
+	}
+
+	@AfterEach
+	public void clearDatabase(@Autowired final DatabaseCleanup databaseCleanup) {
+		databaseCleanup.execute();
 	}
 
 	protected void 요청_응답_확인(ExtractableResponse<Response> response, HttpStatus status) {
