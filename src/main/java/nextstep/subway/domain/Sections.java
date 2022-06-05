9b8d9f02186d4387906e5819f733177dfbcb0e4a
Philip <pch4623@gmail.com>
2022-06-06 02:26:40 +0900

    feat: 케이스별 지하철 구간 추가 기능 구현

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -43,6 +43,46 @@ public class Sections {
         return hasStation;
     }
 
+    public void addFirstSection(Section section) {
+        sections.add(section);
+    }
+
+    public void addUpFinalSection(Section section) {
+        sections.add(0, section);
+        section.addLineDistance();
+    }
+
+    public void addDownFinalSection(Section section) {
+        sections.add(section);
+        section.addLineDistance();
+    }
+
+    public void addMiddleSection(Section section) {
+        int position = findPositionToAddSection(section);
+        addMiddleSection(position, section);
+    }
+
+    private void addMiddleSection(int position, Section section) {
+        Section oldSection = sections.get(position);
+
+        if (oldSection.getDistance() <= section.getDistance()) {
+            throw new IllegalArgumentException("새로운 구간의 길이는 기존 역 사이 길이보다 작아야 합니다.");
+        }
+
+        oldSection.updateUpStation(section.getDownStation());
+        oldSection.updateDistance(oldSection.getDistance() - section.getDistance());
+        sections.add(position, section);
+    }
+
+    private int findPositionToAddSection(Section section) {
+        Section oldSection = sections.stream()
+                .filter(old -> old.getUpStation().equals(section.getUpStation()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("새로운 구간을 추가할 위치를 찾을 수 없습니다."));
+
+        return sections.indexOf(oldSection);
+    }
+
     public List<Section> getSections() {
         return sections;
     }
