abe25adc3a0e829703bbc70bf389643699c62d31
Philip <pch4623@gmail.com>
2022-06-06 02:11:55 +0900

    feat: 지하철 노선 응답 DTO에 구간 정보 추가

--- src/main/java/nextstep/subway/dto/LineResponse.java
+++ src/main/java/nextstep/subway/dto/LineResponse.java
@@ -7,27 +7,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private List<StationResponse> stations = new ArrayList<>();
+    private final Long id;
+    private final String name;
+    private final String color;
+    private final Integer distance;
+    private final List<StationResponse> finalStations;
+    private final List<SectionResponse> sections;
 
-    private LineResponse() {
-    }
-
-    private LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+    private LineResponse(Long id, String name, String color, Integer distance, List<StationResponse> finalStations, List<SectionResponse> sections) {
         this.id = id;
         this.name = name;
         this.color = color;
-        this.stations = stations;
+        this.distance = distance;
+        this.finalStations = finalStations;
+        this.sections = sections;
     }
 
     public static LineResponse of(Line line) {
-        List<StationResponse> stations = Arrays.asList(
-                StationResponse.of(line.getUpStation()),
-                StationResponse.of(line.getDownStation())
+        List<StationResponse> finalStations = Arrays.asList(
+                StationResponse.of(line.getUpFinalStation()),
+                StationResponse.of(line.getDownFinalStation())
         );
-        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
+        List<SectionResponse> sections = new ArrayList<>();
+        line.getAllSections().forEach(section -> sections.add(SectionResponse.of(section)));
+
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), line.getDistance(), finalStations, sections);
     }
 
     public Long getId() {
@@ -42,7 +46,15 @@ public class LineResponse {
         return color;
     }
 
-    public List<StationResponse> getStations() {
-        return stations;
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public List<StationResponse> getFinalStations() {
+        return finalStations;
+    }
+
+    public List<SectionResponse> getSections() {
+        return sections;
     }
 }
