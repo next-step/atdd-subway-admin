343055b018d99dcb718578d3acda2ea2f4d449cb
mannue <sppe12@gmail.com>
2022-06-06 12:42:00 +0900

    feat(LineStationRepository): lineid 로 lineStations 가져오기

--- src/main/java/nextstep/subway/domain/LineStationRepository.java
+++ src/main/java/nextstep/subway/domain/LineStationRepository.java
@@ -2,5 +2,8 @@ package nextstep.subway.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LineStationRepository extends JpaRepository<LineStation, Long> {
+     List<LineStation> findLineStationsByLineId(Long lingId);
 }
