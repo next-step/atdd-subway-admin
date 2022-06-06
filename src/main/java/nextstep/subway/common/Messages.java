853ed7208d68225111231d5974e53925b74db4cf
14km <k.connor614@gmail.com>
2022-06-07 02:02:03 +0900

    feat: 지하철 노선 삭제 에러메세지 추가

--- src/main/java/nextstep/subway/common/Messages.java
+++ src/main/java/nextstep/subway/common/Messages.java
@@ -6,6 +6,7 @@ public class Messages {
 
     public static final String DUPLICATE_SECTION_ERROR = "중복된 지하철 노선을 등록할 수 없습니다.";
     public static final String NOT_MATCH_STATION_ERROR = "상행역과 하행역 둘 중 하나도 포함되지 않은 경우 지하철 노선을 등록할 수 없습니다.";
+    public static final String NOT_MATCH_STATION_DELETE_ERROR = "상행역과 하행역 둘 중 하나도 포함되지 않은 경우 지하철 노선을 삭제할 수 없습니다.";
 
     public static final String NOT_FOUND_SECTION = "등록된 지하철 노선이 없습니다.";
 
