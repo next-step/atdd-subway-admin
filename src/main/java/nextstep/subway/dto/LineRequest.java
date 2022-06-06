a818fd6e3fc1034001fbfdb8e8c853e3c3771d23
jiyeonghwang <gyp0288@naver.com>
2022-06-06 20:22:41 +0900

    feat: create Line Dto - LineResponse.java - LineRequest.java

---
+++ src/main/java/nextstep/subway/dto/LineRequest.java
@@ -0,0 +1,51 @@
+package nextstep.subway.dto;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
+
+public class LineRequest {
+    private String name;
+    private String color;
+    private Long upStationId;
+    private Long downStationId;
+    private Long distance;
+
+    public LineRequest() {
+    }
+
+    public static LineRequest of(String name, String color, Long upStationId, Long downStationId, Long distance) {
+        return new LineRequest(name, color, upStationId, downStationId, distance);
+    }
+
+    private LineRequest(String name, String color, Long upStationId, Long downStationId, Long distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Line toLine(Station upStation, Station downStation) {
+        return Line.of(name, color, upStation, downStation, distance);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
+}
