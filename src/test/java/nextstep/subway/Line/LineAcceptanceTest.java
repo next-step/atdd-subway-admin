5a0c16880e76cc853736ca6a9c2c0e3fc69c8393
jiyeonghwang <gyp0288@naver.com>
2022-06-06 22:07:51 +0900

    test: 노선 조회 테스트 추가

--- src/test/java/nextstep/subway/Line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/Line/LineAcceptanceTest.java
@@ -14,9 +14,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,10 +34,6 @@ public class LineAcceptanceTest {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
-
-        //지하철역 생성
-        upStationId = StationAcceptanceTest.createStation("광교역");
-        downStationId = StationAcceptanceTest.createStation("신사역");
     }
 
     /**
@@ -44,22 +43,9 @@ public class LineAcceptanceTest {
      */
     @DisplayName("노선을 생성한다.")
     @Test
-    void createLine() {
+    void createLineTest() {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "신분당선");
-        params.put("color", "br-red-600");
-        params.put("upStationId", String.valueOf(upStationId));
-        params.put("downStationId", String.valueOf(downStationId));
-        params.put("distance", "10");
-
-        ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/lines")
-                        .then().log().all()
-                        .extract();
+        ExtractableResponse<Response> response = createLine("신분당선", "bg-red-600", "광교역", "신사역", 10L);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -71,5 +57,61 @@ public class LineAcceptanceTest {
                         .then().log().all()
                         .extract();
         assertThat(lineResponse.as(LineResponse.class).getName()).isEqualTo("신분당선");
+    }
+
+    /**
+     * When 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다
+     */
+    @DisplayName("노선을 조회한다.")
+    @Test
+    void getLines() {
+        // when
+        createLine("신분당선", "bg-red-600", "광교역", "신사역", 10L);
+        createLine("2호선", "bg-green-600", "까치산", "신도림", 20L);
+
+        // when
+        ExtractableResponse<Response> response = selectAllLine();
+        List<LineResponse> lines = response.jsonPath().getList(".", LineResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(lines).hasSize(2),
+                () -> assertThat(lines.stream().map(lineResponse -> lineResponse.getName()).collect(Collectors.toList())).contains("신분당선"),
+                () -> assertThat(lines.stream().map(lineResponse -> lineResponse.getName()).collect(Collectors.toList())).contains("2호선")
+        );
+    }
+
+    public ExtractableResponse<Response> createLine(String name, String color, String upStation, String downStation, Long distance) {
+        Map<String, String> params = createParams(name, color, upStation, downStation, distance);
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .body(params)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().post("/lines")
+                        .then().log().all()
+                        .extract();
+
+        return response;
+    }
+
+    public Map<String, String> createParams(String name, String color, String upStation, String downStation, Long distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", String.valueOf(StationAcceptanceTest.createStation(upStation)));
+        params.put("downStationId", String.valueOf(StationAcceptanceTest.createStation(downStation)));
+        params.put("distance", "10");
+
+        return params;
+    }
+
+    public ExtractableResponse<Response> selectAllLine() {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
     }
 }
