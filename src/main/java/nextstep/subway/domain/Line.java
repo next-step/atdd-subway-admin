695fbfc3a66a000873bbbf6eef6f6fdf39f5bf51
jiyeonghwang <gyp0288@naver.com>
2022-06-06 20:21:50 +0900

    feat: create Line

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,65 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Line extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id", nullable = false)
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id", nullable = false)
+    private Station downStation;
+
+    private Long distance;
+
+    protected Line() {
+    }
+
+    private Line(String name, String color, Station upStation, Station downStation, Long distance) {
+        this.name = name;
+        this.color = color;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public static Line of(String name, String color, Station upStation, Station downStation, Long distance) {
+        return new Line(name, color, upStation, downStation, distance);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
+}
