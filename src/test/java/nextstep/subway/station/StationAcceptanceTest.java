7222216bb7ba324f2e4ac747582f40dccff7931d
choi-ys <project.log.062@gmail.com>
2022-06-07 13:10:25 +0900

    refactor: 인수테스트에 반복적으로 작성되는 RestAssured 생성 및 통신 부 추출

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -1,6 +1,9 @@
 package nextstep.subway.station;
 
 import static nextstep.subway.utils.RequestParamUtils.generateRequestParam;
+import static nextstep.subway.utils.RestAssuredUtils.delete;
+import static nextstep.subway.utils.RestAssuredUtils.get;
+import static nextstep.subway.utils.RestAssuredUtils.post;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
@@ -45,29 +48,27 @@ public class StationAcceptanceTest {
         params.put("name", "강남역");
 
         ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+            RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
         List<String> stationNames =
-                RestAssured.given().log().all()
-                        .when().get("/stations")
-                        .then().log().all()
-                        .extract().jsonPath().getList("name", String.class);
+            RestAssured.given().log().all()
+                .when().get("/stations")
+                .then().log().all()
+                .extract().jsonPath().getList("name", String.class);
         assertThat(stationNames).containsAnyOf("강남역");
     }
 
     /**
-     * Given 지하철역을 생성하고
-     * When 기존에 존재하는 지하철역 이름으로 지하철역을 생성하면
-     * Then 지하철역 생성이 안된다
+     * Given 지하철역을 생성하고 When 기존에 존재하는 지하철역 이름으로 지하철역을 생성하면 Then 지하철역 생성이 안된다
      */
     @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
     @Test
@@ -77,19 +78,19 @@ public class StationAcceptanceTest {
         params.put("name", "강남역");
 
         RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/stations")
-                .then().log().all();
+            .body(params)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().post("/stations")
+            .then().log().all();
 
         // when
         ExtractableResponse<Response> response =
-                RestAssured.given().log().all()
-                        .body(params)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when().post("/stations")
-                        .then().log().all()
-                        .extract();
+            RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
@@ -106,27 +107,15 @@ public class StationAcceptanceTest {
         // Given
         final String stationNameProperty = "name";
         Map<String, String> firstStationRequestParams = generateRequestParam(stationNameProperty, "논현역");
-        Response generateFirstStationsResponse = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(firstStationRequestParams)
-            .when().post(BASE_URL)
-            .then().log().all().extract().response();
+        Response generateFirstStationsResponse = post(BASE_URL, firstStationRequestParams).extract().response();
         assertThat(generateFirstStationsResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         Map<String, String> secondStationRequestParams = generateRequestParam(stationNameProperty, "신논현역");
-        Response generateSecondStationsResponse = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(secondStationRequestParams)
-            .when().post(BASE_URL)
-            .then().log().all()
-            .extract().response();
+        Response generateSecondStationsResponse = post(BASE_URL, secondStationRequestParams).extract().response();
         assertThat(generateSecondStationsResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // When
-        Response getAllStationsResponse = RestAssured.given().log().all()
-            .when().get(BASE_URL)
-            .then().log().all()
-            .extract().response();
+        Response getAllStationsResponse = get(BASE_URL).extract().response();
 
         // Then
         assertThat(getAllStationsResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
@@ -147,36 +136,23 @@ public class StationAcceptanceTest {
     void deleteStation() {
         // Given
         final String stationNameProperty = "name";
-        Map<String, String> firstStationRequestParams = generateRequestParam(stationNameProperty, "선릉역");
-        Response generateStationResponse = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(firstStationRequestParams)
-            .when().post(BASE_URL)
-            .then().log().all().extract().response();
+        Map<String, String> requestParam = generateRequestParam(stationNameProperty, "선릉역");
+        Response generateStationResponse = post(BASE_URL, requestParam).extract().response();
         assertThat(generateStationResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         final String stationId = generateStationResponse.body().jsonPath().getString("id");
         final String urlTemplate = String.format(BASE_URL.concat("/%s"), stationId);
 
         // When
-        Response deleteResponse = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when().delete(urlTemplate)
-            .then().log().all()
-            .extract().response();
+        Response deleteResponse = delete(urlTemplate).extract().response();
 
         // Then
         assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
 
-        Response getAllStationsResponse = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when().get(BASE_URL)
-            .then().log().all()
-            .extract().response();
-
+        Response getAllStationsResponse = get(BASE_URL).extract().response();
         assertThat(getAllStationsResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
         List<String> stationNames = getAllStationsResponse.jsonPath().getList(stationNameProperty, String.class);
         assertThat(stationNames)
             .as("지하철역 목록 조회 응답에 삭제한 지하철역 미포함 여부 검증")
-            .doesNotContain(firstStationRequestParams.get(stationNameProperty));
+            .doesNotContain(requestParam.get(stationNameProperty));
     }
 }
