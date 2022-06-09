ce7e10186fce2b64179afcaa519ec9facee544da
millennion-jin <Jadestern2@>
2022-06-09 20:11:54 +0900

    refactor(Section) : Section 생성시의 validation 추가

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -12,7 +12,12 @@ import org.springframework.dao.DataIntegrityViolationException;
 
 @Entity
 public class Section extends BaseEntity {
-    public static final String ERROR_DISTANCE_OVER = "중간 구간의 길이가 상위 구간의 길이보다 길거나 같습니다.";
+    public static final String ERROR_MESSAGE_DISTANCE_OVER = "중간 구간의 길이가 상위 구간의 길이보다 길거나 같습니다.";
+    public static final String ERROR_MESSAGE_DISTANCE_ZERO_OR_NEGATIVE = "역 간 거리는 0 이하가 될 수 없습니다.";
+    public static final String ERROR_MESSAGE_UP_STATION_NULL = "상행역 변수 값이 null 입니다.";
+    public static final String ERROR_MESSAGE_DOWN_STATION_NULL = "하행역 변수 값이 null 입니다.";
+    public static final String ERROR_MESSAGE_DISTANCE_NULL = "역 간 거리의 값이 null 입니다.";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,10 +36,32 @@ public class Section extends BaseEntity {
     }
 
     public Section(Long lineId, Station upStation, Station downStation, Integer distance) {
+        validateSection(upStation, downStation, distance);
+        
         this.lineId = lineId;
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
+    }
+    
+    private void validateSection(Station upStation, Station downStation, Integer distance) {
+        validateNull(upStation, ERROR_MESSAGE_UP_STATION_NULL);
+        validateNull(downStation, ERROR_MESSAGE_DOWN_STATION_NULL);
+        validateNull(distance, ERROR_MESSAGE_DISTANCE_NULL);
+
+        validateDistanceZeroOrNegative(distance);
+    }
+    
+    private void validateNull(Object object, String errorMessage) {
+        if(object == null) {
+            throw new DataIntegrityViolationException(errorMessage);
+        }
+    }
+    
+    private void validateDistanceZeroOrNegative(Integer distance) {
+        if(distance <= 0) {
+            throw new DataIntegrityViolationException(ERROR_MESSAGE_DISTANCE_ZERO_OR_NEGATIVE);
+        }
     }
 
     public boolean match(Section section) {
@@ -48,8 +75,16 @@ public class Section extends BaseEntity {
 
     private void validateDistanceOver(Section section) {
         if (distance <= section.distance) {
-            throw new DataIntegrityViolationException(ERROR_DISTANCE_OVER);
+            throw new DataIntegrityViolationException(ERROR_MESSAGE_DISTANCE_OVER);
         }
+    }
+    
+    public boolean hasStation(Station station) {
+        if(station == null) {
+            return false;
+        }
+
+        return upStation.equals(station) || downStation.equals(station);
     }
 
     public Long getId() {
