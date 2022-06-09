4c3d06aeaa37c6e1243f084efd3e41c1498c0b4b
jaesungahn91 <jaesungahn91@gmail.com>
2022-06-10 02:39:46 +0900

    feat: IllegalArgumentException handler 추가

--- src/main/java/nextstep/subway/ui/SectionController.java
+++ src/main/java/nextstep/subway/ui/SectionController.java
@@ -3,10 +3,7 @@ package nextstep.subway.ui;
 import nextstep.subway.application.SectionService;
 import nextstep.subway.dto.SectionRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SectionController {
@@ -22,6 +19,11 @@ public class SectionController {
                                            @RequestBody SectionRequest sectionRequest) {
         sectionService.saveSection(lineId, sectionRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Void> handleIllegalArgumentException() {
+        return ResponseEntity.badRequest().build();
     }
 
 }
