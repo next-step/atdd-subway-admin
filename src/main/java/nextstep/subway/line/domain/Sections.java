b2bde860b944f5d2b3f3f22ef9cb49aed463971c
lcjltj <lcjltj@gmail.com>
2022-06-11 17:40:02 +0900

    feat: 섹션 추가 관련 메소드 추가
    - Sections add() 섹션 추가 및 validate() 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -7,42 +7,105 @@ import nextstep.subway.station.domain.Station;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Embeddable
 public class Sections {
+    private static final int MIN = 0;
+
     @OneToMany(mappedBy = "line", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Section> list = new ArrayList<>();
 
     public void add(final Section section) {
+        validate(section);
+
+        updateUpStation(section);
+        updateDownStation(section);
         list.add(section);
+    }
+
+    private void validate(final Section section) {
+        existedEachStation(section);
+        notExistedEachStation(section);
+    }
+
+    private void existedEachStation(final Section section) {
+        if (existStation(section)) {
+            throw new LineException(LineExceptionType.STATION_ALL_USED);
+        }
+    }
+
+    private boolean existStation(final Section section) {
+        return containStation(section.getUpStation()) && containStation(section.getDownStation());
+    }
+
+    private void notExistedEachStation(final Section section) {
+        if (list.size() > MIN && notExistStation(section)) {
+            throw new LineException(LineExceptionType.STATION_ALL_NOT_USED);
+        }
+    }
+
+    private boolean notExistStation(final Section section) {
+        return !containStation(section.getUpStation()) && !containStation(section.getDownStation());
+    }
+
+
+    private boolean containStation(final Station station) {
+        return getAllStation().contains(station);
+    }
+
+    private void updateUpStation(final Section section) {
+        list.stream()
+                .filter(it -> it.isEqualsDownStation(section))
+                .findFirst()
+                .ifPresent(it ->it.updateDownStation(section));
+    }
+
+    private void updateDownStation(final Section section) {
+        list.stream()
+                .filter(it -> it.isEqualsUpStation(section))
+                .findFirst()
+                .ifPresent(it ->it.updateUpStation(section));
     }
 
     public boolean contains(final Section section) {
         return list.contains(section);
     }
 
+    public Set<Station> getAllStation() {
+        Set<Station> set = new HashSet<>();
+        for (Section section : list) {
+            set.add(section.getUpStation());
+            set.add(section.getDownStation());
+        }
+        return set;
+    }
+
     public Station finalUpStation() {
-        return list.stream()
-                .filter(Section::isFinalUpStation)
-                .map(Section::getUpStation)
+        return getUpStations().stream()
+                .filter(it -> !getDownStations().contains(it))
                 .findFirst()
-                .orElseThrow(() -> new LineException(LineExceptionType.FINAL_UP_STATION_NOT_FOUND));
+                .orElseThrow(() -> new LineException((LineExceptionType.FINAL_UP_STATION_NOT_FOUND)));
+    }
+
+    private List<Station> getUpStations() {
+        return list.stream()
+                .map(Section::getUpStation)
+                .collect(Collectors.toList());
     }
 
     public Station finalDownStation() {
-        return list.stream()
-                .filter(Section::isFinalDownStation)
-                .map(Section::getDownStation)
+        return getDownStations().stream()
+                .filter(it -> !getUpStations().contains(it))
                 .findFirst()
                 .orElseThrow(() -> new LineException(LineExceptionType.FINAL_DOWN_STATION_NOT_FOUND));
     }
 
-    public List<Section> getList() {
-        return Collections.unmodifiableList(list);
+    private List<Station> getDownStations() {
+        return list.stream()
+                .map(Section::getDownStation)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -64,5 +127,6 @@ public class Sections {
     public int hashCode() {
         return Objects.hash(list);
     }
+
 }
 
