0237286cfd3843b894df5c58edf439ae2921d806
taeyong-sim <sim2ty@naver.com>
2022-06-11 10:08:40 +0900

    feat(SectionDeleteException) : 구간 제거 기능 추가에 따른 예외 클래스 추가

---
+++ src/main/java/nextstep/subway/exception/SectionDeleteException.java
@@ -0,0 +1,12 @@
+package nextstep.subway.exception;
+
+public class SectionDeleteException extends RuntimeException {
+
+    public SectionDeleteException() {
+        super();
+    }
+
+    public SectionDeleteException(String message) {
+        super(message);
+    }
+}
