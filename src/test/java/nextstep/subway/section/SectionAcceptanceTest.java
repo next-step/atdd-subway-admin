106560a9de00dfb6132e93e09f52a8f4621b53f5
jiyeonghwang <gyp0288@naver.com>
2022-06-12 23:46:23 +0900

    test: 새로운 역을 상행역 또는 하행역 종점으로 등록한 경우 테스트

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -6,7 +6,6 @@ import io.restassured.response.Response;
 import nextstep.subway.base.BaseUnitTest;
 import nextstep.subway.dto.LineResponse;
 import nextstep.subway.dto.SectionRequest;
-import nextstep.subway.dto.SectionResponse;
 import nextstep.subway.dto.StationResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import java.util.stream.Collectors;
 
 import static nextstep.subway.Line.LineAcceptanceTest.지하철_노선_생성_신분당선;
 import static nextstep.subway.station.StationAcceptanceTest.createStation;
@@ -30,17 +31,15 @@ public class SectionAcceptanceTest extends BaseUnitTest {
     private StationResponse 판교역;
     private StationResponse 신림역;
     private StationResponse 삼성역;
+    private StationResponse 강남구청역;
+    private StationResponse 미금역;
     private LineResponse 신분당선;
 
     @BeforeEach
     void given() {
         //given
         //지하철역 등록
-        강남역 = createStation("강남역").as(StationResponse.class);
-        광교역 = createStation("광교역").as(StationResponse.class);
-        판교역 = createStation("판교역").as(StationResponse.class);
-        신림역 = createStation("신림역").as(StationResponse.class);
-        삼성역 = createStation("삼성역").as(StationResponse.class);
+        지하철역_등록();
 
         //given
         //노선 등록
@@ -52,16 +51,17 @@ public class SectionAcceptanceTest extends BaseUnitTest {
     void 구간_등록_테스트_동일_상행역() {
         // when
         // 지하철_노선에_지하철역_등록_요청
-        SectionRequest sectionRequest = SectionRequest.of(판교역.getId(), 광교역.getId(), 1L);
+        SectionRequest sectionRequest = SectionRequest.of(강남역.getId(), 판교역.getId(), 1L);
         ExtractableResponse<Response> createResponse = 지하철_구간_등록(신분당선.getId(), sectionRequest);
 
         // then
-        // 지하철_노선에_지하철역_등록
+        // 지하철_노선_조회
         ExtractableResponse<Response> response = 지하철_노선_조회(신분당선.getId());
-        LineResponse lineResponse = response.as(LineResponse.class);
+        LineResponse lineResponse = getLineResponse(response);
 
         assertAll(
                 () -> assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
                 () -> assertThat(lineResponse.getSections().size()).isEqualTo(2),
                 () -> assertThat(lineResponse.getSections().stream()
                         .filter(sectionResponse -> sectionResponse.getUpStationId().equals(강남역.getId()))
@@ -81,9 +81,9 @@ public class SectionAcceptanceTest extends BaseUnitTest {
         ExtractableResponse<Response> createResponse = 지하철_구간_등록(신분당선.getId(), sectionRequest);
 
         // then
-        // 지하철_노선에_지하철역_등록
+        // 지하철_노선_조회
         ExtractableResponse<Response> response = 지하철_노선_조회(신분당선.getId());
-        LineResponse lineResponse = response.as(LineResponse.class);
+        LineResponse lineResponse = getLineResponse(response);
 
         assertAll(
                 () -> assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
@@ -94,6 +94,56 @@ public class SectionAcceptanceTest extends BaseUnitTest {
                 () -> assertThat(lineResponse.getSections().stream()
                         .filter(sectionResponse -> sectionResponse.getUpStationId().equals(판교역.getId()))
                         .findFirst().get().getDistance()).isEqualTo(9)
+        );
+    }
+
+    @DisplayName("새로운 역을 상행 종점으로 등록할 경우")
+    @Test
+    void 상행_종점_등록() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        SectionRequest sectionRequest = SectionRequest.of(강남구청역.getId(), 강남역.getId(), 9L);
+        ExtractableResponse<Response> createResponse = 지하철_구간_등록(신분당선.getId(), sectionRequest);
+
+        // then
+        // 지하철_노선_조회
+        ExtractableResponse<Response> response = 지하철_노선_조회(신분당선.getId());
+        LineResponse lineResponse = getLineResponse(response);
+
+        assertAll(
+                () -> assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(lineResponse.getStations().stream()
+                        .map(StationResponse::getName)
+                        .collect(Collectors.toList()).contains(강남구청역.getName())).isTrue(),
+                () -> assertThat(lineResponse.getStations().stream()
+                        .map(StationResponse::getName)
+                        .collect(Collectors.toList()).contains(강남역.getName())).isFalse()
+        );
+    }
+
+    @DisplayName("새로운 역을 하행 종점으로 등록할 경우")
+    @Test
+    void 하행_종점_등록() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        SectionRequest sectionRequest = SectionRequest.of(광교역.getId(), 미금역.getId(), 9L);
+        ExtractableResponse<Response> createResponse = 지하철_구간_등록(신분당선.getId(), sectionRequest);
+
+        // then
+        // 지하철_노선_조회
+        ExtractableResponse<Response> response = 지하철_노선_조회(신분당선.getId());
+        LineResponse lineResponse = getLineResponse(response);
+
+        assertAll(
+                () -> assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(lineResponse.getStations().stream()
+                        .map(StationResponse::getName)
+                        .collect(Collectors.toList()).contains(미금역.getName())).isTrue(),
+                () -> assertThat(lineResponse.getStations().stream()
+                        .map(StationResponse::getName)
+                        .collect(Collectors.toList()).contains(광교역.getName())).isFalse()
         );
     }
 
@@ -158,5 +208,19 @@ public class SectionAcceptanceTest extends BaseUnitTest {
                 .when().get("/lines/" + id)
                 .then().log().all()
                 .extract();
+    }
+
+    public LineResponse getLineResponse(ExtractableResponse<Response> response) {
+        return response.as(LineResponse.class);
+    }
+
+    private void 지하철역_등록() {
+        강남역 = createStation("강남역").as(StationResponse.class);
+        광교역 = createStation("광교역").as(StationResponse.class);
+        판교역 = createStation("판교역").as(StationResponse.class);
+        신림역 = createStation("신림역").as(StationResponse.class);
+        삼성역 = createStation("삼성역").as(StationResponse.class);
+        미금역 = createStation("미금역").as(StationResponse.class);
+        강남구청역 = createStation("강남구청역").as(StationResponse.class);
     }
 }
