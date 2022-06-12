2e8a523e90dc12ad9f70e0cc78693c3c5609809d
jiyeonghwang <gyp0288@naver.com>
2022-06-12 23:01:59 +0900

    test: 구간 등록 시 길이 계산 테스트

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import nextstep.subway.base.BaseUnitTest;
 import nextstep.subway.dto.LineResponse;
 import nextstep.subway.dto.SectionRequest;
+import nextstep.subway.dto.SectionResponse;
 import nextstep.subway.dto.StationResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,7 @@ import org.springframework.http.MediaType;
 import static nextstep.subway.Line.LineAcceptanceTest.지하철_노선_생성_신분당선;
 import static nextstep.subway.station.StationAcceptanceTest.createStation;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("구간 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -45,27 +47,61 @@ public class SectionAcceptanceTest extends BaseUnitTest {
         신분당선 = 지하철_노선_생성_신분당선(강남역, 광교역, 10L).as(LineResponse.class);
     }
 
-    @DisplayName("역과 역 사이에 새로운 구간을 등록한다.")
+    @DisplayName("역과 역 사이에 새로운 구간을 등록한다. (상행역이 같은 경우)")
     @Test
-    void 구간_등록_테스트() {
+    void 구간_등록_테스트_동일_상행역() {
         // when
         // 지하철_노선에_지하철역_등록_요청
-        SectionRequest sectionRequest = SectionRequest.of(강남역.getId(), 판교역.getId(), 1L);
+        SectionRequest sectionRequest = SectionRequest.of(판교역.getId(), 광교역.getId(), 1L);
         ExtractableResponse<Response> createResponse = 지하철_구간_등록(신분당선.getId(), sectionRequest);
 
         // then
-        // 지하철_노선에_지하철역_등록됨
-        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        // 지하철_노선에_지하철역_등록
         ExtractableResponse<Response> response = 지하철_노선_조회(신분당선.getId());
         LineResponse lineResponse = response.as(LineResponse.class);
-        assertThat(lineResponse.getSections().size()).isEqualTo(2);
+
+        assertAll(
+                () -> assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(lineResponse.getSections().size()).isEqualTo(2),
+                () -> assertThat(lineResponse.getSections().stream()
+                        .filter(sectionResponse -> sectionResponse.getUpStationId().equals(강남역.getId()))
+                        .findFirst().get().getDistance()).isEqualTo(1),
+                () -> assertThat(lineResponse.getSections().stream()
+                        .filter(sectionResponse -> sectionResponse.getUpStationId().equals(판교역.getId()))
+                        .findFirst().get().getDistance()).isEqualTo(9)
+        );
+    }
+
+    @DisplayName("역과 역 사이에 새로운 구간을 등록한다. (하행역이 같은 경우)")
+    @Test
+    void 구간_등록_테스트_동일_하행역() {
+        // when
+        // 지하철_노선에_지하철역_등록_요청
+        SectionRequest sectionRequest = SectionRequest.of(판교역.getId(), 광교역.getId(), 9L);
+        ExtractableResponse<Response> createResponse = 지하철_구간_등록(신분당선.getId(), sectionRequest);
+
+        // then
+        // 지하철_노선에_지하철역_등록
+        ExtractableResponse<Response> response = 지하철_노선_조회(신분당선.getId());
+        LineResponse lineResponse = response.as(LineResponse.class);
+
+        assertAll(
+                () -> assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(lineResponse.getSections().size()).isEqualTo(2),
+                () -> assertThat(lineResponse.getSections().stream()
+                        .filter(sectionResponse -> sectionResponse.getUpStationId().equals(강남역.getId()))
+                        .findFirst().get().getDistance()).isEqualTo(1),
+                () -> assertThat(lineResponse.getSections().stream()
+                        .filter(sectionResponse -> sectionResponse.getUpStationId().equals(판교역.getId()))
+                        .findFirst().get().getDistance()).isEqualTo(9)
+        );
     }
 
     @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없다.")
     @Test
     void 상행역_하행역_노선_등록() {
         // given
-        구간_등록_테스트();
+        구간_등록_테스트_동일_상행역();
 
         // when
         // 동일한 구간 등록
@@ -81,7 +117,7 @@ public class SectionAcceptanceTest extends BaseUnitTest {
     @Test
     void 상행역_하행역_미포함() {
         // given
-        구간_등록_테스트();
+        구간_등록_테스트_동일_상행역();
 
         // when
         // 상행역, 하행역 포함하지 않고 구간 등록
