aab0c8f031accd5b7e6b982125a02dc1ddec1053
Lisc <qqwer153@gmail.com>
2022-06-15 04:08:41 +0900

    feat: DataIntegrityViolationException 처리 추가

--- src/main/java/nextstep/subway/Exception/CommonExceptionHandler.java
+++ src/main/java/nextstep/subway/Exception/CommonExceptionHandler.java
@@ -1,11 +1,20 @@
 package nextstep.subway.Exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class CommonExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity handleIllegalArgumentException(IllegalArgumentException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
