4df3e76a81869965e12667903f5f9aa42868a3de
Lisc <qqwer153@gmail.com>
2022-06-15 03:30:45 +0900

    test: 구간추가 인수테스트 관련 메서드 추가

---
+++ src/test/java/nextstep/subway/section/SectionTestUtils.java
@@ -0,0 +1,33 @@
+package nextstep.subway.section;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.dto.SectionRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SectionTestUtils {
+    public static final String PATH = "/lines";
+
+    public static ExtractableResponse<Response> 구간_추가_요청(Long lineId, Long upStationId, Long downStationId, int distance) {
+        SectionRequest request = new SectionRequest(upStationId, downStationId, distance);
+
+        return RestAssured.given().log().all()
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post(PATH + "/{id}/sections", lineId)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 구간_추가_성공_확인(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static void 구간_추가_실패_확인(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
