2d7f986aa5297e4a7e850d413af214065dc65574
Philip <pch4623@gmail.com>
2022-06-15 21:29:15 +0900

    refactor: equasl, hashcode 오버라이딩 수정

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -121,11 +121,13 @@ public class Section extends BaseEntity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Section section = (Section) o;
-        return Objects.equals(id, section.id);
+        return Objects.equals(id, section.id)
+                && Objects.equals(upStation, section.upStation)
+                && Objects.equals(downStation, section.downStation);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, upStation, downStation);
     }
 }
