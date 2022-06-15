5b36f820382fb11aef4c4de18cd5b8b9e5dd9bb1
sangho lee <sangholee.dev@gmail.com>
2022-06-16 00:13:08 +0900

    refactor: LineRequest stationId primitive 타입으로 변경

--- src/main/java/nextstep/subway/dto/LineRequest.java
+++ src/main/java/nextstep/subway/dto/LineRequest.java
@@ -1,21 +1,19 @@
 package nextstep.subway.dto;
 
-import java.util.List;
 import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Station;
 
 public class LineRequest {
     private String name;
 
     private String color;
 
-    private Long upStationId;
+    private long upStationId;
 
-    private Long downStationId;
+    private long downStationId;
 
     private int distance;
 
-    protected LineRequest(final String name, final String color, final Long upStationId, final Long downStationId,
+    protected LineRequest(final String name, final String color, final long upStationId, final long downStationId,
                           final int distance) {
         this.name = name;
         this.color = color;
