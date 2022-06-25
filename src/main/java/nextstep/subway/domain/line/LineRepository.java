18cb723a93a95c8cc8240106e0897fa071a3d17d
choi-ys <project.log.062@gmail.com>
2022-06-25 16:23:21 +0900

    feat: 구간 삭제 API 일부 구현(상행/하행 종점역 삭제)
    - EP : /lines/{id}/sections?stationsId={stationId}
    - 상행/하행 종점역 삭제 처리 구현
    - 객체 동등성 비교 시, Hibernate Proxy 객체로 인해 equlas가 false 반환되는 현상 해결을 위해, 노선 조회 시 @EntityGraph를 이용한 Join 처리
    
    기능 구현에 따른 TODO List 갱신
    
    TODO
    - 노선 중간에 위치한 역이 포함된 구간 삭제

--- src/main/java/nextstep/subway/domain/line/LineRepository.java
+++ src/main/java/nextstep/subway/domain/line/LineRepository.java
@@ -1,7 +1,11 @@
 package nextstep.subway.domain.line;
 
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
+    @EntityGraph(attributePaths = {"sections.sections", "sections.sections.upStation", "sections.sections.downStation"})
+    Optional<Line> findById(Long id);
 }
