c67016d8e9124842af5760637f41d0a6f3f9e287
earth-h <earth.h.183@gmail.com>
2022-11-09 19:42:38 +0900

    test(acceptance): 인수 조건 검증하는 인수 테스트 작성
    LineAcceptanceTest
    - 인수 조건 검증하는 인수 테스트 작성
      - 지하철 노선 생성

---
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -0,0 +1,72 @@
+package nextstep.subway.station;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@DisplayName("지하철노선 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+    }
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철노선이 생성된다
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @DisplayName("지하철노선을 생성한다.")
+    @Test
+    void createLine() {
+        // when
+        String lineName = "2호선";
+        ExtractableResponse<Response> response = 지하철노선_등록_요청(lineName);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // then
+        List<String> lineNames = 지하철노선_목록_조회_요청().jsonPath().getList("name", String.class);
+        assertThat(lineNames).containsAnyOf(lineName);
+    }
+
+    ExtractableResponse<Response> 지하철노선_등록_요청(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    ExtractableResponse<Response> 지하철노선_목록_조회_요청() {
+        return RestAssured.given().log().all()
+                .when().get("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+}
