5cf52f3f2f3da42caa3c1ca1321465b9f3547181
이동길 <akxkdldhtm@gmail.com>
2022-11-10 23:05:37 +0900

    docs(test): 지하철 노선 목록 조회 인수 테스트 작성

--- src/test/java/nextstep/subway/station/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -37,11 +37,41 @@ public class LineAcceptanceTest {
      */
     @Test
     public void 지하철노선_생성() {
-        List<Long> stationIds = 두개의_지하철_역이_등록되어_있음();
+        List<Long> stationIds = 두개의_지하철_역이_등록되어_있음("강남역", "부산역");
 
         ExtractableResponse<Response> response = 지하철_노선을_생성한다("노선이름", "bg-red-600", 10L, stationIds);
 
-        응답에_요청했던_정보가_모두_포함되어_있다(response,"노선이름","bg-red-600",10L,stationIds);
+        응답에_요청했던_정보가_모두_포함되어_있다(response, "노선이름", "bg-red-600", 10L, stationIds);
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @Test
+    void 지하철노선_목록_조회() {
+
+        지하철_노선_2개를_생성한다();
+
+        ExtractableResponse<Response> response = 지하철_노선_목록을_조회한다();
+
+        지하철_노선_목록을_2개_조회할_수_있다(response);
+    }
+
+    private void 지하철_노선_목록을_2개_조회할_수_있다(ExtractableResponse<Response> response) {
+        assertThat(response.jsonPath().getList("id").size()).isEqualTo(2);
+    }
+
+    private ExtractableResponse<Response> 지하철_노선_목록을_조회한다() {
+        return RequestUtil.getRequest("/lines");
+    }
+
+    private void 지하철_노선_2개를_생성한다() {
+        List<Long> ids = 두개의_지하철_역이_등록되어_있음("강남역", "부산역");
+        지하철_노선을_생성한다("노선1", "color", 100L, ids);
+        ids = 두개의_지하철_역이_등록되어_있음("서울역", "강릉역");
+        지하철_노선을_생성한다("노선2", "color", 100L, ids);
     }
 
     private void 응답에_요청했던_정보가_모두_포함되어_있다(ExtractableResponse<Response> response, String 노선이름, String color, long l, List<Long> stationIds) {
@@ -49,7 +79,7 @@ public class LineAcceptanceTest {
         assertThat(response.jsonPath().getString("name")).isEqualTo(노선이름);
         assertThat(response.jsonPath().getString("color")).isEqualTo(color);
         assertThat(response.jsonPath().getLong("distance")).isEqualTo(l);
-        assertThat(response.jsonPath().getList("stations.id")).contains(stationIds.get(0).intValue(),stationIds.get(1).intValue());
+        assertThat(response.jsonPath().getList("stations.id")).contains(stationIds.get(0).intValue(), stationIds.get(1).intValue());
 
     }
 
@@ -67,9 +97,9 @@ public class LineAcceptanceTest {
         return response;
     }
 
-    private List<Long> 두개의_지하철_역이_등록되어_있음() {
-        지하철역을_생성한다("강남역");
-        지하철역을_생성한다("역삼역");
+    private List<Long> 두개의_지하철_역이_등록되어_있음(String upStationName, String downStationName) {
+        지하철역을_생성한다(upStationName);
+        지하철역을_생성한다(downStationName);
         ExtractableResponse<Response> allStations = 모든_지하철역을_조회한다();
         return allStations.jsonPath().getList("id", Long.class);
     }
