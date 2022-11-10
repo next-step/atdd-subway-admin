9967f6f5db348c8fd86ab46afb0b5dba6c42ce42
sonyoon7 <sonyoon7@gmail.com>
2022-11-11 00:45:57 +0900

    feat (service) 라인 서비스 구현

---
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -0,0 +1,60 @@
+package nextstep.subway.application;
+
+import nextstep.subway.domain.Line;
+import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Station;
+import nextstep.subway.domain.StationRepository;
+import nextstep.subway.dto.LineDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+    private final LineRepository lineRepository;
+
+    private final StationRepository stationRepository;
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    @Transactional
+    public LineDto.Response saveLine(LineDto.CreateRequest lineCreateRequest) {
+        Station upStation = stationRepository.findById((long) lineCreateRequest.getUpStationId()).get();
+        Station downStation = stationRepository.findById((long) lineCreateRequest.getDownStationId()).get();
+        Line persistLine = lineRepository.save(lineCreateRequest.of(upStation, downStation));
+        return LineDto.Response.of(persistLine);
+    }
+
+    public List<LineDto.Response> findAllLines() {
+        List<Line> lines = lineRepository.findAll();
+
+        return lines.stream()
+                .map(LineDto.Response::of)
+                .collect(Collectors.toList());
+    }
+
+    public LineDto.Response findLineById(Long id) {
+        Line lines = lineRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 노선을 찾을 수 없습니다."));
+        return LineDto.Response.of(lines);
+    }
+
+    @Transactional
+    public void deleteLineById(Long id) {
+        lineRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateLine(Long id, LineDto.UpdateRequest updateRequest) {
+        Line line = lineRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 노선을 찾을 수 없습니다."));
+        line.update(updateRequest.getName(), updateRequest.getColor());
+        lineRepository.save(line);
+    }
+}
--- src/main/java/nextstep/subway/dto/LineDto.java
+++ src/main/java/nextstep/subway/dto/LineDto.java
@@ -1,12 +1,14 @@
 package nextstep.subway.dto;
 
 import nextstep.subway.domain.Line;
+import nextstep.subway.domain.Station;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LineDto {
 
-    public static class Request{
+    public static class CreateRequest {
 
         private String name;
         private String color;
@@ -14,15 +16,21 @@ public class LineDto {
         private int downStationId;
         private int distance;
 
-        public Line of() {
-            return new Line(this);
+        public Line of(Station up, Station down) {
+            return new Line.Builder()
+                    .setName(name)
+                    .setColor(color)
+                    .setDistance(distance)
+                    .setUpStation(up)
+                    .setDownStation(down)
+                    .build();
         }
 
         public String getName() {
             return name;
         }
 
-        public Request setName(String name) {
+        public CreateRequest setName(String name) {
             this.name = name;
             return this;
         }
@@ -31,7 +39,7 @@ public class LineDto {
             return color;
         }
 
-        public Request setColor(String color) {
+        public CreateRequest setColor(String color) {
             this.color = color;
             return this;
         }
@@ -40,7 +48,7 @@ public class LineDto {
             return upStationId;
         }
 
-        public Request setUpStationId(int upStationId) {
+        public CreateRequest setUpStationId(int upStationId) {
             this.upStationId = upStationId;
             return this;
         }
@@ -49,7 +57,7 @@ public class LineDto {
             return downStationId;
         }
 
-        public Request setDownStationId(int downStationId) {
+        public CreateRequest setDownStationId(int downStationId) {
             this.downStationId = downStationId;
             return this;
         }
@@ -58,17 +66,80 @@ public class LineDto {
             return distance;
         }
 
-        public Request setDistance(int distance) {
+        public CreateRequest setDistance(int distance) {
             this.distance = distance;
             return this;
         }
     }
 
+    public static class UpdateRequest {
+
+        private String name;
+        private String color;
+
+        public UpdateRequest setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UpdateRequest setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getColor() {
+            return color;
+        }
+    }
+
     public static class Response{
-        public int id;
+        public Long id;
         public String name;
         public String color;
         public List<StationResponse> stations;
         public int distance;
+
+        public Response(long id, String name, String color, List<StationResponse> stations, int distance) {
+            this.id = id;
+            this.name = name;
+            this.color = color;
+            this.stations = stations;
+            this.distance = distance;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public List<StationResponse> getStations() {
+            return stations;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+
+        public static Response of (Line line) {
+            return new LineDto.Response(line.getId(),
+                    line.getName(),
+                    line.getColor(),
+                    line.getStations().stream()
+                            .map(StationResponse::of)
+                            .collect(Collectors.toList()),
+                    line.getDistance());
+        }
     }
+
 }
