3e4128992f7308a85691888baceeca4899d274ae
Hyeongwon-up <leehwdatabase@gmail.com>
2022-11-11 02:43:57 +0900

    feat: Sections 일급 컬렉션 추가

---
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -0,0 +1,24 @@
+package nextstep.subway.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+@Embeddable
+public class Sections {
+
+	@OneToMany(mappedBy = "line", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	private List<Section> sectionList = new ArrayList<>();
+
+	protected Sections() {
+	}
+
+	public Sections(final List<Section> sectionList) {
+		this.sectionList = Collections.unmodifiableList(sectionList);
+	}
+
+}
