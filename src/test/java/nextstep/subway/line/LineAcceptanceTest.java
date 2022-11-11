3ba8205c772e9debff7077e6a8c0970b2748884c
yoonmin-kim <rladbsals23@gmail.com>
2022-11-12 00:09:42 +0900

    feat(line): 지하철 노선 목록 조회 인수 테스트 및 기능 추가

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.line;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,33 +29,65 @@ class LineAcceptanceTest {
     }
 
     /**
-     * When 지하철 노선을 생성하면 Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     * When 지하철 노선을 생성하면
+     * <p>
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
      */
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
         // when
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "신분당선");
-        params.put("color", "bg-red-600");
-        params.put("upStationId", "1");
-        params.put("downStationId", "2");
-        params.put("distance", "10");
+        노선_생성("신분당선", "bg-red-600", "1", "2", "10");
 
-        RestAssured.given().log().all()
+        //then
+        List<String> lineNames = 노선_목록조회("name");
+
+        //then
+        assertThat(lineNames).contains("신분당선");
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * <p>
+     * When 지하철 노선 목록을 조회하면
+     * <p>
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void getLines() {
+        //given
+        노선_생성("신분당선", "bg-red-600", "1", "2", "10");
+        노선_생성("분당선", "bg-green-600", "1", "3", "10");
+
+        //when
+        List<String> lineNames = 노선_목록조회("name");
+
+        //then
+        assertThat(lineNames).contains("신분당선", "분당선");
+    }
+
+    private ExtractableResponse<Response> 노선_생성(String name, String color, String upStationId, String downStationId,
+                                                String distance) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("color", color);
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", distance);
+
+        return RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/lines")
                 .then().log().all()
                 .extract();
+    }
 
-        //then
-        List<String> lineNames = RestAssured.given().log().all()
+    private List<String> 노선_목록조회(String information) {
+        return RestAssured.given().log().all()
                 .when().get("/lines")
                 .then().log().all()
-                .extract().jsonPath().getList("name", String.class);
-
-        //then
-        assertThat(lineNames).contains("신분당선");
+                .extract().jsonPath().getList(information, String.class);
     }
 }
