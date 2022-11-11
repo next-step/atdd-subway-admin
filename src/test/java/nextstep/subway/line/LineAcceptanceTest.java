18258abf7f576e6871b9df8ffbca527946897aff
crongcm <ucm5459@kakao.com>
2022-11-12 00:28:28 +0900

    test: 인수 테스트 작성 - 지하철노선 생성

---
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,80 @@
+package nextstep.subway.line;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import nextstep.subway.AcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+@DisplayName("지하철노선 관련 기능")
+class LineAcceptanceTest extends AcceptanceTest {
+    private static final String 신분당선 = "신분당선";
+    private static final String 신논현역 = "신논현역";
+    private static final String 강남역 = "강남역";
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @Test
+    @DisplayName("지하철 노선을 생성한다.")
+    void createLine() {
+        // when
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", 신분당선);
+        params.put("color", "bg-red-600");
+        params.put("upStationId", 1);
+        params.put("downStationId", 2);
+        params.put("distance", 10);
+
+        ExtractableResponse<Response> 저장된_신분당선 = given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+
+        // then
+        List<String> 조회된_지하철노선_목록 = given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/lines")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().jsonPath().getList("name", String.class);
+
+        assertThat(조회된_지하철노선_목록).contains(신분당선);
+    }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+}
