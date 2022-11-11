786f551c8272a2d549a4a68d6e3b012491a8b6e5
giraffelim <taeyang7704@naver.com>
2022-11-12 01:41:34 +0900

    feat(TestIsolator): 테스트 격리를 위한 TestIsolator 추가

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -3,9 +3,11 @@ package nextstep.subway.station;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.helper.TestIsolator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -19,12 +21,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StationAcceptanceTest {
     @LocalServerPort
     int port;
+    @Autowired
+    private TestIsolator testIsolator;
 
     @BeforeEach
     public void setUp() {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
+        testIsolator.run();
     }
 
     /**
