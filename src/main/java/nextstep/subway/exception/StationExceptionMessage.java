e5d9d7091ae0ebf4845ffa9f85e98c98a96c1533
chunhodong <yhms4432@naver.com>
2022-11-11 16:20:50 +0900

    feat: StationException클래스추가 지하철역관련예외

---
+++ src/main/java/nextstep/subway/exception/StationExceptionMessage.java
@@ -0,0 +1,14 @@
+package nextstep.subway.exception;
+
+public enum StationExceptionMessage {
+    NONE_EXISTS_STATION("지하철역이 존재하지 않습니다");
+    String message;
+
+    StationExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
