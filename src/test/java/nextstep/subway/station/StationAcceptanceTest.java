0314f9c0f3dc9dad1e5fa2b5b981430ff1c2152d
gunkim <gunkim.dev@gmail.com>
2022-11-13 02:27:27 +0900

    test: 테스트 코드 격리

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -3,9 +3,11 @@ package nextstep.subway.station;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.domain.StationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -22,12 +24,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StationAcceptanceTest {
     @LocalServerPort
     int port;
+    @Autowired
+    private StationRepository stationRepository;
 
     @BeforeEach
     public void setUp() {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
+        stationRepository.deleteAll();
     }
 
     /**
