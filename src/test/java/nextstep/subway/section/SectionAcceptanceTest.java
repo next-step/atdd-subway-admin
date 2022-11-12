91a7645753bcd9ec6736ba59abe564332b97125a
ilmare-cbk <cbk104325@gmail.com>
2022-11-13 03:29:05 +0900

    test(SectionAcceptanceTest.java): 새로운 역을 상행 종점으로 등록하는 인수 테스트 추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -69,4 +69,26 @@ class SectionAcceptanceTest {
                         LineStationResponse.from(광교역)
                 );
     }
+
+    /**
+     * Given 새로운 역 A를 생성한다.
+     * When 새로운 역 A를 상행 종점으로 지하철 노선(B역 - C역)에 등록한다.
+     * Then 지하철 노선 조회 시 새로운 역 A가 상행 종점인 것을 확인할 수 있다. (A역 - B역 - C역)
+     */
+    @DisplayName("새로운 역을 상행 종점으로 등록한다.")
+    @Test
+    void createSectionOfUpStation() {
+        StationResponse 압구정역 = 지하철역_생성("압구정역").as(StationResponse.class);
+        SectionRequest 압구정역_신사역_구간 = SectionRequest.of(압구정역.getId(), 신사역.getId(), 5);
+
+        SectionAcceptanceMethods.지하철_노선에_지하철역_등록(this.신분당선.getId(), 압구정역_신사역_구간);
+
+        LineResponse 신분당선 = 지하철_노선_조회(this.신분당선.getId()).as(LineResponse.class);
+        Assertions.assertThat(신분당선.getStations())
+                .containsExactly(
+                        LineStationResponse.from(압구정역),
+                        LineStationResponse.from(신사역),
+                        LineStationResponse.from(광교역)
+                );
+    }
 }
