7f292f25934f1b2e93f11f411ad82c8e631dff4b
ilmare-cbk <cbk104325@gmail.com>
2022-11-13 03:44:06 +0900

    test(SectionAcceptanceTest.java): 구간 등록 예외 케이스 인수 테스트 추가
    - 역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록할 수 없다.

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -1,6 +1,8 @@
 package nextstep.subway.section;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.DatabaseInitializer;
 import nextstep.subway.line.LineAcceptanceMethods;
 import nextstep.subway.line.dto.LineResponse;
@@ -12,9 +14,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import static nextstep.subway.line.LineAcceptanceMethods.지하철_노선_생성;
 import static nextstep.subway.line.LineAcceptanceMethods.지하철_노선_조회;
@@ -112,5 +117,22 @@ class SectionAcceptanceTest {
                         LineStationResponse.from(광교역),
                         LineStationResponse.from(호매실역)
                 );
+    }
+
+    /**
+     * Given 새로운 역 A를 생성한다.
+     * When 지하철 노선(B역 - C역) 중간에(역 사이에) 기존 역 사이 길이보가 크거나 같도록 새로운 역 A를 등록한다.
+     * Then 새로운 역 A 등록(구간 등록)에 실패한다.
+     */
+    @DisplayName("역 사이에 시로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록할 수 없다.")
+    @ParameterizedTest(name = "{index} | {displayName} | {argumentsWithNames}")
+    @ValueSource(ints = {10, 100})
+    void sectionDistanceOver(int input) {
+        StationResponse 강남역 = 지하철역_생성("강남역").as(StationResponse.class);
+        SectionRequest 신사역_강남역_구간 = SectionRequest.of(신사역.getId(), 강남역.getId(), input);
+
+        ExtractableResponse<Response> response = SectionAcceptanceMethods.지하철_노선에_지하철역_등록(this.신분당선.getId(), 신사역_강남역_구간);
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
