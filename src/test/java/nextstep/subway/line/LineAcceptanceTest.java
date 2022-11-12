405063684e2491a27483a7cc81a2dc5c11565c9d
Haseop Lee <haservi@naver.com>
2022-11-12 20:42:23 +0900

    test(Line): 인수테스트 항목 추가

---
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,42 @@
+package nextstep.subway.line;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("지하철 노선 관련 테스트")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @DisplayName("지하철노선 생성")
+    @Test
+    void create_line() {
+
+    }
+
+    @DisplayName("지하철노선 목록 조회")
+    @Test
+    void get_line_list() {
+
+    }
+
+    @DisplayName("지하철노선 조회")
+    @Test
+    void get_line() {
+
+    }
+
+    @DisplayName("지하철노선 수정")
+    @Test
+    void update_line() {
+
+    }
+
+    @DisplayName("지하철노선 삭제")
+    @Test
+    void delete_line() {
+
+    }
+
+
+}
