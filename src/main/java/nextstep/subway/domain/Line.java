66476e1ef2dba26bbb016ca4ae30f43f9449b01b
jimbaemon <jimbae@kakao.com>
2022-11-13 20:53:31 +0900

    feat: 조회, 생성 개발.

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,68 @@
+package nextstep.subway.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+@Entity
+public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String color;
+    private int distance;
+    @ManyToOne
+    @Fetch(FetchMode.JOIN)
+    private Station upStation;
+    @ManyToOne
+    @Fetch(FetchMode.JOIN)
+    private Station downStation;
+
+    protected Line() {
+    }
+
+    public Line(Long id, String name, String color, int distance,
+        Station upStation, Station downStation) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.distance = distance;
+        this.upStation = upStation;
+        this.downStation = downStation;
+    }
+
+    public Line(String name, String color, int distance, Station upStation,
+        Station downStation) {
+        this.name = name;
+        this.color = color;
+        this.distance = distance;
+        this.upStation = upStation;
+        this.downStation = downStation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+}
--- src/main/java/nextstep/subway/dto/StationRequest.java
+++ src/main/java/nextstep/subway/dto/StationRequest.java
@@ -3,6 +3,7 @@ package nextstep.subway.dto;
 import nextstep.subway.domain.Station;
 
 public class StationRequest {
+
     private String name;
 
     public String getName() {
