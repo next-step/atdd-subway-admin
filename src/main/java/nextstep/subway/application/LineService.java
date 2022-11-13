4fe4bb0b4fabea102552d96455124d1ba1c0c99e
iamsojung <sojungkid@gmail.com>
2022-11-13 19:53:13 +0900

    feat: Line 도메인, 레퍼지토리, 서비스 추가

---
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -0,0 +1,15 @@
+package nextstep.subway.application;
+
+import nextstep.subway.domain.LineRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class LineService {
+    private final LineRepository lineRepository;
+
+    public LineService(LineRepository lineRepository) {
+        this.lineRepository = lineRepository;
+    }
+}
