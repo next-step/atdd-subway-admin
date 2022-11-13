f023d063fced8b4ddf32e97211090ab0c345e971
sjk5766 <wjdrnr6662@gmail.com>
2022-11-13 19:57:22 +0900

    test(acceptance): 새로운 역을 상행 종점으로 등록할 경우 인수 조건 및 테스트 추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -44,7 +44,7 @@ public class SectionAcceptanceTest {
      * When 지하철 구간을 추가하면
      * Then 노선에 새로운 지하철 역이 등록된다
      */
-    @DisplayName("노선에 지하철 구간을 추가한다.")
+    @DisplayName("노선 중간에 지하철 구간을 추가한다.")
     @Test
     void addSection() {
         // given
@@ -61,6 +61,36 @@ public class SectionAcceptanceTest {
 
         // when
         ExtractableResponse<Response> response = 지하철_구간_추가(lineId, newStationId, downStationId, 5);
+
+        // then
+        List<String> stationNames = response
+                .jsonPath()
+                .getList("stations.name", String.class);
+        assertThat(stationNames).contains("판교역", "양재역", "강남역");
+    }
+
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 상행성 종점에 지하철 구간을 추가하면
+     * Then 노선에 새로운 지하철 역이 등록된다
+     */
+    @DisplayName("상행 종점에 지하철 구간을 추가한다.")
+    @Test
+    void addUpSection() {
+        // given
+        int distance = 10;
+        String expectLine = "신분당선";
+        Long upStationId = StationAcceptanceTest.지하철_역_생성("판교역")
+                .jsonPath().getLong("id");
+        Long downStationId = StationAcceptanceTest.지하철_역_생성("강남역")
+                .jsonPath().getLong("id");
+        Long lineId = 지하철_노선_생성(expectLine, "주황색", upStationId, downStationId, distance)
+                .jsonPath().getLong("id");
+        Long newStationId = StationAcceptanceTest.지하철_역_생성("양재역")
+                .jsonPath().getLong("id");
+
+        // when
+        ExtractableResponse<Response> response = 지하철_구간_추가(lineId, newStationId, upStationId, 5);
 
         // then
         List<String> stationNames = response
