cb4edc444806c9a76c2722fdd8d57d149fc1a5d5
ilmare-cbk <cbk104325@gmail.com>
2022-11-14 01:08:27 +0900

    refactor(ExceptionMessage.java): 오타 수정

--- src/main/java/nextstep/subway/common/message/ExceptionMessage.java
+++ src/main/java/nextstep/subway/common/message/ExceptionMessage.java
@@ -6,7 +6,7 @@ public class ExceptionMessage {
     public static final String NOT_FOUND_STATION = "지하철역을 찾을 수 없습니다.";
     public static final String UP_STATION_EQUALS_DOWN_STATION = "상행역과 하행역은 같을 수 없습니다.";
     public static final String INVALID_SECTION_DISTANCE = "지하철 구간의 길이는 0보다 큰 값이어야 합니다. (현재 = %d)";
-    public static final String ALREADY_ADDED_SECTION = "이미 등록된 자하철 구간입니다.";
+    public static final String ALREADY_ADDED_SECTION = "이미 등록된 지하철 구간입니다.";
     public static final String NOT_INCLUDE_UP_STATION_AND_DOWN_STATION = "상행/하행 종점역 둘 다 포함하고 있지 않습니다.";
 
     private ExceptionMessage() {
