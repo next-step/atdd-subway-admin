20a61155e4e79e874b4250279e106557058639f5
chunhodong <yhms4432@naver.com>
2022-11-13 13:55:21 +0900

    refactor: Line클래스 validation추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,6 +1,11 @@
 package nextstep.subway.domain;
 
+import nextstep.subway.exception.LineException;
+
 import javax.persistence.*;
+
+import static nextstep.subway.exception.LineExceptionMessage.EMPTY_NAME;
+import static nextstep.subway.exception.LineExceptionMessage.EMPTY_STATION;
 
 @Entity
 public class Line extends BaseEntity {
@@ -20,17 +25,22 @@ public class Line extends BaseEntity {
     @JoinColumn(name = "down_station_id", nullable = false)
     private Station downStation;
 
-
     public Line() {
     }
 
     public Line(String name, String color, long distance, Station upStation, Station downStation) {
+        validateLine(name, upStation, downStation);
         this.name = name;
         this.color = color;
         this.distance = distance;
         this.upStation = upStation;
         this.downStation = downStation;
         this.distance = distance;
+    }
+
+    private void validateLine(String name, Station upStation, Station downStation) {
+        if (name == null || name.isEmpty()) throw new LineException(EMPTY_NAME.getMessage());
+        if (upStation == null || downStation == null) throw new LineException(EMPTY_STATION.getMessage());
     }
 
     public Long getId() {
--- src/main/java/nextstep/subway/exception/LineExceptionMessage.java
+++ src/main/java/nextstep/subway/exception/LineExceptionMessage.java
@@ -1,7 +1,10 @@
 package nextstep.subway.exception;
 
 public enum LineExceptionMessage {
-    NONE_EXISTS_LINE("라인이 존재하지 않습니다");
+    NONE_EXISTS_LINE("라인이 존재하지 않습니다"),
+    EMPTY_NAME("노선이름을 입력해야합니다"),
+    EMPTY_STATION("역정보를 입력해야합니다");
+
     String message;
 
     LineExceptionMessage(String message) {
