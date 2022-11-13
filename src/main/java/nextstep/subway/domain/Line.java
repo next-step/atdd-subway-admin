b6746a02984088ceb17ef2704a6399ca79fe9b48
gunkim <gunkim.dev@gmail.com>
2022-11-14 02:30:32 +0900

    feat: 지하철 노선 도메인 객체 구현

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,62 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Line extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String color;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station upStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station downStation;
+
+    @Column(nullable = false)
+    private int distance;
+
+    protected Line() {
+    }
+
+    public Line(String name, String color, Station upStation, Station downStation, int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void update(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+}
