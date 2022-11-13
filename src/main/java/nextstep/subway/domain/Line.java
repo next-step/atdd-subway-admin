01ed245fbd3f1890425b5139d763be95d22fdde8
baeksoo <kyubeom03@naver.com>
2022-11-13 19:26:48 +0900

    fix: lazy loading 으로 변경

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -2,6 +2,7 @@ package nextstep.subway.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,11 +22,11 @@ public class Line extends BaseEntity {
     @Column(nullable = false)
     private String color;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "up_station_id")
     private Station lastUpStation;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "down_station_id")
     private Station lastDownStation;
 
