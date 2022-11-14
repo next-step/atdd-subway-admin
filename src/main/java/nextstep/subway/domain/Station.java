fc7e3125fcdd3be1766565aabd60630ade098974
yomni <yearsheep@gmail.com>
2022-11-15 01:31:57 +0900

    test: 지하철역 도메인 테스트 추가

--- src/main/java/nextstep/subway/domain/Station.java
+++ src/main/java/nextstep/subway/domain/Station.java
@@ -1,8 +1,11 @@
 package nextstep.subway.domain;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
+@Table(name = "station")
 public class Station extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -10,11 +13,23 @@ public class Station extends BaseEntity {
     @Column(unique = true)
     private String name;
 
-    public Station() {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id", foreignKey = @ForeignKey(name = "fk_station_to_line"))
+    private Line line;
+
+    protected Station() {
     }
 
     public Station(String name) {
         this.name = name;
+    }
+
+    public void toLine(Line line) {
+        this.line = line;
+        List<Station> stations = line.getStations();
+        if (!stations.contains(this)) {
+            stations.add(this);
+        }
     }
 
     public Long getId() {
@@ -23,5 +38,22 @@ public class Station extends BaseEntity {
 
     public String getName() {
         return name;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Station)) return false;
+        Station station = (Station) o;
+        return Objects.equals(getName(), station.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 }
---
+++ src/test/java/nextstep/subway/domain/StationTest.java
@@ -0,0 +1,38 @@
+package nextstep.subway.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class StationTest {
+    @Autowired
+    private StationRepository stationRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+
+
+    @DisplayName("지하철역 생성 테스트")
+    @Test
+    void 지하철역생성_성공() {
+        // given
+        Station sinsa = new Station("신사역");
+
+        // when
+        stationRepository.save(sinsa);
+        flushAndClear();
+        Station station = stationRepository.findById(1L).get();
+
+        // then
+        assertThat(station).isEqualTo(sinsa);
+    }
+
+    private void flushAndClear() {
+        entityManager.flush();
+        entityManager.clear();
+    }
+}
