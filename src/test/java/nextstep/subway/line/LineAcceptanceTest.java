4e741706ed582932730025dbd65c9b367565b5fb
yomni <yearsheep@gmail.com>
2022-11-15 01:33:57 +0900

    test: 지하철 노선 생성 인수 테스트 정의

---
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -0,0 +1,79 @@
+package nextstep.subway.line;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.station.StationTestUtil;
+import nextstep.subway.utils.DatabaseCleanup;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("지하철역 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+    @LocalServerPort
+    int port;
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+    String upStationId;
+    String downStationId;
+
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+        databaseCleanup.execute();
+        upStationId = StationTestUtil.createStation("신사역")
+                .jsonPath()
+                .getString("id");
+        downStationId = StationTestUtil.createStation("광교(경기대)역")
+                .jsonPath()
+                .getString("id");
+    }
+
+    /**
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다.
+     */
+    @DisplayName("지하철 노선을 생성한다")
+    @Test
+    void 지하철노선생성_성공() {
+        // when
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "신분당선");
+        params.put("color", "bg-red-600");
+        params.put("upStationId", upStationId);
+        params.put("downStationId", downStationId);
+        params.put("distance", "10");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+        String lineName = response.jsonPath().getString("name");
+        List<String> stations = response.jsonPath().getList("stations.name", String.class);
+
+        // then
+        assertThat(lineName).isEqualTo("신분당선");
+        assertAll(
+                () -> assertThat(stations).containsAnyOf("신사역"),
+                () -> assertThat(stations).containsAnyOf("광교(경기대)역")
+        );
+    }
+}
