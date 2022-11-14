5b4dd3a43405c2241d24e02577728a305a1c3179
deokmoon <deok2moon@gmail.com>
2022-11-14 19:52:47 +0900

    test: 지하철역 삭제 후, 지하철역 조회 시 삭제된 지하철역 조회 안되는 케이스 테스트

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -3,6 +3,8 @@ package nextstep.subway.station;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import nextstep.subway.domain.Station;
+import nextstep.subway.dto.StationResponse;
 import org.assertj.core.api.PredicateAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,12 +18,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
+    private static final String DELIMITER = "/";
+
     @LocalServerPort
     int port;
 
@@ -140,6 +145,51 @@ public class StationAcceptanceTest {
     @DisplayName("지하철역을 제거한다.")
     @Test
     void deleteStation() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "강남역");
+        long deleteTargetStationIdx;
+        String deleteTargetStationName = "";
 
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all();
+
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract();
+
+        List<StationResponse> stations = response.jsonPath().getList(".", StationResponse.class);
+        Map<Long, String> stationsMap = stations.stream().collect(Collectors.toMap(StationResponse::getId, StationResponse::getName));
+        deleteTargetStationIdx = findDeleteStationIndex(stationsMap, "강남역");
+        deleteTargetStationName = stationsMap.get(deleteTargetStationIdx);
+
+
+        // when
+        RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/stations" + DELIMITER + deleteTargetStationIdx)
+                .then().log().all();
+
+        // then
+        List<String> stationNames =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+
+        assertThat(stationNames).doesNotContain(deleteTargetStationName);
+    }
+
+    private <K, V> K findDeleteStationIndex(Map<K, V> map, V value) {
+        return map.entrySet().stream()
+                .filter(entry -> value.equals(entry.getValue()))
+                .findFirst().map(Map.Entry::getKey)
+                .orElse(null);
     }
 }
