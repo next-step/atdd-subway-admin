6cef135565ec100b1e33a00662d91bed8f69a2be
chunhodong <yhms4432@naver.com>
2022-11-14 16:55:13 +0900

    feat: 구간정보를 위한 Section엔티티클래스추가

---
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -0,0 +1,34 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Section extends BaseEntity {
+    @Id
+    @Column(name = "section_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "up_station_id", nullable = false)
+    private Station upStation;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "down_station_id", nullable = false)
+    private Station downStation;
+    private long distance;
+
+    protected Section() {
+
+    }
+
+    public Section(Line line, Station upStation, Station downStation, long distance) {
+        this.line = line;
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
+}
+
