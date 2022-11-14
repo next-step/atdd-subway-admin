396e3855e36f3a7de95a0690527b355b1e303f78
sixthou <justyuk@naver.com>
2022-11-14 14:27:16 +0900

    refactor : Line 리팩토링 - 불필요 필드 삭제 - 일대다 연관관계 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,14 +1,13 @@
 package nextstep.subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import org.apache.commons.lang3.StringUtils;
 
 @Entity
@@ -24,30 +23,20 @@ public class Line extends BaseEntity {
     @Column(unique = true, nullable = false)
     private String color;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "up_station_id", foreignKey = @ForeignKey(name = "fk_line_up_station"))
-    private Station upStation;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "down_station_id", foreignKey = @ForeignKey(name = "fk_line_down_station"))
-    private Station downStation;
-
-    private Integer distance;
+    @OneToMany(mappedBy = "line")
+    private List<Section> sections = new ArrayList<>();
 
     protected Line() {
 
     }
 
-    public Line(String name, String color, Station upStation, Station downStation, Integer distance) {
+    public Line(String name, String color) {
 
         validName(name);
         validColor(color);
 
         this.name = name;
         this.color = color;
-        this.upStation = upStation;
-        this.downStation = downStation;
-        this.distance = distance;
     }
 
     private static void validColor(String color) {
@@ -72,14 +61,6 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
-    }
-
-    public Station getUpStation() {
-        return upStation;
-    }
-
-    public Station getDownStation() {
-        return downStation;
     }
 
     public void changeNameAndColor(String newName, String newColor) {
