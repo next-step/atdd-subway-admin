312f6861954bde1c7e2003125760315a35876b58
giraffelim <taeyang7704@naver.com>
2022-11-14 22:13:52 +0900

    test(SectionAcceptanceTest): 지하철 구간 추가 인수 테스트 작성

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -1,13 +1,32 @@
 package nextstep.subway.section;
 
+import static nextstep.subway.helper.JsonPathExtractor.getId;
+import static nextstep.subway.line.LineAcceptanceTestFixture.createLine;
+import static nextstep.subway.line.LineAcceptanceTestFixture.findLine;
+import static nextstep.subway.section.SectionAcceptanceTestFixture.addSection;
+import static nextstep.subway.station.StationAcceptanceTestFixture.createStation;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.base.AcceptanceTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
 
 @DisplayName("지하철 구간 관련 기능")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SectionAcceptanceTest extends AcceptanceTest {
+    private Long 서초역;
+    private Long 강남역;
+
+    @BeforeEach
+    void stationSetUp(){
+        서초역 = getId(createStation("서초역"));
+        강남역 = getId(createStation("강남역"));
+    }
 
     /**
      *  Given 지하철 노선을 생성하고
@@ -16,7 +35,15 @@ public class SectionAcceptanceTest extends AcceptanceTest {
      */
     @DisplayName("역 사이에 새로운 역을 등록한다.")
     void addSectionBetweenLine() {
+        // given
+        Long lineId = getId(createLine("2호선", "bg-green-600", 20, 서초역, 강남역));
 
+        // when
+        Long 교대역 = getId(createStation("교대역"));
+        ExtractableResponse<Response> createSectionResponse = addSection(lineId, 교대역, 서초역, 10);
+
+        // then
+        assertThat(createSectionResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     /**
---
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTestFixture.java
@@ -0,0 +1,24 @@
+package nextstep.subway.section;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.MediaType;
+
+public class SectionAcceptanceTestFixture {
+    public static ExtractableResponse<Response> addSection(Long lineId, Long downStationId, Long upStationId, int distance) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("downStationId", downStationId);
+        params.put("upStationId", upStationId);
+        params.put("distance", distance);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines/{id}", lineId)
+                .then().log().all()
+                .extract();
+    }
+}
