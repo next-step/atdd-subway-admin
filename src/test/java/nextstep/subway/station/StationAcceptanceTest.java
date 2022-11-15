b3001381afec3760bb4132af58319709a2e389d0
taewonh <htw1800@gmail.com>
2022-11-15 15:03:50 +0900

    test(StationAcceptanceTest): 추가할 기능에 대한 인수 테스트 샘플 코드 작성

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -116,6 +116,60 @@ public class StationAcceptanceTest {
         assertThat(stationNames).isEmpty();
     }
 
+    /*
+     * When 지하철 노선을 생성하면
+     * Then 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     * */
+    @DisplayName("지하철 노선을 생성한다.")
+    @Test
+    void createLine() {
+
+    }
+
+    /*
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     * */
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void getLines() {
+
+    }
+
+    /*
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     * */
+    @DisplayName("지하철 노선을 조회한다.")
+    @Test
+    void getLine() {
+
+    }
+
+    /*
+    * Given 지하철 노선을 생성하고
+    * When 생성한 지하철 노선을 수정하면
+    * Then 해당 지하철 노선 정보는 수정된다
+    * */
+    @DisplayName("지하철 노선을 수정한다.")
+    @Test
+    void updateLine() {
+
+    }
+
+    /*
+    * Given 지하철 노선을 생성하고
+    * When 생성한 지하철 노선을 삭제하면
+    * Then 해당 지하철 노선 정보는 삭제된다
+    * */
+    @DisplayName("지하철 노선을 삭제한다.")
+    @Test
+    void deleteLine() {
+
+    }
+
     private ValidatableResponse createStation(String name) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
