0f4290b15c1f07b755807e7d807e7ef76b66e25d
chunhodong <yhms4432@naver.com>
2022-11-15 18:21:29 +0900

    refactor: SectionAcceptanceTest에 given,when,then 구분추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -19,6 +19,7 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
     @DisplayName("새로운 역을 상행 종점으로 등록할 경우 등록성공")
     @Test
     void createSectionWithNewUpStation() {
+        //given
         long jamSilStationId = 지하철_역_등록("잠실역").jsonPath().getLong("id");
         long hongDaeStationId = 지하철_역_등록("홍대입구역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", jamSilStationId, hongDaeStationId, 10)
@@ -28,14 +29,17 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
                 .jsonPath()
                 .getLong("id");
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(gangnamStationId, jamSilStationId, 11));
 
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     @DisplayName("새로운 역을 하행 종점으로 등록할 경우 등록성공")
     @Test
     void createSectionWithNewDownStation() {
+        //given
         long jamSilStationId = 지하철_역_등록("잠실역").jsonPath().getLong("id");
         long hongDaeStationId = 지하철_역_등록("홍대입구역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", jamSilStationId, hongDaeStationId, 10)
@@ -45,14 +49,17 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
                 .jsonPath()
                 .getLong("id");
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(hongDaeStationId, gangnamStationId, 2));
 
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     @DisplayName("역 사이에 새로운 역을 등록할 기존 역 사이 길이보다 작으면 등록성공")
     @Test
     void createSectionBetweenSections() {
+        //given
         long jamSilStationId = 지하철_역_등록("잠실역").jsonPath().getLong("id");
         long hongDaeStationId = 지하철_역_등록("홍대입구역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", jamSilStationId, hongDaeStationId, 10)
@@ -62,8 +69,10 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
                 .jsonPath()
                 .getLong("id");
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(jamSilStationId, gangnamStationId, 2));
 
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
@@ -71,6 +80,7 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
     @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음")
     @Test
     void createSectionWithOverDistance() {
+        //given
         long jamSilStationId = 지하철_역_등록("잠실역").jsonPath().getLong("id");
         long hongDaeStationId = 지하철_역_등록("홍대입구역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", jamSilStationId, hongDaeStationId, 10)
@@ -80,8 +90,10 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
                 .jsonPath()
                 .getLong("id");
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(jamSilStationId, gangnamStationId, 10));
 
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 
     }
@@ -89,14 +101,17 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
     @DisplayName("상행역과 하행역이 한 구간에 모두 포함되어 있으면 추가할 수 없음")
     @Test
     void createSectionWithAlreadyExistsSection() {
+        //given
         long upStationId = 지하철_역_등록("강남역").jsonPath().getLong("id");
         long downStationId = 지하철_역_등록("논현역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", upStationId, downStationId, 10)
                 .jsonPath()
                 .getLong("id");
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(upStationId, downStationId, 4));
 
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 
     }
@@ -104,15 +119,18 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
     @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있다면 추가할 수 없음")
     @Test
     void createSectionWithAlreadyExistsAllStation() {
+        //given
         long jamSilStationId = 지하철_역_등록("잠실역").jsonPath().getLong("id");
         long hongDaeStationId = 지하철_역_등록("홍대입구역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", jamSilStationId, hongDaeStationId, 10)
                 .jsonPath().getLong("id");
         long bangBaeStationId = 지하철_역_등록("방배역").jsonPath().getLong("id");
-
         지하철_구간_등록(lineId, new SectionRequest(hongDaeStationId, bangBaeStationId, 4));
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(jamSilStationId, bangBaeStationId, 2));
+
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 
     }
@@ -120,14 +138,17 @@ public class SectionAcceptanceTest extends RestAssuredSetUp {
     @DisplayName("상행역과 하행역이 둘 중 하나도 포함되어있지 않으면 추가할 수 없음")
     @Test
     void createSectionWithNotContainsAnyStation() {
+        //given
         long upStationId = 지하철_역_등록("서대문역").jsonPath().getLong("id");
         long downStationId = 지하철_역_등록("굽은다리역").jsonPath().getLong("id");
         long lineId = 지하철_노선_등록("2호선", "green", "강남역", "잠실역", 10)
                 .jsonPath()
                 .getLong("id");
 
+        //when
         ExtractableResponse<Response> response = 지하철_구간_등록(lineId, new SectionRequest(upStationId, downStationId, 4));
 
+        //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 
     }
