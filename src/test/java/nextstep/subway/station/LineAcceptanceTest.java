43598244020e1e60ef48fbb610cf7ff35f72ad94
Minjae Kim <minjae.kim@mytrum.com>
2022-11-15 17:49:09 +0900

    test: 노선 인수테스트 작성

---
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -0,0 +1,210 @@
+package nextstep.subway.station;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import nextstep.subway.dto.LineResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static nextstep.subway.station.StationAcceptanceTest.지하철역_생성_성공;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.LOCATION;
+
+@DisplayName("노선 관련 기능")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LineAcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+            RestAssured.port = port;
+        }
+        databaseCleanup.execute();
+    }
+
+    /**
+     * When: 지하철 노선을 생성하면
+     * Then: 지하철 노선 목록 조회 시 생성한 노선을 찾을 수 있다
+     */
+    @DisplayName("지하철 노선을 생성한다")
+    @Test
+    void 노선_생성() {
+
+        // given:
+        // 지하철역 두개 생성
+        지하철역_두개_생성_성공();
+
+        // when:
+        // 노선 생성
+        Map<String, Object> lineParams = 노선_생성_요청파라미터("line", "bg-red-600");
+        ExtractableResponse<Response> response = 노선_생성_성공(lineParams);
+
+        //then:
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    /**
+     * Given: 지하철 노선들을 생성하고
+     * When:  지하철 노선들을 조회하면
+     * Then:  생성한 노선들을 찾을 수 있다
+     */
+    @DisplayName("지하철 노선 목록을 가져온다")
+    @Test
+    void 노선_목록() {
+        // given:
+        // 지하철역 두개 생성
+        지하철역_두개_생성_성공();
+
+        // 분당선, 경인선 생성
+        Map<String, Object> boondangLine = 노선_생성_요청파라미터("boondangLine", "bg-red-600");
+        Map<String, Object> kyunginLine = 노선_생성_요청파라미터("kyunginLine", "bg-green-600");
+        LineResponse boondangResponse = 노선_생성_성공(boondangLine).body().jsonPath().getObject("", LineResponse.class);
+        LineResponse kyunginResponse = 노선_생성_성공(kyunginLine).body().jsonPath().getObject("", LineResponse.class);
+
+        // when:
+        // 노선 목록 조회
+        ExtractableResponse<Response> response = 노선_조회("/lines");
+
+        List<Long> lineIds = response.body().jsonPath().getList("id", Long.class);
+
+        // then:
+        assertThat(lineIds).containsAnyOf(boondangResponse.getId(), kyunginResponse.getId());
+    }
+
+    /**
+     * Given: 지하철 노선을 생성하고
+     * When:  지하철 노선을 조회하면
+     * Then:  생성한 지하철 노선을 찾을 수 있다
+     */
+    @DisplayName("단건 지하철 노선을 가져온다")
+    @Test
+    void 노선_단건_조회() {
+
+        // given:
+        // 지하철역 두개 생성
+        지하철역_두개_생성_성공();
+
+        // 분당선 생성
+        Map<String, Object> boondangLine = 노선_생성_요청파라미터("boondangLine", "bg-red-600");
+        ExtractableResponse<Response> createResponse = 노선_생성_성공(boondangLine);
+        String boondangGetUrl = createResponse.headers().get(LOCATION).getValue();
+
+        // when:
+        // 분당선 조회
+        ExtractableResponse<Response> response = 노선_조회(boondangGetUrl);
+        Long lineId = response.body().jsonPath().getLong("id");
+
+        // then:
+        assertThat(lineId).isEqualTo(createResponse.body().jsonPath().getLong("id"));
+    }
+
+    /**
+     * Given: 지하철 노선을 생성하고
+     * When:  지하철 노선을 수정하면
+     * Then:  지하철 노선의 수정을 확인할 수 있다.
+     */
+    @DisplayName("지하철 노선을 수정한다")
+    @Test
+    void 지하철_노선_수정() {
+        // given:
+        // 지하철역 두개 생성
+        지하철역_두개_생성_성공();
+
+        // 분당선 생성
+        Map<String, Object> boondangLine = 노선_생성_요청파라미터("boondangLine", "bg-red-600");
+        ExtractableResponse<Response> createResponse = 노선_생성_성공(boondangLine);
+        String boondangGetUrl = createResponse.headers().get(LOCATION).getValue();
+
+        // when:
+        // 분당선 수정
+        Map<String, String> updateRequest = new HashMap<>();
+        updateRequest.put("name", "다른라인");
+        updateRequest.put("color", "bg-blue-600");
+        ExtractableResponse<Response> updateResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(updateRequest)
+                .when().put(boondangGetUrl)
+                .then().log().all()
+                .extract();
+
+        assertThat(updateResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    /**
+     * Given: 지하철 노선을 생성하고
+     * When:  지하철 노선을 삭제하면
+     * Then:  해당 지하철 노선은 삭제된다
+     */
+    @DisplayName("지하철 노선을 삭제한다")
+    @Test
+    void 지하철_노선_삭제() {
+        // given:
+        // 지하철역 두개 생성
+        지하철역_두개_생성_성공();
+
+        // 분당선 생성
+        Map<String, Object> boondangLine = 노선_생성_요청파라미터("boondangLine", "bg-red-600");
+        ExtractableResponse<Response> createResponse = 노선_생성_성공(boondangLine);
+        String boondangGetUrl = createResponse.headers().get(LOCATION).getValue();
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().delete(boondangGetUrl)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    void 지하철역_두개_생성_성공() {
+        Map<String, String> samjeonStation = new HashMap<>();
+        samjeonStation.put("name", "삼전역");
+        Map<String, String> gangnamStation = new HashMap<>();
+        gangnamStation.put("name", "강남역");
+        지하철역_생성_성공(samjeonStation);
+        지하철역_생성_성공(gangnamStation);
+    }
+
+    Map<String, Object> 노선_생성_요청파라미터(String name, String color) {
+        Map<String, Object> boondangLine = new HashMap<>();
+        boondangLine.put("name", name);
+        boondangLine.put("color", color);
+        boondangLine.put("upStationId", 1);
+        boondangLine.put("downStationId", 2);
+        boondangLine.put("distance", 10);
+        return boondangLine;
+    }
+
+    ExtractableResponse<Response> 노선_생성_성공(Map<String, Object> lineParams) {
+        return RestAssured.given().log().all()
+                .body(lineParams)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/lines")
+                .then().log().all()
+                .extract();
+    }
+
+    ExtractableResponse<Response> 노선_조회(String url) {
+        return RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(url)
+                .then().log().all()
+                .extract();
+    }
+}
