3da67730fca2dc31ff85cd68e3bfd07a351dae3f
namhyun.sung <namhyun.sung@yanolja.com>
2022-11-16 12:56:13 +0900

    test(LineAcceptanceTest): 지하철 노선 목록 조회 테스트 작성

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -15,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("노선 관련 기능")
@@ -53,6 +55,22 @@ public class LineAcceptanceTest {
         assertStatusCode(response, HttpStatus.CREATED);
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("노선 목록을 조회한다.")
+    @Test
+    void getLines () {
+        createLine(new LineRequest("신분당선", "bg-red-600", 1L, 2L, 10));
+        createLine(new LineRequest("분당선", "bg-red-600", 1L, 3L, 10));
+
+        ValidatableResponse response = findAllLines();
+
+        assertThat(extractStations(response)).containsAnyOf("신분당선", "분당선");
+    }
+
     private static ValidatableResponse createLine(LineRequest request) {
         return RestAssured.given().log().all()
             .body(request)
@@ -61,7 +79,18 @@ public class LineAcceptanceTest {
             .then().log().all();
     }
 
+    private static ValidatableResponse findAllLines() {
+        return RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().get("/lines")
+            .then().log().all();
+    }
+
     private static void assertStatusCode(ValidatableResponse response, HttpStatus httpStatus) {
         assertThat(response.extract().statusCode()).isEqualTo(httpStatus.value());
+    }
+
+    private static List<String> extractStations(ValidatableResponse response) {
+        return response.extract().jsonPath().getList("name", String.class);
     }
 }
