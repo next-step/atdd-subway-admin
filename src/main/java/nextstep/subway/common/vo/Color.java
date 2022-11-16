ee05b09397ed379f4dc10fb698cb9991fd7baceb
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-16 19:34:50 +0900

    feat(Color.java) : 기본 생성자

--- src/main/java/nextstep/subway/common/vo/Color.java
+++ src/main/java/nextstep/subway/common/vo/Color.java
@@ -1,12 +1,17 @@
 package nextstep.subway.common.vo;
 
+import javax.persistence.Embeddable;
 import java.util.Objects;
 
+@Embeddable
 public class Color {
 
     public static final String NULL_AND_EMPTY_EXCEPTION_MESSAGE = "색상은 null 이거나 empty일 수 없습니다.";
 
     private String color;
+
+    protected Color() {
+    }
 
     public Color(String color) {
         validate(color);
