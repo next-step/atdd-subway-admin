a9fbe58e0d2892628e544d53924ea02dc7e6cf2a
HJ Kim <hjkim880527@gmail.com>
2022-11-16 11:52:03 +0900

    refactor: Transactional의 readOnly를 true로 변경

--- src/main/java/nextstep/subway/section/application/SectionService.java
+++ src/main/java/nextstep/subway/section/application/SectionService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class SectionService {
     private LineRepository lineRepository;
     private StationRepository stationRepository;
