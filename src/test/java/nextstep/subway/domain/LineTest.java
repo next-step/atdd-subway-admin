ebd6ba8ec3e57bfc35d361f6e158531695be4a8a
cwJohnPark <pierrotcw@gmail.com>
2022-11-16 19:33:44 +0900

    refactor: LineTest

--- src/test/java/nextstep/subway/domain/LineTest.java
+++ src/test/java/nextstep/subway/domain/LineTest.java
@@ -11,13 +11,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
 
+    Line 노선;
     Station 상행종점역 = new Station("합정역");
     Station 하행종점역 = new Station("신촌역");
-    Station 가양역 = new Station("가양역");;
-    Station 홍대역 = new Station("홍대역");;
+    Station 가양역 = new Station("가양역");
+    Station 홍대역 = new Station("홍대역");
     int 종점간_거리 = 10;
 
-    Line 노선;
 
     @BeforeEach
     void setup() {
@@ -26,61 +26,61 @@ class LineTest {
 
     @Test
     void 상행역_기준_구간_추가() {
-        int 구간_거리 = 4;
+        int 구간_거리 = 종점간_거리 - 2;
 
         노선.addSection(상행종점역, 홍대역, 구간_거리);
 
         assertThat(노선.getSections())
                 .isEqualTo(new Sections(
-                        new Section(노선, 상행종점역, 홍대역, 4),
-                        new Section(노선, 홍대역, 하행종점역, 6)
-                        ));
+                        new Section(노선, 상행종점역, 홍대역, 구간_거리),
+                        new Section(노선, 홍대역, 하행종점역, 종점간_거리 - 구간_거리)
+                ));
 
     }
 
     @Test
     void 하행역_기준_구간_추가() {
-        int 구간_거리 = 4;
+        int 구간_거리 = 종점간_거리 - 2;
 
         노선.addSection(홍대역, 하행종점역, 구간_거리);
 
         assertThat(노선.getSections())
                 .isEqualTo(new Sections(
-                        new Section(노선, 상행종점역, 홍대역, 6),
-                        new Section(노선, 홍대역, 하행종점역, 4)
-                ));
-    }
-
-    @Test
-    void 하행종점역_기준_구간_추가() {
-        int 구간_거리 = 11;
-
-        노선.addSection(홍대역, 상행종점역, 구간_거리);
-
-        assertThat(노선.getSections())
-                .isEqualTo(new Sections(
-                        new Section(노선, 홍대역, 상행종점역, 11),
-                        new Section(노선, 상행종점역, 하행종점역, 10)
+                        new Section(노선, 상행종점역, 홍대역, 종점간_거리 - 구간_거리),
+                        new Section(노선, 홍대역, 하행종점역, 구간_거리)
                 ));
     }
 
     @Test
     void 상행종점역_기준_구간_추가() {
-        int 구간_거리 = 11;
+        int 구간_거리 = 종점간_거리;
+
+        노선.addSection(홍대역, 상행종점역, 구간_거리);
+
+        assertThat(노선.getSections())
+                .isEqualTo(new Sections(
+                        new Section(노선, 홍대역, 상행종점역, 구간_거리),
+                        new Section(노선, 상행종점역, 하행종점역, 종점간_거리)
+                ));
+    }
+
+    @Test
+    void 하행종점역_기준_구간_추가() {
+        int 구간_거리 = 종점간_거리;
 
         노선.addSection(하행종점역, 홍대역, 구간_거리);
 
         assertThat(노선.getSections())
                 .isEqualTo(new Sections(
-                        new Section(노선, 상행종점역, 하행종점역, 10),
-                        new Section(노선, 하행종점역, 홍대역, 11)
+                        new Section(노선, 상행종점역, 하행종점역, 종점간_거리),
+                        new Section(노선, 하행종점역, 홍대역, 구간_거리)
                 ));
     }
 
     @Test
     @DisplayName("역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면 등록을 할 수 없음")
     void 구간_거리_검증() {
-        int 구간_거리 = 종점간_거리 +1;
+        int 구간_거리 = 종점간_거리 + 1;
 
         assertThatThrownBy(() -> 노선.addSection(홍대역, 하행종점역, 구간_거리))
                 .isInstanceOf(CannotAddSectionException.class);
@@ -90,7 +90,6 @@ class LineTest {
 
     @Test
     void 등록하려는_구간_거리는_0이_될_수_없다() {
-        Station 홍대역 = new Station("홍대역");
         int 구간_거리 = 0;
 
         assertThatThrownBy(() -> 노선.addSection(홍대역, 하행종점역, 구간_거리))
@@ -153,6 +152,7 @@ class LineTest {
                 .extracting(Sections::getStations)
                 .isEqualTo(new Stations(홍대역, 하행종점역));
     }
+
     @Test
     void 가운데_역을_제거할_경우_거리는_두_구간의_거리의_합으로_정함() {
         노선.addSection(상행종점역, 가양역, 4);
@@ -175,6 +175,7 @@ class LineTest {
         assertThatThrownBy(() -> 노선.removeSection(하행종점역))
                 .isInstanceOf(CannotRemoveSectionException.class);
     }
+
     @Test
     void 노선에_등록되어있지_않은_역을_제거할_수_없음() {
         assertThatThrownBy(() -> 노선.removeSection(가양역))
