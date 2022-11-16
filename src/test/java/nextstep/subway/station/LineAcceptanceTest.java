00e555e73f301586edc643b6ef11d55d75bd6b7b
오준오 <jack.ohjoonoh@gmail.com>
2022-11-16 23:50:05 +0900

    test(line) : 지하철 노선 목록 조회 인수테스트

--- src/test/java/nextstep/subway/station/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -1,7 +1,10 @@
 package nextstep.subway.station;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,9 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 @DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class LineAcceptanceTest {
     @LocalServerPort
     int port;
@@ -21,7 +27,14 @@ public class LineAcceptanceTest {
         {
             put("id", 1L);
             put("name", "1호선");
-            put("color", "yellow darken-1");
+            put("color", "blue darken-1");
+        }
+    };
+    Map<String, Object> _2호선 = new HashMap<String, Object>(){
+        {
+            put("id", 2L);
+            put("name", "2호선");
+            put("color", "green darken-1");
         }
     };
 
@@ -44,6 +57,23 @@ public class LineAcceptanceTest {
         지하철_노선_목록에서_조회된다(lineId);
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선 목록 조회한다.")
+    @Test
+    void inquiryLine() {
+        // given
+        long lineId = 지하철_노선을_생성한다(_1호선);
+        long lineId2 = 지하철_노선을_생성한다(_2호선);
+
+        // when
+        // then
+        지하철_노선_목록에서_조회된다(lineId, lineId2);
+    }
+
     private long 지하철_노선을_생성한다(Map<String, Object> line) {
         return RestAssured.given().log().all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -54,10 +84,13 @@ public class LineAcceptanceTest {
             .jsonPath().getLong("id");
     }
 
-    private void 지하철_노선_목록에서_조회된다(long lineId) {
-        RestAssured.given().log().all()
-            .when().get("/stations")
+    private void 지하철_노선_목록에서_조회된다(Long ... lineId) {
+        List<Long> lineIds = RestAssured.given().log().all()
+            .when().get("/lines")
             .then().log().all()
-            .statusCode(HttpStatus.OK.value());
+            .statusCode(HttpStatus.OK.value())
+            .extract()
+            .jsonPath().getList("id", Long.class);
+        assertThat(lineIds).containsAnyOf(lineId);
     }
 }
