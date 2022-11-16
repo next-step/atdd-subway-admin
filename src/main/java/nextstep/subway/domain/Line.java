16817e7cff36e3ce8ef7976d96e4e1415a6bba10
dongha-byun <byunsw4@naver.com>
2022-11-16 14:56:58 +0900

    Feat: 엔티티 추가 - station 과 line 의 m:n 매핑을 위한 LineStation 엔티티 추가

--- src/main/java/nextstep/subway/domain/Line.java
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -1,12 +1,17 @@
 package nextstep.subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import nextstep.subway.dto.LineEditRequest;
-import org.springframework.transaction.annotation.Transactional;
 
 @Entity
 public class Line extends BaseEntity {
@@ -21,7 +26,17 @@ public class Line extends BaseEntity {
     @Column(unique = true)
     private String color;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "line_id")
+    private List<LineStation> lineStations = new ArrayList<>();
+
     protected Line(){}
+
+    public Line(String name, String color, Station preStation, Station station, Integer distance) {
+        this(name, color);
+        lineStations.add(new LineStation(null, preStation, 0));
+        lineStations.add(new LineStation(preStation, station, distance));
+    }
 
     public Line(String name, String color) {
         this(null, name, color);
@@ -33,7 +48,6 @@ public class Line extends BaseEntity {
         this.color = color;
     }
 
-    @Transactional
     public void editLine(LineEditRequest lineEditRequest){
         this.name = lineEditRequest.getName();
         this.color = lineEditRequest.getColor();
@@ -49,5 +63,59 @@ public class Line extends BaseEntity {
 
     public String getColor() {
         return color;
+    }
+
+    public List<LineStation> getLineStations() {
+        return lineStations;
+    }
+
+    public void addSection(Station preStation, Station station, Integer distance) {
+
+        if( isStationPresent(preStation) && isStationPresent(station)){
+            throw new IllegalArgumentException("시작/도착 역이 이미 존재합니다.");
+        }
+
+        if( !isStationPresent(preStation) && !isStationPresent(station)){
+            throw new IllegalArgumentException("시작/도착 역이 모두 존재하지 않습니다.");
+        }
+
+        if(isStationPresent(preStation)){
+            this.lineStations.stream()
+                    .filter(lineStation -> preStation.equals(lineStation.getPreStation()))
+                    .findFirst()
+                    .ifPresent(lineStation -> lineStation.updateLineStation(station, lineStation.getStation(), distance));
+        }
+
+        if(isStationPresent(station)){
+            this.lineStations.stream()
+                    .filter(lineStation -> station.equals(lineStation.getStation()))
+                    .findFirst()
+                    .ifPresent(lineStation -> lineStation.updateLineStation(lineStation.getPreStation(), preStation, distance));
+        }
+
+        this.lineStations.add(new LineStation(preStation, station, distance));
+    }
+
+    private boolean isStationPresent(Station station){
+        return this.lineStations.stream()
+                .anyMatch(lineStation -> lineStation.getStation().equals(station));
+    }
+
+    public List<LineStation> getOrderStations(){
+        Optional<LineStation> first = this.getLineStations().stream()
+                .filter(lineStation -> lineStation.getPreStation() == null)
+                .findAny();
+
+        List<LineStation> orders = new ArrayList<>();
+        while(first.isPresent()){
+
+            LineStation tmp = first.get();
+            orders.add(tmp);
+            first = this.getLineStations().stream()
+                    .filter(lineStation -> tmp.getStation() == lineStation.getPreStation())
+                    .findAny();
+        }
+
+        return orders;
     }
 }
---
+++ src/main/java/nextstep/subway/domain/LineStation.java
@@ -0,0 +1,72 @@
+package nextstep.subway.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class LineStation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pre_station_id")
+    private Station preStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "station_id")
+    private Station station;
+
+    private Integer distance;
+
+    protected LineStation(){
+
+    }
+
+    public LineStation(Station preStation, Station station, Integer distance) {
+        this.preStation = preStation;
+        this.station = station;
+        this.distance = distance;
+    }
+
+    public void updateLineStation(Station preStation, Station station, Integer distance){
+        if(preStation!=null && this.distance <= distance){
+            throw new IllegalArgumentException("신규 등록 구간 거리는 기존 거리보다 크면 안됩니다.");
+        }
+        this.preStation = preStation;
+        this.station = station;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Station getPreStation() {
+        return preStation;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "LineStation{" +
+                "id=" + id +
+                ", preStation=" + preStation +
+                ", station=" + station +
+                ", distance=" + distance +
+                '}';
+    }
+}
