f203398b0029290cd021f298db828e058465da0b
dongha-byun <byunsw4@naver.com>
2022-11-17 03:09:44 +0900

    Refactor: 구간 추가 시, 추가 직후에는 성공.실패 여부만 return 하도록 api 수정

--- src/main/java/nextstep/subway/ui/LineStationController.java
+++ src/main/java/nextstep/subway/ui/LineStationController.java
@@ -1,8 +1,6 @@
 package nextstep.subway.ui;
 
-import nextstep.subway.application.LineService;
 import nextstep.subway.application.LineStationService;
-import nextstep.subway.dto.LineResponse;
 import nextstep.subway.dto.SectionRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,18 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LineStationController {
 
     private final LineStationService lineStationService;
-    private final LineService lineService;
 
-    public LineStationController(LineStationService lineStationService, LineService lineService) {
+    public LineStationController(LineStationService lineStationService) {
         this.lineStationService = lineStationService;
-        this.lineService = lineService;
     }
 
     @PostMapping("/lines/{id}/sections")
     public ResponseEntity addSection(@PathVariable("id") Long id,
                                      @RequestBody SectionRequest sectionRequest){
         lineStationService.addSection(id, sectionRequest);
-        LineResponse line = lineService.findLine(id);
-        return ResponseEntity.ok(line);
+        return ResponseEntity.ok().build();
     }
 }
