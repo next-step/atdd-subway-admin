733c45d54bca071afebbabbcfe91ecfbd1ec8b21
seogineer <dgseo8981@gmail.com>
2022-11-16 11:24:38 +0900

    refactor: 불필요한 메소드 제거

--- src/main/java/nextstep/subway/application/LineService.java
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -2,14 +2,12 @@ package nextstep.subway.application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
 import nextstep.subway.domain.Station;
 import nextstep.subway.domain.StationRepository;
 import nextstep.subway.dto.LineRequest;
 import nextstep.subway.dto.LineResponse;
-import nextstep.subway.dto.StationResponse;
 import nextstep.subway.exception.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,12 +37,6 @@ public class LineService {
 
     private Station getStation(Long stationId) {
         return stationRepository.getById(stationId);
-    }
-
-    private List<StationResponse> findStationsByLineId(Long lineId){
-        return stationRepository.findByLine_Id(lineId).stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
     }
 
     public List<LineResponse> findAllLines() {
