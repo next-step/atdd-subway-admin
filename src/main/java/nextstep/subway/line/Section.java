f59b161ab8710bae9b407eb6a40cf46f9b8faed4
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-16 19:40:30 +0900

    feat(Section.java) : 구간 클래스

---
+++ src/main/java/nextstep/subway/line/Section.java
@@ -0,0 +1,29 @@
+package nextstep.subway.line;
+
+import nextstep.subway.common.vo.Distance;
+import nextstep.subway.station.domain.Station;
+
+import javax.persistence.*;
+
+@Entity
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+
+    @Embedded
+    private Distance distance;
+}
