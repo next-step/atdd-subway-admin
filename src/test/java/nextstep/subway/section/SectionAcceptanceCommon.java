365b56667b0569d89fab2a12245b7976060b38ff
chunhodong <yhms4432@naver.com>
2022-11-16 15:25:47 +0900

    test: 지하철구간삭제 테스트픽처추가

--- src/test/java/nextstep/subway/section/SectionAcceptanceCommon.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceCommon.java
@@ -17,5 +17,12 @@ public class SectionAcceptanceCommon {
                 .then().log().all()
                 .extract();
     }
+
+    public static ExtractableResponse<Response> 지하철_구간_삭제(long lineId, long stationId) {
+        return RestAssured.given().log().all()
+                .when().delete("/lines/{id}/sections?stationId={stationId}",lineId,stationId)
+                .then().log().all()
+                .extract();
+    }
 }
 
