2cc39f8019756df761b2c36cc3fe1a0678ea9e30
dongha-byun <byunsw4@naver.com>
2022-11-18 04:55:23 +0900

    Refactor: 성능 개선 관련 리펙터링

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -2,7 +2,9 @@ package nextstep.subway.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
@@ -15,66 +17,91 @@ public class Sections {
     @JoinColumn(name = "line_id")
     private List<Section> sections = new ArrayList<>();
 
-    protected Sections(){
+    protected Sections() {
 
     }
 
-    public void add(Station preStation, Station station, Integer distance){
+    public void add(Station preStation, Station station, Integer distance) {
         this.sections.add(new Section(preStation, station, distance));
     }
 
-    public void addSection(Station preStation, Station station, Integer distance){
-        validateAllIncludeStation(preStation, station);
-        validateNotIncludeStation(preStation, station);
+    public void addSection(Station preStation, Station station, Integer distance) {
+        Map<Station, Section> toMap = getSectionsToMap();
+        validateStation(preStation, station, toMap);
 
-        if(isStationPresent(preStation)){
-            this.sections.stream()
-                    .filter(section -> preStation.equals(section.getPreStation()))
-                    .findFirst()
-                    .ifPresent(section -> section.updateSection(station, section.getStation(), distance));
+        if (toMap.containsKey(preStation)) {
+            updateSectionForPreStation(preStation, station, distance);
         }
 
-        if(isStationPresent(station)){
-            this.sections.stream()
-                    .filter(section -> station.equals(section.getStation()))
-                    .findFirst()
-                    .ifPresent(section -> section.updateSection(section.getPreStation(), preStation, distance));
+        if (toMap.containsKey(station)) {
+            updateSectionForStation(preStation, station, distance);
         }
 
         add(preStation, station, distance);
     }
 
-    private void validateNotIncludeStation(Station preStation, Station station) {
-        if( !isStationPresent(preStation) && !isStationPresent(station)){
-            throw new IllegalArgumentException("시작/도착 역이 모두 존재하지 않습니다.");
-        }
+    private void updateSectionForStation(Station preStation, Station station, Integer distance) {
+        this.sections.stream()
+                .filter(section -> station.equals(section.getStation()))
+                .findFirst()
+                .ifPresent(section -> section.updateSection(section.getPreStation(), preStation, distance));
     }
 
-    private void validateAllIncludeStation(Station preStation, Station station) {
-        if( isStationPresent(preStation) && isStationPresent(station)){
+    private void updateSectionForPreStation(Station preStation, Station station, Integer distance) {
+        this.sections.stream()
+                .filter(section -> preStation.equals(section.getPreStation()))
+                .findFirst()
+                .ifPresent(section -> section.updateSection(station, section.getStation(), distance));
+    }
+
+    private void validateStation(Station preStation, Station station, Map<Station, Section> toMap) {
+        validateAllIncludeStation(preStation, station, toMap);
+        validateNotIncludeStation(preStation, station, toMap);
+    }
+
+    private void validateAllIncludeStation(Station preStation, Station station, Map<Station, Section> toMap) {
+        if (toMap.containsKey(preStation) && toMap.containsKey(station)) {
             throw new IllegalArgumentException("시작/도착 역이 이미 존재합니다.");
         }
     }
 
-    private boolean isStationPresent(Station station){
-        return this.sections.stream()
-                .anyMatch(section -> station.equals(section.getStation()));
+    private void validateNotIncludeStation(Station preStation, Station station, Map<Station, Section> toMap) {
+        if (!toMap.containsKey(preStation) && !toMap.containsKey(station)) {
+            throw new IllegalArgumentException("시작/도착 역이 모두 존재하지 않습니다.");
+        }
     }
 
-    public List<Section> getOrderStations(){
-        Optional<Section> first = this.sections.stream()
-                .filter(section -> section.getPreStation() == null)
-                .findAny();
+    private Map<Station, Section> getSectionsToMap() {
+        return sections.stream().collect(Collectors.toMap(
+                Section::getStation, section -> section
+        ));
+    }
+
+
+    public List<Section> getOrderStations() {
+        Map<Station, Section> map = getSectionsToMapByPreStation();
+        Section section1 = findFirstSection().orElse(null);
 
         List<Section> orders = new ArrayList<>();
-        while(first.isPresent()){
-            Section tmp = first.get();
+        while (section1 != null) {
+            Section tmp = section1;
             orders.add(tmp);
-            first = this.sections.stream()
-                    .filter(section -> tmp.getStation() == section.getPreStation())
-                    .findAny();
+            section1 = map.get(tmp.getStation());
         }
+
         return orders;
+    }
+
+    private Map<Station, Section> getSectionsToMapByPreStation() {
+        return sections.stream().collect(Collectors.toMap(
+                Section::getPreStation, section -> section
+        ));
+    }
+
+    private Optional<Section> findFirstSection() {
+        return this.sections.stream()
+                .filter(section -> section.getPreStation() == null)
+                .findAny();
     }
 
     public List<Section> getSections() {
