6b690aaa5395f46be3bb2125c4b6f9756762bd54
이동길 <akxkdldhtm@gmail.com>
2022-11-18 00:33:25 +0900

    doc: 하행역에서 끝나는 구간 추가 인수 테스트 작성

--- src/test/java/nextstep/subway/station/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/SectionAcceptanceTest.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 import static nextstep.subway.station.StationAcceptanceTest.assertStatus;
+import static nextstep.subway.station.StationAcceptanceTest.지하철역을_생성한다;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 구간 관련 기능")
@@ -42,8 +43,8 @@ public class SectionAcceptanceTest {
         }
         databaseCleaner.clear();
 
-        상행역 = StationAcceptanceTest.지하철역을_생성한다("상행역").as(StationResponse.class);
-        하행역 = StationAcceptanceTest.지하철역을_생성한다("하행역").as(StationResponse.class);
+        상행역 = 지하철역을_생성한다("상행역").as(StationResponse.class);
+        하행역 = 지하철역을_생성한다("하행역").as(StationResponse.class);
     }
 
     /**
@@ -52,7 +53,7 @@ public class SectionAcceptanceTest {
      * Then 구간이 생성된다.
      */
     @Test
-    void 구간_추가_정상() {
+    void 상행선에서_시작한_내부_구간_추가_정상() {
 
         LineResponse lineResponse = LineAcceptanceTest.지하철_노선을_생성한다("노선", "색상", 10L, Arrays.asList(상행역.getId(), 하행역.getId())).as(LineResponse.class);
 
@@ -61,12 +62,55 @@ public class SectionAcceptanceTest {
         추가된_구간을_확인할_수_있다(response);
     }
 
+    /**
+     * Given 노선이 등록되어 있을 때
+     * When 하행역에서 끝나는 연결된 구간을 추가하면
+     * Then 구간이 생성된다.
+     */
+    @Test
+    void 하행선에서_끝나는_내부_구간_추가_정상() {
+
+        LineResponse lineResponse = LineAcceptanceTest.지하철_노선을_생성한다("노선", "색상", 10L, Arrays.asList(상행역.getId(), 하행역.getId())).as(LineResponse.class);
+
+        ExtractableResponse<Response> response = 하행역에서_끝나는_구간을_추가한다(lineResponse.getId(), 하행역, "신규역", 5L);
+
+        추가된_구간을_확인할_수_있다(response);
+    }
+
+    /**
+     * Given 노선이 등록되어 있을 때
+     * When 하행역에서 끝나면서 노선과 거리가 같은 구간을 추가하면
+     * Then 구간_생성을_실패한다.
+     */
+    @Test
+    void 하행선에서_끝나지만_거리가_부적절한_경우() {
+
+        LineResponse lineResponse = LineAcceptanceTest.지하철_노선을_생성한다("노선", "색상", 10L, Arrays.asList(상행역.getId(), 하행역.getId())).as(LineResponse.class);
+
+        ExtractableResponse<Response> response = 하행역에서_끝나는_구간을_추가한다(lineResponse.getId(), 하행역, "신규역", 10L);
+
+        구간_생성을_실패한다(response);
+    }
+
+    private void 구간_생성을_실패한다(ExtractableResponse<Response> response) {
+        assertStatus(response,HttpStatus.BAD_REQUEST);
+    }
+
+    private ExtractableResponse<Response> 하행역에서_끝나는_구간을_추가한다(Long lineId, StationResponse 하행역, String 신규역, long distance) {
+        StationResponse response = 지하철역을_생성한다(신규역).as(StationResponse.class);
+        HashMap<Object, Object> params = new HashMap<>();
+        params.put("downStationId",하행역.getId());
+        params.put("upStationId",response.getId());
+        params.put("distance",distance);
+        return RequestUtil.postRequest(String.format(ServiceUrl.URL_SECTIONS,lineId),params);
+    }
+
     private void 추가된_구간을_확인할_수_있다(ExtractableResponse<Response> response) {
         assertStatus(response, HttpStatus.CREATED);
     }
 
     private ExtractableResponse<Response> 상행역에서_시작하는_구간을_추가한다(Long lineId, StationResponse 상행역, String 신규역, long distance) {
-        StationResponse response = StationAcceptanceTest.지하철역을_생성한다(신규역).as(StationResponse.class);
+        StationResponse response = 지하철역을_생성한다(신규역).as(StationResponse.class);
         HashMap<Object, Object> params = new HashMap<>();
         params.put("downStationId",response.getId());
         params.put("upStationId",상행역.getId());
