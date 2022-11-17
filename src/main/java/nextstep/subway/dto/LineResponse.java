b7d9d1a5c3cc2eca201b36989f7a6264f77c44b0
taewonh <htw1800@gmail.com>
2022-11-17 15:17:32 +0900

    refactor: LineResponse의 중첩 콜렉션 제거
    - Station 정보(id, name)을 담고 있는 Map을 클래스로 추출
    - 추출한 StationResponse는 해당 LineResponse 내부에서만 사용 가능한 inner 클래스로 적용
    - inner 클래스 리스트를 생성하는 내부 메소드 역시 외부에서 사용 불가능하도록 private 처리

--- src/main/java/nextstep/subway/dto/LineResponse.java
+++ src/main/java/nextstep/subway/dto/LineResponse.java
@@ -4,6 +4,7 @@ import nextstep.subway.domain.Line;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LineResponse {
 
@@ -13,9 +14,9 @@ public class LineResponse {
 
     private final String color;
 
-    private final List<Map<String, Object>> stations;
+    private final List<StationResponse> stations;
 
-    public LineResponse(Long id, String name, String color, List<Map<String, Object>> stations) {
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -23,7 +24,7 @@ public class LineResponse {
     }
 
     public static LineResponse of(Line line) {
-        List<Map<String, Object>> stations = line.getStations();
+        List<StationResponse> stations = createStations(line);
         return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
     }
 
@@ -39,7 +40,40 @@ public class LineResponse {
         return color;
     }
 
-    public List<Map<String, Object>> getStations() {
+    public List<StationResponse> getStations() {
         return stations;
+    }
+
+    private static List<StationResponse> createStations(Line line) {
+        return line.getStations()
+                .stream()
+                .map(LineResponse::createStationResponse)
+                .collect(Collectors.toList());
+    }
+
+    private static StationResponse createStationResponse(Map<String, Object> station) {
+        long id = (long) station.get("id");
+        String name = (String) station.get("name");
+        return new StationResponse(id, name);
+    }
+
+    private static class StationResponse {
+
+        private Long id;
+
+        private String name;
+
+        public StationResponse(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
