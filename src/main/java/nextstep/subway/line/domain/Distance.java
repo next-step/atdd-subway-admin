3402ec32a6fe31fb06bd54abfaba412365998001
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-18 11:49:25 +0900

    test(Distance.java) : 거리 단위 테스트

--- src/main/java/nextstep/subway/line/domain/Distance.java
+++ src/main/java/nextstep/subway/line/domain/Distance.java
@@ -5,11 +5,20 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class Distance {
 
+    public static final int MINIMUM_DISTANCE = 0;
     private int distance;
 
-    protected Distance() {}
+    protected Distance() {
+    }
 
     public Distance(int distance) {
+        validate(distance);
         this.distance = distance;
+    }
+
+    private static void validate(int distance) {
+        if (distance <= MINIMUM_DISTANCE) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/nextstep/subway/line/DistanceTest.java
+++ src/test/java/nextstep/subway/line/DistanceTest.java
@@ -6,14 +6,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("거리")
 public class DistanceTest {
 
+    @DisplayName("거리 생성")
+    @ParameterizedTest
+    @ValueSource(ints = {1})
+    void constructor(int distance) {
+        assertThatNoException().isThrownBy(() -> new Distance(distance));
+    }
+
     @DisplayName("거리는 0보다 커야한다.")
     @ParameterizedTest
-    @ValueSource(ints = {-1})
+    @ValueSource(ints = {-1, 0})
     void negative(int distance) {
-        assertThatNoException().isThrownBy(() -> new Distance(distance));
+        assertThatThrownBy(() -> new Distance(distance))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
