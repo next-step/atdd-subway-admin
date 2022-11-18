30a2214411903fa3e9c68cf835ae70fd425c9e2c
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-18 10:07:22 +0900

    refactor(Sections.java) : 메서드 분리

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -26,40 +26,55 @@ public class Sections {
         List<Station> stations = new ArrayList<>();
         Station firstUpStation = findFirstUpStation();
         stations.add(firstUpStation);
-        for (Section section : this.sections) {
-            if (firstUpStation.equals(section.getUpStation())) {
-                stations.add(section.getDownStation());
-                firstUpStation = section.getDownStation();
-            }
-        }
-        while (isTrue(firstUpStation)) {
-            if (isTrue(firstUpStation)) {
-                for (Section section : this.sections) {
-                    if (firstUpStation.equals(section.getUpStation())) {
-                        stations.add(section.getDownStation());
-                        firstUpStation = section.getDownStation();
-                    }
-                }
-            }
+        Station firstDownStation = findDownStationByUpStation(firstUpStation);
+        stations.add(firstDownStation);
+        Station upStation = firstDownStation;
+        while (!isLastDownStation(upStation)) {
+            addStation(stations, upStation);
+            upStation = findDownStationByUpStation(upStation);
         }
         return stations;
-    }
-
-    private boolean isTrue(Station firstUpStation) {
-        for (Section section : this.sections) {
-            if (firstUpStation.equals(section.getUpStation())) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private Station findFirstUpStation() {
         Station firstUpStation = this.sections.get(0).getUpStation();
         for (Section section : this.sections) {
-            if (firstUpStation.equals(section.getDownStation())) {
-                firstUpStation = section.getUpStation();
-            }
+            firstUpStation = findUpStation(firstUpStation, section);
+        }
+        return firstUpStation;
+    }
+
+    private Station findDownStationByUpStation(Station upStation) {
+        Station downStation = upStation;
+        for (Section section : this.sections) {
+            downStation = findDownStationByUpStation(upStation, downStation, section);
+        }
+        return downStation;
+    }
+
+    private boolean isLastDownStation(Station upStation) {
+        return this.sections
+                .stream()
+                .noneMatch(section -> upStation.equals(section.getUpStation()));
+    }
+
+    private static Station findDownStationByUpStation(Station upStation, Station downStation, Section section) {
+        if (upStation.equals(section.getUpStation())) {
+            downStation = section.getDownStation();
+        }
+        return downStation;
+    }
+
+    private void addStation(List<Station> stations, Station upStation) {
+        this.sections.stream()
+                .filter(section -> upStation.equals(section.getUpStation()))
+                .map(Section::getDownStation)
+                .forEach(stations::add);
+    }
+
+    private static Station findUpStation(Station firstUpStation, Section section) {
+        if (firstUpStation.equals(section.getDownStation())) {
+            firstUpStation = section.getUpStation();
         }
         return firstUpStation;
     }
