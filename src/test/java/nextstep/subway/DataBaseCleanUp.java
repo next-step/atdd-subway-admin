092a8182f33e59980a6274071a69b5cea6f32ef6
jimbaemon <jimbae@kakao.com>
2022-11-18 20:40:22 +0900

    fix: connection close 안해줘서 pool 오버되던 문제 처리.

--- src/test/java/nextstep/subway/DataBaseCleanUp.java
+++ src/test/java/nextstep/subway/DataBaseCleanUp.java
@@ -3,27 +3,29 @@ package nextstep.subway;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataBaseCleanUp {
-	@Autowired
-	private DataSource dataSource;
 
-	public void cleanUp() throws SQLException {
-		Connection connection = dataSource.getConnection();
+    @Autowired
+    private DataSource dataSource;
 
-		connection.createStatement().execute("SET FOREIGN_KEY_CHECKS = 0");
+    public void cleanUp() throws SQLException {
+        Connection connection = dataSource.getConnection();
 
-		ResultSet tables = connection.getMetaData().getTables(connection.getCatalog(), null, null, new String[] {"TABLE"});
-		while (tables.next()){
-			connection.createStatement().execute("TRUNCATE TABLE " + tables.getString("TABLE_NAME"));
-		}
+        connection.createStatement().execute("SET FOREIGN_KEY_CHECKS = 0");
 
-		connection.createStatement().execute("SET FOREIGN_KEY_CHECKS = 1");
-	}
+        ResultSet tables = connection.getMetaData()
+            .getTables(connection.getCatalog(), null, null, new String[]{"TABLE"});
+        while (tables.next()) {
+            connection.createStatement()
+                .execute("TRUNCATE TABLE " + tables.getString("TABLE_NAME"));
+        }
+
+        connection.createStatement().execute("SET FOREIGN_KEY_CHECKS = 1");
+        connection.close();
+    }
 }
