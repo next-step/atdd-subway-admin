bedad34db8a7540c3b1efafb84e10dcc4699ff31
choiyongseok90 <badaelephant>
2022-11-18 09:09:56 +0900

    feat: 노선 서비스에 구간관련 기능 추가

--- src/main/java/nextstep/subway/service/LineService.java
+++ src/main/java/nextstep/subway/service/LineService.java
@@ -1,16 +1,17 @@
 package nextstep.subway.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javassist.NotFoundException;
 import nextstep.subway.domain.Line;
-import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Station;
-import nextstep.subway.domain.StationRepository;
+import nextstep.subway.repository.LineRepository;
+import nextstep.subway.repository.StationRepository;
 import nextstep.subway.dto.LineRequest;
 import nextstep.subway.dto.LineResponse;
-import nextstep.subway.dto.StationRequest;
+import nextstep.subway.dto.SectionRequest;
+import nextstep.subway.dto.SectionResponse;
 import nextstep.subway.dto.StationResponse;
 import nextstep.subway.value.ErrMsg;
 import org.springframework.stereotype.Service;
@@ -20,10 +21,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 public class LineService {
-    private LineRepository lineRepository;
+    private final LineRepository lineRepository;
+    private final StationRepository stationRepository;
 
-    public LineService(LineRepository lineRepository) {
+
+    public LineService(LineRepository lineRepository, StationRepository stationRepository) {
         this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
     }
 
     @Transactional
@@ -61,5 +65,37 @@ public class LineService {
         return lineRepository.findById(id).orElseThrow(
                 () -> new NotFoundException(ErrMsg.notFoundLine(id))
         );
+    }
+    public Station findStationById(Long id) throws NotFoundException {
+        return stationRepository.findById(id).orElseThrow(
+                () -> new NotFoundException(ErrMsg.notFoundStation(id))
+        );
+    }
+
+    public List<StationResponse> findLineStations(Long id) throws NotFoundException {
+        Line line = findLineById(id);
+        return line.getOrderedStations().stream().map(StationResponse::of).collect(Collectors.toList());
+    }
+    @Transactional
+    public SectionResponse addSection(Long id, SectionRequest sectionRequest) throws NotFoundException {
+        Station upStation = findStationById(sectionRequest.getUpStationId());
+        Station downStation = findStationById(sectionRequest.getDownStationId());
+        Line line = findLineById(id);
+        Section section = new Section(upStation, downStation, sectionRequest.getDistance());
+        line.addSection(section);
+        lineRepository.flush();
+        return SectionResponse.of(section);
+    }
+
+    public List<SectionResponse> findLineSections(Long id) throws NotFoundException {
+        Line line = findLineById(id);
+        return line.getSections().getAll().stream().map(SectionResponse::of).collect(Collectors.toList());
+    }
+
+    public SectionResponse findLineSection(Long lineId, Long sectionId) throws NotFoundException {
+        Line line = findLineById(lineId);
+        return SectionResponse.of(line.getSections().getSectionById(sectionId).orElseThrow(
+                ()-> new NotFoundException(ErrMsg.notFoundSection(sectionId))
+        ));
     }
 }
