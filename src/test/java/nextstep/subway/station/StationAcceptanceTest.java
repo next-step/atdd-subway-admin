c1e87719cf02a57b838ba0ceba709622d10e0030
민형준 <aksgudwns@gmail.com>
2022-11-18 21:50:34 +0900

    refactor - 각각의 인수테스트 격리

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -6,11 +6,14 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,11 +26,18 @@ public class StationAcceptanceTest {
     @LocalServerPort
     int port;
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
     @BeforeEach
     public void setUp() {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
+        databaseCleaner.execute();
     }
 
     /**
@@ -35,8 +45,8 @@ public class StationAcceptanceTest {
      * Then 지하철역이 생성된다
      * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
      */
-    @DisplayName("지하철역을 생성한다.")
     @Test
+    @DisplayName("지하철역을 생성한다.")
     void createStationTest() {
         // when
         String stationName = "강남역";
@@ -54,11 +64,11 @@ public class StationAcceptanceTest {
      * When 기존에 존재하는 지하철역 이름으로 지하철역을 생성하면
      * Then 지하철역 생성이 안된다
      */
-    @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
     @Test
+    @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성한다.")
     void createStationWithDuplicateNameTest() {
         // given
-        String stationName = "신촌역";
+        String stationName = "강남역";
         createStation(stationName);
 
         // when
@@ -73,12 +83,12 @@ public class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    @DisplayName("지하철역을 조회한다.")
     @Test
+    @DisplayName("지하철역을 조회한다.")
     void getStationsTest() {
         //given
-        String stationName1 = "홍대입구역";
-        String stationName2 = "합정역";
+        String stationName1 = "강남역";
+        String stationName2 = "석촌역";
         createStation(stationName1);
         createStation(stationName2);
 
@@ -86,7 +96,9 @@ public class StationAcceptanceTest {
         final List<String> stationNames = getStationNames();
 
         //then
+        //size 테스트
         assertThat(stationNames.size()).isEqualTo(2);
+        //이름 포함 테스트
         assertThat(stationNames.stream()
                 .allMatch(stationName -> stationName.equals(stationName1) || stationName.equals(stationName2)))
                 .isTrue();
@@ -97,11 +109,11 @@ public class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    @DisplayName("지하철역을 제거한다.")
     @Test
+    @DisplayName("지하철역을 제거한다.")
     void deleteStationTest() {
         //given
-        String stationName = "잠실역";
+        String stationName = "강남역";
         String id = createStationAndGetId(stationName);
 
         //when
@@ -114,8 +126,13 @@ public class StationAcceptanceTest {
                         .when().get("/stations")
                         .then().log().all()
                         .extract();
-
-        assertThat(getStationNames().size()).isEqualTo(0);
+        final List<String> stationNames = getStationNames();
+        //size 테스트
+        assertThat(stationNames.size())
+                .isEqualTo(0);
+        //이름 포함 테스트
+        assertThat(stationNames.stream().anyMatch(stationName::equals))
+                .isFalse();
     }
 
     /**
@@ -160,4 +177,5 @@ public class StationAcceptanceTest {
                 .when().delete("/stations/" + id)
                 .then().log().all();
     }
+
 }
