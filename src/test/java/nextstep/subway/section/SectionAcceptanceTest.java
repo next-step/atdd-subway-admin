1c05dd83f4990bdb3275275e2b8c5363759e8338
Haseop Lee <haservi@naver.com>
2022-11-19 01:04:04 +0900

    style(Test): Assertions import로 지정

--- src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.java
@@ -4,7 +4,6 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
 import nextstep.subway.dto.SectionRequest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +17,7 @@ import static nextstep.subway.section.SectionAcceptanceFixture.노선_구간_제
 import static nextstep.subway.section.SectionAcceptanceFixture.지하철_구간_생성_요청;
 import static nextstep.subway.station.StationAcceptanceFixture.지하철역_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("지하철 구간 등록 인수 테스트")
 public class SectionAcceptanceTest extends AcceptanceTest {
@@ -123,7 +123,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         );
 
         // then
-        Assertions.assertAll(
+        assertAll(
                 () -> 요청_성공(신림_잠실_구간_응답),
                 () -> 요청_성공(신림_왕십리_구간_응답)
         );
@@ -225,7 +225,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         List<String> 지하철_노선_구간 = 제이슨_경로_얻기(사당_강남_노선_응답).getList("stations.name");
 
         // then
-        Assertions.assertAll(
+        assertAll(
                 () -> 삭제_요청_성공(신림역_제거_응답),
                 () -> assertThat(지하철_노선_구간).containsExactly("사당역", "강남역")
         );
@@ -248,7 +248,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         List<String> 지하철_노선_구간 = 제이슨_경로_얻기(사당_강남_노선_응답).getList("stations.name");
 
         // then
-        Assertions.assertAll(
+        assertAll(
                 () -> 삭제_요청_성공(강남역_제거_응답),
                 () -> assertThat(지하철_노선_구간).containsExactly("신림역", "사당역")
         );
@@ -272,7 +272,7 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         List<String> 지하철_노선_구간 = 제이슨_경로_얻기(신림_강남_노선_응답).getList("stations.name");
 
         // then
-        Assertions.assertAll(
+        assertAll(
                 () -> 삭제_요청_성공(사당역_제거_응답),
                 () -> assertThat(지하철_노선_구간).containsExactly("신림역", "강남역")
         );
