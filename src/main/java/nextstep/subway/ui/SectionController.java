f36c8bdbab742315fb77c60f004593550400b73b
sixthou <justyuk@naver.com>
2022-11-18 14:31:26 +0900

    feat : delete 메서드 기본 구조 추가

--- src/main/java/nextstep/subway/ui/SectionController.java
+++ src/main/java/nextstep/subway/ui/SectionController.java
@@ -4,10 +4,12 @@ import nextstep.subway.application.SectionService;
 import nextstep.subway.dto.SectionRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +25,16 @@ public class SectionController {
     public ResponseEntity addSection(@PathVariable Long id, @RequestBody SectionRequest sectionRequest) {
         try {
             sectionService.addSection(id, sectionRequest);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/lines/{id}/sections")
+    public ResponseEntity deleteStation(@PathVariable Long id, @RequestParam Long stationId) {
+        try {
+
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
