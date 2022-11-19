18944bb0efd7f977b92fa2834268d91952dcf738
sawhong <sawhong@gmarket.com>
2022-11-19 23:45:29 +0900

    fix: 코드 수정에 따른 오류 수정

--- src/main/java/nextstep/subway/dto/LineResponse.java
+++ src/main/java/nextstep/subway/dto/LineResponse.java
@@ -9,10 +9,21 @@ public class LineResponse {
     private Long id;
     private String name;
     private String color;
-
-    private int distance;
     private List<StationResponse> stations;
 
+    public static LineResponse of(Line line) {
+        List<StationResponse> stations = line.getLineStations()
+                .stream().map(StationResponse::of)
+                .collect(Collectors.toList());
+
+        return new LineResponse(line.getId(), line.getName(), line.getColor(), stations);
+    }
+    public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.stations = stations;
+    }
 
     public Long getId() {
         return id;
@@ -26,21 +37,4 @@ public class LineResponse {
         return color;
     }
 
-    public LineResponse(Long id, String name, String color, int distance, List<StationResponse> stations) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.distance = distance;
-        this.stations = stations;
-    }
-
-    public static LineResponse of(Line line) {
-        List<StationResponse> stations = line.getRelationStation()
-                .stream().map(StationResponse::of)
-                .collect(Collectors.toList());
-
-        return new LineResponse(
-                line.getId(), line.getName(), line.getColor(), line.getDistance(), stations
-        );
-    }
 }
