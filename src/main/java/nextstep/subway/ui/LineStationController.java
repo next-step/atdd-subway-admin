8b2d48048859d207ca16cdd6624d408607e99d53
dongha-byun <byunsw4@naver.com>
2022-11-19 20:45:06 +0900

    Feat: 노선 삭제 api 추가

--- src/main/java/nextstep/subway/ui/LineStationController.java
+++ src/main/java/nextstep/subway/ui/LineStationController.java
@@ -3,9 +3,11 @@ package nextstep.subway.ui;
 import nextstep.subway.application.LineStationService;
 import nextstep.subway.dto.SectionRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +23,13 @@ public class LineStationController {
     public ResponseEntity addSection(@PathVariable("id") Long id,
                                      @RequestBody SectionRequest sectionRequest){
         lineStationService.addSection(id, sectionRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/lines/{id}/sections")
+    public ResponseEntity deleteSection(@PathVariable("id") Long lineId,
+                                        @RequestParam("stationId") Long stationId){
+        lineStationService.deleteSection(lineId, stationId);
         return ResponseEntity.ok().build();
     }
 }
