b7224c687e4948a0f3fee3561183a103b917a7c5
yoonmin-kim <rladbsals23@gmail.com>
2022-11-19 22:11:31 +0900

    refactor: cascade 를 활용할 수 있는 모든 부분에 적용

--- src/main/java/nextstep/subway/application/LineService.java
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -4,9 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import nextstep.subway.domain.Line;
-import nextstep.subway.domain.Section;
 import nextstep.subway.domain.repository.LineRepository;
-import nextstep.subway.domain.repository.SectionRepository;
 import nextstep.subway.dto.LineRequest;
 import nextstep.subway.dto.LineResponse;
 import org.springframework.stereotype.Service;
@@ -18,61 +16,54 @@ public class LineService {
 
     private final LineRepository lineRepository;
     private final StationService stationService;
-    private final SectionRepository sectionRepository;
 
-    public LineService(LineRepository lineRepository, StationService stationService,
-                       SectionRepository sectionRepository) {
+    public LineService(LineRepository lineRepository, StationService stationService) {
         this.lineRepository = lineRepository;
         this.stationService = stationService;
-        this.sectionRepository = sectionRepository;
     }
 
     @Transactional
     public Long saveLine(LineRequest lineRequest) {
         setUpDownStation(lineRequest);
-        Section section = sectionRepository
-                .save(new Section(lineRequest.toLine(), lineRequest.getDistance(), lineRequest.getUpStation(),
-                        lineRequest.getDownStation()));
-        return section.getLineId();
+
+        Line line = lineRequest.toLine();
+        line.addSection(lineRequest.getDistance(), lineRequest.getUpStation(), lineRequest.getDownStation());
+
+        return lineRepository.save(line)
+                .getId();
     }
 
     public List<LineResponse> findAllLines() {
         List<Line> lines = lineRepository.findAll();
 
         return lines.stream()
-                .map(this::getLineResponse)
+                .map(LineResponse::of)
                 .collect(Collectors.toList());
     }
 
     public Line findEntityWithSectionsById(Long lineId) {
-        return lineRepository.findWithSectionsById(lineId).orElseThrow(EntityNotFoundException::new);
+        return lineRepository.findWithSectionsById(lineId)
+                .orElseThrow(EntityNotFoundException::new);
     }
 
-    public LineResponse findResponseById(Long id) {
-        Line line = lineRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        return getLineResponse(line);
-    }
+    public LineResponse findResponseById(Long lineId) {
+        Line line = lineRepository.findById(lineId)
+                .orElseThrow(EntityNotFoundException::new);
 
-    public LineResponse findResponseByName(String name) {
-        Line line = lineRepository.findByName(name).orElseThrow(EntityNotFoundException::new);
-        return getLineResponse(line);
+        return LineResponse.of(line);
     }
 
     @Transactional
     public void updateLine(String name, LineRequest lineRequest) {
-        Line line = lineRepository.findByName(name).orElseThrow(EntityNotFoundException::new);
+        Line line = lineRepository.findByName(name)
+                .orElseThrow(EntityNotFoundException::new);
+
         line.change(lineRequest.getName(), lineRequest.getColor());
     }
 
     @Transactional
     public void deleteLineById(Long lineId) {
-        sectionRepository.findByLineId(lineId).ifPresent(sections -> sections.forEach(
-                sectionRepository::delete));
         lineRepository.deleteById(lineId);
-    }
-
-    private LineResponse getLineResponse(Line line) {
-        return LineResponse.of(line);
     }
 
     private void setUpDownStation(LineRequest lineRequest) {
