098c7664a2740b781b3f9287f1bb4163b3f3cefa
deokmoon <deok2moon@gmail.com>
2022-11-20 23:31:11 +0900

    test: 지하철 노선 생성 테스트코드 작성

--- src/test/java/nextstep/subway/Line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/Line/LineAcceptanceTest.java
@@ -1,17 +1,32 @@
 package nextstep.subway.Line;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LineAcceptanceTest {
+
+    private static Map<String, String> of(String lineName, String color) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", lineName);
+        map.put("color", color);
+        return map;
+    }
 
     @LocalServerPort
     int port;
@@ -30,6 +45,26 @@ class LineAcceptanceTest {
     @DisplayName("지하철 노선을 생성한다.")
     @Test
     void createLine() {
+        // when
+        Map<String, String> params = LineAcceptanceTest.of("2호선", "green");
 
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .body(params)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().post("/lines")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+
+        // then
+        List<String> lineNames =
+                RestAssured.given().log().all()
+                        .when().get("/lines")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+        assertThat(lineNames).containsAnyOf("2호선");
     }
 }
