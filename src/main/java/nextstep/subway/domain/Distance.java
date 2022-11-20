7d400db30bf7e8b404ba6923e6b84ddde945a37a
Minjae Kim <minjae.kim@mytrum.com>
2022-11-20 09:58:44 +0900

    refactor: 구간 길이 minus 메서드 생성

--- src/main/java/nextstep/subway/domain/Distance.java
+++ src/main/java/nextstep/subway/domain/Distance.java
@@ -1,6 +1,7 @@
 package nextstep.subway.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Distance {
@@ -23,9 +24,37 @@ public class Distance {
         this.distance = distance;
     }
 
+    public boolean equalsOrGreaterThan(Distance distance) {
+        return this.distance >= distance.distance;
+    }
+
     private void valiPositives(int distance) {
         if (distance <= 0) {
             throw new IllegalArgumentException("distance는 0보다 커야합니다. 입력값:" + distance);
         }
+    }
+
+    public Distance minus(Distance distance) {
+        if (this.distance <= distance.getDistance()) {
+            return this;
+        }
+        return new Distance(this.distance - distance.getDistance());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Distance distance1 = (Distance) o;
+        return distance == distance1.distance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
     }
 }
