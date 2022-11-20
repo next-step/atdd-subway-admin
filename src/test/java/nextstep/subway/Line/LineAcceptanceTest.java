b9ab5d470b00ca421c678cb43eb389300b15d60e
deokmoon <deok2moon@gmail.com>
2022-11-21 00:35:14 +0900

    test: 인수테스트 요구사항 테스트코드에 작성

--- src/test/java/nextstep/subway/Line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/Line/LineAcceptanceTest.java
@@ -67,4 +67,45 @@ class LineAcceptanceTest {
                         .extract().jsonPath().getList("name", String.class);
         assertThat(lineNames).containsAnyOf("2호선");
     }
+
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선 목록 조회.")
+    @Test
+    void showCreatedLines() {
+
+    }
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 조회하면
+     * Then 생성한 지하철 노선의 정보를 응답받을 수 있다.
+     */
+    @DisplayName("지하철 특정 노선 조회.")
+    @Test
+    void retrieveTheLine() {
+
+    }
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 수정하면
+     * Then 해당 지하철 노선 정보는 수정된다
+     */
+    @DisplayName("지하철 특정 노선 수정.")
+    @Test
+    void updateTheLine() {
+
+    }
+    /**
+     * Given 지하철 노선을 생성하고
+     * When 생성한 지하철 노선을 삭제하면
+     * Then 해당 지하철 노선 정보는 삭제된다
+     */
+    @DisplayName("지하철 특정 노선 삭제.")
+    @Test
+    void deleteTheLine() {
+
+    }
 }
