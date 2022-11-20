943419847ac2d464a72f552a4349b86e7422cce1
Minjae Kim <minjae.kim@mytrum.com>
2022-11-20 09:59:14 +0900

    feat: 구간 목록 일급 컬렉션 생성

---
+++ src/main/java/nextstep/subway/domain/SectionLineUp.java
@@ -0,0 +1,132 @@
+package nextstep.subway.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 노선 구간 목록을 관리하는 일급 컬렉션
+ */
+@Embeddable
+public class SectionLineUp {
+
+    @JoinColumn(name = "line_id")
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Section> sectionList = new ArrayList<>();
+
+    protected SectionLineUp() {
+    }
+
+    public List<Section> getSectionList() {
+        return sectionList;
+    }
+
+    public void init(Line line, Section section) {
+        if (!sectionList.isEmpty()) {
+            throw new IllegalStateException("새로 생성된 노선이 아닙니다");
+        }
+        sectionList.add(section);
+        section.registerLine(line);
+    }
+
+    public void add(Line line, Section section) {
+        if (!sectionList.isEmpty()) {
+            validUnknownStation(section);
+        }
+
+        if (isExternalSection(line, section)) {
+            createExternalSection(section, line);
+        }
+
+        if (isInternalSection(line, section)) {
+            createInternalSection(section);
+        }
+    }
+
+    /* 구간 종점 밖에 구간이 추가되는 경우 */
+    private boolean isExternalSection(Line line, Section section) {
+        return section.isSameUpStationId(line.getDownStationId()) ||
+                section.isSameDownStationId(line.getUpStationId());
+    }
+
+    /* 구간 목록 내부에 구간이 추가되는 경우 */
+    private boolean isInternalSection(Line line, Section section) {
+        return section.isSameUpStationId(line.getUpStationId()) ||
+                section.isSameDownStationId(line.getDownStationId());
+    }
+
+    private void validUnknownStation(Section section) {
+        if (sectionList.stream().noneMatch(streamSection -> streamSection.isKnownSection(section))) {
+            throw new IllegalArgumentException("상행역, 하행역이 노선에 포함되어 있지 않습니다. 상행역ID:" + section.getUpStationId() +
+                    ", 하행역ID:" + section.getDownStationId());
+        }
+    }
+
+    private void createExternalSection(Section section, Line line) {
+        sectionList.add(section);
+        section.registerLine(line);
+    }
+
+    private void createInternalSection(Section section) {
+        validInternal(section);
+        List<Section> createUpdateSection = new ArrayList<>();
+
+        /* 상행역과 연결 된 역을 추가할 경우 */
+        sectionList.stream().filter(streamSection -> streamSection.isSameUpStation(section))
+                .findFirst()
+                .ifPresent(streamSection -> createUpdateSection.addAll(createUpSection(streamSection, section)));
+        /* 하행역과 연결 된 역을 추가할 경우 */
+        sectionList.stream().filter(streamSection -> streamSection.isSameDownStation(section))
+                .findFirst()
+                .ifPresent(streamSection -> createUpdateSection.addAll(createDownSection(streamSection, section)));
+
+        /* 노선 목록을 lazy 하게 변경할 목적 */
+        sectionList.addAll(createUpdateSection);
+    }
+
+    private List<Section> createUpSection(Section source, Section add) {
+        sectionList.remove(source);
+        source.updateDistance(source.minusDistance(add.getDistance()));
+        source.updateUpStationId(add.getDownStationId());
+        return Arrays.asList(source, add);
+    }
+
+    private List<Section> createDownSection(Section source, Section add) {
+        sectionList.remove(source);
+        source.updateDistance(source.minusDistance(add.getDistance()));
+        source.updateDownStationId(add.getUpStationId());
+        return Arrays.asList(source, add);
+    }
+
+    private void validInternal(Section section) {
+        validSameSection(section);
+        validDistance(section);
+    }
+
+    private void validSameSection(Section section) {
+        if (sectionList.stream()
+                .anyMatch(streamSection -> streamSection.isKnownStationId(section.getUpStationId())) &&
+                sectionList.stream()
+                        .anyMatch(streamSection -> streamSection.isKnownStationId(section.getDownStationId()))) {
+            throw new IllegalArgumentException(
+                    "이미 중복된 구간이 있습니다. 상행선id:" + section.getUpStationId()
+                            + ", 하행선id:" + section.getDownStationId());
+        }
+    }
+
+    private void validDistance(Section section) {
+        if (sectionList.stream()
+                .filter(streamSection -> streamSection.isSameUpStationId(section.getUpStationId()))
+                .anyMatch(section::distanceEquealsOrGreaterThan) ||
+                sectionList.stream()
+                        .filter(streamSection -> streamSection.isSameDownStationId(section.getDownStationId()))
+                        .anyMatch(section::distanceEquealsOrGreaterThan)) {
+            throw new IllegalArgumentException("기존 역 사이의 길이보다 크거나 같습니다. 입력값:" + section.getDistance());
+        }
+    }
+}
