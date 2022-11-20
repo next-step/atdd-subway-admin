024d1e251c72e80f5554d6de6334407971cb0437
Minjae Kim <minjae.kim@mytrum.com>
2022-11-20 10:00:13 +0900

    feat: 구간 추가 기능 추가

--- src/main/java/nextstep/subway/application/LineService.java
+++ src/main/java/nextstep/subway/application/LineService.java
@@ -2,9 +2,13 @@ package nextstep.subway.application;
 
 import nextstep.subway.domain.Line;
 import nextstep.subway.domain.LineRepository;
+import nextstep.subway.domain.Section;
+import nextstep.subway.domain.SectionRepository;
 import nextstep.subway.domain.Station;
 import nextstep.subway.dto.LineResponse;
 import nextstep.subway.dto.LineUpdateRequest;
+import nextstep.subway.dto.SectionCreateRequest;
+import nextstep.subway.dto.SectionCreateResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,13 +24,18 @@ public class LineService {
     private LineRepository lineRepository;
     private StationService stationService;
 
-    public LineService(LineRepository lineRepository, StationService stationService) {
+    private SectionRepository sectionRepository;
+
+    public LineService(LineRepository lineRepository, StationService stationService,
+            SectionRepository sectionRepository) {
         this.lineRepository = lineRepository;
         this.stationService = stationService;
+        this.sectionRepository = sectionRepository;
     }
 
     @Transactional(isolation = READ_COMMITTED)
     public LineResponse create(Line line) {
+        line.initSectionLineUp(new Section(line));
         final Line resultLine = lineRepository.save(line);
         List<Station> stations = findStations(line);
         return LineResponse.fromLineStations(resultLine, stations);
@@ -48,12 +57,27 @@ public class LineService {
     @Transactional(isolation = READ_COMMITTED)
     public void update(long id, LineUpdateRequest request) {
         Optional<Line> line = lineRepository.findById(id);
-        lineRepository.save(line.orElseThrow(IllegalArgumentException::new)
+        line.orElseThrow(IllegalArgumentException::new)
                 .updateName(request.getName())
-                .updateColor(request.getColor()));
+                .updateColor(request.getColor());
     }
 
     private List<Station> findStations(Line line) {
         return stationService.findAllById(line.getStationIds());
+    }
+
+    @Transactional(isolation = READ_COMMITTED)
+    public SectionCreateResponse createSection(long id, SectionCreateRequest request) {
+        Line line = lineRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("요청한 노선을 찾을 수 없습니다. 노선ID:" + id));
+        line.addSection(request.toSection());
+        refreshDistance(line);
+        return SectionCreateResponse.of(line, line.getSectionList());
+    }
+
+    private void refreshDistance(Line line) {
+        line.updateDistance(sectionRepository.findAllByLine(line).stream()
+                .mapToInt(Section::getDistanceIntValue)
+                .sum());
     }
 }
