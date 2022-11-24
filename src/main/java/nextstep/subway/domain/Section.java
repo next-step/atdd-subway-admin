d0d26f8db59062e831104da665b8269844ffab33
지영진 <youngjin.jee@nhn.com>
2022-11-24 20:48:25 +0900

    feat: Section entity 및 repository 추가

---
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -0,0 +1,49 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Section extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int distance;
+    @OneToOne
+    @JoinColumn(name = "up_station_id")
+    private Station upStation;
+    @OneToOne
+    @JoinColumn(name = "down_station_id")
+    private Station downStation;
+    @ManyToOne
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    public Section() {
+    }
+
+    public Section(int distance, Station upStation, Station downStation) {
+        this.distance = distance;
+        this.upStation = upStation;
+        this.downStation = downStation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Station getUpStation() {
+        return upStation;
+    }
+
+    public Station getDownStation() {
+        return downStation;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+}
