7baa84c2ed9d4ddde8057d02d712322543a7dc52
tlaqk229 <114732525+tlaqk229@users.noreply.github.com>
2022-11-24 21:37:29 +0900

    1단계 - 지하철역 인수 테스트 작성 리뷰 요청드립니다 (#993)
    * docs(mission): 1단계 미션 요구사항 정리
    
    * test(StationAcceptance): 지하철 조회 인수 테스트 작성
    
    * test(StationAcceptance): 지하철 삭제 인수 테스트 작성
    
    * fix(StationAcceptanceTest): 여러 테스트 한번에 실행 가능하도록 테스트 격리

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -14,10 +14,12 @@ import org.springframework.http.MediaType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StationAcceptanceTest {
     @LocalServerPort
@@ -25,9 +27,7 @@ public class StationAcceptanceTest {
 
     @BeforeEach
     public void setUp() {
-        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
-            RestAssured.port = port;
-        }
+        RestAssured.port = port;
     }
 
     /**
@@ -96,20 +96,85 @@ public class StationAcceptanceTest {
     /**
      * Given 2개의 지하철역을 생성하고
      * When 지하철역 목록을 조회하면
-     * Then 2개의 지하철역을 응답 받는다
+     * Then 지하철역 목록을 응답 받는다
+     * Then 생성한 지하철역 2개가 응답받은 지하철역 목록에 존재한다
      */
     @DisplayName("지하철역을 조회한다.")
     @Test
     void getStations() {
+        // given
+        String[] createStations = new String[] {"강남역", "교대역"};
+        for (String createStation : createStations) {
+            createStationForTest(createStation);
+        }
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        // then
+        List<String> stationsNames = response.jsonPath().getList("name", String.class);
+        for (String createStation : createStations) {
+            assertThat(stationsNames.contains(createStation)).isTrue();
+        }
     }
 
     /**
      * Given 지하철역을 생성하고
      * When 그 지하철역을 삭제하면
+     * Then 지하철역이 삭제된다
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
     @DisplayName("지하철역을 제거한다.")
     @Test
     void deleteStation() {
+        // given
+        String stationId = createStationForTest("강남역");
+
+        // when
+        ExtractableResponse<Response> response =
+                RestAssured.given().log().all()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .when().delete("/stations/"+stationId)
+                        .then().log().all()
+                        .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+
+        // then
+        List<String> stationNamesAfterDelete =
+                RestAssured.given().log().all()
+                        .when().get("/stations")
+                        .then().log().all()
+                        .extract().jsonPath().getList("name", String.class);
+        assertThat(stationNamesAfterDelete.contains("강남역")).isFalse();
+    }
+
+    /**
+     * 테스트를 위해 지하철역을 생성
+     * @param stationName 생성할 지하철역 이름
+     * @return 생성된 지하철역 id
+     */
+    String createStationForTest(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+
+        ExtractableResponse<Response> response =
+            RestAssured.given().log().all()
+                    .body(params)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().post("/stations")
+                    .then().log().all()
+                    .extract();
+
+        return Integer.toString(response.jsonPath().get("id"));
     }
 }
