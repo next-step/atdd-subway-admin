b55b990db893d6dda5bf30f72d28c4908551ae8b
cylee9409 <cylee9409@gmail.com>
2022-11-25 15:25:09 +0900

    [feat] 이미 등록된 상하행역 혹은 등록되지 않은 상하행역에 대한 예외 처리 기능 구현

--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -2,6 +2,7 @@ package nextstep.subway.domain;
 
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
+import java.security.DigestException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,8 @@ import java.util.List;
 public class Sections {
 
     private static final String INVALID_DISTANCE_EXCEPTION = "유효하기 않은 거리로는 구간을 생성할 수 없습니다.";
+    private static final String STATIONS_DO_NOT_EXIST_EXCEPTION = "상하행역 모두 존재하지 않아 구간을 생성할 수 없습니다.";
+    private static final String STATIONS_ALREADY_EXIT_EXCEPTION = "상하행역 기존에 존재하므로 구간을 생성할 수 없습니다.";
 
     @OneToMany(mappedBy = "line")
     List<Section> sections = new ArrayList<>();
@@ -19,6 +22,8 @@ public class Sections {
             setInitSection(newSection);
             return this;
         }
+
+        validateNewSection(newSection);
 
         if (exitsUpStation(newSection)) {
             insertNewDownStation(newSection);
@@ -91,6 +96,16 @@ public class Sections {
                 .filter(section -> section.hasDownStation(station))
                 .findFirst()
                 .orElse(null);
+    }
+
+    private void validateNewSection(Section newSection) {
+        if (exitsUpStation(newSection) && exitsDownStation(newSection)) {
+            throw new IllegalArgumentException(STATIONS_ALREADY_EXIT_EXCEPTION);
+        }
+
+        if (!exitsUpStation(newSection) && !exitsDownStation(newSection)) {
+            throw new IllegalArgumentException(STATIONS_DO_NOT_EXIST_EXCEPTION);
+        }
     }
 
     private void addGeneratedSection(Line line, Station upStation, Station downStation, int distance) {
