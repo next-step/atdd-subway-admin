46a46f3a4cd0f08ba86e3462186979d6ee11a929
mansik <90mansin@gmail.com>
2022-11-28 01:05:06 +0900

    test: 노선 조회 테스트 구현

--- src/test/java/nextstep/subway/line/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/LineAcceptanceTest.java
@@ -1,10 +1,11 @@
-package nextstep.subway.station;
+package nextstep.subway.line;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import nextstep.subway.dto.LineRequest;
 import nextstep.subway.dto.StationResponse;
+import nextstep.subway.station.StationAcceptanceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,7 +59,7 @@ public class LineAcceptanceTest {
      *   When 지하철 노선 목록을 조회하면
      *   Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
      */
-    @DisplayName("노선을 조회한다.")
+    @DisplayName("노선 목록을 조회한다.")
     @Test
     public void getLines() {
         // given
@@ -68,12 +69,38 @@ public class LineAcceptanceTest {
         지하철_노선_생성(firstLineRequest);
         지하철_노선_생성(secondLineRequest);
         // when
-        List<String> lines = 지하철_노선_조회();
+        List<String> lines = 지하철_노선_목록_조회();
         // then
         assertAll(
                 () -> assertThat(lines.size()).isEqualTo(2),
                 () -> assertThat(lines).containsAnyOf("신분당선", "분당선")
         );
+    }
+
+    /*
+     *   Given 지하철 노선을 생성하고
+     *   When 생성한 지하철 노선을 조회하면
+     *   Then 생성한 지하철 노선의 정보를 응받받을 수 있다.
+     */
+    @DisplayName("아이디로 지하철 노선을 조회한다.")
+    @Test
+    public void getLine() {
+        // given
+        LineRequest lineRequest = new LineRequest("신분당선", "bg-red-600", upStationId, downStationId, 10);
+        ExtractableResponse<Response> response = 지하철_노선_생성(lineRequest);
+        // when
+        String lineName = 지하철_노선_조회(response.jsonPath().getString("id"));
+
+        // then
+        assertThat(lineName).isEqualTo(lineRequest.getName());
+
+    }
+
+    private String 지하철_노선_조회(String id) {
+        return RestAssured.given().log().all()
+                .when().get("/lines/" + id)
+                .then().log().all()
+                .extract().path("name");
     }
 
 
@@ -90,7 +117,7 @@ public class LineAcceptanceTest {
         return response;
     }
 
-    public List<String> 지하철_노선_조회() {
+    public List<String> 지하철_노선_목록_조회() {
         return RestAssured.given().log().all()
                 .when().get("/lines")
                 .then().log().all()
