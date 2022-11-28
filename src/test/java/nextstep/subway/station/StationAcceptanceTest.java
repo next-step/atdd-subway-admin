e28c0e4856fbc7f2866f5ab6e95d9cab56d2124e
OptimistLabyrinth <keesung0705@gmail.com>
2022-11-28 22:32:00 +0900

    feat: DirtiesContext 적용하기, public 또는 private 명확하게 표기하기
    [1]
    DirtiesContext 를 BEFORE_EACH_TEST_METHOD 로 지정하면
    테스트 케이스 사이 사이에 ApplicationContext 를 초기화시켜준다
    
    오류 원인:
    WebEnvironment 에서 사용하는 LocalServerPort 는 테스트 케이스마다 바뀌지만
    RestAssured.port 는 거기에 맞춰서 변하지 않아서 오류가 발생했다
    
    [2]
    public, private modifier 명시하기

--- src/test/java/nextstep/subway/station/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/StationAcceptanceTest.java
@@ -7,12 +7,12 @@ import io.restassured.response.ValidatableResponse;
 import nextstep.subway.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,15 +22,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 관련 기능")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class StationAcceptanceTest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class StationAcceptanceTest {
     @LocalServerPort
-    int port;
+    private int port;
 
     @BeforeEach
     public void setUp() {
-        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
-            RestAssured.port = port;
-        }
+        RestAssured.port = port;
     }
 
     /**
@@ -38,24 +37,21 @@ class StationAcceptanceTest {
      * Then 지하철역이 생성된다
      * Then 지하철역 목록 조회 시 생성한 역을 찾을 수 있다
      */
-    @Nested
+    @Test
     @DisplayName("지하철역을 생성한다.")
-    class CreateStation {
-        @Test
-        void success() {
-            // given
-            final String stationName = "잠실역";
+    public void createStation_success() {
+        // given
+        final String stationName = "강남역";
 
-            // when
-            final ExtractableResponse<Response> response = createStation(stationName).extract();
+        // when
+        final ExtractableResponse<Response> response = createStation(stationName).extract();
 
-            // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
-            // then
-            final List<String> stationNames = getStationNames();
-            assertThat(stationNames).contains(stationName);
-        }
+        // then
+        final List<String> stationNames = getStationNames();
+        assertThat(stationNames).contains(stationName);
     }
 
     /**
@@ -63,21 +59,18 @@ class StationAcceptanceTest {
      * When 기존에 존재하는 지하철역 이름으로 지하철역을 생성하면
      * Then 지하철역 생성이 안된다
      */
-    @Nested
+    @Test
     @DisplayName("기존에 존재하는 지하철역 이름으로 지하철역을 생성할 수 없다.")
-    class CreateStationWithDuplicateName {
-        @Test
-        void badRequest() {
-            // given
-            final String stationName = "강남역";
-            createStation(stationName);
+    public void createStationWithDuplicateName_badRequest() {
+        // given
+        final String stationName = "강남역";
+        createStation(stationName);
 
-            // when
-            final ExtractableResponse<Response> response = createStation(stationName).extract();
+        // when
+        final ExtractableResponse<Response> response = createStation(stationName).extract();
 
-            // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        }
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     /**
@@ -85,27 +78,24 @@ class StationAcceptanceTest {
      * When 지하철역 목록을 조회하면
      * Then 2개의 지하철역을 응답 받는다
      */
-    @Nested
+    @Test
     @DisplayName("지하철역을 조회한다.")
-    class GetStations {
-        @Test
-        void ok() {
-            // given
-            final String stationName1 = "삼성역";
-            createStation(stationName1);
-            final String stationName2 = "역삼역";
-            createStation(stationName2);
+    public void getStations_ok() {
+        // given
+        final String stationName1 = "강남역";
+        createStation(stationName1);
+        final String stationName2 = "역삼역";
+        createStation(stationName2);
 
-            // when
-            final ExtractableResponse<Response> response = getStations().extract();
+        // when
+        final ExtractableResponse<Response> response = getStations().extract();
 
-            // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 
-            // then
-            final List<String> stationNames = getStationNames();
-            assertThat(stationNames).contains(stationName1, stationName2);
-        }
+        // then
+        final List<String> stationNames = getStationNames();
+        assertThat(stationNames).contains(stationName1, stationName2);
     }
 
     /**
@@ -113,31 +103,28 @@ class StationAcceptanceTest {
      * When 그 지하철역을 삭제하면
      * Then 그 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
      */
-    @Nested
+    @Test
     @DisplayName("지하철역을 제거한다.")
-    class DeleteStation {
-        @Test
-        void notFoundInList() {
-            // given
-            final String targetStationName = "교대역";
-            final Long createdId = createStationResponse(targetStationName).as(StationResponse.class).getId();
+    public void deleteStation_notFoundInList() {
+        // given
+        final String targetStationName = "강남역";
+        final Long createdId = createStationResponse(targetStationName).as(StationResponse.class).getId();
 
-            // given
-            deleteStation(createdId).extract();
+        // given
+        deleteStation(createdId).extract();
 
-            // when
-            final ExtractableResponse<Response> response = getStations().extract();
+        // when
+        final ExtractableResponse<Response> response = getStations().extract();
 
-            // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
 
-            // then
-            final List<String> stationNames = getStationNames();
-            assertThat(stationNames).doesNotContain(targetStationName);
-        }
+        // then
+        final List<String> stationNames = getStationNames();
+        assertThat(stationNames).doesNotContain(targetStationName);
     }
 
-    Response createStationResponse(String value) {
+    private Response createStationResponse(String value) {
         final Map<String, String> params = new HashMap<>();
         params.put("name", value);
         return RestAssured.given().log().all()
@@ -146,21 +133,21 @@ class StationAcceptanceTest {
                 .when().post("/stations");
     }
 
-    ValidatableResponse createStation(String value) {
+    private ValidatableResponse createStation(String value) {
         return createStationResponse(value).then().log().all();
     }
 
-    ValidatableResponse getStations() {
+    private ValidatableResponse getStations() {
         return RestAssured.given().log().all()
                 .when().get("/stations")
                 .then().log().all();
     }
 
-    List<String> getStationNames() {
+    private List<String> getStationNames() {
         return getStations().extract().jsonPath().getList("name", String.class);
     }
 
-    ValidatableResponse deleteStation(long stationId) {
+    private ValidatableResponse deleteStation(long stationId) {
         return RestAssured.given().log().all()
                 .pathParam("id", stationId)
                 .when().delete("/stations/{id}")
