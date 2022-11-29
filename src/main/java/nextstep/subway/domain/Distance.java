ad589b525fae1a80b64b5a3cde46819e356ecd39
민형준 <aksgudwns@gmail.com>
2022-11-29 15:24:35 +0900

    refactor - Distance 객체 상수명칭 변경

--- src/main/java/nextstep/subway/domain/Distance.java
+++ src/main/java/nextstep/subway/domain/Distance.java
@@ -6,9 +6,9 @@ import java.util.Objects;
 @Embeddable
 public class Distance {
 
-    private static final int TERMINAL_SECTION_DISTANCE_VALUE = 0;
-    private static final int MINIMUM_DISTANCE = 1;
-    private static final Distance TERMINAL_SECTION_DISTANCE = new Distance(TERMINAL_SECTION_DISTANCE_VALUE, true);
+    private static final int TERMINAL_SECTION_VALUE = 0;
+    private static final int MINIMUM_LIMIT_VALUE = 0;
+    private static final Distance TERMINAL_SECTION_DISTANCE = new Distance(TERMINAL_SECTION_VALUE, true);
 
     private int distance;
 
@@ -16,12 +16,12 @@ public class Distance {
 
     }
 
-    private Distance(int distance, boolean isTerminalSectionDistance) {
-        if(isTerminalSectionDistance) {
+    private Distance(int distance, boolean isTerminalSection) {
+        if(isTerminalSection) {
             this.distance = distance;
             return;
         }
-        if (distance < MINIMUM_DISTANCE) {
+        if (distance <= MINIMUM_LIMIT_VALUE) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_DISTANCE_VALUE.getMessage());
         }
         this.distance = distance;
@@ -35,28 +35,6 @@ public class Distance {
         return distance;
     }
 
-    public Distance subtract(Distance distance) {
-        int newDistance = this.distance - distance.getDistance();
-        if (newDistance < MINIMUM_DISTANCE) {
-            throw new IllegalArgumentException(ErrorMessage.EXCEED_SECTION_DISTANCE.getMessage());
-        }
-        this.distance = newDistance;
-        return this;
-    }
-
-    public void setEndSectionDistance() {
-        this.distance = TERMINAL_SECTION_DISTANCE_VALUE;
-    }
-
-    public Distance add(Distance distance) {
-        this.distance += distance.getDistance();
-        return this;
-    }
-
-    public static Distance getTerminalSectionDistance() {
-        return TERMINAL_SECTION_DISTANCE;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -68,6 +46,28 @@ public class Distance {
     @Override
     public int hashCode() {
         return Objects.hash(distance);
+    }
+
+    public Distance subtract(Distance distance) {
+        int newDistance = this.distance - distance.getDistance();
+        if (newDistance <= MINIMUM_LIMIT_VALUE) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEED_SECTION_DISTANCE.getMessage());
+        }
+        this.distance = newDistance;
+        return this;
+    }
+
+    public void setEndSectionDistance() {
+        this.distance = TERMINAL_SECTION_VALUE;
+    }
+
+    public Distance add(Distance distance) {
+        this.distance += distance.getDistance();
+        return this;
+    }
+
+    public static Distance getTerminalSectionDistance() {
+        return TERMINAL_SECTION_DISTANCE;
     }
 
 }
