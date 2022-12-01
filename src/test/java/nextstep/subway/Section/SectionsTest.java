d656b9d082bef4ba18a5d02b4ded93fefda9a03f
deokmoon <deok2moon@gmail.com>
2022-12-02 02:24:38 +0900

    test: 상하행 정렬 테스트 코드 작성

--- src/test/java/nextstep/subway/Section/SectionsTest.java
+++ src/test/java/nextstep/subway/Section/SectionsTest.java
@@ -3,15 +3,52 @@ package nextstep.subway.Section;
 import nextstep.subway.domain.Section;
 import nextstep.subway.domain.Sections;
 import nextstep.subway.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
 class SectionsTest {
+    private Station stationA;
+    private Station stationB;
+    private Station stationC;
+    private Station stationD;
+    private Station stationE;
+    private Station stationF;
+    private Sections sections;
+
+    @BeforeEach
+    void setUp() {
+        stationA = new Station("A");
+        ReflectionTestUtils.setField(stationA, "id", 1L);
+        stationB = new Station("B");
+        ReflectionTestUtils.setField(stationB, "id", 2L);
+        stationC = new Station("C");
+        ReflectionTestUtils.setField(stationC, "id", 3L);
+        stationD = new Station("D");
+        ReflectionTestUtils.setField(stationD, "id", 4L);
+        stationE = new Station("E");
+        ReflectionTestUtils.setField(stationE, "id", 5L);
+        stationF = new Station("F");
+        ReflectionTestUtils.setField(stationF, "id", 6L);
+        sections = new Sections(new ArrayList<>(Arrays.asList(
+                Section.of(stationC, stationD, 10),
+                Section.of(stationB, stationC, 10),
+                Section.of(stationA, stationB, 10),
+                Section.of(stationD, stationE, 10)
+        )));
+    }
+
     @Test
     void addSection() {
         // given
@@ -34,6 +71,23 @@ class SectionsTest {
         // when // then
         assertThatThrownBy(() -> sections.add(section))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상행역 부터 하행역 순으로 정렬 확인.")
+    @Test
+    void sortedStations() {
+        //when
+        List<Station> actual = sections.getSortedStations();
+
+        //then
+        assertAll(() -> {
+            assertThat(actual.size()).isEqualTo(5);
+            assertThat(actual.get(0)).isEqualTo(stationA);
+            assertThat(actual.get(1)).isEqualTo(stationB);
+            assertThat(actual.get(2)).isEqualTo(stationC);
+            assertThat(actual.get(3)).isEqualTo(stationD);
+            assertThat(actual.get(4)).isEqualTo(stationE);
+        });
     }
 
 }
