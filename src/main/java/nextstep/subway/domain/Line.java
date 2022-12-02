6748a29b67fdf9096efdd91987579c1a4ece97ce
tlaqk229 <tlaqk229@gmail.com>
2022-12-02 18:46:46 +0900

    feat(Line): 도메인 클래스 추가 (지하철 노선)

---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,33 @@
+package nextstep.subway.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Line extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Line() {
+    }
+
+    public Line(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
