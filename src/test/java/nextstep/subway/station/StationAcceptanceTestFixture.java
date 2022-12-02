a17db623066f466d41f569d47113c1c30f444007
tlaqk229 <tlaqk229@gmail.com>
2022-12-02 12:05:05 +0900

    refactor(StationAcceptanceTest): 테스트 코드 리팩토링
    - fixture 분리 (setUp, method)
    - 가독성을 위해 메소드명, 변수명 수정

---
+++ src/test/java/nextstep/subway/station/StationAcceptanceTestFixture.java
@@ -0,0 +1,53 @@
+package nextstep.subway.station;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+
+class StationAcceptanceTestFixture {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    /**
+     * 지하철역 조회
+     *
+     * @return 지하철역 조회 Response
+     */
+    protected static ExtractableResponse<Response> 지하철역_조회() {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    /**
+     * 지하철역 생성
+     *
+     * @param stationName 생성할 지하철역 이름
+     * @return 생성된 지하철역 id
+     */
+    protected static ExtractableResponse<Response> 지하철역_생성(String stationName) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", stationName);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+}
